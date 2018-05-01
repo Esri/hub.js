7c686cc68e20854a2262493809980cc4867871cc
john gravois <jgravois@esri.com>
2018-05-01 14:37:27 -0700

    start to stub out an initiative interface

--- packages/common-types/src/index.ts
+++ packages/common-types/src/index.ts
@@ -7,8 +7,37 @@ import { IItem } from "@esri/arcgis-rest-common-types";
  * One small step for Hub, one large leap for hubkind
  */
 export interface IInitiative {
-  item?: IItem;
-  data?: {
-    [propName: string]: any;
+  item?: IInitiativeItem;
+  data?: IInitiativeData;
+}
+
+export interface IInitiativeItem extends IItem {
+  type: "Hub Initiative";
+}
+
+export interface IInitiativeData {
+  assets: [any];
+  source: string;
+  values: {
+    collaborationGroupId: string;
+    openDataGroupId: string;
+    followerGroups: any;
+    steps: [string];
+    bannerImage: any;
+    website: IHaveNoIdeaWhatToCallThis;
+    informTools: IHaveNoIdeaWhatToCallThis;
+    listenTools: IHaveNoIdeaWhatToCallThis;
+    monitorTools: IHaveNoIdeaWhatToCallThis;
+    initiativeKey: string;
   };
+}
+
+export interface IHaveNoIdeaWhatToCallThis {
+  title: string;
+  description: string;
+  id: string;
+  templates: any;
+  templateItemIds: any;
+  configuredItemIds: [string];
+  items: any;
 }
