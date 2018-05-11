c2d19e1db7b235d7023da6c8d042ada0fe6a142e
john gravois <jgravois@esri.com>
2018-05-11 14:46:43 -0700

    feat(auth): new auth package with method to fetch old login info
    AFFECTS PACKAGES:
    @esri/hub-auth
    
    ISSUES CLOSED: #6

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -1,13 +1,54 @@
 /* Copyright (c) 2018 Environmental Systems Research Institute, Inc.
  * Apache-2.0 */
 
-import { request, IRequestOptions } from "@esri/arcgis-rest-request";
 import { UserSession } from "@esri/arcgis-rest-auth";
 
+// not an ES6 module, 900B, no dependencies
+// i'd have to figure out how to get Rollup to bundle it
+// const Cookies = require("js-cookie");
+// https://www.npmjs.com/package/js-cookie
+
+/*
+i modified the jasmine.json to skip running auth tests in Node.js.
+it'd probably be (much) smarter to just mock document/window
+*/
+
 /**
- * Checks to see if login information is already stored in a cookie.
- * @returns A Promise that will resolve with the domains associated with the site.
+ * @browserOnly (sic)
+ * Checks cookies/localStorage to see if the user is already signed in.
+ * @returns just the token for now
  */
-export function login(): string {
-  return "yes!";
+export function checkSignInStatus(): Promise<UserSession> {
+  let auth: string;
+  let session: UserSession;
+
+  // first check for an esri_auth cookie
+  // https://stackoverflow.com/questions/10730362/get-cookie-by-name
+  const value = "; " + document.cookie;
+  const parts = value.split("; " + "esri_auth=");
+  if (parts.length === 2) {
+    auth = parts
+      .pop()
+      .split(";")
+      .shift();
+  }
+
+  if (!auth) {
+    // next try local storage
+    auth = window.localStorage.getItem("torii-provider-arcgis");
+  }
+  if (auth) {
+    const authJson = JSON.parse(decodeURIComponent(auth));
+    session = new UserSession({
+      token: authJson.token,
+      tokenExpires: new Date(authJson.expires)
+    });
+  }
+
+  // since we're isolating synchronous logic, is it even helpful to return a promise?
+  return new Promise((resolve, reject) => {
+    if (session) resolve(session);
+    // if nothing is found, abort
+    else reject("No evidence of authentication found.");
+  });
 }
--- packages/auth/test/auth.test.ts
+++ packages/auth/test/auth.test.ts
@@ -1,12 +1,49 @@
-import { login } from "../src/index";
-import * as fetchMock from "fetch-mock";
+import { checkSignInStatus } from "../src/index";
+// import * as fetchMock from "fetch-mock";
+
+const TOMORROW = new Date().setDate(new Date().getDate() + 1);
+
+const oldauth = {
+  email: "casey",
+  token: "XYOl7P..",
+  culture: "en",
+  region: "WO",
+  expires: TOMORROW,
+  allSSL: false,
+  accountId: "uCXeTVfooP4IIcx",
+  role: "account_admin",
+  urlKey: "subdomain",
+  customBaseUrl: "maps.arcgis.com"
+};
 
 describe("login", () => {
-  afterEach(fetchMock.restore);
+  it("should return an informative message when no evidence of previous login can be found", done => {
+    checkSignInStatus().catch(error => {
+      expect(error).toEqual("No evidence of authentication found.");
+      done();
+    });
+  });
 
-  it("should return something excited", done => {
-    const check = login();
-    expect(check).toEqual("yes!");
-    done();
+  it("should return a session when localStorage is identified", done => {
+    window.localStorage.setItem(
+      "torii-provider-arcgis",
+      JSON.stringify(oldauth)
+    );
+
+    checkSignInStatus().then(session => {
+      expect(session.token).toEqual("XYOl7P..");
+      done();
+    });
+  });
+
+  it("should return a session when a cookie with the goods is found", done => {
+    document.cookie = `esri_auth=${encodeURIComponent(
+      JSON.stringify(oldauth)
+    )}; other=something`;
+
+    checkSignInStatus().then(session => {
+      expect(session.token).toEqual("XYOl7P..");
+      done();
+    });
   });
 });
