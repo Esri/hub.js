55617513c35dd16b1b8fee86a1b82ff243d9e343
john gravois <jgravois@esri.com>
2018-07-08 10:25:21 -0700

    and description too

--- packages/annotations/src/add.ts
+++ packages/annotations/src/add.ts
@@ -14,6 +14,7 @@ import { IFeature } from "@esri/arcgis-rest-common-types";
 export interface IAnnoFeature extends IFeature {
   attributes: {
     target: string;
+    description: string;
     [key: string]: any;
   };
 }
