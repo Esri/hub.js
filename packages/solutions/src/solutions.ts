e7d97d9af39238faf162ea8d35f8ed52be4fa4b9
john gravois <jgravois@esri.com>
2018-08-01 13:25:24 -0700

    feat(:baby_bottle:): stubbed in new empty solutions package
    AFFECTS PACKAGES:
    @esri/hub-solutions

---
+++ packages/solutions/src/solutions.ts
@@ -0,0 +1,11 @@
+/* Copyright (c) 2018 Environmental Systems Research Institute, Inc.
+ * Apache-2.0 */
+
+import { request } from "@esri/arcgis-rest-request";
+
+/**
+ * @returns not much
+ */
+export function comingSoon(): Promise<any> {
+  return request("https://www.arcgis.com/sharing/rest/info");
+}
