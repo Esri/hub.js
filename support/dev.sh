5e68ba07de9b0a642b07421d24b1be3aa1421475
Tom Wayson <tom@tomwayson.com>
2018-10-05 09:04:28 -0700

    chore(tooling): add scripts to re-run builds in watch mode for specified packages
    ended up having to use onchange for the UMD build, but it works
    added a support script to wrap the
    lerna command
    
    AFFECTS PACKAGES:
    @esri/hub-annotations
    @esri/hub-auth
    @esri/hub-common
    @esri/hub-domains
    @esri/hub-initiatives
    @esri/hub-sites
    @esri/hub-solutions
    
    ISSUES CLOSED: #78

---
+++ support/dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# extract build type and scope from the command line args
+BUILD_TYPE=$1
+SCOPE=$2
+# echo $BUILD_TYPE $SCOPE
+
+# run the dev script for that build type in the scoped packages
+  lerna run --scope "$SCOPE" dev:$BUILD_TYPE --parallel
