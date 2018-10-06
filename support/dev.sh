a69301243aab4edb2429211bdc5bda20d139ec87
Tom Wayson <tom@tomwayson.com>
2018-10-06 07:14:21 -0700

    revert the order of dev bash script args

--- support/dev.sh
+++ support/dev.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# extract scope and build type from the command line args
-SCOPE=$1
-BUILD_TYPE=$2
+# extract build type and scope from the command line args
+BUILD_TYPE=$1
+SCOPE=$2
 
 # run the dev script for that build type in the scoped packages
-  lerna run --scope "$SCOPE" dev:$BUILD_TYPE --parallel
+lerna run --scope "$SCOPE" dev:$BUILD_TYPE --parallel
