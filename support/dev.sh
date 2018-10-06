e94b2d431e7343c936200ab8659d7fdbc25dd521
john gravois <jgravois@esri.com>
2018-10-05 18:07:57 -0700

    no bash script

--- support/dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# extract scope and build type from the command line args
-SCOPE=$1
-BUILD_TYPE=$2
-
-# run the dev script for that build type in the scoped packages
-  lerna run --scope "$SCOPE" dev:$BUILD_TYPE --parallel
