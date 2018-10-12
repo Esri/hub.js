d48224d1966a7299fc3fa70684255f7c8a57056a
Jaisudha Purushothaman <jpurushothaman@esri.com>
2018-10-11 17:51:12 -0700

    WIP(Adding events package): function to support getting the Hub event service
    AFFECTS PACKAGES:
    @esri/hub-events

--- packages/events/test/util.test.ts
+++ packages/events/test/util.test.ts
@@ -1,7 +1,6 @@
 import { getEventServiceUrl } from "../src/util";
 import {
   adminEventSearchResponse,
-  adminOnlyEventSearchResponse,
   orgEventSearchResponse,
   publicEventSearchResponse,
   emptyEventSearchResponse
@@ -24,32 +23,11 @@ describe("getEventServiceUrl", () => {
     getEventServiceUrl("5bc").then(result => {
       expect(paramsSpy.calls.count()).toEqual(1);
       const opts = paramsSpy.calls.argsFor(0)[0] as ISearchRequestOptions;
-      expect(opts.searchForm.q).toEqual(
+      expect(opts.searchForm.q).toContain(
         "typekeywords:hubEventsLayer AND orgid:5bc"
       );
       expect(result).toBe(
-        "https://services.arcgis.com/uCXeTVveQzP4IIcx/arcgis/rest/services/Hub%20Events/FeatureServer/0"
-      );
-      done();
-    });
-  });
-
-  it("should return an admin event service url", done => {
-    // stub searchItems
-    const paramsSpy = spyOn(items, "searchItems").and.returnValue(
-      new Promise(resolve => {
-        resolve(adminOnlyEventSearchResponse);
-      })
-    );
-
-    getEventServiceUrl("5bc").then(result => {
-      expect(paramsSpy.calls.count()).toEqual(1);
-      const opts = paramsSpy.calls.argsFor(0)[0] as ISearchRequestOptions;
-      expect(opts.searchForm.q).toEqual(
-        "typekeywords:hubEventsLayer AND orgid:5bc"
-      );
-      expect(result).toBe(
-        "https://services.arcgis.com/uCXeTVveQzP4IIcx/arcgis/rest/services/Hub%20Events/FeatureServer/0"
+        "https://services.arcgis.com/uCXeTVveQzP4IIcx/arcgis/rest/services/Hub Events/FeatureServer/0"
       );
       done();
     });
@@ -66,11 +44,11 @@ describe("getEventServiceUrl", () => {
     getEventServiceUrl("5bc").then(result => {
       expect(paramsSpy.calls.count()).toEqual(1);
       const opts = paramsSpy.calls.argsFor(0)[0] as ISearchRequestOptions;
-      expect(opts.searchForm.q).toEqual(
+      expect(opts.searchForm.q).toContain(
         "typekeywords:hubEventsLayer AND orgid:5bc"
       );
       expect(result).toBe(
-        "https://services.arcgis.com/uCXeTVveQzP4IIcx/arcgis/rest/services/Hub%20Events%20(org)/FeatureServer/0"
+        "https://services.arcgis.com/uCXeTVveQzP4IIcx/arcgis/rest/services/Hub Events (org)/FeatureServer/0"
       );
       done();
     });
@@ -87,11 +65,11 @@ describe("getEventServiceUrl", () => {
     getEventServiceUrl("5bc").then(result => {
       expect(paramsSpy.calls.count()).toEqual(1);
       const opts = paramsSpy.calls.argsFor(0)[0] as ISearchRequestOptions;
-      expect(opts.searchForm.q).toEqual(
+      expect(opts.searchForm.q).toContain(
         "typekeywords:hubEventsLayer AND orgid:5bc"
       );
       expect(result).toBe(
-        "https://services.arcgis.com/uCXeTVveQzP4IIcx/arcgis/rest/services/Hub%20Events%20(public)/FeatureServer/0"
+        "https://services.arcgis.com/uCXeTVveQzP4IIcx/arcgis/rest/services/Hub Events (public)/FeatureServer/0"
       );
       done();
     });
@@ -108,7 +86,7 @@ describe("getEventServiceUrl", () => {
     getEventServiceUrl("v8b").catch(error => {
       expect(paramsSpy.calls.count()).toEqual(1);
       const opts = paramsSpy.calls.argsFor(0)[0] as ISearchRequestOptions;
-      expect(opts.searchForm.q).toEqual(
+      expect(opts.searchForm.q).toContain(
         "typekeywords:hubEventsLayer AND orgid:v8b"
       );
       expect(error.message).toBe(
