d64806908d05c4e42a36d7190326357144b509ec
Jaisudha Purushothaman <jpurushothaman@esri.com>
2018-10-29 16:08:36 -0700

    adding getEventQueryFromType util method

--- packages/events/src/util.ts
+++ packages/events/src/util.ts
@@ -2,8 +2,9 @@
  * Apache-2.0 */
 
 import { IRequestOptions } from "@esri/arcgis-rest-request";
-import { IItem } from "@esri/arcgis-rest-common-types";
 import { searchItems, ISearchResult } from "@esri/arcgis-rest-items";
+import { IQueryFeaturesRequestOptions } from "@esri/arcgis-rest-feature-service";
+import { UserSession } from "@esri/arcgis-rest-auth";
 
 /**
  * Fetch the events service associated with a Hub Site.
@@ -54,4 +55,49 @@ export function getEventServiceUrl(
       throw Error("No events service found. Events are likely not enabled.");
     }
   });
+}
+
+/**
+ * Get the events query based on type.
+ * @param type - string to indicate event type with the options `upcoming`, `past`, `cancelled` and `draft`
+ * @param params - query features request options
+ * @param requestOptions - request options that may include authentication
+ * @returns an IQueryFeaturesRequestOptions with modified Where and OrderBy params
+ */
+export function getEventQueryFromType(
+  type: string,
+  params: IQueryFeaturesRequestOptions,
+  requestOptions?: IRequestOptions
+): IQueryFeaturesRequestOptions {
+  // this allows us to ask for type === upcoming | past | cancelled | draft
+  // and get an appropriate `where` and `orderByFields`
+  let typeWhere;
+  if (type === "cancelled") {
+    if (!params.orderByFields) {
+      // if orderByFields was passed in, use it, otherwise use appropriate one for cancelled
+      params.orderByFields = "EditDate DESC";
+    }
+    typeWhere = `isCancelled=1 AND status<>'draft'`;
+  } else if (type === "draft") {
+    if (!params.orderByFields) {
+      params.orderByFields = "EditDate DESC";
+    }
+    const session = requestOptions.authentication as UserSession;
+    const user = session ? session.username : null;
+    typeWhere = `Creator = '${user}' AND status = 'draft'`;
+  } else {
+    if (!params.orderByFields) {
+      // if orderByFields was passed in, use it, otherwise use appropriate one for type
+      params.orderByFields =
+        type === "upcoming" ? "startDate ASC" : "startDate DESC";
+    }
+    const operator = type === "upcoming" ? ">" : "<=";
+    typeWhere = `endDate${operator}CURRENT_TIMESTAMP AND (isCancelled<>1 OR isCancelled IS NULL) AND status<>'draft'`;
+  }
+  if (params.where) {
+    params.where = params.where + ` AND ${typeWhere}`;
+  } else {
+    params.where = typeWhere;
+  }
+  return params;
 }
