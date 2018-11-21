5129b3735b6e6ae9583b8d233f49f5d8e26b24f6
Jaisudha Purushothaman <jpurushothaman@esri.com>
2018-11-20 19:22:12 -0800

    adding tests, and geometry support and fixes

--- packages/events/src/search.ts
+++ packages/events/src/search.ts
@@ -6,31 +6,43 @@ import {
   queryFeatures,
   IQueryFeaturesResponse
 } from "@esri/arcgis-rest-feature-service";
-
+import { IGeometry } from "@esri/arcgis-rest-common-types";
 import { getItem } from "@esri/arcgis-rest-items";
 
 export interface IEventResourceObject {
-  id: string;
+  id: number | string;
   type: "events" | "sites";
   attributes: {
     [key: string]: any;
   };
+  imageUrl?: string;
+  geometry?: IGeometry;
 }
 
 /**
  * ```js
- * import { searchEvents } from "@esri/hub-events";
- * // by default, all annotations will be retrieved
- * searchEvents({ url: eventsUrl + "/0" })
+ * import { getEventQueryFromType, searchEvents } from "@esri/hub-events";
+ *
+ * const queryOptions: IQueryFeaturesRequestOptions = {
+ *  ...eventUrlOptions,
+ *  url: eventsUrl,
+ * }
+ * const eventType = "upcoming"; //| "past" | "cancelled" | "draft"; //(We never deal with other types in event list widget)
+ * const eventOptions: IQueryFeaturesRequestOptions = getEventQueryFromType(eventType, queryOptions);
+ * searchEvents({ url: eventsUrl })
  *   .then(response => {
  *     // {
  *     //   data: [{
- *     //     id: "Site1",
+ *     //     id: "4",
  *     //     type: "events",
- *     //     attributes: {title: "Vision Zero", ...}
+ *     //     attributes: {title: "Vision Zero", siteId: "CityofX", ...},
+ *     //     geometry: {
+ *     //       "x": -74.310680054965559,
+ *     //       "y": 40.723010058860787
+ *     //     }
  *     //   }],
  *     //   included: [{
- *     //     id: "Site1",
+ *     //     id: "CityofX",
  *     //     type: "sites",
  *     //     attributes: { id: "CityofX", url: "https://foo/bar"}
  *     //   }]
@@ -41,59 +53,70 @@ export interface IEventResourceObject {
  * @param requestOptions - request options that may include authentication
  * @returns A Promise that will resolve with decorated features from the annotation service for a Hub enabled ArcGIS Online organization.
  */
-export function searchAnnotations(
+export function searchEvents(
   requestOptions: IQueryFeaturesRequestOptions
 ): Promise<{ data: IEventResourceObject[]; included: IEventResourceObject[] }> {
+  if (requestOptions.returnGeometry !== false) {
+    requestOptions.returnGeometry = true;
+  }
   return queryFeatures(requestOptions).then(response => {
     const siteIds: string[] = [];
     const data: IEventResourceObject[] = [];
     const cacheBust = new Date().getTime();
-    return requestOptions.authentication
-      .getToken(requestOptions.url)
-      .then(token => {
-        (response as IQueryFeaturesResponse).features.forEach(function(event) {
-          const attributes = event.attributes;
-          attributes.imageUrl = "";
-          if (attributes.imageAttributes) {
-            const imageAttributes = JSON.parse(attributes.imageAttributes);
-            if (imageAttributes.crop) {
-              attributes.imageUrl = `${requestOptions.url}/${
-                attributes.OBJECTID
-              }/attachments/${imageAttributes.crop}?v=${cacheBust}`;
-              if (token) {
-                attributes.imageUrl += `&token=${token}`;
+    return (
+      requestOptions.authentication
+        // TODO: what happens if there is no authentication? aka public view
+        // Will getToken fail safe in that scenario?
+        .getToken(requestOptions.url)
+        .then(token => {
+          (response as IQueryFeaturesResponse).features.forEach(function(
+            event
+          ) {
+            const attributes = event.attributes;
+            const geometry = event.geometry;
+            let imageUrl = null;
+            if (attributes.imageAttributes) {
+              const imageAttributes = JSON.parse(attributes.imageAttributes);
+              if (imageAttributes.crop) {
+                imageUrl = `${requestOptions.url}/${
+                  attributes.OBJECTID
+                }/attachments/${imageAttributes.crop}?v=${cacheBust}`;
+                // if (token) {
+                imageUrl += `&token=${token}`;
+                // }
               }
             }
-          }
-          data.push({
-            id: attributes.OBJECTID,
-            type: "events",
-            attributes
-          });
-          if (siteIds.indexOf(attributes.siteId) === -1) {
-            siteIds.push(attributes.siteId);
-          }
-        });
-
-        const getSiteInfo = siteIds.map(siteId => getItem(siteId));
-
-        return Promise.all(getSiteInfo).then(siteInfo => {
-          const included: IEventResourceObject[] = [];
-
-          siteInfo.forEach(siteItem => {
-            included.push({
-              id: siteItem.id,
-              type: `sites`,
-              // not passing all site item information since that's not necessary.
-              attributes: {
-                id: siteItem.id,
-                url: siteItem.url
-              }
+            data.push({
+              id: attributes.OBJECTID,
+              type: "events",
+              imageUrl,
+              attributes,
+              geometry
             });
+            if (siteIds.indexOf(attributes.siteId) === -1) {
+              siteIds.push(attributes.siteId);
+            }
           });
+          const getSiteInfo = siteIds.map(siteId => getItem(siteId));
 
-          return { included, data };
-        });
-      });
+          return Promise.all(getSiteInfo).then(siteInfo => {
+            const included: IEventResourceObject[] = [];
+
+            siteInfo.forEach(siteItem => {
+              included.push({
+                id: siteItem.id,
+                type: `sites`,
+                // not passing all site item information since that's not necessary.
+                attributes: {
+                  id: siteItem.id,
+                  url: siteItem.url
+                }
+              });
+            });
+
+            return { included, data };
+          });
+        })
+    );
   });
 }
---
+++ packages/events/test/search.test.ts
@@ -0,0 +1,139 @@
+import { searchEvents } from "../src/search";
+
+import { publicEventSearchResponse } from "./mocks/ago_search";
+
+import {
+  eventQueryResponse,
+  eventQueryResponseEmpty,
+  eventResponse,
+  eventResponseEmpty,
+  siteResponse71a58,
+  siteResponse7c395
+} from "./mocks/event_search";
+
+import * as featureService from "@esri/arcgis-rest-feature-service";
+import * as item from "@esri/arcgis-rest-items";
+import { IQueryFeaturesRequestOptions } from "@esri/arcgis-rest-feature-service";
+import * as token from "@esri/arcgis-rest-auth";
+import { IRequestOptions } from "@esri/arcgis-rest-request";
+
+describe("searchEvents", () => {
+  const ro = {
+    authentication: {
+      portal: "https://some.portal.com/arcgis/sharing/rest",
+      getToken() {
+        return Promise.resolve("FAKE-TOKEN");
+      }
+    }
+  } as IRequestOptions;
+
+  it("should query for events when no parameters are passed", done => {
+    const queryParamsSpy = spyOn(
+      featureService,
+      "queryFeatures"
+    ).and.returnValue(
+      new Promise(resolve => {
+        resolve(eventQueryResponse);
+      })
+    );
+
+    // stub getItem
+    const siteParamsSpy = spyOn(item, "getItem").and.returnValues(
+      new Promise(resolve => {
+        resolve(siteResponse71a58);
+      }),
+      new Promise(resolve => {
+        resolve(siteResponse7c395);
+      })
+    );
+
+    searchEvents({
+      url: publicEventSearchResponse.results[0].url + "/0",
+      ...ro
+    }).then(response => {
+      expect(queryParamsSpy.calls.count()).toEqual(1);
+      expect(siteParamsSpy.calls.count()).toEqual(2);
+      const queryOpts = queryParamsSpy.calls.argsFor(
+        0
+      )[0] as IQueryFeaturesRequestOptions;
+      const site1Opts = siteParamsSpy.calls.argsFor(0)[0] as string;
+      const site2Opts = siteParamsSpy.calls.argsFor(1)[0] as string;
+
+      expect(queryOpts.url).toBe(
+        publicEventSearchResponse.results[0].url + "/0"
+      );
+      expect(site1Opts).toBe("71a58");
+      expect(site2Opts).toBe("7c395");
+      done();
+    });
+  });
+
+  it("should not fetch site info if no features are returned from search", done => {
+    const queryParamsSpy = spyOn(
+      featureService,
+      "queryFeatures"
+    ).and.returnValue(
+      new Promise(resolve => {
+        resolve(eventQueryResponseEmpty);
+      })
+    );
+
+    const siteParamsSpy = spyOn(item, "getItem");
+
+    searchEvents({
+      url: publicEventSearchResponse.results[0].url + "/0",
+      ...ro
+    }).then(response => {
+      expect(queryParamsSpy.calls.count()).toEqual(1);
+      expect(siteParamsSpy.calls.count()).toEqual(0);
+
+      const opts = queryParamsSpy.calls.argsFor(
+        0
+      )[0] as IQueryFeaturesRequestOptions;
+
+      expect(opts.url).toBe(publicEventSearchResponse.results[0].url + "/0");
+      expect(response.data.length).toBe(eventResponseEmpty.data.length);
+      expect(response.data.length).toBe(eventResponseEmpty.data.length);
+      expect(response.included.length).toBe(eventResponseEmpty.included.length);
+      done();
+    });
+  });
+
+  it("should allow users to fetch without geometry", done => {
+    const queryParamsSpy = spyOn(
+      featureService,
+      "queryFeatures"
+    ).and.returnValue(
+      new Promise(resolve => {
+        resolve(eventQueryResponse);
+      })
+    );
+
+    const siteParamsSpy = spyOn(item, "getItem").and.returnValues(
+      new Promise(resolve => {
+        resolve(siteResponse71a58);
+      }),
+      new Promise(resolve => {
+        resolve(siteResponse7c395);
+      })
+    );
+
+    searchEvents({
+      url: publicEventSearchResponse.results[0].url + "/0",
+      returnGeometry: false,
+      ...ro
+    }).then(response => {
+      expect(queryParamsSpy.calls.count()).toEqual(1);
+      expect(siteParamsSpy.calls.count()).toEqual(2);
+      const queryOpts = queryParamsSpy.calls.argsFor(
+        0
+      )[0] as IQueryFeaturesRequestOptions;
+      expect(queryOpts.url).toBe(
+        publicEventSearchResponse.results[0].url + "/0"
+      );
+      expect(response.data.length).toBe(eventResponse.data.length);
+      expect(response.included).toEqual(eventResponse.included);
+      done();
+    });
+  });
+});
