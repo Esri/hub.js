1593cb5e4f2120fd520cb12c917fee891fec17d7
Tom Wayson <tom@tomwayson.com>
2019-04-22 15:39:40 -0700

    renames getHubAPIUrl() to getHubApiUrl(); deprecates getHubUrl()

--- packages/common/src/api.ts
+++ packages/common/src/api.ts
@@ -2,16 +2,16 @@ import { IRequestOptions, getPortalUrl } from "@esri/arcgis-rest-request";
 
 /**
  * ```js
- * import { getHubAPIUrl() } from "@esri/hub-common";
+ * import { getHubApiUrl() } from "@esri/hub-common";
  * //
- * getHubAPIUrl({ portal: "https://custom.maps.arcgis.com/sharing/rest" })
+ * getHubApiUrl({ portal: "https://custom.maps.arcgis.com/sharing/rest" })
  * >> "https://hub.arcgis.com"
  * ```
  * Retrieves the Hub API Url associated with a specific ArcGIS Online organization.
  * @param requestOptions - request options that may include authentication
  * @returns the associated Hub API Url as a string.
  */
-export function getHubAPIUrl(requestOptions: IRequestOptions): string {
+export function getHubApiUrl(requestOptions: IRequestOptions): string {
   const portalUrl = getPortalUrl(requestOptions);
   if (portalUrl.match(/(qaext|\.mapsqa\.)/)) {
     return "https://hubqa.arcgis.com";
--- packages/common/test/api.test.ts
+++ packages/common/test/api.test.ts
@@ -1,4 +1,4 @@
-import { getHubAPIUrl } from "../src/api";
+import { getHubApiUrl } from "../src/api";
 import { IRequestOptions } from "@esri/arcgis-rest-request";
 
 describe("getHubApiUrl", () => {
@@ -11,38 +11,38 @@ describe("getHubApiUrl", () => {
     }
   } as IRequestOptions;
   it("can retrieve undefined", () => {
-    expect(getHubAPIUrl(ro)).toBe(undefined);
+    expect(getHubApiUrl(ro)).toBe(undefined);
   });
 
   it("can retrieve prod base url", () => {
     ro.authentication.portal = "https://something.maps.arcgis.com/sharing/rest";
-    expect(getHubAPIUrl(ro)).toBe("https://hub.arcgis.com");
+    expect(getHubApiUrl(ro)).toBe("https://hub.arcgis.com");
   });
 
   it("can retrieve qa base url", () => {
     ro.authentication.portal =
       "https://something.mapsqa.arcgis.com/sharing/rest";
-    expect(getHubAPIUrl(ro)).toBe("https://hubqa.arcgis.com");
+    expect(getHubApiUrl(ro)).toBe("https://hubqa.arcgis.com");
   });
 
   it("can retrieve dev base url", () => {
     ro.authentication.portal =
       "https://something.mapsdevext.arcgis.com/sharing/rest";
-    expect(getHubAPIUrl(ro)).toBe("https://hubdev.arcgis.com");
+    expect(getHubApiUrl(ro)).toBe("https://hubdev.arcgis.com");
   });
 
   it("can retrieve prod base url", () => {
     ro.authentication.portal = "https://www.arcgis.com/sharing/rest";
-    expect(getHubAPIUrl(ro)).toBe("https://hub.arcgis.com");
+    expect(getHubApiUrl(ro)).toBe("https://hub.arcgis.com");
   });
 
   it("can retrieve qa base url 2", () => {
     ro.authentication.portal = "https://qaext.arcgis.com/sharing/rest";
-    expect(getHubAPIUrl(ro)).toBe("https://hubqa.arcgis.com");
+    expect(getHubApiUrl(ro)).toBe("https://hubqa.arcgis.com");
   });
 
   it("can retrieve dev base url 2", () => {
     ro.authentication.portal = "https://devext.arcgis.com/sharing/rest";
-    expect(getHubAPIUrl(ro)).toBe("https://hubdev.arcgis.com");
+    expect(getHubApiUrl(ro)).toBe("https://hubdev.arcgis.com");
   });
 });
