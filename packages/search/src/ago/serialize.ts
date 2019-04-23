08225f297ef8fb977b2cac53be1b640cc4b1273b
pranavkulkarni <pranav.sk.90@gmail.com>
2019-04-23 14:38:33 -0400

    agg handled correctly

--- packages/search/src/ago/serialize.ts
+++ packages/search/src/ago/serialize.ts
@@ -14,15 +14,44 @@ import { encodeFilters } from "./helpers/encode-filters";
  * @returns {string}
  */
 export function serialize(searchParams: ISearchParams): string {
-  // 1. handle simple params like non-filters like q, sort etc.
+  // 1. handle simple params like non-filters like q, sort etc which have <string: string> type.
   const encodedNonFilters = Object.keys(searchParams)
-    .filter(param => !isFilterable(param))
+    .filter(
+      param => typeof searchParams[param] !== "object" && !isFilterable(param)
+    )
     .map(param => `${param}=${encodeURIComponent(searchParams[param])}`)
     .join("&");
   // 2. handle other params like tags, source, hasApi, groupIds that can be filtered
   const filters = createFilters(searchParams);
   const encodedFilters = encodeFilters(filters);
-  return [encodedNonFilters, encodedFilters].join("&");
+  // 3. handle agg
+  const encodedAggParts = [];
+  let encodedAgg = "";
+  if (searchParams.agg) {
+    if (searchParams.agg.fields) {
+      encodedAggParts.push(
+        `${encodeURIComponent("agg[fields]")}=${encodeURIComponent(
+          searchParams.agg.fields
+        )}`
+      );
+    }
+    if (searchParams.agg.size) {
+      encodedAggParts.push(
+        `${encodeURIComponent("agg[size]")}=${encodeURIComponent(
+          searchParams.agg.size.toString()
+        )}`
+      );
+    }
+    if (searchParams.agg.mode) {
+      encodedAggParts.push(
+        `${encodeURIComponent("agg[mode]")}=${encodeURIComponent(
+          searchParams.agg.mode
+        )}`
+      );
+    }
+    encodedAgg = encodedAggParts.join("&");
+  }
+  return [encodedNonFilters, encodedFilters, encodedAgg].join("&");
 }
 
 export function isFilterable(field: string) {
