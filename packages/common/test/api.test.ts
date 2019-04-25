43f4348a326076cc362f1d805d4822b0ab06f93c
john gravois <jgravois@esri.com>
2019-04-25 14:42:51 -0700

    ive never understood less than i do in this moment

--- packages/common/test/api.test.ts
+++ packages/common/test/api.test.ts
@@ -6,6 +6,7 @@ describe("getHubApiUrl", () => {
   beforeEach(() => {
     ro = {
       authentication: {
+        portal: null,
         getToken() {
           return Promise.resolve("FAKE-TOKEN");
         }
