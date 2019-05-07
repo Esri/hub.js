aebc17d8d2c35d29d0028832770e10dbafec3ed1
pranavkulkarni <pranav.sk.90@gmail.com>
2019-05-07 17:03:49 -0400

    downloadable types from common

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -5,3 +5,4 @@ export * from "./util";
 export * from "./types";
 export * from "./api";
 export * from "./content";
+export * from "./categories";
--- packages/search/src/ago/helpers/aggs/downloadable.ts
+++ packages/search/src/ago/helpers/aggs/downloadable.ts
@@ -1,4 +1,5 @@
 import { ISearchResult, IItem } from "@esri/arcgis-rest-portal";
+import { downloadableTypes } from "@esri/hub-common";
 
 // implements the 'downloadable' facet from AGO results. V3 datasets have this property computed
 // during the harvesting process but AGO results need this result computed at runtime
@@ -20,49 +21,3 @@ export function downloadable(response: ISearchResult<IItem>): any {
     { downloadable: { true: 0, false: 0 } }
   );
 }
-
-// eligible types are listed here: http://doc.arcgis.com/en/arcgis-online/reference/supported-items.htm
-const downloadableTypes = [
-  "360 VR Experience",
-  "Application",
-  "CityEngine Web Scene",
-  "Code Sample",
-  "CSV Collection",
-  "CSV",
-  "CAD Drawing",
-  "Desktop Application",
-  "Desktop Application Template",
-  "Desktop Style",
-  "File Geodatabase",
-  "GeoJson",
-  "Geoprocessing Package",
-  "Geoprocessing Sample",
-  "Image",
-  "iWork Keynote",
-  "iWork Numbers",
-  "KML Collection",
-  "KML",
-  "Layer",
-  "Layer File",
-  "Layer Package",
-  "Layout",
-  "Locator Package",
-  "Map Package",
-  "Map Service Definition",
-  "Map Template",
-  "Microsoft Excel",
-  "Microsoft Powerpoint",
-  "Microsoft Visio",
-  "Microsoft Word",
-  "Operations Dashboard Add In",
-  "PDF",
-  "Pro Map",
-  "Project Package",
-  "Project Template",
-  "Raster function template",
-  "Rule Package",
-  "Service Definition",
-  "Shapefile",
-  "Vector Tile Package",
-  "Workflow Manager Package"
-];
