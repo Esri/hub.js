5172ac63bb0b35722c2f68e71fb74eb93b00a89e
Haoliang Yu <haoliang.yu@outlook.com>
2019-09-16 16:26:11 -0400

    fix failing tests

--- packages/search/test/ago/helpers/filters/collection.test.ts
+++ packages/search/test/ago/helpers/filters/collection.test.ts
@@ -9,7 +9,7 @@ describe("collection test", () => {
       }
     };
     const expected =
-      '(type:"CSV Collection" OR type:"CSV" OR type:"Feature Collection Template" OR type:"Feature Collection" OR type:"Feature Layer" OR type:"Feature Service" OR type:"File Geodatabase" OR type:"GeoJSON" OR type:"GeoJson" OR type:"KML Collection" OR type:"KML" OR type:"Microsoft Excel" OR type:"Raster Layer" OR type:"Shapefile" OR type:"Stream Service" OR type:"Table" OR type:"CAD Drawing" OR type:"Document Link" OR type:"Hub Page" OR type:"Image" OR type:"iWork Keynote" OR type:"iWork Numbers" OR type:"iWork Pages" OR type:"Microsoft Powerpoint" OR type:"Microsoft Visio" OR type:"Microsoft Word" OR type:"PDF" OR type:"Pro Map" OR type:"Report Template")';
+      '(type:"CSV Collection" OR type:"CSV" OR type:"Feature Collection Template" OR type:"Feature Collection" OR type:"Feature Layer" OR type:"Feature Service" OR type:"File Geodatabase" OR type:"GeoJSON" OR type:"GeoJson" OR type:"KML Collection" OR type:"KML" OR type:"Microsoft Excel" OR type:"Raster Layer" OR type:"Shapefile" OR type:"Stream Service" OR type:"Table" OR type:"CAD Drawing" OR type:"Document Link" OR type:"Hub Page" OR type:"Site Page" OR type:"Image" OR type:"iWork Keynote" OR type:"iWork Numbers" OR type:"iWork Pages" OR type:"Microsoft Powerpoint" OR type:"Microsoft Visio" OR type:"Microsoft Word" OR type:"PDF" OR type:"Pro Map" OR type:"Report Template")';
     expect(collectionFilter(queryFilters)).toBe(expected);
   });
 
