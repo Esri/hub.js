cc105701065b6e06bdeefe9da987d539453d4b94
Andrew Tate <drewctate@gmail.com>
2020-04-17 13:43:29 -0600

    withoutByProp

--- packages/common/src/utils/index.ts
+++ packages/common/src/utils/index.ts
@@ -3,5 +3,6 @@ export * from "./fail-safe";
 export * from "./generate-random-string";
 export * from "./includes";
 export * from "./is-guid";
-export * from "./slugify";
 export * from "./map-by";
+export * from "./slugify";
+export * from "./without-by-prop";
