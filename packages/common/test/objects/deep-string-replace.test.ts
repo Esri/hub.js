e9e6eccaf25d486592e2584f010cc994b47f3028
Andrew Tate <drewctate@gmail.com>
2020-04-20 10:17:49 -0600

    deepStringReplace

---
+++ packages/common/test/objects/deep-string-replace.test.ts
@@ -0,0 +1,57 @@
+import deepStringReplace from "../../src/objects/deep-string-replace";
+
+describe("deepStringReplace", function() {
+  it("works with a string", function() {
+    const before = {
+      foo: {
+        bar: "replace"
+      },
+      baz: [{ value: "replace" }, { value: "replace" }, { value: "replace" }],
+      beep: "replace"
+    };
+
+    const after = deepStringReplace(before, "replace", "replaced");
+
+    expect(after).toEqual(
+      {
+        foo: {
+          bar: "replaced"
+        },
+        baz: [
+          { value: "replaced" },
+          { value: "replaced" },
+          { value: "replaced" }
+        ],
+        beep: "replaced"
+      },
+      "replaced deep strings"
+    );
+  });
+
+  it("works with a regex", function() {
+    const before = {
+      foo: {
+        bar: "replace"
+      },
+      baz: [{ value: "replace" }, { value: "replace" }, { value: "replace" }],
+      beep: "replace"
+    };
+
+    const after = deepStringReplace(before, /replace/, "replaced");
+
+    expect(after).toEqual(
+      {
+        foo: {
+          bar: "replaced"
+        },
+        baz: [
+          { value: "replaced" },
+          { value: "replaced" },
+          { value: "replaced" }
+        ],
+        beep: "replaced"
+      },
+      "replaced deep strings"
+    );
+  });
+});
