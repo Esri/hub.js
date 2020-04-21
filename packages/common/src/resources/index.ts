07110646afd2a52251d74eaaa443abb41032e159
Andrew Tate <drewctate@gmail.com>
2020-04-21 13:25:33 -0600

    Moving fetchHubTranslation

--- packages/common/src/resources/index.ts
+++ packages/common/src/resources/index.ts
@@ -1,4 +1,3 @@
 export * from "./convert-solution-template-resources-to-assets";
-export * from "./fetch-hub-translation";
 export * from "./get-item-thumbnail-url";
 export * from "./string-to-blob";
