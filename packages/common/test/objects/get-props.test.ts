d7bdd2f5a63db7ca18847228e7aca7dea7f0ac68
Andrew Tate <drewctate@gmail.com>
2020-04-22 14:15:17 -0600

    getProps branch coverage

--- packages/common/test/objects/get-props.test.ts
+++ packages/common/test/objects/get-props.test.ts
@@ -9,7 +9,10 @@ describe("getProps", function() {
       }
     };
 
-    const [prop1, prop2] = getProps(obj, ["foo.bar", "foo.boop"]);
+    const props = getProps(obj, ["foo.bar", "foo.boop", "foo.noexist"]);
+    expect(props.length).toEqual(2, "Only returned existant props");
+
+    const [prop1, prop2] = props;
     expect(prop1).toEqual("baz");
     expect(prop2).toEqual("beep");
   });
