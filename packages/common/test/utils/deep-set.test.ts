e70fa1b43143f28a597e71aacec86643ddf89870
Andrew Tate <drewctate@gmail.com>
2020-04-22 11:49:07 -0600

    deepSet code coverage

--- packages/common/test/utils/deep-set.test.ts
+++ packages/common/test/utils/deep-set.test.ts
@@ -2,9 +2,34 @@ import { deepSet } from "../../src";
 
 describe("deepSet", function() {
   it("sets deep properties", function() {
-    const foo: Record<string, any> = {};
+    const foo: Record<string, any> = {
+      horses: {
+        types: {
+          spalding: true
+        }
+      },
+      apple: null,
+      whale: "dolphin",
+      cars: {
+        toyota: "camry"
+      }
+    };
+    deepSet(foo, "horses.types.spalding", false);
     deepSet(foo, "bar.baz", "beep");
+    deepSet(foo, "apple.orange", "banana");
+    deepSet(foo, "whale", "shark");
+    deepSet(foo, "cars", { honda: "accord" });
 
-    expect(foo.bar.baz).toBe("beep");
+    expect(foo.horses.types.spalding).toBeFalsy("sets deep path when exists");
+    expect(foo.bar.baz).toBe("beep", "constructs path if doesnt exist");
+    expect(foo.apple.orange).toBe("banana", "overwrites nulls");
+    expect(foo.whale).toBe("shark", "overwrites non-object values");
+    expect(foo.cars).toEqual(
+      {
+        toyota: "camry",
+        honda: "accord"
+      },
+      "merges objects"
+    );
   });
 });
