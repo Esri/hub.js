778011daab63132f8fdab62a1c3420f16d94ea15
Andrew Tate <drewctate@gmail.com>
2020-05-12 09:28:28 -0600

    feat: _getHttpAndHttpsUris
    AFFECTS PACKAGES:
    @esri/hub-common

---
+++ packages/common/src/urls/_get-http-and-https-uris.ts
@@ -0,0 +1,12 @@
+/**
+ * Given a url without a protocol or with either http or https, return an array
+ * that contains both the http and https version
+ * @param {string} uri Url with either http or https, or no protocol
+ */
+export function _getHttpAndHttpsUris(uri: string) {
+  if (!uri) {
+    return [];
+  }
+  const domain = uri.replace(/^http(s?):\/\//, "");
+  return [`http://${domain}`, `https://${domain}`];
+}
--- packages/common/src/urls/index.ts
+++ packages/common/src/urls/index.ts
@@ -3,3 +3,4 @@ export * from "./get-portal-api-url";
 export * from "./get-portal-url";
 export * from "./hub-cdn-urlmap";
 export * from "./strip-protocol";
+export * from "./_get-http-and-https-uris";
