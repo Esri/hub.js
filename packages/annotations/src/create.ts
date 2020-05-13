51482ce02df9f8114bd8ebc4b80316c965f719af
trescube <stephen.k.hess@gmail.com>
2020-05-13 01:51:01 -0400

    first stab at refactoring

--- packages/annotations/src/create.ts
+++ packages/annotations/src/create.ts
@@ -46,7 +46,7 @@ export interface ICreateAnnoOptions extends IUserRequestOptions {
 export function createAnnotationService(
   requestOptions: ICreateAnnoOptions
 ): Promise<any> {
-  const session = requestOptions.authentication as UserSession;
+  const session = requestOptions.authentication;
 
   // check to see if an annotation service has already been created in the organization
   return searchItems({
@@ -56,105 +56,140 @@ export function createAnnotationService(
     if (searchResponse.results.length > 0) {
       // if the org already has a hosted annotations service, dont create another one
       return searchResponse.results[0];
-    } else {
-      return getPortal(requestOptions.orgId, {
-        authentication: requestOptions.authentication
-      }).then(portalResponse => {
-        const clonedServiceDefinition = JSON.parse(
-          JSON.stringify(annotationServiceDefinition)
-        );
-        // use the default extent of the organization for the new service
-        if (portalResponse.defaultExtent) {
-          clonedServiceDefinition.extent = portalResponse.defaultExtent;
-        }
+    } 
 
-        const description = `Feature service for Hub annotations. DO NOT DELETE THIS SERVICE. It stores the public annotations (comments) for all Hub items in your organization.`;
+    return getPortal(requestOptions.orgId, {
+      authentication: requestOptions.authentication
+    }).then(portalResponse => {
+      const clonedServiceDefinition = cloneObject(annotationServiceDefinition);
 
-        // const options:ICreateServiceOptions (no workee)
-        const options = {
-          authentication: session,
-          item: {
-            // NOTE: this will also be the item title (until we update it in a few seconds)
-            name: "hub_annotations",
-            description,
-            serviceDescription: description,
-            copyrightText: "",
-            capabilities: clonedServiceDefinition.capabilities,
-            spatialReference: defaultExtent.spatialReference,
-            xssPreventionInfo: {
-              xssPreventionEnabled: true,
-              xssPreventionRule: "InputOutput",
-              xssInputRule: "sanitizeInvalid"
-            },
-            maxRecordCount: 2000,
-            hasStaticData: false,
-            units: "esriMeters",
-            // anonymous users cant update or delete existing comments, only add new ones
-            editorTrackingInfo
-          }
-        };
+      // use the default extent of the organization for the new service
+      if (portalResponse.defaultExtent) {
+        clonedServiceDefinition.extent = portalResponse.defaultExtent;
+      }
 
-        // if one isnt yet present, create a new one
-        return createFeatureService(options).then(createResponse => {
-          if (!createResponse.success) {
-            throw new ArcGISRequestError(
-              `Failure to create service. One common cause is the presence of an existing service that shares the same name.`
-            );
-          }
-          // next, update the layer definition (using a template)
-          return addToServiceDefinition(createResponse.serviceurl, {
-            authentication: session,
-            layers: [clonedServiceDefinition]
-          } as IAddToServiceDefinitionOptions).then(() => {
-            // sometimes TS likes session, sometimes it likes options.authentication
-            return updateItem({
-              authentication: session,
-              item: {
-                id: createResponse.itemId,
-                typeKeywords: [
-                  "ArcGIS Server",
-                  "Data",
-                  "Feature Access",
-                  "Feature Service",
-                  "Service",
-                  "Singlelayer",
-                  "Hosted Service",
-                  // set the typeKeyword for later searches
-                  "hubAnnotationLayer"
-                ],
-                title: "Hub Annotations",
-                snippet: options.item.serviceDescription
-              }
-            }).then(() => {
-              // prevent the item from being accidentally deleted
-              return protectItem({
-                id: createResponse.itemId,
-                authentication: session
-              }).then(() => {
-                // share the item publically
-                return setItemAccess({
-                  id: createResponse.itemId,
-                  access: "public",
-                  authentication: session
-                }).then(() => {
-                  return new Promise(resolve =>
-                    resolve({
-                      success: true,
-                      itemId: createResponse.itemId
-                    })
-                  );
-                });
-              });
+      // if one isn't yet present, create a new one
+      return initializeFeatureService(session, clonedServiceDefinition.capabilities).then((createResponse: any) => {
+        const itemId = createResponse.itemId
+
+        // next, update the layer definition (using a template)
+        return addLayersToService(session, createResponse.serviceurl, clonedServiceDefinition).then(() => {
+          // sometimes TS likes session, sometimes it likes options.authentication
+          return updateItemDefinition(session, itemId).then(() => {
+            return preventAccidentalDeletion(session, itemId).then(() => {
+              return makeItemPublic(session, itemId).then(success(itemId));
             });
           });
         });
-        /*
-          firing off all the requests at once would be preferable, but in my own testing,
-          the sharing wasn't updated consistently
-
-          return Promise.all([ ... ])
-        */
       });
-    }
+    });    
   });
+}
+
+// TODO: this should use @esri/hub-common once the imports are sorted out
+function cloneObject (obj: any) {
+  return JSON.parse(JSON.stringify(obj))
+}
+
+function initializeFeatureService (session: UserSession, capabilities: any) {
+  const description = 'Feature service for Hub annotations. DO NOT DELETE THIS SERVICE. It stores the public annotations (comments) for all Hub items in your organization.';
+
+  // const options:ICreateServiceOptions (no workee)
+  const options = {
+    authentication: session,
+    item: {
+      // NOTE: this will also be the item title (until we update it in a few seconds)
+      name: "hub_annotations",
+      description,
+      serviceDescription: description,
+      copyrightText: "",
+      capabilities,
+      spatialReference: defaultExtent.spatialReference,
+      xssPreventionInfo: {
+        xssPreventionEnabled: true,
+        xssPreventionRule: "InputOutput",
+        xssInputRule: "sanitizeInvalid"
+      },
+      maxRecordCount: 2000,
+      hasStaticData: false,
+      units: "esriMeters",
+      // anonymous users cant update or delete existing comments, only add new ones
+      editorTrackingInfo
+    }
+  };
+
+
+  return new Promise((resolve, reject) => {
+    createFeatureService(options).then(createResponse => {
+      if (!createResponse.success) {
+        return reject(new ArcGISRequestError(
+          `Failure to create service. One common cause is the presence of an existing service that shares the same name.`
+        ))
+      }
+  
+      return resolve(createResponse)
+    })  
+  });
+}
+
+function addLayersToService (session: UserSession, serviceUrl: string, clonedServiceDefinition: any) {
+  const serviceDefinitionOptions = {
+    authentication: session,
+    layers: [clonedServiceDefinition]
+  }
+
+  return addToServiceDefinition(serviceUrl, serviceDefinitionOptions)
+}
+
+function updateItemDefinition (session: UserSession, id: string) {
+  const snippet = 'Feature service for Hub annotations. DO NOT DELETE THIS SERVICE. It stores the public annotations (comments) for all Hub items in your organization.';
+
+  const updateItemOptions = {
+    authentication: session,
+    item: {
+      id,
+      typeKeywords: [
+        "ArcGIS Server",
+        "Data",
+        "Feature Access",
+        "Feature Service",
+        "Service",
+        "Singlelayer",
+        "Hosted Service",
+        // set the typeKeyword for later searches
+        "hubAnnotationLayer"
+      ],
+      title: "Hub Annotations",
+      snippet
+    }
+  }
+
+  return updateItem(updateItemOptions)
+}
+
+function preventAccidentalDeletion (session: UserSession, id: string) {
+  return protectItem({
+    id,
+    authentication: session
+  })
+}
+
+function makeItemPublic (session: UserSession, id: string) {
+  // share the item publically
+  return setItemAccess({
+    id,
+    access: "public",
+    authentication: session
+  })
+}
+
+function success (itemId: string) {
+  return () => {
+    return new Promise(resolve =>
+      resolve({
+        success: true,
+        itemId
+      })
+    );  
+  }
 }
