3a02c57658a34b0746df30362b99d7a052a54aa6
Andrew Tate <drewctate@gmail.com>
2020-05-19 16:30:57 -0600

    feat: addSolutionResourceUrlToAssets
    AFFECTS PACKAGES:
    @esri/hub-common

---
+++ packages/common/test/resources/add-solution-resource-url-to-assets.test.ts
@@ -0,0 +1,69 @@
+import {
+  addSolutionResourceUrlToAssets,
+  IModelTemplate,
+  IHubRequestOptions
+} from "../../src";
+
+describe("addSolutionResourceUrlToAssets", () => {
+  it("attaches solution assets when bundle item id", async () => {
+    const tmpl = ({
+      bundleItemId: "123",
+      itemId: "item-id",
+      assets: [
+        {
+          mimeType: "image/png",
+          name: "thumbnail.png",
+          type: "thumbnail"
+        }
+      ]
+    } as unknown) as IModelTemplate;
+
+    const ro = ({
+      portalSelf: {
+        portalHostname: "foo-bar",
+        isPortal: true
+      }
+    } as unknown) as IHubRequestOptions;
+
+    const res = addSolutionResourceUrlToAssets(tmpl, ro);
+
+    expect(res).toEqual([
+      {
+        name: "thumbnail.png",
+        type: "thumbnail",
+        url:
+          "https://foo-bar/sharing/rest/content/items/123/resources/item-id-thumbnail.png"
+      }
+    ]);
+  });
+
+  it("does nothing when no bundle item id", async () => {
+    const tmpl = ({
+      itemId: "item-id",
+      assets: [
+        {
+          mimeType: "image/png",
+          name: "thumbnail.png",
+          type: "thumbnail"
+        }
+      ]
+    } as unknown) as IModelTemplate;
+
+    const ro = ({
+      portalSelf: {
+        portalHostname: "foo-bar",
+        isPortal: true
+      }
+    } as unknown) as IHubRequestOptions;
+
+    const res = addSolutionResourceUrlToAssets(tmpl, ro);
+
+    expect(res).toEqual([
+      {
+        mimeType: "image/png",
+        name: "thumbnail.png",
+        type: "thumbnail"
+      }
+    ]);
+  });
+});
