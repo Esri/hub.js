d9636feeafb7aa8740834d7955d4d7a1f0026f27
Andrew Tate <drewctate@gmail.com>
2020-05-21 16:37:38 -0600

    feat: removePage
    AFFECTS PACKAGES:
    @esri/hub-sites

--- packages/sites/src/pages/index.ts
+++ packages/sites/src/pages/index.ts
@@ -1,1 +1,2 @@
 export * from "./remove-site-from-page";
+export * from "./remove-page";
---
+++ packages/sites/src/pages/remove-page.ts
@@ -0,0 +1,60 @@
+import {
+  IModel,
+  IHubRequestOptions,
+  getModel,
+  mapBy,
+  getWithDefault,
+  failSafe,
+  unprotectModel
+} from "@esri/hub-common";
+import { unlinkSiteAndPage } from "../unlink-site-and-page";
+import { removeItem } from "@esri/arcgis-rest-portal";
+
+/**
+ * Remove a Page Item. This deletes the item.
+ * @param {Object | String} idOrModel Model object or Item Id
+ * @param {IRequestOptions} requestOptions
+ */
+export function removePage(
+  idOrModel: string | IModel,
+  requestOptions: IHubRequestOptions
+) {
+  let modelPromise = Promise.resolve(idOrModel);
+  if (typeof idOrModel === "string") {
+    modelPromise = getModel(idOrModel, requestOptions);
+  }
+  let pageModel: IModel;
+  // fire it to get the model...
+  return modelPromise
+    .then(model => {
+      pageModel = model as IModel;
+      // get the id's of the sites this page is linked to...
+      const linkedSites = mapBy(
+        "id",
+        getWithDefault(pageModel, "data.values.sites", [])
+      );
+      // we need to unlink the page from all sites. However, these calls *could* fail
+      // if the current user lacks rights to save the site item, so we just make sure these
+      // always resolve. In the Ember service code, we used `allSettled` but that's RSVP special sauce
+      const failSafeUnlink = failSafe(unlinkSiteAndPage);
+      return Promise.all(
+        linkedSites.map((siteId: string) => {
+          const opts = Object.assign(
+            {
+              pageModel,
+              siteId
+            },
+            requestOptions
+          );
+          return failSafeUnlink(opts);
+        })
+      );
+    })
+    .then(() => {
+      return unprotectModel(pageModel, requestOptions);
+    })
+    .then(() => {
+      const opts = Object.assign({ id: pageModel.item.id }, requestOptions);
+      return removeItem(opts);
+    });
+}
