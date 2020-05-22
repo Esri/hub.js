aef4f805806d0c424f498439b1f574a9078cd8f5
Andrew Tate <drewctate@gmail.com>
2020-05-22 11:48:37 -0600

    feat: getPageDependencies
    AFFECTS PACKAGES:
    @esri/hub-sites

--- packages/sites/src/pages/index.ts
+++ packages/sites/src/pages/index.ts
@@ -2,3 +2,4 @@ export * from "./remove-site-from-page";
 export * from "./remove-page";
 export * from "./update-page";
 export * from "./get-page-edit-url";
+export * from "./get-page-dependencies";
