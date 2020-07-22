5a681c568ef2355611089d6089b1f23ac859ed70
Rich <rich.gwozdz@gmail.com>
2020-07-22 12:22:30 -0700

    Simplify format converter.

--- packages/downloads/src/hub/format-converter.ts
+++ packages/downloads/src/hub/format-converter.ts
@@ -1,10 +1,16 @@
 import { DownloadFormat } from "../download-format"
 
-export function convertToHubFormat(format: DownloadFormat ) {
-  if (format === 'Shapefile') return 'shp';
-  if (format === 'CSV') return 'csv';
-  if (format === 'File Geodatabase') return 'fgdb';
-  if (format === 'GeoJson') return 'geojson';
-  if (format === 'KML') return 'kml';
-  throw new Error (`Unsupported Hub download format: ${format}`)
+const HUB_FORMAT_LOOKUP: any = {
+  'Shapefile': 'shp',
+  'CSV': 'csv',
+  'File Geodatabase': 'fgdb',
+  'GeoJson': 'geojson',
+  'KML': 'kml'
+}
+
+export function convertToHubFormat(format: DownloadFormat ): string {
+  if (!HUB_FORMAT_LOOKUP[format]) {
+    throw new Error (`Unsupported Hub download format: ${format}`)
+  }
+  return HUB_FORMAT_LOOKUP[format];
 }
