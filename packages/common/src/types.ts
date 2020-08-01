a1304061356bd96cb50e70ea84dda634a572c065
Tom Wayson <tom@tomwayson.com>
2020-07-31 18:19:34 -0700

    feat(types): add common Hub types needed for getContent()
    AFFECTS PACKAGES:
    @esri/hub-common

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,7 +1,7 @@
 /* Copyright (c) 2018 Environmental Systems Research Institute, Inc.
  * Apache-2.0 */
 
-import { IItem, IUser, IGroup } from "@esri/arcgis-rest-types";
+import { IItem, IUser, IGroup, IGeometry } from "@esri/arcgis-rest-types";
 import { IPortal } from "@esri/arcgis-rest-portal";
 import { IUserRequestOptions } from "@esri/arcgis-rest-auth";
 
@@ -108,26 +108,138 @@ export type IBatch = any[];
 export type IBatchTransform = (value: any) => any;
 
 export interface IGetSurveyModelsResponse {
-  form: IModel,
-  featureService: IModel,
-  fieldworker: IModel,
-  stakeholder: IModel
-};
+  form: IModel;
+  featureService: IModel;
+  fieldworker: IModel;
+  stakeholder: IModel;
+}
 
 export interface IGetGroupSharingDetailsResults {
-  group: IGroup,
-  modelsToShare: IModel[]
-};
+  group: IGroup;
+  modelsToShare: IModel[];
+}
 
 export interface IRevertableTaskSuccess {
-  status: "fullfilled",
-  revert: (...args: any[]) => Promise<any>,
-  results: any
-};
+  status: "fullfilled";
+  revert: (...args: any[]) => Promise<any>;
+  results: any;
+}
 
 export interface IRevertableTaskFailed {
-  status: "rejected",
-  error: Error
-};
+  status: "rejected";
+  error: Error;
+}
 
-export type IRevertableTaskResult = IRevertableTaskSuccess | IRevertableTaskFailed;
+export type IRevertableTaskResult =
+  | IRevertableTaskSuccess
+  | IRevertableTaskFailed;
+
+/**
+ * Hub Types
+ */
+export type HubType =
+  | "member"
+  | "team"
+  | "event"
+  | "dataset"
+  | "document"
+  | "map "
+  | "app "
+  | "site"
+  | "initiative"
+  | "template"
+  | "organization";
+export type VisibilityOptions = "private" | "org" | "public";
+export type ControlOptions = "view" | "edit" | "admin";
+
+// TW: basing this on https://opendata.arcgis.com/api/v3/datasets/7a153563b0c74f7eb2b3eae8a66f2fbb_0/
+// but that may not be accurate
+export interface IHubGeography {
+  size?: number; // what is this?
+  center?: [number, number];
+  geometry?: IGeometry;
+}
+
+export interface IHubResource {
+  name: string; // Generic term for the primary label (title, fullname, username, etc.)
+  summary?: string; // snippet or other summary
+  // publisher: IHubOwner // TODO: better name? item.owner with more user metadata
+
+  // Derived metadata
+  hubType: HubType;
+  // TW: why is this on IHubResource instead of IHubContent?
+  permissions: {
+    // overrides item.access with more attributes. could flatten.
+    visibility: VisibilityOptions; // item.access
+    control?: ControlOptions; // itemControl
+    groups?: IGroup[]; // item.sharing.groups via content/users/:username/items/:id
+  };
+
+  // Explicit data information since this is a common confusion + bug report
+  createdDate: Date; // formal metadata || new Date(item.created)
+  createdDateSource?: string; // description of what was used for this attribute
+  updatedDate: Date; // formal metadata || new Date(item.modified)
+  updatedDateSource?: string; // description of what was used for this attribute
+  thumbnailUrl?: string; // Full URL. item.thumbnail with host + path
+
+  // boundary will default to the item extent
+  // but can be overwritten by enrichments from the Hub API (inline)
+  // or fetched from a location such as /resources/boundary.json
+  boundary?: IHubGeography;
+
+  // Additional metadata from custom/formal elements
+  metadata?: any;
+  // Unique or additional formal metadata that will be displayed in sidebar
+}
+
+export interface IHubContent extends IHubResource, IItem {
+  // license: IHubLicense // [Future] item.licenseInfo
+
+  publishedDate: Date; // formal metadata || new Date(item.created)
+  publishedDateSource?: string; // description of what was used for this attribute
+
+  // Hub configuration metadata
+  actionLinks?: IActionLink[]; // item.properties.links
+  hubActions?: object; // item.properties.actions - enable/disable standard actions like `createWebmap` or `createStorymap`
+
+  metrics?: {
+    // Set visibility for telemetry metrics. Nested object future-proofing, but could flatten.
+    visibility: VisibilityOptions | "updateGroups"; // item.properties.metrics
+  };
+
+  // TW: not sure about contentUrl - why not just use item.url???
+  // contentUrl: string // Link to the raw content. item.url in most (but not all) item types
+
+  // potential future props
+  // contentDisplay?: 'thumbnail' | 'map' // [Future] View configuration options such as cartography, charts, table, etc.
+  // source: IHubCommunity // [Future] each of these has common metadata like `title`, `thumbnail` , and `link`
+
+  // Content specific values. Combination of relevant item.data, layer info, enrichments, configuration settings
+  // could use values instead - which is common within item.data.values
+  // TW: why not just add these on the sub types? Examples:
+  // - IHubDocument has .format but not .fields or .recordNumebr, etc
+  // - IHubDataset has .fields, .recordNumber, but not .basemap, etc
+  // attributes?: {
+  //     // Dataset, e.g.
+  //     fields?: [IField]
+  //     recordNumber?: number
+  //     // Map, e.g.
+  //     layers?: []
+  //     basemap?: string
+  //     // Document
+  //     format?: 'link' | 'PDF' | 'MS Word' | 'MS Excel' | 'Text'
+  //     // Initiative
+  //     stage?: string
+  //     followersNumber?: number
+  //     coreTeamId?: string
+  //     contentTeamId?: string
+  //     supportingTeamsNumber?: number
+  // }
+}
+
+// Optional configured app links that replace "Create StoryMap" with links to specific apps/sites
+// per https://esriarlington.tpondemand.com/entity/96316-content-viewer-sees-associated-app-links
+export interface IActionLink {
+  title: string;
+  url: string;
+}
