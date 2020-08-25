4bdebe5d2ec341c7df05349f3c67824ee490fcb8
Andrew Tate <drewctate@gmail.com>
2020-08-25 11:54:35 -0500

    feat: allowing options to be passed into stringToBlob
    AFFECTS PACKAGES:
    @esri/hub-common

--- packages/common/src/resources/string-to-blob.ts
+++ packages/common/src/resources/string-to-blob.ts
@@ -3,7 +3,10 @@
  * NOTE: This is not currently supported in Node
  * @param {string} the string
  */
-export function stringToBlob(s: string): any {
+export function stringToBlob(
+  s: string,
+  options: BlobPropertyBag = { type: "application/octet-stream" }
+) {
   /* istanbul ignore next */
   if (typeof Blob !== "undefined") {
     const bytes = [];
@@ -11,7 +14,7 @@ export function stringToBlob(s: string): any {
       bytes[i] = s.charCodeAt(i);
     }
     const encoded = new Uint8Array(bytes);
-    return new Blob([encoded], { type: "application/octet-stream" });
+    return new Blob([encoded], options);
   } else {
     throw new Error(`stringToBlob is not currently supported on Node`);
   }
