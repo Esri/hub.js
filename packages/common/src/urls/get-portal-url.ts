36f82e63c64f5df8537d10ced3d758b02bf80f3c
Tom Wayson <tom@tomwayson.com>
2020-08-26 10:16:51 -0700

    add usage examples to doc comments for getPortalUrl() and getPortalApiUrl()

--- packages/common/src/urls/get-portal-url.ts
+++ packages/common/src/urls/get-portal-url.ts
@@ -6,6 +6,23 @@ import {
 import { IHubRequestOptions } from "../types";
 
 /**
+ * ```js
+ * import { getPortalUrl } from "@esri/hub-common";
+ * // from a portal API URL
+ * let portalUrl = getPortalUrl("https://org.maps.arcgis.com/sharing/rest"); // https://org.maps.arcgis.com
+ * // from an enterprise portal self response (IPortal)
+ * let portalSelf = { isPortal: true, portalHostname: "server.example.org" };
+ * portalUrl = getPortalUrl(portalSelf); // https://server.example.org
+ * // from an online portal self response (IPortal)
+ * portalSelf = { isPortal: false, urlKey: "org", customBaseUrl: "maps.arcgis.com" };
+ * portalUrl = getPortalUrl(portalSelf); // https://org.maps.arcgis.com
+ * // from hub request options (IHubRequestOptions) with a portal self (IPortal)
+ * let requestOptions = { isPortal: false, portalSelf };
+ * portalUrl = getPortalUrl(requestOptions); // https://org.maps.arcgis.com
+ * // from request options (IRequestOptions) with a portal (string)
+ * requestOptions = { portal: "https://org.maps.arcgis.com/sharing/rest" };
+ * portalUrl = getPortalUrl(requestOptions); // https://org.maps.arcgis.com
+ * ```
  * Derive a portal's base URL from the portal's API URL, a portal object, or request options
  * @param urlOrObject a portal API URL, a portal object, or request options containing either of those
  * @returns The portal base URL, defaults to `https://www.arcgis.com`
