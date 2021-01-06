0cb6a740e3bd296dc67494b9b2cb6d2260b77b86
Dave Bouwman <dave@davebouwman.com>
2021-01-06 16:53:04 -0700

    fix(sites): coerce numeric site title to string
    affects: @esri/hub-sites
    
    Ensure that domain add/update coerce numeric site title to a string

--- packages/sites/test/domains/add-domain.test.ts
+++ packages/sites/test/domains/add-domain.test.ts
@@ -1,5 +1,5 @@
-import { IHubRequestOptions } from "@esri/hub-common";
-import { addDomain } from "../../src/domains";
+import { IHubRequestOptions, cloneObject, getProp } from "@esri/hub-common";
+import { addDomain, IDomainEntry } from "../../src/domains";
 import * as fetchMock from "fetch-mock";
 import * as _checkStatusAndParseJsonModule from "../../src/domains/_check-status-and-parse-json";
 
@@ -18,7 +18,7 @@ describe("addDomain", function() {
     sslOnly: true
   };
 
-  it("throws error on portal", async function() {
+  it("sends request in AGO", async function() {
     const ro = { isPortal: false } as IHubRequestOptions;
 
     spyOn(
@@ -30,6 +30,40 @@ describe("addDomain", function() {
 
     const res = await addDomain(domainEntry, ro);
     expect(fetchMock.done()).toBeTruthy("fetch should have been called once");
+    expect(res.success).toBeTruthy("json parsed and response returned");
+  });
+
+  it("converts title to string", async function() {
+    const entry = ({
+      clientKey: "hdlf3lCPRguoTFr6",
+      domain: "zebra-dc.hubqa.arcgis.com",
+      hostname: "zebra-dc.hubqa.arcgis.com",
+      id: "146663",
+      orgId: "97KLIFOSt5CxbiRI",
+      orgKey: "dc",
+      orgTitle: "Washington, DC R&D Center (QA)",
+      permanentRedirect: false,
+      siteId: "9697f67b6d6343fa823dcdbe2d172073",
+      siteTitle: 1234,
+      sslOnly: true
+    } as unknown) as IDomainEntry;
+    const ro = { isPortal: false } as IHubRequestOptions;
+
+    spyOn(
+      _checkStatusAndParseJsonModule,
+      "_checkStatusAndParseJson"
+    ).and.returnValue(Promise.resolve({ success: true }));
+
+    fetchMock.post("end:api/v3/domains", {});
+
+    const res = await addDomain(entry, ro);
+    expect(fetchMock.done()).toBeTruthy("fetch should have been called once");
+    const opts = fetchMock.lastOptions("end:api/v3/domains");
+    const body = JSON.parse(opts.body as string);
+    expect(getProp(body, "siteTitle")).toBe(
+      "1234",
+      "should coerce numeric title to a string"
+    );
     expect(res.success).toBeTruthy("json parsed and response returned");
   });
 
