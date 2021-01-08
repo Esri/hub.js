e8f121f77782e7367b0fef22ac7a83a4409acf6d
drspacemanphd <markabrown87@gmail.com>
2021-01-07 23:09:02 -0500

    f/164823 added merge function for aggregations

---
+++ packages/search/src/util/aggregations/merge-aggregations.ts
@@ -0,0 +1,121 @@
+interface IAggregation {
+  label: string | number;
+  aggValue: number;
+}
+
+export interface IAggregationResult {
+  fieldName: string;
+  aggregations: IAggregation[];
+}
+
+type mergeFunc = (aggValueOne: number, aggValueTwo: number) => number;
+
+type AggregationMap = Record<string, number>;
+type AggregationResultMap = Record<string, AggregationMap>;
+
+const defaultMergeFunc = (aggValueOne: number, aggValueTwo: number) =>
+  (aggValueOne || 0) + (aggValueTwo || 0);
+
+export function mergeAggregations(
+  aggs: IAggregationResult[][] = [],
+  mergeFunction: mergeFunc = defaultMergeFunc
+): IAggregationResult[] {
+  if (!Array.isArray || aggs.length === 0) {
+    return null;
+  }
+
+  const mergedMap: AggregationResultMap = aggs.reduce(
+    (mergedAggs: AggregationResultMap, aggResultList: IAggregationResult[]) => {
+      _addAggResultsToMap(mergedAggs, aggResultList, mergeFunction);
+      return mergedAggs;
+    },
+    {} as AggregationResultMap
+  );
+
+  return _convertMapToResult(mergedMap);
+}
+
+function _addAggResultsToMap(
+  mergedAggs: AggregationResultMap,
+  aggResultList: IAggregationResult[],
+  mergeFunction: mergeFunc
+): AggregationResultMap {
+  aggResultList.forEach((aggResult: IAggregationResult) => {
+    if (!mergedAggs[aggResult.fieldName]) {
+      _addNewAggFieldToMap(mergedAggs, aggResult);
+    } else {
+      _combineMapAndResult(
+        mergedAggs[aggResult.fieldName],
+        aggResult,
+        mergeFunction
+      );
+    }
+  });
+
+  return mergedAggs;
+}
+
+function _addNewAggFieldToMap(
+  mergedAggs: AggregationResultMap,
+  aggResult: IAggregationResult
+): AggregationResultMap {
+  mergedAggs[aggResult.fieldName] = _convertAggsToMap(aggResult.aggregations);
+  return mergedAggs;
+}
+
+function _combineMapAndResult(
+  aggFieldMap: AggregationMap,
+  aggResult: IAggregationResult,
+  mergeFunction: mergeFunc
+): AggregationMap {
+  if (!aggResult || !aggResult.aggregations) {
+    return aggFieldMap;
+  }
+
+  aggResult.aggregations.forEach((agg: IAggregation) => {
+    if (!aggFieldMap[agg.label]) {
+      aggFieldMap[agg.label] = agg.aggValue;
+    } else {
+      aggFieldMap[agg.label] = mergeFunction(
+        aggFieldMap[agg.label],
+        agg.aggValue
+      );
+    }
+  });
+
+  return aggFieldMap;
+}
+
+function _convertAggsToMap(aggregations: IAggregation[]): AggregationMap {
+  if (!aggregations) {
+    return {};
+  }
+
+  return aggregations.reduce(
+    (aggLabelValueMap: AggregationMap, agg: IAggregation) => {
+      aggLabelValueMap[agg.label] = agg.aggValue;
+      return aggLabelValueMap;
+    },
+    {} as AggregationMap
+  );
+}
+
+function _convertMapToResult(
+  mergedMap: AggregationResultMap
+): IAggregationResult[] {
+  return Object.keys(mergedMap).map(fieldName => {
+    const aggregations: IAggregation[] = Object.keys(mergedMap[fieldName]).map(
+      (name: string) => {
+        return {
+          label: name,
+          aggValue: mergedMap[fieldName][name]
+        };
+      }
+    );
+
+    return {
+      fieldName,
+      aggregations
+    };
+  });
+}
---
+++ packages/search/test/util/aggregations/merge-aggregations.test.ts
@@ -0,0 +1,600 @@
+import {
+  AggregationResult,
+  mergeAggregations
+} from "../../../src/util/aggregations/merge-aggregations";
+
+describe("Merge Aggregation Function", () => {
+  it("can properly merge several lists of aggregations with a default merge function", () => {
+    // Setup
+    const aggregationsOne: AggregationResult[] = [
+      {
+        fieldName: "access",
+        aggregations: [
+          {
+            label: "public",
+            aggValue: 5
+          },
+          {
+            label: "private",
+            aggValue: 3
+          },
+          {
+            label: 0,
+            aggValue: 2
+          }
+        ]
+      },
+      {
+        fieldName: "category",
+        aggregations: [
+          {
+            label: 1,
+            aggValue: 23
+          },
+          {
+            label: "a_category",
+            aggValue: 10
+          }
+        ]
+      }
+    ];
+
+    const aggregationsTwo: AggregationResult[] = [
+      {
+        fieldName: "access",
+        aggregations: [
+          {
+            label: "public",
+            aggValue: 17
+          },
+          {
+            label: "shared",
+            aggValue: 5
+          },
+          {
+            label: 1,
+            aggValue: 4
+          }
+        ]
+      },
+      {
+        fieldName: "type",
+        aggregations: [
+          {
+            label: 1,
+            aggValue: 12
+          },
+          {
+            label: "Feature Layer",
+            aggValue: 7
+          }
+        ]
+      }
+    ];
+
+    const aggregationsThree: AggregationResult[] = [
+      {
+        fieldName: "category",
+        aggregations: [
+          {
+            label: "a_category",
+            aggValue: 12
+          },
+          {
+            label: 0,
+            aggValue: 9
+          }
+        ]
+      },
+      {
+        fieldName: "type",
+        aggregations: [
+          {
+            label: 0,
+            aggValue: 6
+          },
+          {
+            label: "Feature Layer",
+            aggValue: 2
+          }
+        ]
+      }
+    ];
+
+    const expectedMergedAggregations: AggregationResult[] = [
+      {
+        fieldName: "access",
+        aggregations: [
+          {
+            label: "0",
+            aggValue: 2
+          },
+          {
+            label: "1",
+            aggValue: 4
+          },
+          {
+            label: "public",
+            aggValue: 22
+          },
+          {
+            label: "private",
+            aggValue: 3
+          },
+          {
+            label: "shared",
+            aggValue: 5
+          }
+        ]
+      },
+      {
+        fieldName: "category",
+        aggregations: [
+          {
+            label: "0",
+            aggValue: 9
+          },
+          {
+            label: "1",
+            aggValue: 23
+          },
+          {
+            label: "a_category",
+            aggValue: 22
+          }
+        ]
+      },
+      {
+        fieldName: "type",
+        aggregations: [
+          {
+            label: "0",
+            aggValue: 6
+          },
+          {
+            label: "1",
+            aggValue: 12
+          },
+          {
+            label: "Feature Layer",
+            aggValue: 9
+          }
+        ]
+      }
+    ];
+
+    // Test
+    const actualMergedAggregations: AggregationResult[] = mergeAggregations([
+      aggregationsOne,
+      aggregationsTwo,
+      aggregationsThree
+    ]);
+
+    // Assert
+    expect(expectedMergedAggregations).toEqual(actualMergedAggregations);
+  });
+
+  it("can properly merge several lists of aggregations with a custom merge function", () => {
+    // Setup
+    const aggregationsOne: AggregationResult[] = [
+      {
+        fieldName: "access",
+        aggregations: [
+          {
+            label: "public",
+            aggValue: 5
+          },
+          {
+            label: "private",
+            aggValue: 3
+          },
+          {
+            label: 0,
+            aggValue: 2
+          }
+        ]
+      },
+      {
+        fieldName: "category",
+        aggregations: [
+          {
+            label: 1,
+            aggValue: 23
+          },
+          {
+            label: "a_category",
+            aggValue: 10
+          }
+        ]
+      }
+    ];
+
+    const aggregationsTwo: AggregationResult[] = [
+      {
+        fieldName: "access",
+        aggregations: [
+          {
+            label: "public",
+            aggValue: 17
+          },
+          {
+            label: "shared",
+            aggValue: 5
+          },
+          {
+            label: 1,
+            aggValue: 4
+          }
+        ]
+      },
+      {
+        fieldName: "type",
+        aggregations: [
+          {
+            label: 1,
+            aggValue: 12
+          },
+          {
+            label: "Feature Layer",
+            aggValue: 7
+          }
+        ]
+      }
+    ];
+
+    const aggregationsThree: AggregationResult[] = [
+      {
+        fieldName: "category",
+        aggregations: [
+          {
+            label: "a_category",
+            aggValue: 12
+          },
+          {
+            label: 0,
+            aggValue: 9
+          }
+        ]
+      },
+      {
+        fieldName: "type",
+        aggregations: [
+          {
+            label: 0,
+            aggValue: 6
+          },
+          {
+            label: "Feature Layer",
+            aggValue: 2
+          }
+        ]
+      }
+    ];
+
+    const mergeFunc: any = (one: number, two: number) => Math.max(one, two);
+
+    const expectedMergedAggregations: AggregationResult[] = [
+      {
+        fieldName: "access",
+        aggregations: [
+          {
+            label: "0",
+            aggValue: 2
+          },
+          {
+            label: "1",
+            aggValue: 4
+          },
+          {
+            label: "public",
+            aggValue: 17
+          },
+          {
+            label: "private",
+            aggValue: 3
+          },
+          {
+            label: "shared",
+            aggValue: 5
+          }
+        ]
+      },
+      {
+        fieldName: "category",
+        aggregations: [
+          {
+            label: "0",
+            aggValue: 9
+          },
+          {
+            label: "1",
+            aggValue: 23
+          },
+          {
+            label: "a_category",
+            aggValue: 12
+          }
+        ]
+      },
+      {
+        fieldName: "type",
+        aggregations: [
+          {
+            label: "0",
+            aggValue: 6
+          },
+          {
+            label: "1",
+            aggValue: 12
+          },
+          {
+            label: "Feature Layer",
+            aggValue: 7
+          }
+        ]
+      }
+    ];
+
+    // Test
+    const actualMergedAggregations: AggregationResult[] = mergeAggregations(
+      [aggregationsOne, aggregationsTwo, aggregationsThree],
+      mergeFunc
+    );
+
+    // Assert
+    expect(expectedMergedAggregations).toEqual(actualMergedAggregations);
+  });
+
+  it("can properly ignore non-existent aggValues", () => {
+    // Setup
+    const aggregationsOne: AggregationResult[] = [
+      {
+        fieldName: "access",
+        aggregations: [
+          {
+            label: "public",
+            aggValue: null
+          },
+          {
+            label: "private",
+            aggValue: 3
+          },
+          {
+            label: 0,
+            aggValue: 2
+          }
+        ]
+      },
+      {
+        fieldName: "category",
+        aggregations: [
+          {
+            label: 1,
+            aggValue: 23
+          },
+          {
+            label: "a_category",
+            aggValue: 10
+          }
+        ]
+      }
+    ];
+
+    const aggregationsTwo: AggregationResult[] = [
+      {
+        fieldName: "access",
+        aggregations: [
+          {
+            label: "public",
+            aggValue: 17
+          },
+          {
+            label: "shared",
+            aggValue: 5
+          },
+          {
+            label: 1,
+            aggValue: 4
+          }
+        ]
+      }
+    ];
+
+    const aggregationsThree: AggregationResult[] = [
+      {
+        fieldName: "category",
+        aggregations: [
+          {
+            label: "a_category",
+            aggValue: undefined
+          },
+          {
+            label: 0,
+            aggValue: 9
+          }
+        ]
+      }
+    ];
+
+    const expectedMergedAggregations: AggregationResult[] = [
+      {
+        fieldName: "access",
+        aggregations: [
+          {
+            label: "0",
+            aggValue: 2
+          },
+          {
+            label: "1",
+            aggValue: 4
+          },
+          {
+            label: "public",
+            aggValue: 17
+          },
+          {
+            label: "private",
+            aggValue: 3
+          },
+          {
+            label: "shared",
+            aggValue: 5
+          }
+        ]
+      },
+      {
+        fieldName: "category",
+        aggregations: [
+          {
+            label: "0",
+            aggValue: 9
+          },
+          {
+            label: "1",
+            aggValue: 23
+          },
+          {
+            label: "a_category",
+            aggValue: 10
+          }
+        ]
+      }
+    ];
+
+    // Test
+    const actualMergedAggregations: AggregationResult[] = mergeAggregations([
+      aggregationsOne,
+      aggregationsTwo,
+      aggregationsThree
+    ]);
+
+    // Assert
+    expect(expectedMergedAggregations).toEqual(actualMergedAggregations);
+  });
+
+  it("can properly default falsey or empty aggregations", () => {
+    // Setup
+    const aggregationsOne: AggregationResult[] = [
+      {
+        fieldName: "access",
+        aggregations: [
+          {
+            label: "public",
+            aggValue: 5
+          },
+          {
+            label: "private",
+            aggValue: 3
+          },
+          {
+            label: 0,
+            aggValue: 2
+          }
+        ]
+      },
+      {
+        fieldName: "category",
+        aggregations: []
+      },
+      {
+        fieldName: "type",
+        aggregations: undefined
+      }
+    ];
+
+    const aggregationsTwo: AggregationResult[] = [
+      {
+        fieldName: "access",
+        aggregations: [
+          {
+            label: "public",
+            aggValue: undefined
+          },
+          {
+            label: "shared",
+            aggValue: 5
+          },
+          {
+            label: 1,
+            aggValue: 4
+          }
+        ]
+      },
+      {
+        fieldName: "type",
+        aggregations: null
+      }
+    ];
+
+    const aggregationsThree: AggregationResult[] = [
+      {
+        fieldName: "category",
+        aggregations: [
+          {
+            label: "a_category",
+            aggValue: 20
+          },
+          {
+            label: 0,
+            aggValue: 9
+          }
+        ]
+      }
+    ];
+
+    const expectedMergedAggregations: AggregationResult[] = [
+      {
+        fieldName: "access",
+        aggregations: [
+          {
+            label: "0",
+            aggValue: 2
+          },
+          {
+            label: "1",
+            aggValue: 4
+          },
+          {
+            label: "public",
+            aggValue: 5
+          },
+          {
+            label: "private",
+            aggValue: 3
+          },
+          {
+            label: "shared",
+            aggValue: 5
+          }
+        ]
+      },
+      {
+        fieldName: "category",
+        aggregations: [
+          {
+            label: "0",
+            aggValue: 9
+          },
+          {
+            label: "a_category",
+            aggValue: 20
+          }
+        ]
+      },
+      {
+        fieldName: "type",
+        aggregations: []
+      }
+    ];
+
+    // Test
+    const actualMergedAggregations: AggregationResult[] = mergeAggregations([
+      aggregationsOne,
+      aggregationsTwo,
+      aggregationsThree
+    ]);
+
+    // Assert
+    expect(expectedMergedAggregations).toEqual(actualMergedAggregations);
+  });
+});
