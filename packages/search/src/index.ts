8801379c8c09c2971bea1e6eb0ce354b5f6fcac5
drspacemanphd <markabrown87@gmail.com>
2021-01-11 09:02:30 -0500

    wip

--- packages/search/src/index.ts
+++ packages/search/src/index.ts
@@ -1,9 +1,30 @@
 /* Copyright (c) 2018 Environmental Systems Research Institute, Inc.
  * Apache-2.0 */
 
+import { UserSession } from "@esri/arcgis-rest-auth";
+import { ICursorSearchResults } from "./hub";
+
 export {
   agoSearch,
   agoFormatItemCollection,
   serialize,
   computeItemsFacets
 } from "./ago";
+
+export interface ISearchService<T> {
+  search(params: unknown): Promise<ICursorSearchResults<T>> | ICursorSearchResults<T>
+}
+
+export abstract class SearchService<T> implements ISearchService<T> {
+  private portalBaseUrl: string;
+  private userSession: UserSession;
+  private apiUrl: string;
+
+  constructor(portalBaseUrl: string, userSession: UserSession, apiUrl?: string) {
+    this.portalBaseUrl = portalBaseUrl;
+    this.userSession = userSession;
+    this.apiUrl = apiUrl;
+  }
+
+  abstract search(params: unknown): Promise<ICursorSearchResults<T>> | ICursorSearchResults<T>
+}
