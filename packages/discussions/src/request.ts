8640766a22cebdf29a361f098722f8185d548c38
Chase Gruber <cgruber@esri.com>
2021-03-03 10:26:51 -0500

    now geometry works?

--- packages/discussions/src/request.ts
+++ packages/discussions/src/request.ts
@@ -1,42 +1,39 @@
-import { RequestOptions } from "./types";
+import { IRequestOptions } from "./types";
 
-export function request (
-  url: string,
-  options: RequestOptions
-) {
+export function request(url: string, options: IRequestOptions) {
   const headers = new Headers();
-  headers.append('Content-Type', 'application/json');
-  
-  let token = options.token;
+  headers.append("Content-Type", "application/json");
+
+  const token = options.token;
   // if (options.authentication) {
   //   token = options.authentication.getToken();
   // }
   if (token) {
-    headers.append('Authorization', `Bearer ${token}`);
+    headers.append("Authorization", `Bearer ${token}`);
   }
 
   const opts: RequestInit = {
     headers,
-    method: options.method || 'GET',
-    mode: options.mode || 'cors',
-    cache: options.cache || 'no-cache',
-    credentials: options.credentials || 'include'
+    method: options.method || "GET",
+    mode: options.mode || "cors",
+    cache: options.cache || "no-cache",
+    credentials: options.credentials || "include"
   };
 
-  const apiBase = options.portalUrl || 'http://localhost/api/v1';
+  const apiBase = options.portalUrl || "http://localhost/api/v1";
 
   if (options.params) {
     const { query, body } = options.params;
-  
+
     if (query) {
       const queryParams = new URLSearchParams(query).toString();
       url += `?${queryParams}`;
     }
-  
+
     if (body) {
       opts.body = JSON.stringify(body);
     }
   }
 
-  return fetch([apiBase, url].join('/'), opts);
+  return fetch([apiBase, url].join("/"), opts);
 }
