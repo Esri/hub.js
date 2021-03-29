40e47c1aeb9b904c1ebbe847ad029c680d47d16b
Tom Wayson <tom@tomwayson.com>
2021-03-29 14:43:15 -0700

    refactor(sites): remove no-op fns for un/linkig sites & pages
    affects: @esri/hub-sites
    
    BREAKING CHANGE:
    removes linkPageToSiteItem, linkPageToSite, unlinkPageFromSiteItem, unlinkPageFromSite

--- packages/sites/test/link-page-to-site.test.ts
+++
@@ -1,8 +0,0 @@
-import { linkPageToSite } from "../src";
-import { IHubRequestOptions } from "@esri/hub-common";
-
-describe("linkPageToSite", () => {
-  it("throws deprecation error", async () => {
-    expect(() => linkPageToSite({}, {} as IHubRequestOptions)).toThrowError();
-  });
-});
