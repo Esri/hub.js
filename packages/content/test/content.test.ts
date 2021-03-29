4d4355453b06d3b8fd1afe700ee22ecaa835d9e2
Tom Wayson <tom@tomwayson.com>
2021-03-29 12:34:57 -0700

    refactor(content): remove comingSoon()
    affects: @esri/hub-content
    
    BREAKING CHANGE:
    removes comingSoon()

--- packages/content/test/content.test.ts
+++ packages/content/test/content.test.ts
@@ -1,11 +1,10 @@
 import * as fetchMock from "fetch-mock";
 import * as arcgisRestPortal from "@esri/arcgis-rest-portal";
 import { IHubRequestOptions, IModel, IHubContent } from "@esri/hub-common";
-import { comingSoon, getContent, _enrichDates } from "../src/index";
+import { getContent, _enrichDates } from "../src/index";
 import * as portalModule from "../src/portal";
 import * as hubModule from "../src/hub";
 import { mockUserSession } from "./test-helpers/fake-user-session";
-import { assert } from "console";
 
 describe("get content", () => {
   let requestOpts: IHubRequestOptions;
@@ -213,25 +212,6 @@ describe("get content", () => {
         done();
       });
     });
-  });
-});
-
-// TODO: remove this next major release
-describe("what's currently here, which aint much", () => {
-  afterEach(fetchMock.restore);
-  it("should make an item request w/o fetching data", done => {
-    fetchMock.once("*", { the: "goods" });
-    comingSoon()
-      .then(response => {
-        const [url, options] = fetchMock.calls()[0];
-        expect(url).toEqual("https://www.arcgis.com/sharing/rest/info");
-        expect(options.body).toContain("f=json");
-        expect(response.the).toEqual("goods");
-        done();
-      })
-      .catch(e => {
-        fail(e);
-      });
   });
 });
 
