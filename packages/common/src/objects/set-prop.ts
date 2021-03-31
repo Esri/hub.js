5faf79ebfd902f5caf341c6d189029e789bc8483
Andrew Tate <drewctate@gmail.com>
2021-03-31 16:55:32 -0500

    setProp uses deepSet under the hood

--- packages/common/src/objects/set-prop.ts
+++ packages/common/src/objects/set-prop.ts
@@ -1,3 +1,5 @@
+import { deepSet } from "./deep-set";
+
 /**
  * Sets a deep object property, constructing the property path as necessary
  *
@@ -5,18 +7,9 @@
  * @param val - the value we want to set it to
  * @param obj - the target object
  */
-export function setProp(path: string | string[], val: any, obj: any): boolean {
-  const props = Array.isArray(path) ? path : path.split(".");
-  if (props.length > 1) {
-    // does the object have this prop? if not, create it as an object
-    if (!obj.hasOwnProperty(props[0])) {
-      obj[props[0]] = {};
-    }
-    return setProp(props.slice(1), val, obj[props[0]]);
-    // recurse back into this, but with one less entry in the props
-  } else {
-    // this is the last one, the one we want to replace
-    obj[props[0]] = val;
-    return true;
+export function setProp(path: string | string[], val: any, obj: any) {
+  if (Array.isArray(path)) {
+    path = path.join(".");
   }
+  deepSet(obj, path, val);
 }
--- packages/common/test/objects/set-prop.test.ts
+++ packages/common/test/objects/set-prop.test.ts
@@ -1,19 +1,19 @@
 import { setProp } from "../../src";
 
 describe("setProp", () => {
-  it("works with a string path", async () => {
+  it("works with a string path", () => {
     const obj = {};
     setProp("foo.bar", true, obj);
     expect(obj).toEqual({ foo: { bar: true } });
   });
 
-  it("works with an array path", async () => {
+  it("works with an array path", () => {
     const obj = {};
     setProp(["foo", "bar"], true, obj);
     expect(obj).toEqual({ foo: { bar: true } });
   });
 
-  it("uses an existing path when available", async () => {
+  it("uses an existing path when available", () => {
     const obj = { foo: { bar: false } };
     setProp(["foo", "bar"], true, obj);
     expect(obj).toEqual({ foo: { bar: true } });
