aa4a03ae9bc55d5358378cb29008c6a87be584a5
Andrew Tate <drewctate@gmail.com>
2021-03-31 16:16:22 -0500

    fix(site drafts): applying schema migrations to site drafts
    affects: @esri/hub-sites
    
    ISSUES CLOSED: #498

--- packages/sites/src/drafts/fetch-draft.ts
+++ packages/sites/src/drafts/fetch-draft.ts
@@ -1,6 +1,21 @@
-import { IHubRequestOptions } from "@esri/hub-common";
+import { getProp, IDraft, IHubRequestOptions } from "@esri/hub-common";
 import { getItemResource } from "@esri/arcgis-rest-portal";
 import { _getMostRecentDraftName } from "./_get-most-recent-draft-name";
+import { upgradeDraftSchema } from "./upgrade-draft-schema";
+
+function isSiteDraft(draft: IDraft) {
+  // Maybe a better way to do this, but can't use item type
+  // because we have Web Mapping Application sites in the wild
+  // and typeKeywords don't exist on drafts.
+  //
+  // We could always request the site item and check it as part of
+  // this but I'd rather not since that's an extra XHR and this is a
+  // pretty robust schema check.
+  return (
+    getProp(draft, "data.values.capabilities") !== undefined &&
+    getProp(draft, "data.values.theme") !== undefined
+  );
+}
 
 /**
  * Fetches the draft for a site or page if exists.
@@ -11,8 +26,8 @@ export function fetchDraft(
   siteOrPageId: string,
   hubRequestOptions: IHubRequestOptions
 ) {
-  return _getMostRecentDraftName(siteOrPageId, hubRequestOptions).then(
-    draftResourceName => {
+  return _getMostRecentDraftName(siteOrPageId, hubRequestOptions)
+    .then(draftResourceName => {
       let ret = null;
       if (draftResourceName) {
         ret = getItemResource(siteOrPageId, {
@@ -23,6 +38,11 @@ export function fetchDraft(
         });
       }
       return ret;
-    }
-  );
+    })
+    .then((draft: IDraft) => {
+      if (draft && isSiteDraft(draft)) {
+        draft = upgradeDraftSchema(draft);
+      }
+      return draft;
+    });
 }
