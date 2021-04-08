56356ef9d7c2fa747b71dcab208b34171f8cde8f
Vivian Zhang <vzhang@esri.com>
2021-04-08 12:11:22 -0700

    Add search-team-members and test
    Signed-off-by: Vivian Zhang <vzhang@esri.com>

---
+++ packages/teams/src/utils/search-team-members.ts
@@ -0,0 +1,18 @@
+import {
+  searchGroupUsers,
+  ISearchGroupUsersResult
+} from "@esri/arcgis-rest-portal";
+import { IHubRequestOptions } from "@esri/hub-common";
+
+/**
+ * Get the users(actual USER OBJECTS) that are members of the Group that backs the Team
+ * @param id team id
+ * @param hubRequestOptions
+ * @returns
+ */
+export function searchTeamMembers(
+  id: string,
+  hubRequestOptions: IHubRequestOptions
+): Promise<ISearchGroupUsersResult> {
+  return searchGroupUsers(id, hubRequestOptions);
+}
---
+++ packages/teams/test/utils/search-team-members.test.ts
@@ -0,0 +1,36 @@
+import * as portalModule from "@esri/arcgis-rest-portal";
+import { searchTeamMembers } from "../../src/utils/search-team-members";
+import { IHubRequestOptions } from "@esri/hub-common";
+import { UserSession } from "@esri/arcgis-rest-auth";
+
+describe("searchTeamMembers", () => {
+  const authentication = new UserSession({
+    username: "portal-user",
+    portal: "http://portal.com/sharing/rest",
+    token: "123"
+  });
+  authentication.getToken = () =>
+    new Promise(resolve => {
+      resolve("123");
+    });
+  const ro = {
+    authentication
+  } as IHubRequestOptions;
+
+  it("should call with the right group id", async () => {
+    const groupId = "foobarbaz";
+    const searchGroupUsersSpy = spyOn(
+      portalModule,
+      "searchGroupUsers"
+    ).and.returnValue(Promise.resolve({}));
+
+    const res = await searchTeamMembers(groupId, ro);
+
+    expect(res).toBeTruthy();
+
+    expect(searchGroupUsersSpy.calls.argsFor(0)[0]).toEqual(
+      groupId,
+      "search group users spy called with correct group id"
+    );
+  });
+});
