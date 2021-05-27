ba87a6ec06ff1c29a24b333cf8ba692343a1b646
Chase Gruber <cgruber@esri.com>
2021-05-27 10:20:26 -0400

    update api base

--- packages/discussions/test/request.test.ts
+++ packages/discussions/test/request.test.ts
@@ -66,8 +66,7 @@ describe("authenticateRequest", () => {
 describe("apiRequest", () => {
   const response = { ok: true };
 
-  const hubApiUrl =
-    "https://ingress.eks.qa.hub.geocloud.com/api/discussions/v1";
+  const hubApiUrl = "https://hubqa.arcgis.com/api/discussions/v1";
   const url = "foo";
 
   let expectedOpts: RequestInit;
