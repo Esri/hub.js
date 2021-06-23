45c79e7c7264a47099587bedff11e808be59423f
Julianna Apicella <juliannaapicella@gmail.com>
2021-06-23 11:59:51 -0400

    chore(common): add isValidExtent functions
    affects: @esri/hub-common
    
    ISSUES CLOSED: #1263

--- packages/common/src/extent/index.ts
+++ packages/common/src/extent/index.ts
@@ -2,3 +2,35 @@ export * from "./get-geographic-org-extent";
 export * from "./extent-to-bbox";
 export * from "./get-org-extent-as-bbox";
 export * from "./create-extent";
+
+function isExtentCoordinateArray(extent: object) {
+  return (
+    Array.isArray(extent) &&
+    Array.isArray(extent[0]) &&
+    Array.isArray(extent[1])
+  );
+}
+
+function isExtentEnvelope(extent: any) {
+  return isExtentCoordinateArray(extent.coordinates);
+}
+
+function isExtentJSON(extent: any) {
+  return ["xmin", "ymin", "xmax", "ymax"].every(
+    key => typeof extent[key] === "number"
+  );
+}
+
+/**
+ * Check if the given extent is in a known format
+ * @param  {Object} extent extent in any format
+ * @return {Boolean}       indicator
+ */
+export function isValidExtent(extent: object) {
+  return (
+    !!extent &&
+    [isExtentEnvelope, isExtentCoordinateArray, isExtentJSON].some(test =>
+      test(extent)
+    )
+  );
+}
