0ab0adc014e582a96e7272e997cbd431b768d4ee
Andrew Tate <drewctate@gmail.com>
2021-07-22 08:32:17 -0500

    test(hub-sites): test the domain re-exports
    affects: @esri/hub-sites

--- packages/sites/src/domains/index.ts
+++ packages/sites/src/domains/index.ts
@@ -1,16 +1,30 @@
 export {
+  /* istanbul ignore next */
   addDomain,
+  /* istanbul ignore next */
   domainExistsPortal,
+  /* istanbul ignore next */
   domainExists,
+  /* istanbul ignore next */
   ensureUniqueDomainName,
+  /* istanbul ignore next */
   getDomainsForSite,
+  /* istanbul ignore next */
   getUniqueDomainNamePortal,
+  /* istanbul ignore next */
   getUniqueDomainName,
+  /* istanbul ignore next */
   isDomainForLegacySite,
+  /* istanbul ignore next */
   isDomainUsedElsewhere,
+  /* istanbul ignore next */
   isValidDomain,
+  /* istanbul ignore next */
   lookupDomain,
+  /* istanbul ignore next */
   removeDomain,
+  /* istanbul ignore next */
   updateDomain,
+  /* istanbul ignore next */
   IDomainEntry,
 } from "@esri/hub-common";
---
+++ packages/sites/test/domains/index.test.ts
@@ -0,0 +1,36 @@
+import {
+  addDomain,
+  domainExistsPortal,
+  domainExists,
+  ensureUniqueDomainName,
+  getDomainsForSite,
+  getUniqueDomainNamePortal,
+  getUniqueDomainName,
+  isDomainForLegacySite,
+  isDomainUsedElsewhere,
+  isValidDomain,
+  lookupDomain,
+  removeDomain,
+  updateDomain,
+  IDomainEntry,
+} from "../../src";
+
+describe("proxy domain exports", () => {
+  it("they are all defined", () => {
+    [
+      addDomain,
+      domainExistsPortal,
+      domainExists,
+      ensureUniqueDomainName,
+      getDomainsForSite,
+      getUniqueDomainNamePortal,
+      getUniqueDomainName,
+      isDomainForLegacySite,
+      isDomainUsedElsewhere,
+      isValidDomain,
+      lookupDomain,
+      removeDomain,
+      updateDomain,
+    ].forEach((fn) => expect(fn).toBeDefined());
+  });
+});
