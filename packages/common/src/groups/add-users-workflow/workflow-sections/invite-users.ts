74f7632ef20b333ce86317df546b3a87678d1179
Ben Stoltz <bstoltz@esri.com>
2021-08-16 09:42:32 -0400

    jsdoc comment updated && specify admin/member in typing

--- packages/common/src/groups/add-users-workflow/workflow-sections/invite-users.ts
+++ packages/common/src/groups/add-users-workflow/workflow-sections/invite-users.ts
@@ -14,6 +14,7 @@ import { IAuthenticationManager } from "@esri/arcgis-rest-request";
  * @param {object[]} users
  * @param {object} authentication
  * @param {number} expiration How long the invite will be active (in minutes)
+ * @param {string} role What role should they be added as. Defaults to group member
  *
  * @returns {object|null} Result of the transaction (null if no users are passed in)
  */
@@ -22,7 +23,7 @@ export function inviteUsers(
   users: IUser[],
   authentication: IAuthenticationManager,
   expiration = 20160, // default to 2 week expiration TODO: is this actually 2 weeks?
-  role: string = "group_member" // default to group member, but allow for team_admin as well
+  role: "group_member" | "group_admin" = "group_member" // default to group member, but allow for team_admin as well
 ): Promise<IInviteGroupUsersResult> {
   let response: Promise<IInviteGroupUsersResult> = Promise.resolve(null);
   if (users.length) {
