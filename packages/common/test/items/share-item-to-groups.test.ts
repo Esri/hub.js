8c7ce13990b79f8512a0dde12791c393e3d3fab4
Ben Stoltz <bstoltz@esri.com>
2022-01-05 15:08:14 -0500

    fix(hub-common): pass item owner name to shareItemToGroups and unshareItemFromGroups

--- packages/common/test/items/share-item-to-groups.test.ts
+++ packages/common/test/items/share-item-to-groups.test.ts
@@ -2,19 +2,24 @@ import { shareItemToGroups } from "../../src";
 import * as portal from "@esri/arcgis-rest-portal";
 import { mockUserSession } from "../test-helpers/fake-user-session";
 
-describe("shareItemToGroups", function() {
-  it("delegates to arcgis-rest-js", async function() {
+describe("shareItemToGroups", function () {
+  it("delegates to arcgis-rest-js", async function () {
     const shareItemSpy = spyOn(portal, "shareItemWithGroup").and.callFake(
       (itemId: string) =>
         Promise.resolve({
           notSharedWith: [],
-          itemId
+          itemId,
         })
     );
 
-    const responses = await shareItemToGroups("item-id", ["grp1", "grp2"], {
-      authentication: mockUserSession
-    });
+    const responses = await shareItemToGroups(
+      "item-id",
+      ["grp1", "grp2"],
+      {
+        authentication: mockUserSession,
+      },
+      "bob"
+    );
 
     expect(shareItemSpy.calls.count()).toEqual(2);
     expect(shareItemSpy.calls.argsFor(0)[0].groupId).toEqual("grp1");
--- packages/common/test/items/unshare-item-from-groups.test.ts
+++ packages/common/test/items/unshare-item-from-groups.test.ts
@@ -2,19 +2,24 @@ import { unshareItemFromGroups } from "../../src";
 import * as portal from "@esri/arcgis-rest-portal";
 import { mockUserSession } from "../test-helpers/fake-user-session";
 
-describe("unshareItemFromGroups", function() {
-  it("delegates to arcgis-rest-js", async function() {
+describe("unshareItemFromGroups", function () {
+  it("delegates to arcgis-rest-js", async function () {
     const unshareItemSpy = spyOn(portal, "unshareItemWithGroup").and.callFake(
       (itemId: string) =>
         Promise.resolve({
           notUnsharedFrom: [],
-          itemId
+          itemId,
         })
     );
 
-    const responses = await unshareItemFromGroups("item-id", ["grp1", "grp2"], {
-      authentication: mockUserSession
-    });
+    const responses = await unshareItemFromGroups(
+      "item-id",
+      ["grp1", "grp2"],
+      {
+        authentication: mockUserSession,
+      },
+      "bob"
+    );
 
     expect(unshareItemSpy.calls.count()).toEqual(2);
     expect(unshareItemSpy.calls.argsFor(0)[0].groupId).toEqual("grp1");
