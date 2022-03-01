0bdba19d3295a7cca3915c655f7825357acc5416
Tom Wayson <tom@tomwayson.com>
2022-03-01 12:32:55 -0800

    docs(hub-common): add docs for fetchContent()
    affects: @esri/hub-common

--- packages/common/src/content/fetch.ts
+++ packages/common/src/content/fetch.ts
@@ -119,6 +119,18 @@ const fetchContentBySlug = async (
   });
 };
 
+/**
+ * Fetch enriched content from the Portal and Hub APIs.
+ * @param identifier content slug or id
+ * @param options Request options with additional options to control how the content or enrichments are fetched
+ * @returns A content object composed of the backing item and enrichments
+ *
+ * ```js
+ * import { fetchContent } from '@esri/hub-common'
+ * // fetch content by slug
+ * const content = await fetchContent('my-org::item-name')
+ * ```
+ */
 export const fetchContent = (
   identifier: string,
   options?: IFetchContentOptions
