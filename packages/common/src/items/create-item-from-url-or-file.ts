1e95b6cbc72f6708f906308366ff37aa81116696
Ben Stoltz <bstoltz@esri.com>
2022-05-09 10:15:05 -0400

    feat(hub-common): createItemFromUrlOrFile can also share created item to n groups
    * feat(hub-common): createItemFromUrlOrFile can also share created item to n groups
    
    * feat(hub-common): createItemFromUrlOrFile refactor to use object function param w/ interface
    
    * docs(hub-common): update createItemFromUrlOrFile documentation

--- packages/common/src/items/create-item-from-url-or-file.ts
+++ packages/common/src/items/create-item-from-url-or-file.ts
@@ -1,9 +1,19 @@
 import { IUserRequestOptions } from "@esri/arcgis-rest-auth";
-import { ICreateItemResponse, setItemAccess } from "@esri/arcgis-rest-portal";
-import { IItemAdd } from "@esri/arcgis-rest-types";
+import {
+  ICreateItemResponse,
+  setItemAccess,
+  shareItemWithGroup,
+} from "@esri/arcgis-rest-portal";
+import { IGroup, IItemAdd } from "@esri/arcgis-rest-types";
+import { failSafe, isUpdateGroup } from "../utils";
 import { createItemFromFile } from "./create-item-from-file";
 import { createItemFromUrl } from "./create-item-from-url";
 import { _waitForItemReady } from "./_internal/_wait-for-item-ready";
+
+interface ICreateItemFromUrlOrFileOptions extends IUserRequestOptions {
+  item: IItemAdd;
+  groups?: IGroup[];
+}
 
 /**
  * Creates an item in online from either url or file.
@@ -11,14 +21,14 @@ import { _waitForItemReady } from "./_internal/_wait-for-item-ready";
  * If access is not private then we make a call to update that.
  *
  * @export
- * @param {IItemAdd} item Item to be uploaded into online
- * @param {IUserRequestOptions} requestOptions
+ * @param {ICreateItemFromUrlOrFileOptions} createItemFromUrlOrFileOptions Input params (item, groups?, requestoptions)
  * @return {*}  {Promise<string>} AGO item id
  */
 export async function createItemFromUrlOrFile(
-  item: IItemAdd,
-  requestOptions: IUserRequestOptions
+  createItemFromUrlOrFileOptions: ICreateItemFromUrlOrFileOptions
 ): Promise<ICreateItemResponse> {
+  const { item, groups, ...userRequestOptions } =
+    createItemFromUrlOrFileOptions;
   // Is there a file or data url?
   const shouldWaitForItemReady = item.dataUrl || item.file;
   let createdItem: ICreateItemResponse;
@@ -26,15 +36,15 @@ export async function createItemFromUrlOrFile(
   // If there is a file then we create the item and chunk the file
   // while multithread uploading it
   if (item.file) {
-    createdItem = await createItemFromFile(item, requestOptions);
+    createdItem = await createItemFromFile(item, userRequestOptions);
     // Otherwise it's being created from a url.
   } else {
-    createdItem = await createItemFromUrl(item, requestOptions);
+    createdItem = await createItemFromUrl(item, userRequestOptions);
   }
 
   // If there is a file or data url we want to check to see if / when the item is ready.
   if (shouldWaitForItemReady) {
-    await _waitForItemReady(createdItem.id, requestOptions);
+    await _waitForItemReady(createdItem.id, userRequestOptions);
   }
 
   // If the item access is NOT private (which is the sharing access level by default)
@@ -44,8 +54,24 @@ export async function createItemFromUrlOrFile(
       id: createdItem.id,
       owner: item.owner,
       access: item.access,
-      ...requestOptions,
+      ...userRequestOptions,
     });
+  }
+
+  // If group ids were passedd in then make share calls to each.
+  if (groups?.length) {
+    const failSafeShare = failSafe(shareItemWithGroup);
+    await Promise.all(
+      groups.map((group: IGroup) =>
+        failSafeShare({
+          id: createdItem.id,
+          owner: item.owner,
+          groupId: group.id,
+          confirmItemControl: isUpdateGroup(group),
+          ...userRequestOptions,
+        })
+      )
+    );
   }
 
   return createdItem;
--- packages/common/test/items/create-item-from-url-or-file.test.ts
+++ packages/common/test/items/create-item-from-url-or-file.test.ts
@@ -1,12 +1,12 @@
 import { IUserRequestOptions } from "@esri/arcgis-rest-auth";
-import { IItemAdd } from "@esri/arcgis-rest-types";
+import { IGroup, IItemAdd } from "@esri/arcgis-rest-types";
 import { createItemFromUrlOrFile } from "../../src/items/create-item-from-url-or-file";
 import * as createItemFromFileModule from "../../src/items/create-item-from-file";
 import * as createItemFromUrlModule from "../../src/items/create-item-from-url";
 import * as _waitForItemReadyModule from "../../src/items/_internal/_wait-for-item-ready";
 import * as portal from "@esri/arcgis-rest-portal";
 
-describe("createContent", () => {
+describe("createItemFromUrlOrFile", () => {
   it("creates an item from url", async () => {
     // request options
     const ro = {
@@ -44,14 +44,28 @@ describe("createContent", () => {
     const setItemAccessSpy = spyOn(portal, "setItemAccess").and.callFake(() =>
       Promise.resolve()
     );
+    const shareItemWithGroupSpy = spyOn(
+      portal,
+      "shareItemWithGroup"
+    ).and.callFake(() => Promise.resolve());
 
-    const result = await createItemFromUrlOrFile(item, ro);
+    const result = await createItemFromUrlOrFile({
+      item,
+      groups: [
+        { id: "123", capabilities: ["updateitemcontrol"] } as unknown as IGroup,
+        { id: "abc", capabilities: [] } as unknown as IGroup,
+        { id: "456", capabilities: [] } as unknown as IGroup,
+        { id: "def", capabilities: [] } as unknown as IGroup,
+      ],
+      ...ro,
+    });
 
     expect(result).toEqual({ id: "123abc", success: true, folder: "test" });
     expect(createItemFromFileSpy).not.toHaveBeenCalled();
     expect(createItemFromUrlSpy).toHaveBeenCalledTimes(1);
     expect(_waitForItemReadySpy).toHaveBeenCalledTimes(1);
     expect(setItemAccessSpy).toHaveBeenCalledTimes(1);
+    expect(shareItemWithGroupSpy).toHaveBeenCalledTimes(4);
   });
   it("creates an item from url without dataUrl", async () => {
     // request options
@@ -89,14 +103,19 @@ describe("createContent", () => {
     const setItemAccessSpy = spyOn(portal, "setItemAccess").and.callFake(() =>
       Promise.resolve()
     );
+    const shareItemWithGroupSpy = spyOn(
+      portal,
+      "shareItemWithGroup"
+    ).and.callFake(() => Promise.resolve());
 
-    const result = await createItemFromUrlOrFile(item, ro);
+    const result = await createItemFromUrlOrFile({ item, groups: [], ...ro });
 
     expect(result).toEqual({ id: "123abc", success: true, folder: "test" });
     expect(createItemFromFileSpy).not.toHaveBeenCalled();
     expect(createItemFromUrlSpy).toHaveBeenCalledTimes(1);
     expect(_waitForItemReadySpy).not.toHaveBeenCalled();
     expect(setItemAccessSpy).toHaveBeenCalledTimes(1);
+    expect(shareItemWithGroupSpy).not.toHaveBeenCalled();
   });
   it("creates an item from file", async () => {
     // request options
@@ -139,7 +158,7 @@ describe("createContent", () => {
       Promise.resolve()
     );
 
-    const result = await createItemFromUrlOrFile(item, ro);
+    const result = await createItemFromUrlOrFile({ item, ...ro });
 
     expect(result).toEqual({ id: "123abc", success: true, folder: "test" });
     expect(createItemFromFileSpy).toHaveBeenCalledTimes(1);
