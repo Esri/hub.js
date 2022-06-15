c19b627c0c66b85eb3912cffd941a6ee103caa25
Dave Bouwman <dave@davebouwman.com>
2022-06-15 11:53:11 -0600

    refactor: trivial change to kick release, after adding service account to esri org

--- packages/common/src/search/_searchUsers.ts
+++ packages/common/src/search/_searchUsers.ts
@@ -37,6 +37,8 @@ export interface IHubUser extends IUser {
  *
  * **Note** Authentication is required
  *
+ *
+ *
  * Note: in the future, there may be options to search via
  * a Hub specific API
  * @param filter
