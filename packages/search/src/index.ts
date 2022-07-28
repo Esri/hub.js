8fdfac1b631d7e4a1ae7d6d0ce5676f98ef8a23b
Tom Wayson <tom@tomwayson.com>
2022-07-28 11:27:33 -0700

    refactor(hub-search): remove content search classes and functions
    affects: @esri/hub-common, @esri/hub-search
    
    BREAKING CHANGE:
    remove ContentSearchService, searchContent(), searchDatasets()

--- packages/search/src/index.ts
+++ packages/search/src/index.ts
@@ -1,18 +1,17 @@
 /* Copyright (c) 2018 Environmental Systems Research Institute, Inc.
  * Apache-2.0 */
 /* istanbul ignore file */
-export {
-  ContentSearchService,
-  searchContent,
-  searchDatasets,
-} from "./content/index";
 
-export {
-  agoSearch,
-  agoFormatItemCollection,
-  serialize,
-  computeItemsFacets,
-} from "./ago";
+// DEPRECATED: this package will be deprecated
+// it has currently includes only the functions that are still in use
 
-export * from "./util";
+// these are the only 2 functions used in opendata-ui
+// and agoSearch is used in hub-indexer
+// once we no longer use them there
+export { agoSearch, serialize } from "./ago";
+
 export * from "./types";
+
+// utils are used by hub-indexer, but no where else
+// we should move them either there or to hub-common
+export * from "./util";
