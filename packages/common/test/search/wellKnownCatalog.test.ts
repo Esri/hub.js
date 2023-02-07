a84a7f24de4eec50428719b24c3415a2795ff364
Vivian <vzhang@esri.com>
2023-02-06 22:50:47 -0800

    fix(hub-common): more tests
    affects: @esri/hub-common

--- packages/common/test/search/wellKnownCatalog.test.ts
+++ packages/common/test/search/wellKnownCatalog.test.ts
@@ -8,7 +8,7 @@ import { mockUser } from "../test-helpers/fake-user";
 
 describe("getWellKnownCatalog", () => {
   it("returns the expected catalog", () => {
-    const chk = getWellKnownCatalog(
+    let chk = getWellKnownCatalog(
       "mockI18nScope",
       "myContent",
       "item",
@@ -17,6 +17,18 @@ describe("getWellKnownCatalog", () => {
     expect(chk.scopes).toBeDefined();
     expect(chk.scopes?.item?.filters).toEqual([
       { predicates: [{ owner: "vader" }] },
+    ]);
+    expect(chk.collections?.map((a) => a.key)).toEqual([
+      "appAndMap",
+      "dataset",
+      "document",
+      "feedback",
+      "site",
+    ]);
+    chk = getWellKnownCatalog("mockI18nScope", "favorites", "item", mockUser);
+    expect(chk.scopes).toBeDefined();
+    expect(chk.scopes?.item?.filters).toEqual([
+      { predicates: [{ group: "7654321" }] },
     ]);
     expect(chk.collections?.map((a) => a.key)).toEqual([
       "appAndMap",
@@ -35,10 +47,14 @@ describe("getWellKnownCatalog", () => {
     ).toThrowError('User needed to get "favorites" catalog');
   });
   it("does not throw if not passing a user for a catalog that does not require it", () => {
-    const chk = getWellKnownCatalog("mockI18nScope", "organization", "item");
+    let chk = getWellKnownCatalog("mockI18nScope", "organization", "item");
     expect(chk.scopes).toBeDefined();
     expect(chk.scopes?.item?.filters).toEqual([
       { predicates: [{ access: "org" }] },
+    ]);
+    chk = getWellKnownCatalog("mockI18nScope", "world", "item");
+    expect(chk.scopes?.item?.filters).toEqual([
+      { predicates: [{ access: "public" }] },
     ]);
   });
   it("throws if passed an invalid entity type", () => {
