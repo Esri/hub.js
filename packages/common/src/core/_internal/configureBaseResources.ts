5252dbdf8e43ab8a3906b8cc888619ea888efd2f
Ben Stoltz <bstoltz@esri.com>
2023-02-14 13:07:37 -0500

    feat: remove old comment

--- packages/common/src/core/_internal/configureBaseResources.ts
+++ packages/common/src/core/_internal/configureBaseResources.ts
@@ -9,8 +9,6 @@ export function configureBaseResources(
   resources: any,
   entityResourceMap: any
 ): Array<{ filename: string; resource: any }> {
-  // Set up obj tracking resource prop && filename (KEEP IN SYNC WITH getPropertyMap()
-  // for additional resources!)
   return Object.entries(resources).reduce((acc, resourceProp) => {
     const fileName = entityResourceMap[resourceProp[0]];
     if (fileName) {
