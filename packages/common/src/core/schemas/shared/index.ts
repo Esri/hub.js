3e5038e840731089561a89f13087eeb55ca730a8
juliannemarik <julianne.marik@gmail.com>
2023-03-15 16:18:20 -0500

    chore: add separate shared directory for shared rules and subschemas

---
+++ packages/common/src/core/schemas/shared/index.ts
@@ -0,0 +1,2 @@
+export * from "./subschemas";
+export * from "./uiSchemaRules";
---
+++ packages/common/src/core/schemas/shared/subschemas.ts
@@ -0,0 +1,23 @@
+export const ENTITY_NAME_SCHEMA = {
+  type: "string",
+  minLength: 1,
+  maxLength: 250,
+};
+
+export const ITEM_PICKER_SCHEMA = {
+  type: "array",
+  items: {
+    type: "string",
+  },
+};
+
+export const EXTENT_SCHEMA = {
+  type: "object",
+  properties: {
+    spacialReference: { type: "object" },
+    xmax: { type: ["number", "null"] },
+    xmin: { type: ["number", "null"] },
+    ymax: { type: ["number", "null"] },
+    ymin: { type: ["number", "null"] },
+  },
+};
