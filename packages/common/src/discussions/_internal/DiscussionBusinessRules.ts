1210acae83ed639bfdab44c922dfc908d910049c
Randy Weber <rweber@esri.com>
2023-03-20 14:58:41 -0400

    feat(): make discussions permission policies resemble initiatives and projects

--- packages/common/src/discussions/_internal/DiscussionBusinessRules.ts
+++ packages/common/src/discussions/_internal/DiscussionBusinessRules.ts
@@ -65,7 +65,7 @@ export const DiscussionPermissionPolicies: IPermissionPolicy[] = [
     permission: "hub:discussion:view",
     subsystems: ["discussions"],
     authenticated: false,
-    licenses: [],
+    licenses: ["hub-premium"],
   },
   {
     permission: "hub:discussion:edit",
