d220df37ef1b719d2a46eb6a9618ebfc886094d6
juliannemarik <julianne.marik@gmail.com>
2023-03-20 17:38:26 -0500

    feat: add basic schema and edit uiSchema for initiatives

---
+++ packages/common/src/core/schemas/initiatives/schema.ts
@@ -0,0 +1,19 @@
+import { IConfigurationSchema } from "../types";
+
+export const HubInitiativeSchema: IConfigurationSchema = {
+  required: ["name"],
+  type: "object",
+  properties: {
+    name: {
+      type: "string",
+      minLength: 1,
+      maxLength: 250,
+    },
+    summary: {
+      type: "string",
+    },
+    description: {
+      type: "string",
+    },
+  },
+} as unknown as IConfigurationSchema;
---
+++ packages/common/src/core/schemas/initiatives/uiSchemas/edit.ts
@@ -0,0 +1,45 @@
+import { IUiSchema } from "../../types";
+
+/**
+ * complete edit UI Schema for Hub Initiatives
+ */
+export const uiSchema: IUiSchema = {
+  type: "Layout",
+  elements: [
+    {
+      type: "Section",
+      labelKey: "{{i18nScope}}.sections.basicInfo.label",
+      elements: [
+        {
+          labelKey: "{{i18nScope}}.fields.name.label",
+          scope: "/properties/name",
+          type: "Control",
+        },
+        {
+          labelKey: "{{i18nScope}}.fields.summary.label",
+          scope: "/properties/summary",
+          type: "Control",
+          options: {
+            control: "hub-field-input-input",
+            type: "textarea",
+            helperText: {
+              labelKey: "{{i18nScope}}.fields.summary.helperText",
+            },
+          },
+        },
+        {
+          labelKey: "{{i18nScope}}.fields.description.label",
+          scope: "/properties/description",
+          type: "Control",
+          options: {
+            control: "hub-field-input-input",
+            type: "textarea",
+            helperText: {
+              labelKey: "{{i18nScope}}.fields.description.helperText",
+            },
+          },
+        },
+      ],
+    },
+  ],
+};
