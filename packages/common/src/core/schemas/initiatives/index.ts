7624c013ab45c1f75b861cc4ea3a6682dba0a1d5
juliannemarik <julianne.marik@gmail.com>
2023-03-21 08:15:04 -0500

    fix: export named uiSchemas - resolves project getEditorConfig test

--- packages/common/src/core/schemas/initiatives/index.ts
+++ packages/common/src/core/schemas/initiatives/index.ts
@@ -1,3 +1,4 @@
 export * from "./schema";
+export { uiSchema as HubInitiativeEditUiSchema } from "./uiSchemas/edit";
 
 export type HubInitiativeEditorConfigType = "edit";
--- packages/common/src/core/schemas/projects/index.ts
+++ packages/common/src/core/schemas/projects/index.ts
@@ -1,3 +1,5 @@
 export * from "./schema";
+export { uiSchema as HubProjectCreateUiSchema } from "./uiSchemas/create";
+export { uiSchema as HubProjectEditUiSchema } from "./uiSchemas/edit";
 
 export type HubProjectEditorConfigType = "create" | "edit";
