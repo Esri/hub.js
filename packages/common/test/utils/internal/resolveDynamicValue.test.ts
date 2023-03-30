8d9ec933011ff2350be71e0e3841d2546f253e1e
Dave Bouwman <dave@davebouwman.com>
2023-03-30 16:12:07 -0600

    test: resolveValues and resolveValue tests

---
+++ packages/common/test/utils/internal/resolveDynamicValue.test.ts
@@ -0,0 +1,135 @@
+import { IPortal, IUser } from "@esri/arcgis-rest-portal";
+import M from "minimatch";
+import {
+  ArcGISContextManager,
+  IArcGISContext,
+  DynamicValueDefinition,
+} from "../../../src";
+import { resolveDynamicValue } from "../../../src/utils/internal/resolveDynamicValue";
+import { MOCK_AUTH } from "../../mocks/mock-auth";
+
+fdescribe("resolveDynamicValue:", () => {
+  let context: IArcGISContext;
+  let itemQrySpy: jasmine.Spy;
+  let portalSpy: jasmine.Spy;
+  let serviceSpy: jasmine.Spy;
+  beforeEach(async () => {
+    // When we pass in all this information, the context
+    // manager will not try to fetch anything, so no need
+    // to mock those calls
+    const authdCtxMgr = await ArcGISContextManager.create({
+      authentication: MOCK_AUTH,
+      currentUser: {
+        username: "casey",
+        privileges: ["portal:user:createItem"],
+      } as unknown as IUser,
+      portal: {
+        name: "DC R&D Center",
+        id: "BRXFAKE",
+        urlKey: "fake-org",
+        properties: {
+          hub: {
+            enabled: true,
+          },
+        },
+      } as unknown as IPortal,
+      portalUrl: "https://org.maps.arcgis.com",
+    });
+    context = authdCtxMgr.context;
+
+    itemQrySpy = spyOn(
+      require("../../../src/utils/internal/resolveItemQueryValues"),
+      "resolveItemQueryValues"
+    ).and.callFake(() => Promise.resolve({ item: "spy" }));
+    portalSpy = spyOn(
+      require("../../../src/utils/internal/resolvePortalValues"),
+      "resolvePortalValues"
+    ).and.callFake(() => Promise.resolve({ portal: "spy" }));
+    serviceSpy = spyOn(
+      require("../../../src/utils/internal/resolveServiceQueryValues"),
+      "resolveServiceQueryValues"
+    ).and.callFake(() => Promise.resolve({ service: "spy" }));
+  });
+
+  it("handles static-value internally", async () => {
+    const def: DynamicValueDefinition = {
+      type: "static-value",
+      value: 12,
+      outPath: "cost",
+    };
+    const result = await resolveDynamicValue(def, context);
+    expect(result).toEqual({ cost: 12 });
+    expect(itemQrySpy).not.toHaveBeenCalled();
+    expect(portalSpy).not.toHaveBeenCalled();
+    expect(serviceSpy).not.toHaveBeenCalled();
+  });
+  it("delegates to item query resolver", async () => {
+    const def: DynamicValueDefinition = {
+      type: "item-query",
+      outPath: "cost",
+      query: {
+        targetEntity: "item",
+        filters: [
+          {
+            operation: "AND",
+            predicates: [{ id: "00c" }],
+          },
+        ],
+      },
+      sourcePath: "views",
+      aggregation: "count",
+    };
+    const result = await resolveDynamicValue(def, context);
+    expect(result).toEqual({ item: "spy" });
+    expect(itemQrySpy).toHaveBeenCalled();
+    expect(portalSpy).not.toHaveBeenCalled();
+    expect(serviceSpy).not.toHaveBeenCalled();
+  });
+  it("delegates to service resolver", async () => {
+    const def: DynamicValueDefinition = {
+      type: "service-query",
+      outPath: "cost",
+      options: {
+        url: "https://services.arcgis.com/abc/arcgis/rest/services/MyService/FeatureServer/0",
+        where: "1=1",
+        field: "budget",
+        statisticType: "sum",
+      },
+      aggregation: "count",
+    };
+    const result = await resolveDynamicValue(def, context);
+    expect(result).toEqual({ service: "spy" });
+    expect(itemQrySpy).not.toHaveBeenCalled();
+    expect(portalSpy).not.toHaveBeenCalled();
+    expect(serviceSpy).toHaveBeenCalled();
+  });
+  it("delegates to portal resolver", async () => {
+    const def: DynamicValueDefinition = {
+      type: "portal",
+      sourcePath: "orgKey",
+      outPath: "urlKey",
+    };
+    const result = await resolveDynamicValue(def, context);
+    expect(result).toEqual({ portal: "spy" });
+    expect(itemQrySpy).not.toHaveBeenCalled();
+    expect(portalSpy).toHaveBeenCalled();
+    expect(serviceSpy).not.toHaveBeenCalled();
+  });
+  it("throws for unknown type", async () => {
+    const def: DynamicValueDefinition = {
+      type: "other",
+      sourcePath: "orgKey",
+      outPath: "urlKey",
+    } as unknown as DynamicValueDefinition;
+    try {
+      await resolveDynamicValue(def, context);
+    } catch (err) {
+      expect(err).toEqual(
+        new Error("Cannot resolve value - unexpected source.")
+      );
+      expect(itemQrySpy).not.toHaveBeenCalled();
+      expect(portalSpy).not.toHaveBeenCalled();
+      expect(serviceSpy).not.toHaveBeenCalled();
+    }
+  });
+});
---
+++ packages/common/test/utils/internal/resolveDynamicValues.test.ts
@@ -0,0 +1,52 @@
+import { IPortal, IUser } from "@esri/arcgis-rest-portal";
+import {
+  ArcGISContextManager,
+  IArcGISContext,
+  DynamicValueDefinition,
+} from "../../../src";
+import { resolveDynamicValues } from "../../../src/utils/internal/resolveDynamicValues";
+import { MOCK_AUTH } from "../../mocks/mock-auth";
+
+fdescribe("resolveDynamicValues:", () => {
+  let context: IArcGISContext;
+  beforeEach(async () => {
+    // When we pass in all this information, the context
+    // manager will not try to fetch anything, so no need
+    // to mock those calls
+    const authdCtxMgr = await ArcGISContextManager.create({
+      authentication: MOCK_AUTH,
+      currentUser: {
+        username: "casey",
+        privileges: ["portal:user:createItem"],
+      } as unknown as IUser,
+      portal: {
+        name: "DC R&D Center",
+        id: "BRXFAKE",
+        urlKey: "fake-org",
+        properties: {
+          hub: {
+            enabled: true,
+          },
+        },
+      } as unknown as IPortal,
+      portalUrl: "https://org.maps.arcgis.com",
+    });
+    context = authdCtxMgr.context;
+  });
+
+  it("delegates to resolveDynamicValue", () => {
+    const fnSpy = spyOn(
+      require("../../../src/utils/internal/resolveDynamicValue"),
+      "resolveDynamicValue"
+    ).and.callFake(() => Promise.resolve({}));
+    const def: DynamicValueDefinition = {
+      type: "static-value",
+      value: 12,
+      outPath: "cost",
+    };
+    const result = resolveDynamicValues([def], context);
+    expect(fnSpy).toHaveBeenCalled();
+    const chkDef = fnSpy.calls.mostRecent().args[0];
+    expect(chkDef).toEqual(def);
+  });
+});
