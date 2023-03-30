3644d1c9ffc3a7e7c58e86ca6da58c4e33ab83d6
Dave Bouwman <dave@davebouwman.com>
2023-03-30 12:32:20 -0600

    feat: update getProp to support array indexing, find and $use (#1008)
    * feat: update getProp to support array indexing, find and $use
    
    * docs: add $use to doc string

--- packages/common/src/objects/get-prop.ts
+++ packages/common/src/objects/get-prop.ts
@@ -1,10 +1,84 @@
 /**
- * Get a property out of a deeply nested object
- * Does not handle anything but nested object graph
+ * Get a property out of a deeply nested object. Supports array indexing
+ * and property matching.
+ *
+ * Examples:
+ * ```js
+ * getProp({a: {b: {c: 1}}}, "a.b.c") => 1
+ * getProp({a: {b: {c: 1}}}, "a.b.d") => undefined
+ * getProp({a: {b: {c: 1}}}, "a.b") => {c: 1}
+ * getProp({a: {b: [1,2]}}, "a.b[0]") => 1
+ * getProp({a: {b: {c: 1}, b2: { $use: "a.b"}}, "a.b2") => {c: 1}
+ * getProp({a: [{key: "x", val:1}, {key: "y", val:3}]}, "a[1]") => {key: "y", val:3}
+ * getProp({a: [{key: "x", val:1}, {key: "y", val:3}]}, "a[1].val") => 3
+ * getProp({a: [{key: "x", val:1}, {key: "y", val:3}]}, "a[findBy(key,y)].val") => 3
+ * getProp({a: [{key: "x", val:1}, {key: "y", val:3}]}, "a[findBy(val,1)].key") => "x"
+ * ```
  */
-export function getProp(obj: { [index: string]: any }, path: string): any {
-  return path.split(".").reduce(function(prev, curr) {
-    /* istanbul ignore next no need to test undefined scenario */
-    return prev ? prev[curr] : undefined;
+export const getProp = (obj: { [index: string]: any }, path: string): any => {
+  return path.split(".").reduce(function (prev: any, curr: string) {
+    const match = curr.match(/\[(.*?)\]$/);
+    if (match) {
+      const indexOrFnName = match[1];
+      const propName = curr.replace(match[0], "");
+
+      if (parseInt(indexOrFnName, 10) > -1) {
+        const idx = parseInt(indexOrFnName, 10);
+        // check that the array exists and that the index is valid
+        if (Array.isArray(prev[propName]) && prev[propName][idx]) {
+          return prev[propName][idx];
+        } else {
+          return undefined;
+        }
+      } else {
+        // we have an operation that we want to apply to the array
+        return applyOperation(prev[propName], indexOrFnName);
+      }
+    } else {
+      if (prev && prev[curr] !== undefined) {
+        if (prev[curr] !== null && prev[curr].$use) {
+          return getProp(obj, prev[curr].$use);
+        } else {
+          return prev[curr];
+        }
+      } else {
+        return undefined;
+      }
+    }
   }, obj);
+};
+
+function applyOperation(arr: any[], operation: string): any {
+  // split up the operation into the operationName and the arguments
+  // e.g. "findBy(key,'projects')" => ["findBy", "key, 'projects'"]
+  const parts = operation.split("(");
+  // lots of guards...
+  if (!parts || parts.length !== 2) return undefined;
+
+  let result: any;
+
+  const fnName = parts[0];
+  const args = parts[1]
+    .replace(")", "")
+    .split(",")
+    .map((p) => p.trim());
+  switch (fnName) {
+    case "findBy":
+      const prop = args[0];
+      let val: number | string;
+      if (isNumeric(args[1])) {
+        val = parseFloat(args[1]);
+      } else {
+        val = args[1].replace(/'/g, "");
+      }
+      result = arr.find((p: any) => p[prop] === val);
+      break;
+    default:
+      result = undefined;
+  }
+  return result;
+}
+
+function isNumeric(n: any): boolean {
+  return !isNaN(parseFloat(n));
 }
--- packages/common/test/objects/get-prop.test.ts
+++ packages/common/test/objects/get-prop.test.ts
@@ -1,17 +1,90 @@
 import { getProp } from "../../src/objects/get-prop";
 
-describe("getProp", () => {
-  it("can get a deep property", () => {
-    const obj = {
-      b: {
-        c: {
-          d: "peekaboo"
-        },
-        color: "orange"
+describe("getProp:", () => {
+  let testObj: any;
+  beforeEach(() => {
+    testObj = {
+      color: "red",
+      colors: ["red", "orange", "yellow", "blue", "green"],
+      colorVals: [
+        { color: "red", value: "#FF0000", cost: 100 },
+        { color: "orange", value: "#FFA500", cost: 102 },
+        { color: "yellow", value: "#FFFF00", cost: 40 },
+      ],
+      nest: {
+        someBoolProp: true,
+        someOtherBoolProp: false,
+        nullVal: null,
       },
-      size: "small"
-    } as any;
-    const c = getProp(obj, "b.c.d");
-    expect(c).toBe("peekaboo");
+      properties: {
+        metrics: {
+          funding: {
+            source: {
+              type: "static-value",
+              value: 1000,
+            },
+          },
+          countyFunding: {
+            $use: "properties.metrics.funding",
+          },
+        },
+      },
+    };
+  });
+  it("return top level property", () => {
+    const c = getProp(testObj, "color");
+    expect(c).toBe("red");
+  });
+
+  it("returns booleans as expected", () => {
+    expect(getProp(testObj, "nest.someBoolProp")).toBe(true);
+    expect(getProp(testObj, "nest.someOtherBoolProp")).toBe(false);
+  });
+  it("returns null values", () => {
+    expect(getProp(testObj, "nest.nullVal")).toBeNull();
+  });
+  it("return undefined for missing top level property", () => {
+    const c = getProp(testObj, "notdefined");
+    expect(c).not.toBeDefined();
+  });
+  it("return top level property as object", () => {
+    const c = getProp(testObj, "properties");
+    expect(c).toEqual(testObj.properties);
+  });
+  it("returns deep nested object", () => {
+    const c = getProp(testObj, "properties.metrics.funding");
+    expect(c).toEqual(testObj.properties.metrics.funding);
+  });
+  it("redirects via $use", () => {
+    const c = getProp(testObj, "properties.metrics.countyFunding");
+    expect(c).toEqual(testObj.properties.metrics.funding);
+  });
+  it("returns entry in array by index", () => {
+    const c = getProp(testObj, "colors[2]");
+    expect(c).toEqual("yellow");
+  });
+  it("returns undefined for entry in array by invalid index", () => {
+    const c = getProp(testObj, "colors[20]");
+    expect(c).not.toBeDefined();
+  });
+  it("returns undefined for entry in array by invalid index", () => {
+    const c = getProp(testObj, "colors[blah]");
+    expect(c).not.toBeDefined();
+  });
+  it("returns entry in array by property string value", () => {
+    const c = getProp(testObj, "colorVals[findBy(color,'orange')].cost");
+    expect(c).toEqual(102);
+  });
+  it("returns entry in array by property number value", () => {
+    const c = getProp(testObj, "colorVals[findBy(cost,102)].color");
+    expect(c).toEqual("orange");
+  });
+  it("returns undefined for entry array by property invalid value", () => {
+    const c = getProp(testObj, "colorVals[findBy(cost,199)].color");
+    expect(c).not.toBeDefined();
+  });
+  it("returns undefined if invalid function", () => {
+    const c = getProp(testObj, "colorVals[filterBy(cost,199)].color");
+    expect(c).not.toBeDefined();
   });
 });
