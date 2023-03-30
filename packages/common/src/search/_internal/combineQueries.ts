1c3619ac02800c2ef84886288b0a3c0684759ced
Dave Bouwman <dave@davebouwman.com>
2023-03-30 15:23:23 -0600

    feat: combineQueries internal fn

---
+++ packages/common/src/search/_internal/combineQueries.ts
@@ -0,0 +1,29 @@
+import { IQuery } from "../types/IHubCatalog";
+
+/**
+ * @internal
+ * Naieve implementation to merge two queries into one. This
+ * simply combines the filters of the two queries. It does not attempt
+ * to disabiguate the filters or predicates. It is up to the caller
+ * to ensure that the queries are compatible and don't result in a null set
+ * of results.
+ * @param queries
+ * @returns
+ */
+
+export const combineQueries = (queries: IQuery[]): IQuery => {
+  // check tht all queries are for the same entity type
+  const targetEntity = queries[0].targetEntity;
+  if (queries.some((q) => q.targetEntity !== targetEntity)) {
+    throw new Error("Cannot combine queries for different entity types");
+  }
+
+  // combine the filters from all the queries
+  const filters = queries.reduce((acc, q) => [...acc, ...q.filters], []);
+
+  const result: IQuery = {
+    targetEntity,
+    filters,
+  };
+  return result;
+};
---
+++ packages/common/test/search/_internal/combineQueries.test.ts
@@ -0,0 +1,68 @@
+import { combineQueries } from "../../../src/search/_internal/combineQueries";
+import { cloneObject, IQuery } from "../../../src";
+const harnessQueries: IQuery[] = [
+  {
+    targetEntity: "item",
+    filters: [
+      {
+        operation: "AND",
+        predicates: [
+          {
+            type: "Hub Site Application",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    targetEntity: "item",
+    filters: [
+      {
+        operation: "OR",
+        predicates: [
+          {
+            type: "Hub Page",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    targetEntity: "user",
+    filters: [
+      {
+        operation: "OR",
+        predicates: [
+          {
+            name: "Vader",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+fdescribe("combineQueries:", () => {
+  it("works with one query", () => {
+    const qry = cloneObject(harnessQueries[0]);
+    const result = combineQueries([qry]);
+    expect(result).toEqual(qry);
+  });
+  it("works with multiple queries", () => {
+    const qrys = [harnessQueries[0], harnessQueries[1]];
+    const result = combineQueries(qrys);
+    expect(result.targetEntity).toEqual("item");
+    expect(result.filters.length).toBe(2);
+  });
+
+  it("throws if all targetEntities are not the same", () => {
+    const qrys = [harnessQueries[0], harnessQueries[2]];
+    try {
+      combineQueries(qrys);
+    } catch (err) {
+      expect(err.message).toEqual(
+        "Cannot combine queries for different entity types"
+      );
+    }
+  });
+});
