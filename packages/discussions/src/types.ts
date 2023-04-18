426c3053fc20573b2df9bc37305b0170fded27e9
Randy Weber <52967037+rweber-esri@users.noreply.github.com>
2023-04-18 09:23:16 -0400

    feat(): add additional reactions to PostReaction enum (#1030)
    * feat(): add additional reactions to PostReaction enum
    
    * feat(): tweak reactions

--- packages/discussions/src/types.ts
+++ packages/discussions/src/types.ts
@@ -24,14 +24,31 @@ export enum SortOrder {
  * @enum {string}
  */
 export enum PostReaction {
+  CLAPPING_HANDS = "clapping_hands",
+  CONFUSED = "confused",
+  DOWN_ARROW = "down_arrow",
+  EYES = "eyes",
+  FACE_WITH_TEARS_OF_JOY = "face_with_tears_of_joy",
+  FIRE = "fire",
+  GRINNING = "grinning",
+  HEART = "heart",
+  LAUGH = "laugh",
+  ONE_HUNDRED = "one_hundred",
+  PARTYING = "partying",
+  PARTY_POPPER = "party_popper",
+  RAISING_HANDS = "raising_hands",
+  ROCKET = "rocket",
+  SAD = "sad",
+  SLIGHTLY_SMILING = "slightly_smiling",
+  SURPRISED = "surprised",
+  THINKING = "thinking",
   THUMBS_UP = "thumbs_up",
   THUMBS_DOWN = "thumbs_down",
-  THINKING = "thinking",
-  HEART = "heart",
-  ONE_HUNDRED = "one_hundred",
-  SAD = "sad",
-  LAUGH = "laugh",
-  SURPRISED = "surprised",
+  TROPHY = "trophy",
+  UP_ARROW = "up_arrow",
+  WAVING_HAND = "waving_hand",
+  WINKING = "winking",
+  WORLD_MAP = "world_map",
 }
 
 /**
