adfd01813e3f34a6cdb865f028880ee617449736
Ben Stoltz <bstoltz@esri.com>
2023-04-18 11:53:53 -0400

    feat: update tests

--- packages/common/test/utils/deep-set.test.ts
+++ packages/common/test/utils/deep-set.test.ts
@@ -1,24 +1,30 @@
 import { deepSet } from "../../src";
 
-describe("deepSet", function() {
-  it("sets deep properties", function() {
+describe("deepSet", function () {
+  it("sets deep properties", function () {
     const foo: Record<string, any> = {
       horses: {
         types: {
-          spalding: true
-        }
+          spalding: true,
+        },
       },
       apple: null,
       whale: "dolphin",
       cars: {
-        toyota: "camry"
-      }
+        toyota: "camry",
+      },
+      location: {
+        type: "org",
+        extent: [[], []],
+        geometry: {},
+      },
     };
     deepSet(foo, "horses.types.spalding", false);
     deepSet(foo, "bar.baz", "beep");
     deepSet(foo, "apple.orange", "banana");
     deepSet(foo, "whale", "shark");
     deepSet(foo, "cars", { honda: "accord" });
+    deepSet(foo, "location", { type: "none" }, true);
 
     expect(foo.horses.types.spalding).toBeFalsy("sets deep path when exists");
     expect(foo.bar.baz).toBe("beep", "constructs path if doesnt exist");
@@ -27,9 +33,10 @@ describe("deepSet", function() {
     expect(foo.cars).toEqual(
       {
         toyota: "camry",
-        honda: "accord"
+        honda: "accord",
       },
       "merges objects"
     );
+    expect(foo.location).toEqual({ type: "none" }, "replaces objects");
   });
 });
