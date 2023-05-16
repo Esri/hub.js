b003bb4e8f8d6e3c9d81f9536d2dff8ea438c3ae
Jeremy Sabat <jeremysabat@gmail.com>
2023-05-16 10:15:30 -0700

    feat(hub-discussions): update canModifyPostStatus to use channelAcl i… (#1060)
    * feat(hub-discussions): update canModifyPostStatus to use channelAcl if available
    
    affects: @esri/hub-discussions
    
    * test(hub-discussions): add test for channelPermission.canModifyPostStatus
    
    affects: @esri/hub-discussions
    
    * test(hub-discussions): fix tests for coverage
    
    affects: @esri/hub-discussions
    
    * fix(hub-discussions): make isAuthorizedToModerate parameter required
    
    affects: @esri/hub-discussions

---
+++ packages/discussions/src/utils/posts/can-modify-post-status.ts
@@ -0,0 +1,70 @@
+import { IGroup } from "@esri/arcgis-rest-types";
+import { IChannel, IDiscussionsUser, SharingAccess } from "../../types";
+import { isOrgAdmin } from "../platform";
+import { ChannelPermission } from "../channel-permission";
+
+const ADMIN_GROUP_ROLES = Object.freeze(["owner", "admin"]);
+
+export function canModifyPostStatus(
+  channel: IChannel,
+  user: IDiscussionsUser
+): boolean {
+  const { channelAcl } = channel;
+
+  if (channelAcl) {
+    const channelPermission = new ChannelPermission(channelAcl);
+    return channelPermission.canModifyPostStatus(user, channel.creator);
+  }
+
+  return isAuthorizedToModifyStatusByLegacyPermissions(user, channel);
+}
+
+function isAuthorizedToModifyStatusByLegacyPermissions(
+  user: IDiscussionsUser,
+  channel: IChannel
+): boolean {
+  const { username, groups: userGroups, orgId: userOrgId } = user;
+  const {
+    access,
+    groups: channelGroups,
+    orgs: channelOrgs,
+    creator: channelCreator,
+  } = channel;
+
+  if (!username) {
+    return false;
+  }
+
+  if (channelCreator === username) {
+    return true;
+  }
+
+  if (access === SharingAccess.PRIVATE) {
+    return isAuthorizedToModifyStatusByLegacyGroup(channelGroups, userGroups);
+  }
+
+  // public or org access
+  return channelOrgs.includes(userOrgId) && isOrgAdmin(user);
+}
+
+/**
+ * Ensure the user is an owner/admin of one of the channel groups
+ */
+function isAuthorizedToModifyStatusByLegacyGroup(
+  channelGroups: string[],
+  userGroups: IGroup[] = []
+) {
+  return channelGroups.some((channelGroupId: string) => {
+    return userGroups.some((group: IGroup) => {
+      const {
+        id: userGroupId,
+        userMembership: { memberType: userMemberType },
+      } = group;
+
+      return (
+        channelGroupId === userGroupId &&
+        ADMIN_GROUP_ROLES.includes(userMemberType)
+      );
+    });
+  });
+}
