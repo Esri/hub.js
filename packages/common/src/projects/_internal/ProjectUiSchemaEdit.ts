7405550b77c8d3f855daba35d31e077a51678db0
Ben Stoltz <bstoltz@esri.com>
2023-05-23 17:14:48 -0400

    feat: project featured image alt text (#1067)
    * feat: project featured image alt text
    
    * feat: tweak to match translation path

--- packages/common/src/projects/_internal/ProjectUiSchemaEdit.ts
+++ packages/common/src/projects/_internal/ProjectUiSchemaEdit.ts
@@ -69,6 +69,16 @@ export const uiSchema: IUiSchema = {
           },
         },
         {
+          labelKey: "{{i18nScope}}.fields.featuredImage.altText.label",
+          scope: "/properties/view/properties/featuredImageAltText",
+          type: "Control",
+          options: {
+            helperText: {
+              labelKey: "{{i18nScope}}.fields.featuredImage.altText.helperText",
+            },
+          },
+        },
+        {
           labelKey: "{{i18nScope}}.fields.tags.label",
           scope: "/properties/tags",
           type: "Control",
