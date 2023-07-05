c6d204f38f51490282507bb4242010b82e0e5c4a
juliannemarik <julianne.marik@gmail.com>
2023-07-05 12:59:14 -0500

    feat: add workspaceRelative url to enriched project search result

--- packages/common/src/projects/fetch.ts
+++ packages/common/src/projects/fetch.ts
@@ -20,6 +20,7 @@ import { getProp } from "../objects/get-prop";
 import { getItemThumbnailUrl } from "../resources/get-item-thumbnail-url";
 import { getItemHomeUrl } from "../urls/get-item-home-url";
 import { getItemIdentifier } from "../items";
+import { getRelativeWorkspaceUrl } from "../core/_internal/getRelativeWorkspaceUrl";
 
 /**
  * @private
@@ -96,6 +97,7 @@ export async function enrichProjectSearchResult(
       self: "not-implemented",
       siteRelative: "not-implemented",
       thumbnail: "not-implemented",
+      workspaceRelative: "not-implemented",
     },
   };
 
@@ -127,6 +129,10 @@ export async function enrichProjectSearchResult(
     result.type,
     identifier,
     item.typeKeywords
+  );
+  result.links.workspaceRelative = getRelativeWorkspaceUrl(
+    result.type,
+    identifier
   );
 
   return result;
