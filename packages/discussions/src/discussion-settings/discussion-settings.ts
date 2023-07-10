b9ccb6f398cde1e0356a8a7160e0a53a5ab10a5c
velveetachef <jeremysabat@gmail.com>
2023-07-10 16:06:08 -0700

    feat(hub-discussions): add function removeDiscussionSetting with associated interfaces
    affects: @esri/hub-discussions

--- packages/discussions/src/discussion-settings/discussion-settings.ts
+++ packages/discussions/src/discussion-settings/discussion-settings.ts
@@ -1,9 +1,34 @@
 import { request } from "../request";
-import { ICreateDiscussionSettingParams, IDiscussionSetting } from "../types";
+import {
+  ICreateDiscussionSettingParams,
+  IDiscussionSetting,
+  IRemoveDiscussionSettingParams,
+} from "../types";
 
+/**
+ * create discussion settings
+ *
+ * @export
+ * @param {ICreateDiscussionSettingParams} options
+ * @return {*} {Promise<IDiscussionSetting>}
+ */
 export function createDiscussionSetting(
   options: ICreateDiscussionSettingParams
 ): Promise<IDiscussionSetting> {
   options.httpMethod = "POST";
   return request(`/discussion_settings`, options);
+}
+
+/**
+ * remove discussion settings
+ *
+ * @export
+ * @param {IRemoveDiscussionSettingParams} options
+ * @return {*} {Promise<IDiscussionSetting>}
+ */
+export function removeDiscussionSetting(
+  options: IRemoveDiscussionSettingParams
+): Promise<IDiscussionSetting> {
+  options.httpMethod = "DELETE";
+  return request(`/discussion_settings/${options.id}`, options);
 }
