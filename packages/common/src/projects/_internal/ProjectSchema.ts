ade244538d003d3da1d9f0ce8e069ec57f154940
juliannemarik <julianne.marik@gmail.com>
2023-07-10 15:42:48 -0500

    chore: update existing entity schemas to use shared item entity subschema

--- packages/common/src/projects/_internal/ProjectSchema.ts
+++ packages/common/src/projects/_internal/ProjectSchema.ts
@@ -1,12 +1,5 @@
 import { PROJECT_STATUSES, IConfigurationSchema } from "../../core";
-import {
-  ENTITY_ACCESS_SCHEMA,
-  ENTITY_CATEGORIES_SCHEMA,
-  ENTITY_NAME_SCHEMA,
-  ENTITY_SUMMARY_SCHEMA,
-  ENTITY_TAGS_SCHEMA,
-  ENTITY_TIMELINE_SCHEMA,
-} from "../../core/schemas/shared";
+import { HubItemEntitySubschema } from "../../core/schemas/shared/hubItemEntitySubschema";
 
 export type ProjectEditorType = (typeof ProjectEditorTypes)[number];
 export const ProjectEditorTypes = [
@@ -18,50 +11,17 @@ export const ProjectEditorTypes = [
  * defines the JSON schema for a Hub Project's editable fields
  */
 export const ProjectSchema: IConfigurationSchema = {
-  required: ["name"],
-  type: "object",
+  ...HubItemEntitySubschema,
   properties: {
-    name: ENTITY_NAME_SCHEMA,
-    summary: ENTITY_SUMMARY_SCHEMA,
-    description: {
-      type: "string",
-    },
-    access: ENTITY_ACCESS_SCHEMA,
+    ...HubItemEntitySubschema.properties,
     groups: {
       type: "array",
-      items: {
-        type: "string",
-      },
+      items: { type: "string" },
     },
     status: {
       type: "string",
       default: PROJECT_STATUSES.notStarted,
       enum: Object.keys(PROJECT_STATUSES),
     },
-    location: {
-      type: "object",
-      default: { type: "none" },
-    },
-    tags: ENTITY_TAGS_SCHEMA,
-    categories: ENTITY_CATEGORIES_SCHEMA,
-    view: {
-      type: "object",
-      properties: {
-        featuredContentIds: {
-          type: "array",
-          maxItems: 4,
-          items: {
-            type: "string",
-          },
-        },
-        featuredImage: {
-          type: "object",
-        },
-        featuredImageAltText: {
-          type: "string",
-        },
-        timeline: ENTITY_TIMELINE_SCHEMA,
-      },
-    },
   },
-} as unknown as IConfigurationSchema;
+} as IConfigurationSchema;
--- packages/common/src/sites/_internal/SiteSchema.ts
+++ packages/common/src/sites/_internal/SiteSchema.ts
@@ -1,10 +1,5 @@
 import { IConfigurationSchema } from "../../core";
-import {
-  ENTITY_ACCESS_SCHEMA,
-  ENTITY_CATEGORIES_SCHEMA,
-  ENTITY_NAME_SCHEMA,
-  ENTITY_TAGS_SCHEMA,
-} from "../../core/schemas/shared";
+import { HubItemEntitySubschema } from "../../core/schemas/shared/hubItemEntitySubschema";
 
 export type SiteEditorType = (typeof SiteEditorTypes)[number];
 export const SiteEditorTypes = ["hub:site:edit"] as const;
@@ -13,27 +8,5 @@ export const SiteEditorTypes = ["hub:site:edit"] as const;
  * defines the JSON schema for a Hub Site's editable fields
  */
 export const SiteSchema: IConfigurationSchema = {
-  required: ["name"],
-  type: "object",
-  properties: {
-    name: ENTITY_NAME_SCHEMA,
-    summary: {
-      type: "string",
-    },
-    description: {
-      type: "string",
-    },
-    access: ENTITY_ACCESS_SCHEMA,
-    groups: {
-      type: "array",
-      items: {
-        type: "string",
-      },
-    },
-    location: {
-      type: "object",
-    },
-    tags: ENTITY_TAGS_SCHEMA,
-    categories: ENTITY_CATEGORIES_SCHEMA,
-  },
-} as unknown as IConfigurationSchema;
+  ...HubItemEntitySubschema,
+} as IConfigurationSchema;
