6d5c55961773e9fe1a059255abdc86cfbdcc5137
Mike Juniper <mjuniper@esri.com>
2023-07-19 10:51:25 -0600

    feat: page permission policy is included (#1125)

--- packages/common/src/permissions/HubPermissionPolicies.ts
+++ packages/common/src/permissions/HubPermissionPolicies.ts
@@ -5,6 +5,7 @@ import { DiscussionPermissionPolicies } from "../discussions/_internal/Discussio
 import { ContentPermissionPolicies } from "../content/_internal/ContentBusinessRules";
 
 import { IPermissionPolicy, Permission } from "./types";
+import { PagePermissionPolicies } from "../pages/_internal/PageBusinessRules";
 
 // Examples of possible Permission Policies
 // const DiscussionPermissionPolicies: IPermissionPolicy[] = [
@@ -69,6 +70,7 @@ export const HubPermissionsPolicies: IPermissionPolicy[] = [
   ...InitiativePermissionPolicies,
   ...DiscussionPermissionPolicies,
   ...ContentPermissionPolicies,
+  ...PagePermissionPolicies,
 ];
 
 /**
