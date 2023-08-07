4349f13ab956d7307f57139697353c98815a9ab7
Michael Juniper <mjuniper@esri.com>
2023-08-07 15:28:43 -0600

    feat: workspace gating

--- packages/common/src/sites/_internal/SiteBusinessRules.ts
+++ packages/common/src/sites/_internal/SiteBusinessRules.ts
@@ -34,12 +34,12 @@ export const SiteCapabilityPermissions: ICapabilityPermission[] = [
   {
     entity: "site",
     capability: "settings",
-    permissions: ["hub:site:edit", "hub:site:view", "hub:workspace:released"],
+    permissions: ["hub:site:edit", "hub:site:view"],
   },
   {
     entity: "site",
     capability: "content",
-    permissions: ["hub:site:edit", "hub:workspace:released"],
+    permissions: ["hub:site:edit"],
   },
   {
     entity: "site",
