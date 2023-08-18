dba47c3120780e451a5f9dc6d4a115b398610c0a
Jordan Sanz <jsanz@esri.com>
2023-08-18 12:37:44 -0400

    feat(): add where clause breakdown into migration

--- packages/common/src/sites/_internal/_migrate-summary-stat-card-configs.ts
+++ packages/common/src/sites/_internal/_migrate-summary-stat-card-configs.ts
@@ -1,6 +1,13 @@
 import { setProp } from "../../objects";
 import { IModel, IDraft } from "../../types";
 import { cloneObject } from "../../util";
+import { IField } from "@esri/arcgis-rest-feature-layer";
+
+interface IExpression {
+  field?: IField;
+  values?: Array<string | number | Date>;
+  key?: string;
+}
 
 /**
  * Reconfigure summary stat card properties to align with the
@@ -48,9 +55,9 @@ function _migrateSummaryStatSchemaVersion1_7<T extends IModel | IDraft>(
                     0,
                     values.url?.lastIndexOf("FeatureServer") + 13
                   ),
+              expressionSet: whereToExpressionSet(values.where),
             },
-            serverTimeout: values.serverTimeout,
-            where: values.where,
+            serverTimeout: values.timeout,
             textAlign: migrateTextAlign(values.statValueAlign),
             valueColor:
               values.statValueColor === null
@@ -63,29 +70,13 @@ function _migrateSummaryStatSchemaVersion1_7<T extends IModel | IDraft>(
             allowUnitFormatting: false,
             allowLink: false,
           };
-
-          delete card.component.settings.title;
-          delete card.component.settings.statFieldName;
-          delete card.component.settings.statFieldType;
-          delete card.component.settings.statType;
-          delete card.component.settings.statValueAlign;
-          delete card.component.settings.statValueColor;
-          delete card.component.settings.itemTitle;
-          delete card.component.settings.layerName;
-          delete card.component.settings.currencyCode;
-          delete card.component.settings.decimalPlaces;
-          delete card.component.settings.formatNumberGroupings;
-          delete card.component.settings.leadingText;
-          delete card.component.settings.showAsCurrency;
-          delete card.component.settings.titleAlign;
-          delete card.component.settings.trailingTextAlign;
         }
         return card;
       }),
     })),
   }));
 
-  setProp("item.properties.schemaVersion", 1.6, clone);
+  setProp("item.properties.schemaVersion", 1.7, clone);
   return clone;
 }
 
@@ -98,4 +89,128 @@ function migrateTextAlign(align: string): string {
     case "right":
       return "end";
   }
+}
+
+/**
+ * Takes a where clause from a summary statistic card and transforms it into
+ * the new expression set array for usage in the new summary stat card.
+ *
+ * This is a brittle function to be used just for the migration of the <=1.6 of the summary stat card into
+ * the 1.7 version of the summary stat card.
+ * @param where A where clause as a string. Ex: "Category = 'abc' AND date <= TIMESTAMP '2023-01-01 00:00:00"
+ * @returns IExpression[] expression set of the where clause, broken down into individual expressions
+ */
+export function whereToExpressionSet(where: string): IExpression[] {
+  let expressionSet: IExpression[] = [];
+  if (where) {
+    const whereClauses = where.split("AND");
+    expressionSet = whereClauses.map((clause) => {
+      let expression: IExpression;
+
+      // could be number or date
+      if (clause.includes(">=")) {
+        const { fieldName, value } = handleClauseSplit(clause, ">=");
+        const finalValue = value.includes("TIMESTAMP")
+          ? handleDateInClause(value)
+          : value;
+        const fieldType = value.includes("TIMESTAMP")
+          ? "esriFieldTypeDate"
+          : undefined;
+        expression = makeExpression(fieldName, fieldType, [finalValue]);
+      }
+
+      // could be number or date AND value is second in values array
+      else if (clause.includes("<=")) {
+        const { fieldName, value } = handleClauseSplit(clause, "<=");
+        const finalValue = value.includes("TIMESTAMP")
+          ? handleDateInClause(value)
+          : value;
+        const fieldType = value.includes("TIMESTAMP")
+          ? "esriFieldTypeDate"
+          : undefined;
+        expression = makeExpression(fieldName, fieldType, [
+          undefined,
+          finalValue,
+        ]);
+      }
+
+      // string match
+      else if (clause.includes("=")) {
+        const { fieldName, value } = handleClauseSplit(clause, "=");
+        const cleanedValue = removeQuotesAroundValue(value);
+        expression = makeExpression(fieldName, "esriFieldTypeString", [
+          cleanedValue,
+        ]);
+      }
+
+      return expression;
+    });
+  }
+  return expressionSet;
+}
+
+/**
+ * Splits a clause by a given parameter.
+ * @param clause String to split into pieces
+ * @param splitBy How to split the string
+ * @returns The field name and value from the clause, split
+ */
+function handleClauseSplit(
+  clause: string,
+  splitBy: string = ""
+): { fieldName: string; value: string } {
+  let result = { fieldName: "", value: "" };
+  const pieces = clause.split(splitBy);
+
+  if (pieces.length === 2) {
+    const fieldName = pieces[0].trim();
+    const value = pieces[1].trim();
+    result = { fieldName, value };
+  }
+
+  return result;
+}
+
+/**
+ * Takes a piece of a where clause with a date in it, and extracts just the YYYY-MM-DD.
+ * @param value - where clause piece with value
+ * @returns YYYY-MM-DD as string
+ */
+function handleDateInClause(value: string): string {
+  const dateAndTimePieces = value.split("'");
+  const dateAndTime = dateAndTimePieces[1].trim();
+  const date = dateAndTime.split(" ")[0].trim();
+  return date;
+}
+
+/**
+ * Makes an expression for the expression set.
+ * @param fieldName Name of IField
+ * @param fieldType Type of IField
+ * @param values Values in clause
+ * @returns IExpression to be used in an expression set.
+ */
+function makeExpression(
+  fieldName: string,
+  fieldType: string,
+  values: Array<string | number | Date>
+): IExpression {
+  const expression = {
+    field: {
+      name: fieldName,
+      type: fieldType,
+    } as IField,
+    values,
+  };
+  return expression;
+}
+
+/**
+ * Removes single quotes around a value. Used to remove the leading and trailing
+ * single quotes around a string value for a where clause.
+ * @param value string value with single quotes around it
+ * @returns string value without leading or trailing single quotes
+ */
+function removeQuotesAroundValue(value: string) {
+  return value.replace(/^\'+|\'+$/g, "");
 }
---
+++ packages/common/test/sites/_internal/_migrateSummaryStatCardConfig.test.ts
@@ -0,0 +1,320 @@
+import { _migrateSummaryStatCardConfigs } from "../../../src/sites/_internal/_migrate-summary-stat-card-configs";
+import { IModel, cloneObject, IDraft, setProp } from "../../../src";
+
+function getSiteModel(cardSettings) {
+  return {
+    item: {
+      id: "3ef",
+      title: "Some Site",
+      type: "Hub Site",
+      properties: {
+        schemaVersion: 1.6,
+      },
+    },
+    data: {
+      values: {
+        layout: {
+          sections: [
+            {
+              rows: [
+                {
+                  cards: [
+                    {
+                      component: {
+                        name: "summary-statistic-card",
+                        settings: cardSettings,
+                      },
+                      width: 12,
+                    },
+                  ],
+                },
+                {
+                  cards: [
+                    {
+                      component: {
+                        name: "markdown-card",
+                        settings: {
+                          schemaVersion: 2.1,
+                          markdown:
+                            '<h4 style="color: rgb(0, 0, 0);"><b>Dataset Type Items</b></h4>',
+                        },
+                      },
+                      width: 12,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      },
+    },
+  } as unknown as IModel;
+}
+
+function getCardSettings(
+  align: string,
+  where: string | undefined,
+  color: string | null
+) {
+  return {
+    cardId: "cmmqtolwk",
+    currencyCode: "USD",
+    decimalPlaces: 2,
+    formatNumberGroupings: true,
+    itemId: "60dfdb1f778c4480b8c4135b1e9a2ecb",
+    itemTitle: "Field Type Table",
+    layerId: 0,
+    layerName: "Table",
+    leadingText: "",
+    showAsCurrency: false,
+    statFieldName: "Float",
+    statFieldType: "esriFieldTypeDouble",
+    statType: "sum",
+    statValueAlign: align,
+    statValueColor: color,
+    timeout: 30,
+    title: "Statistic Title",
+    titleAlign: "left",
+    trailingText: "trailing text...",
+    trailingTextRight: "right",
+    url: "https://servicesqa.arcgis.com/Xj56SBi2udA78cC9/arcgis/rest/services/Field_Type_Table/FeatureServer/0",
+    where,
+  };
+}
+
+function getExpectedCardSettings(
+  textAlign: string,
+  expressionSet,
+  color: string | undefined
+) {
+  return {
+    cardId: "cmmqtolwk",
+    dynamicMetric: {
+      itemId: ["60dfdb1f778c4480b8c4135b1e9a2ecb"],
+      layerId: "0",
+      field: "Float",
+      fieldType: "esriFieldTypeDouble",
+      statistic: "sum",
+      serviceUrl:
+        "https://servicesqa.arcgis.com/Xj56SBi2udA78cC9/arcgis/rest/services/Field_Type_Table/FeatureServer",
+      expressionSet,
+    },
+    type: "dynamic",
+    cardTitle: "Statistic Title",
+    serverTimeout: 30,
+    textAlign,
+    valueColor: color,
+    trailingText: "trailing text...",
+    allowUnitFormatting: false,
+    allowLink: false,
+  };
+}
+
+const modelWithBadLayout = {
+  item: {
+    properties: {
+      schemaVersion: 1.5,
+    },
+  },
+  data: {
+    values: {
+      layout: {
+        sections: [
+          {
+            rows: [
+              {
+                cards: [],
+              },
+              {
+                otherProp:
+                  "This is an invalid row, lacking a .cards prop - it should be ignored",
+              },
+            ],
+          },
+          {
+            otherProp:
+              "this is an invalid section lacking a .rows prop - it should just be ignored",
+          },
+        ],
+      },
+    },
+  },
+} as unknown as IModel;
+
+describe("_ensure-summary-stat-card", () => {
+  describe("Site model", function () {
+    let model: IModel;
+
+    it("does not apply changes if schemaVersion is already 1.7", function () {
+      model = cloneObject(getSiteModel(getCardSettings("left", "", null)));
+      setProp("item.properties.schemaVersion", 1.7, model);
+      const expected: IModel = cloneObject(model);
+      const results = _migrateSummaryStatCardConfigs<IModel>(model);
+      expect(results).toEqual(expected);
+    });
+
+    it("does not apply changes if invalid model", function () {
+      model = cloneObject(getSiteModel(getCardSettings("left", "", null)));
+      const expected: IModel = cloneObject(modelWithBadLayout);
+      setProp("item.properties.schemaVersion", 1.7, expected);
+      const results =
+        _migrateSummaryStatCardConfigs<IModel>(modelWithBadLayout);
+      expect(results).toEqual(expected);
+    });
+
+    describe("text align & color variations", function () {
+      it('sets a site model accurately: align = left, where = "", color = null', () => {
+        model = cloneObject(getSiteModel(getCardSettings("left", "", null)));
+        const expected: IModel = cloneObject(
+          getSiteModel(getExpectedCardSettings("start", [], undefined))
+        );
+        setProp("item.properties.schemaVersion", 1.7, expected);
+        const results = _migrateSummaryStatCardConfigs<IModel>(model);
+        expect(results).toEqual(expected);
+      });
+
+      it('sets a site model accurately: align = center, where = "", color = null', () => {
+        model = cloneObject(getSiteModel(getCardSettings("center", "", null)));
+        const expected: IModel = cloneObject(
+          getSiteModel(getExpectedCardSettings("center", [], undefined))
+        );
+        setProp("item.properties.schemaVersion", 1.7, expected);
+        const results = _migrateSummaryStatCardConfigs<IModel>(model);
+        expect(results).toEqual(expected);
+      });
+
+      it('sets a site model accurately: align = right, where = "", color = null', () => {
+        model = cloneObject(getSiteModel(getCardSettings("right", "", null)));
+        const expected: IModel = cloneObject(
+          getSiteModel(getExpectedCardSettings("end", [], undefined))
+        );
+        setProp("item.properties.schemaVersion", 1.7, expected);
+        const results = _migrateSummaryStatCardConfigs<IModel>(model);
+        expect(results).toEqual(expected);
+      });
+
+      it('sets a site model accurately: align = left, where = "", color = "#a4a4a4"', () => {
+        model = cloneObject(
+          getSiteModel(getCardSettings("left", "", "#a4a4a4"))
+        );
+        const expected: IModel = cloneObject(
+          getSiteModel(getExpectedCardSettings("start", [], "#a4a4a4"))
+        );
+        setProp("item.properties.schemaVersion", 1.7, expected);
+        const results = _migrateSummaryStatCardConfigs<IModel>(model);
+        expect(results).toEqual(expected);
+      });
+    });
+  });
+
+  describe("Site draft", function () {
+    // TODO: once site draft is implemented (two PRs needed), write these
+  });
+
+  describe("WhereToExpressionSet", function () {
+    let model: IModel;
+
+    it("handles a case of a string, dates, and numbers", () => {
+      const where =
+        "String = 'a' AND Date >= TIMESTAMP '2023-01-01 00:00:00' AND Date <= TIMESTAMP '2023-01-03 23:59:59' AND SmInteger >= 1 AND SmInteger <= 1";
+      const expressionSet = [
+        {
+          field: { name: "String", type: "esriFieldTypeString" },
+          values: ["a"],
+        },
+        {
+          field: { name: "Date", type: "esriFieldTypeDate" },
+          values: ["2023-01-01"],
+        },
+        {
+          field: { name: "Date", type: "esriFieldTypeDate" },
+          values: [undefined, "2023-01-03"],
+        },
+        {
+          field: { name: "SmInteger", type: undefined },
+          values: ["1"],
+        },
+        {
+          field: { name: "SmInteger", type: undefined },
+          values: [undefined, "1"],
+        },
+      ];
+      model = cloneObject(getSiteModel(getCardSettings("left", where, null)));
+
+      const expected: IModel = cloneObject(
+        getSiteModel(getExpectedCardSettings("start", expressionSet, undefined))
+      );
+      setProp("item.properties.schemaVersion", 1.7, expected);
+      const results = _migrateSummaryStatCardConfigs<IModel>(model);
+      expect(results).toEqual(expected);
+    });
+
+    it("handles a case of string, dates, and numbers case 2", () => {
+      const where =
+        " Float <= 4.55 AND Field Name = 'This is a string' AND Date >= TIMESTAMP '2023-07-17 00:00:00' AND Float >= 3.99 AND Date <= TIMESTAMP '2023-07-17 23:59:59'";
+      const expressionSet = [
+        {
+          field: { name: "Float", type: undefined },
+          values: [undefined, "4.55"],
+        },
+        {
+          field: { name: "Field Name", type: "esriFieldTypeString" },
+          values: ["This is a string"],
+        },
+        {
+          field: { name: "Date", type: "esriFieldTypeDate" },
+          values: ["2023-07-17"],
+        },
+        {
+          field: { name: "Float", type: undefined },
+          values: ["3.99"],
+        },
+        {
+          field: { name: "Date", type: "esriFieldTypeDate" },
+          values: [undefined, "2023-07-17"],
+        },
+      ];
+      model = cloneObject(getSiteModel(getCardSettings("left", where, null)));
+
+      const expected: IModel = cloneObject(
+        getSiteModel(getExpectedCardSettings("start", expressionSet, undefined))
+      );
+      setProp("item.properties.schemaVersion", 1.7, expected);
+      const results = _migrateSummaryStatCardConfigs<IModel>(model);
+      expect(results).toEqual(expected);
+    });
+
+    it("handles a case of one clause", () => {
+      const where = "Float <= 4.55";
+      const expressionSet = [
+        {
+          field: { name: "Float", type: undefined },
+          values: [undefined, "4.55"],
+        },
+      ];
+      model = cloneObject(getSiteModel(getCardSettings("left", where, null)));
+
+      const expected: IModel = cloneObject(
+        getSiteModel(getExpectedCardSettings("start", expressionSet, undefined))
+      );
+      setProp("item.properties.schemaVersion", 1.7, expected);
+      const results = _migrateSummaryStatCardConfigs<IModel>(model);
+      expect(results).toEqual(expected);
+    });
+
+    it("handles an empty where clause", () => {
+      const expressionSet = [];
+      model = cloneObject(
+        getSiteModel(getCardSettings("left", undefined, null))
+      );
+
+      const expected: IModel = cloneObject(
+        getSiteModel(getExpectedCardSettings("start", expressionSet, undefined))
+      );
+      setProp("item.properties.schemaVersion", 1.7, expected);
+      const results = _migrateSummaryStatCardConfigs<IModel>(model);
+      expect(results).toEqual(expected);
+    });
+  });
+});
