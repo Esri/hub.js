3604fc8af1c83d7786d4e6c15b75d42fb716bd2a
juliannemarik <julianne.marik@gmail.com>
2023-09-06 16:44:19 -0500

    feat: refactor discussion uiSchemas to be dynamic

--- packages/common/src/discussions/_internal/DiscussionUiSchemaEdit.ts
+++ packages/common/src/discussions/_internal/DiscussionUiSchemaEdit.ts
@@ -1,177 +1,212 @@
-import { IUiSchema } from "../../core";
+import { IUiSchema } from "../../core/schemas/types";
+import { IArcGISContext } from "../../ArcGISContext";
+import { IHubDiscussion } from "../../core/types";
+import { getFeaturedImageUrl } from "../../core/schemas/internal/getFeaturedImageUrl";
+import { getTagItems } from "../../core/schemas/internal/getTagItems";
+import { getCategoryItems } from "../../core/schemas/internal/getCategoryItems";
+import { getLocationExtent } from "../../core/schemas/internal/getLocationExtent";
+import { getLocationOptions } from "../../core/schemas/internal/getLocationOptions";
 
 /**
  * @private
- * complete edit uiSchema for Hub discussions - this defines
- * how the schema properties should be rendered in the
- * discussion editing experience
+ * constructs the complete edit uiSchema for Hub Discussions.
+ * This defines how the schema properties should be
+ * rendered in the discussion editing experience
  */
-export const uiSchema: IUiSchema = {
-  type: "Layout",
-  elements: [
-    {
-      type: "Section",
-      labelKey: "{{i18nScope}}.sections.basicInfo.label",
-      elements: [
-        {
-          labelKey: "{{i18nScope}}.fields.name.label",
-          scope: "/properties/name",
-          type: "Control",
-          options: {
-            messages: [
-              {
-                type: "ERROR",
-                keyword: "required",
-                icon: true,
-                labelKey: "{{i18nScope}}.fields.name.requiredError",
+export const buildUiSchema = async (
+  i18nScope: string,
+  entity: IHubDiscussion,
+  context: IArcGISContext
+): Promise<IUiSchema> => {
+  return {
+    type: "Layout",
+    elements: [
+      {
+        type: "Section",
+        labelKey: `${i18nScope}.sections.basicInfo.label`,
+        elements: [
+          {
+            labelKey: `${i18nScope}.fields.name.label`,
+            scope: "/properties/name",
+            type: "Control",
+            options: {
+              messages: [
+                {
+                  type: "ERROR",
+                  keyword: "required",
+                  icon: true,
+                  labelKey: `${i18nScope}.fields.name.requiredError`,
+                },
+              ],
+            },
+          },
+          {
+            labelKey: `${i18nScope}.fields.prompt.label`,
+            scope: "/properties/prompt",
+            type: "Control",
+            options: {
+              helperText: {
+                labelKey: `${i18nScope}.fields.prompt.helperText`,
               },
-            ],
-          },
-        },
-        {
-          labelKey: "{{i18nScope}}.fields.prompt.label",
-          scope: "/properties/prompt",
-          type: "Control",
-          options: {
-            helperText: {
-              labelKey: "{{i18nScope}}.fields.prompt.helperText",
+              messages: [
+                {
+                  type: "ERROR",
+                  keyword: "required",
+                  icon: true,
+                  labelKey: `${i18nScope}.fields.prompt.requiredError`,
+                },
+              ],
             },
-            messages: [
-              {
-                type: "ERROR",
-                keyword: "required",
-                icon: true,
-                labelKey: "{{i18nScope}}.fields.prompt.requiredError",
+          },
+          {
+            labelKey: `${i18nScope}.fields.summary.label`,
+            scope: "/properties/summary",
+            type: "Control",
+            options: {
+              control: "hub-field-input-input",
+              type: "textarea",
+              helperText: {
+                labelKey: `${i18nScope}.fields.summary.helperText`,
               },
-            ],
-          },
-        },
-        {
-          labelKey: "{{i18nScope}}.fields.summary.label",
-          scope: "/properties/summary",
-          type: "Control",
-          options: {
-            control: "hub-field-input-input",
-            type: "textarea",
-            helperText: {
-              labelKey: "{{i18nScope}}.fields.summary.helperText",
             },
           },
-        },
-        {
-          labelKey: "{{i18nScope}}.fields.description.label",
-          scope: "/properties/description",
-          type: "Control",
-          options: {
-            control: "hub-field-input-input",
-            type: "textarea",
-            helperText: {
-              labelKey: "{{i18nScope}}.fields.description.helperText",
+          {
+            labelKey: `${i18nScope}.fields.description.label`,
+            scope: "/properties/description",
+            type: "Control",
+            options: {
+              control: "hub-field-input-input",
+              type: "textarea",
+              helperText: {
+                labelKey: `${i18nScope}.fields.description.helperText`,
+              },
             },
           },
-        },
-        {
-          labelKey: "{{i18nScope}}.fields._thumbnail.label",
-          scope: "/properties/_thumbnail",
-          type: "Control",
-          options: {
-            control: "hub-field-input-image-picker",
-            maxWidth: 727,
-            maxHeight: 484,
-            aspectRatio: 1.5,
-            helperText: {
-              labelKey: "{{i18nScope}}.fields._thumbnail.helperText",
+          {
+            labelKey: `${i18nScope}.fields._thumbnail.label`,
+            scope: "/properties/_thumbnail",
+            type: "Control",
+            options: {
+              control: "hub-field-input-image-picker",
+              imgSrc: entity.thumbnailUrl,
+              maxWidth: 727,
+              maxHeight: 484,
+              aspectRatio: 1.5,
+              helperText: {
+                labelKey: `${i18nScope}.fields._thumbnail.helperText`,
+              },
             },
           },
-        },
-        {
-          labelKey: "{{i18nScope}}.fields.featuredImage.label",
-          scope: "/properties/view/properties/featuredImage",
-          type: "Control",
-          options: {
-            control: "hub-field-input-image-picker",
-            maxWidth: 727,
-            maxHeight: 484,
-            aspectRatio: 1.5,
-            sizeDescription: {
-              labelKey: "{{i18nScope}}.fields.featuredImage.sizeDescription",
+          {
+            labelKey: `${i18nScope}.fields.featuredImage.label`,
+            scope: "/properties/view/properties/featuredImage",
+            type: "Control",
+            options: {
+              control: "hub-field-input-image-picker",
+              imgSrc:
+                entity.view?.featuredImageUrl &&
+                getFeaturedImageUrl(entity, context),
+              maxWidth: 727,
+              maxHeight: 484,
+              aspectRatio: 1.5,
+              sizeDescription: {
+                labelKey: `${i18nScope}.fields.featuredImage.sizeDescription`,
+              },
             },
           },
-        },
-        {
-          labelKey: "{{i18nScope}}.fields.featuredImage.altText.label",
-          scope: "/properties/view/properties/featuredImageAltText",
-          type: "Control",
-          options: {
-            helperText: {
-              labelKey: "{{i18nScope}}.fields.featuredImage.altText.helperText",
+          {
+            labelKey: `${i18nScope}.fields.featuredImage.altText.label`,
+            scope: "/properties/view/properties/featuredImageAltText",
+            type: "Control",
+            options: {
+              helperText: {
+                labelKey: `${i18nScope}.fields.featuredImage.altText.helperText`,
+              },
             },
           },
-        },
-        {
-          labelKey: "{{i18nScope}}.fields.featuredImage.name.label",
-          scope: "/properties/view/properties/featuredImageName",
-          type: "Control",
-        },
-        {
-          labelKey: "{{i18nScope}}.fields.tags.label",
-          scope: "/properties/tags",
-          type: "Control",
-          options: {
-            control: "hub-field-input-combobox",
-            allowCustomValues: true,
-            selectionMode: "multiple",
-            placeholderIcon: "label",
+          {
+            labelKey: `${i18nScope}.fields.featuredImage.name.label`,
+            scope: "/properties/view/properties/featuredImageName",
+            type: "Control",
           },
-        },
-        {
-          labelKey: "{{i18nScope}}.fields.categories.label",
-          scope: "/properties/categories",
-          type: "Control",
-          options: {
-            control: "hub-field-input-combobox",
-            allowCustomValues: false,
-            selectionMode: "multiple",
-            placeholderIcon: "select-category",
+          {
+            labelKey: `${i18nScope}.fields.tags.label`,
+            scope: "/properties/tags",
+            type: "Control",
+            options: {
+              control: "hub-field-input-combobox",
+              items: await getTagItems(
+                entity,
+                context.portal.id,
+                context.hubRequestOptions
+              ),
+              allowCustomValues: true,
+              selectionMode: "multiple",
+              placeholderIcon: "label",
+            },
           },
-        },
-      ],
-    },
-    {
-      type: "Section",
-      labelKey: "{{i18nScope}}.sections.location.label",
-      elements: [
-        {
-          scope: "/properties/location",
-          type: "Control",
-          options: {
-            control: "hub-field-input-location-picker",
+          {
+            labelKey: `${i18nScope}.fields.categories.label`,
+            scope: "/properties/categories",
+            type: "Control",
+            options: {
+              control: "hub-field-input-combobox",
+              items: await getCategoryItems(
+                context.portal.id,
+                context.hubRequestOptions
+              ),
+              allowCustomValues: false,
+              selectionMode: "multiple",
+              placeholderIcon: "select-category",
+            },
           },
-        },
-      ],
-    },
-    {
-      type: "Section",
-      labelKey: "{{i18nScope}}.sections.settings.label",
-      elements: [
-        {
-          labelKey: "{{i18nScope}}.fields.discussable.label",
-          scope: "/properties/isDiscussable",
-          type: "Control",
-          options: {
-            control: "hub-field-input-radio",
-            labels: [
-              "{{{{i18nScope}}.fields.discussable.enabled.label:translate}}",
-              "{{{{i18nScope}}.fields.discussable.disabled.label:translate}}",
-            ],
-            descriptions: [
-              "{{{{i18nScope}}.fields.discussable.enabled.description:translate}}",
-              "{{{{i18nScope}}.fields.discussable.disabled.description:translate}}",
-            ],
-            icons: ["speech-bubbles", "circle-disallowed"],
+        ],
+      },
+      {
+        type: "Section",
+        labelKey: `${i18nScope}.sections.location.label`,
+        elements: [
+          {
+            scope: "/properties/location",
+            type: "Control",
+            options: {
+              control: "hub-field-input-location-picker",
+              extent: await getLocationExtent(
+                entity,
+                context.hubRequestOptions
+              ),
+              options: await getLocationOptions(
+                entity,
+                context.portal.name,
+                context.hubRequestOptions
+              ),
+            },
           },
-        },
-      ],
-    },
-  ],
+        ],
+      },
+      {
+        type: "Section",
+        labelKey: `${i18nScope}.sections.settings.label`,
+        elements: [
+          {
+            labelKey: `${i18nScope}.fields.discussable.label`,
+            scope: "/properties/isDiscussable",
+            type: "Control",
+            options: {
+              control: "hub-field-input-radio",
+              labels: [
+                `{{${i18nScope}.fields.discussable.enabled.label:translate}}`,
+                `{{${i18nScope}.fields.discussable.disabled.label:translate}}`,
+              ],
+              descriptions: [
+                `{{${i18nScope}.fields.discussable.enabled.description:translate}}`,
+                `{{${i18nScope}.fields.discussable.disabled.description:translate}}`,
+              ],
+              icons: ["speech-bubbles", "circle-disallowed"],
+            },
+          },
+        ],
+      },
+    ],
+  };
 };
