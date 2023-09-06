002a9e14bd79bbf89c3c2fb3cfbdcca9c2de3310
juliannemarik <julianne.marik@gmail.com>
2023-09-06 16:24:06 -0500

    feat: refactor group uiSchemas to be dynamic

--- packages/common/src/groups/_internal/GroupUiSchemaEdit.ts
+++ packages/common/src/groups/_internal/GroupUiSchemaEdit.ts
@@ -1,60 +1,69 @@
-import { IUiSchema } from "../../core";
+import { IUiSchema } from "../../core/schemas/types";
+import { IArcGISContext } from "../../ArcGISContext";
+import { IHubGroup } from "../../core/types";
 
 /**
  * @private
- * Complete edit uiSchema for Hub Groups - this defines
- * how the schema properties should be rendered in the
- * group editing experience
+ * constructs the complete edit uiSchema for Hub Groups
+ * This defines how the schema properties should be
+ * rendered in the group editing experience
  */
-export const uiSchema: IUiSchema = {
-  type: "Layout",
-  elements: [
-    {
-      type: "Section",
-      labelKey: "{{i18nScope}}.sections.basicInfo.label",
-      elements: [
-        {
-          labelKey: "{{i18nScope}}.fields.name.label",
-          scope: "/properties/name",
-          type: "Control",
-          options: {
-            messages: [
-              {
-                type: "ERROR",
-                keyword: "required",
-                icon: true,
-                labelKey: "{{i18nScope}}.fields.name.requiredError",
+export const buildUiSchema = async (
+  i18nScope: string,
+  entity: IHubGroup,
+  context: IArcGISContext
+): Promise<IUiSchema> => {
+  return {
+    type: "Layout",
+    elements: [
+      {
+        type: "Section",
+        labelKey: `${i18nScope}.sections.basicInfo.label`,
+        elements: [
+          {
+            labelKey: `${i18nScope}.fields.name.label`,
+            scope: "/properties/name",
+            type: "Control",
+            options: {
+              messages: [
+                {
+                  type: "ERROR",
+                  keyword: "required",
+                  icon: true,
+                  labelKey: `${i18nScope}.fields.name.requiredError`,
+                },
+              ],
+            },
+          },
+          {
+            labelKey: `${i18nScope}.fields.summary.label`,
+            scope: "/properties/summary",
+            type: "Control",
+            options: {
+              control: "hub-field-input-input",
+              type: "textarea",
+            },
+          },
+          {
+            labelKey: `${i18nScope}.fields._thumbnail.label`,
+            scope: "/properties/_thumbnail",
+            type: "Control",
+            options: {
+              control: "hub-field-input-image-picker",
+              imgSrc: entity.thumbnailUrl,
+              maxWidth: 727,
+              maxHeight: 484,
+              aspectRatio: 1,
+              helperText: {
+                labelKey: `${i18nScope}.fields._thumbnail.helperText`,
               },
-            ],
-          },
-        },
-        {
-          labelKey: "{{i18nScope}}.fields.summary.label",
-          scope: "/properties/summary",
-          type: "Control",
-          options: {
-            control: "hub-field-input-input",
-            type: "textarea",
-          },
-        },
-        {
-          labelKey: "{{i18nScope}}.fields._thumbnail.label",
-          scope: "/properties/_thumbnail",
-          type: "Control",
-          options: {
-            control: "hub-field-input-image-picker",
-            maxWidth: 727,
-            maxHeight: 484,
-            aspectRatio: 1,
-            helperText: {
-              labelKey: "{{i18nScope}}.fields._thumbnail.helperText",
-            },
-            sizeDescription: {
-              labelKey: "{{i18nScope}}.fields._thumbnail.sizeDescription",
+              sizeDescription: {
+                labelKey: `${i18nScope}.fields._thumbnail.sizeDescription`,
+              },
             },
           },
-        },
-      ],
-    },
-  ],
+        ],
+      },
+    ],
+  };
 };
--- packages/common/src/groups/_internal/GroupUiSchemaSettings.ts
+++ packages/common/src/groups/_internal/GroupUiSchemaSettings.ts
@@ -1,44 +1,53 @@
 import { IUiSchema } from "../../core";
+import { IArcGISContext } from "../../ArcGISContext";
+import { IHubGroup } from "../../core/types";
 
 /**
  * @private
- * Complete settings uiSchema for Hub Groups - this defines
- * how the schema properties should be rendered in the
- * group settings experience
+ * constructs the complete settings uiSchema for Hub Groups
+ * This defines how the schema properties should be
+ * rendered in the group settings experience
  */
-export const uiSchema: IUiSchema = {
-  type: "Layout",
-  elements: [
-    {
-      type: "Section",
-      labelKey: "{{i18nScope}}.sections.membershipAccess.label",
-      elements: [
-        {
-          labelKey: "{{i18nScope}}.fields.membershipAccess.label",
-          scope: "/properties/membershipAccess",
-          type: "Control",
-          options: {
-            control: "hub-field-input-radio",
-            labels: [
-              "{{{{i18nScope}}.fields.membershipAccess.org:translate}}",
-              "{{{{i18nScope}}.fields.membershipAccess.collab:translate}}",
-              "{{{{i18nScope}}.fields.membershipAccess.any:translate}}",
-            ],
+export const buildUiSchema = async (
+  i18nScope: string,
+  entity: IHubGroup,
+  context: IArcGISContext
+): Promise<IUiSchema> => {
+  return {
+    type: "Layout",
+    elements: [
+      {
+        type: "Section",
+        labelKey: `${i18nScope}.sections.membershipAccess.label`,
+        elements: [
+          {
+            labelKey: `${i18nScope}.fields.membershipAccess.label`,
+            scope: "/properties/membershipAccess",
+            type: "Control",
+            options: {
+              control: "hub-field-input-radio",
+              labels: [
+                `{{${i18nScope}.fields.membershipAccess.org:translate}}`,
+                `{{${i18nScope}.fields.membershipAccess.collab:translate}}`,
+                `{{${i18nScope}.fields.membershipAccess.any:translate}}`,
+              ],
+              disabled: [false, false, entity.isSharedUpdate],
+            },
           },
-        },
-        {
-          labelKey: "{{i18nScope}}.fields.contributeContent.label",
-          scope: "/properties/isViewOnly",
-          type: "Control",
-          options: {
-            control: "hub-field-input-radio",
-            labels: [
-              "{{{{i18nScope}}.fields.contributeContent.all:translate}}",
-              "{{{{i18nScope}}.fields.contributeContent.admins:translate}}",
-            ],
+          {
+            labelKey: `${i18nScope}.fields.contributeContent.label`,
+            scope: "/properties/isViewOnly",
+            type: "Control",
+            options: {
+              control: "hub-field-input-radio",
+              labels: [
+                `{{${i18nScope}.fields.contributeContent.all:translate}}`,
+                `{{${i18nScope}.fields.contributeContent.admins:translate}}`,
+              ],
+            },
           },
-        },
-      ],
-    },
-  ],
+        ],
+      },
+    ],
+  };
 };
