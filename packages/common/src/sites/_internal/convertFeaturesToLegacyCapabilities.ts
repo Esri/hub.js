ef3795164a2a691dadc7db11a8cce2684bc22c61
juliannemarik <julianne.marik@gmail.com>
2023-09-13 13:49:54 -0500

    chore: add function comment to convertFeaturesToLegacyCapabilities

--- packages/common/src/sites/_internal/convertFeaturesToLegacyCapabilities.ts
+++ packages/common/src/sites/_internal/convertFeaturesToLegacyCapabilities.ts
@@ -1,6 +1,19 @@
 import { getProp } from "../../objects";
 import { IModel } from "../../types";
 
+/**
+ * Site capabilities are currently saved as an array on the
+ * site.data.values.capabilities. We want to start migrating
+ * these legacy capabilities over to the permissions system
+ * using features; however, we still need to continue mapping
+ * updates to the features over to the legacy capabilities
+ * array until the existing site capabilities in our application
+ * are plumbed to work off of permissions
+ * TODO: Remove once site capabilities use permissions
+ *
+ * @param modelToUpdate
+ * @param currentModel
+ */
 export const convertFeaturesToLegacyCapabilities = (
   modelToUpdate: IModel,
   currentModel: IModel
