82ceec4acc2040e483b2a4d422152fd02a62384c
Santosh Shrestha <94729944+sansth1010@users.noreply.github.com>
2023-09-18 22:29:57 -0500

    feat(hub-common): add application type content edit link (#1217)
    * feat(hub-common): add application type content edit link
    
    * fix(hub-common): fix tests and add source comments for edit content url
    
    * feat(hub-common): add portal item url as a default content edit URL

--- packages/common/src/content/_internal/computeProps.ts
+++ packages/common/src/content/_internal/computeProps.ts
@@ -6,7 +6,7 @@ import { bBoxToExtent, extentToPolygon, isBBox } from "../../extent";
 import { IExtent } from "@esri/arcgis-rest-types";
 import Geometry = __esri.Geometry;
 import { getItemHomeUrl } from "../../urls/get-item-home-url";
-import { getHubRelativeUrl } from "./internalContentUtils";
+import { getContentEditUrl, getHubRelativeUrl } from "./internalContentUtils";
 import { IHubLocation } from "../../core/types/IHubLocation";
 import { IHubEditableContent } from "../../core/types/IHubEditableContent";
 import { getRelativeWorkspaceUrl } from "../../core/getRelativeWorkspaceUrl";
@@ -62,8 +62,8 @@ export function computeProps(
     ),
     workspaceRelative: getRelativeWorkspaceUrl("content", content.id),
     thumbnail: thumbnailUrl,
+    contentEditUrl: getContentEditUrl(model.item, requestOptions),
   };
-
   // cannot be null otherwise we'd get a validation
   // error that doesn't let us save the form
   content.licenseInfo = model.item.licenseInfo || "";
