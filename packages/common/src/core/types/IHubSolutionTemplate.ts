a263b6e43ca834314091973a0b8151f5d2544bd7
juliannemarik <julianne.marik@gmail.com>
2023-09-27 08:58:02 -0500

    feat: scaffold minimal solution template entity and interface

---
+++ packages/common/src/core/types/IHubSolutionTemplate.ts
@@ -0,0 +1,5 @@
+import { IWithSlug } from "../traits";
+import { IHubItemEntity } from "./IHubItemEntity";
+
+/** Defines the properties of a Hub Solution Template entity */
+export interface IHubSolutionTemplate extends IHubItemEntity, IWithSlug {}
