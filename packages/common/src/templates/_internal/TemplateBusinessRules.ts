4f601aa48a63d92e41dddde1e0c4a01601026ab1
juliannemarik <julianne.marik@gmail.com>
2023-09-28 13:31:29 -0500

    feat: scaffold template business rules

---
+++ packages/common/src/templates/_internal/TemplateBusinessRules.ts
@@ -0,0 +1,69 @@
+import { IFeatureFlags, IPermissionPolicy } from "../../permissions/types";
+
+/**
+ * Default features for a Template. These are the features
+ * that can be enabled / disabled by the entity owner
+ */
+export const TemplateDefaultFeatures: IFeatureFlags = {};
+
+/**
+ * @private
+ * Template permissions: these define the requirements
+ * any user must meet to perform related actions
+ */
+export const TemplatePermissions = [
+  "hub:template",
+  "hub:template:create",
+  "hub:template:delete",
+  "hub:template:edit",
+  "hub:template:view",
+  "hub:template:workspace:overview",
+  "hub:template:workspace:details",
+  "hub:template:workspace:collaborators",
+  "hub:template:workspace:settings",
+] as const;
+
+/**
+ * @private
+ * Template permission policies
+ */
+export const TemplatePermissionPolicies: IPermissionPolicy[] = [
+  {
+    permission: "hub:template",
+    availability: ["alpha"], // gate to alpha for now
+    services: ["portal"],
+  },
+  {
+    permission: "hub:template:create",
+    dependencies: ["hub:template"],
+    authenticated: true,
+    privileges: ["portal:user:createItem"],
+  },
+  {
+    permission: "hub:template:view",
+    dependencies: ["hub:template"],
+  },
+  {
+    permission: "hub:template:edit",
+    dependencies: ["hub:template"],
+    authenticated: true,
+    entityEdit: true,
+  },
+  {
+    permission: "hub:template:workspace:overview",
+    dependencies: ["hub:template:view"],
+  },
+  {
+    permission: "hub:template:workspace:details",
+    dependencies: ["hub:template:edit"],
+  },
+  {
+    permission: "hub:template:workspace:collaborators",
+    dependencies: ["hub:template:edit"],
+  },
+  {
+    permission: "hub:template:workspace:settings",
+    dependencies: ["hub:template:edit"],
+    entityOwner: true,
+  },
+];
