cdbcf98623fac7c46381dd69606e5c14decf5e5e
Jordan Sanz <jsanz@esri.com>
2023-10-02 17:54:36 -0400

    feat(): remove alpha from initiativetemplate business rules

--- packages/common/src/initiative-templates/_internal/InitiativeTemplateBusinessRules.ts
+++ packages/common/src/initiative-templates/_internal/InitiativeTemplateBusinessRules.ts
@@ -32,7 +32,7 @@ export const InitiativeTemplatePermissions = [
 export const InitiativeTemplatePermissionPolicies: IPermissionPolicy[] = [
   {
     permission: "hub:initiativeTemplate",
-    availability: ["alpha"], // gate to just alpha for now
+    // availability: ["alpha"], // gate to just alpha for now
     services: ["portal"],
   },
   {
