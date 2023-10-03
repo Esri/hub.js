466da956ea5874de7c7b6f78d52b14af9f206978
juliannemarik <julianne.marik@gmail.com>
2023-10-03 08:41:56 -0500

    feat: implement enrichTemplateSearchResult functionality

--- packages/common/src/templates/fetch.ts
+++ packages/common/src/templates/fetch.ts
@@ -1,13 +1,21 @@
 import { IRequestOptions } from "@esri/arcgis-rest-request";
 import { IHubTemplate } from "../core/types/IHubTemplate";
-import { IModel } from "../types";
-import { isGuid } from "../utils";
+import { IHubRequestOptions, IModel } from "../types";
+import { isGuid, mapBy } from "../utils";
 import { IItem, getItem } from "@esri/arcgis-rest-portal";
 import { getItemBySlug } from "../items/slugs";
 import { fetchModelFromItem } from "../models";
 import { PropertyMapper } from "../core/_internal/PropertyMapper";
 import { getPropertyMap } from "./_internal/getPropertyMap";
 import { computeProps } from "./_internal/computeProps";
+import { IHubSearchResult } from "../search/types";
+import { getFamily } from "../content/get-family";
+import { unique } from "../util";
+import { parseInclude } from "../search/_internal/parseInclude";
+import { fetchItemEnrichments } from "../items/_enrichments";
+import { getProp } from "../objects";
+import { computeLinks } from "./_internal/computeLinks";
+import { getActivatedTemplateType } from "./utils";
 
 /**
  * @private
@@ -55,4 +63,68 @@ export async function convertItemToTemplate(
   // 3. Compute + set various properties on the IHubTemplate
   // that cannot be directly mapped from the item
   return computeProps(model, template, requestOptions);
+}
+
+/**
+ * @private
+ * Fetch template specific enrichments
+ * @param item
+ * @param include
+ * @param requestOptions
+ */
+export async function enrichTemplateSearchResult(
+  item: IItem,
+  include: string[],
+  requestOptions: IHubRequestOptions
+): Promise<IHubSearchResult> {
+  const result: IHubSearchResult = {
+    access: item.access,
+    id: item.id,
+    type: item.type,
+    name: item.title,
+    owner: item.owner,
+    typeKeywords: item.typeKeywords,
+    tags: item.tags,
+    categories: item.categories,
+    summary: item.snippet || item.description,
+    createdDate: new Date(item.created),
+    createdDateSource: "item.created",
+    updatedDate: new Date(item.modified),
+    updatedDateSource: "item.modified",
+    family: getFamily(item.type),
+    links: {
+      self: "not-implemented",
+      siteRelative: "not-implemented",
+      thumbnail: "not-implemented",
+      workspaceRelative: "not-implemented",
+    },
+  };
+
+  // 1. optionally enrich the template item with
+  // well-known item enrichments
+  const DEFAULTS: string[] = [];
+  // merge default and provided "include" enrichments
+  include = [...DEFAULTS, ...include].filter(unique);
+  // Parse the includes into a valid set of enrichments
+  const specs = include.map(parseInclude);
+  // Extract out the low-level enrichments needed
+  const enrichments = mapBy("enrichment", specs).filter(unique);
+  // fetch the enrichments
+  let enriched = {};
+  if (enrichments.length) {
+    enriched = await fetchItemEnrichments(item, enrichments, requestOptions);
+  }
+  // map the enriched props onto the result
+  specs.forEach((spec) => {
+    result[spec.prop] = getProp(enriched, spec.path);
+  });
+
+  // 2. append relevant links onto the search result - these
+  // are the same links that get appended to the template entity
+  result.links = computeLinks(item, requestOptions);
+
+  // 3. append additional template-specific properties
+  result.activatedType = getActivatedTemplateType(item);
+
+  return result;
 }
--- packages/common/test/templates/fetch.test.ts
+++ packages/common/test/templates/fetch.test.ts
@@ -8,9 +8,13 @@ import {
 import {
   fetchTemplate,
   convertItemToTemplate,
+  enrichTemplateSearchResult,
 } from "../../src/templates/fetch";
 import * as portalModule from "@esri/arcgis-rest-portal";
 import * as getItemBySlugModule from "../../src/items/slugs";
+import * as fetchItemEnrichmentsModule from "../../src/items/_enrichments";
+import * as computeLinksModule from "../../src/templates/_internal/computeLinks";
+import * as templateUtilsModule from "../../src/templates/utils";
 
 describe("templates: fetch module", () => {
   let authdCtxMgr: ArcGISContextManager;
@@ -62,6 +66,63 @@ describe("templates: fetch module", () => {
       expect(chk).toBeNull();
     });
   });
+
+  describe("enrichTemplateSearchResult", () => {
+    let fetchItemEnrichmentsSpy: jasmine.Spy;
+    let computLinksSpy: jasmine.Spy;
+    let getActivatedTemplateTypeSpy: jasmine.Spy;
+
+    beforeEach(() => {
+      fetchItemEnrichmentsSpy = spyOn(
+        fetchItemEnrichmentsModule,
+        "fetchItemEnrichments"
+      ).and.callFake(() => Promise.resolve({}));
+      computLinksSpy = spyOn(
+        computeLinksModule,
+        "computeLinks"
+      ).and.returnValue({ self: "some-link" });
+      getActivatedTemplateTypeSpy = spyOn(
+        templateUtilsModule,
+        "getActivatedTemplateType"
+      ).and.returnValue("StoryMap");
+    });
+    afterEach(() => {
+      fetchItemEnrichmentsSpy.calls.reset();
+      computLinksSpy.calls.reset();
+      getActivatedTemplateTypeSpy.calls.reset();
+    });
+
+    it("enriches the templates with well-known item enrichments", async () => {
+      await enrichTemplateSearchResult(
+        TEMPLATE_ITEM,
+        ["someEnrichment AS enrichment"],
+        authdCtxMgr.context.hubRequestOptions
+      );
+
+      expect(fetchItemEnrichmentsSpy).toHaveBeenCalledTimes(1);
+    });
+    it("enriches the template with well-known links", async () => {
+      const chk = await enrichTemplateSearchResult(
+        TEMPLATE_ITEM,
+        [],
+        authdCtxMgr.context.hubRequestOptions
+      );
+
+      expect(computLinksSpy).toHaveBeenCalledTimes(1);
+      expect(chk.links).toEqual({ self: "some-link" });
+    });
+    it("enriches the template with the activated solution type", async () => {
+      const chk = await enrichTemplateSearchResult(
+        TEMPLATE_ITEM,
+        [],
+        authdCtxMgr.context.hubRequestOptions
+      );
+
+      expect(getActivatedTemplateTypeSpy).toHaveBeenCalledTimes(1);
+      expect(chk.activatedType).toBe("StoryMap");
+    });
+  });
+
   describe("convertItemToTemplate", () => {
     it("converts the item correctly", async () => {
       const getItemDataSpy = spyOn(portalModule, "getItemData").and.returnValue(
