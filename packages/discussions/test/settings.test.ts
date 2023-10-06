73945451a988a582b069907b79352397362015e9
velveetachef <jeremysabat@gmail.com>
2023-10-06 13:06:37 -0700

    fix(hub-discussions): rename DiscussionSettings to Settings for types and functions that call the ap
    affects: @esri/hub-discussions

---
+++ packages/discussions/test/settings.test.ts
@@ -0,0 +1,111 @@
+import * as req from "../src/request";
+import {
+  createSetting,
+  fetchSetting,
+  removeSetting,
+  updateSetting,
+} from "../src/settings";
+import {
+  SettingType,
+  ICreateSetting,
+  ICreateSettingParams,
+  IDiscussionsRequestOptions,
+  IRemoveSettingParams,
+  IUpdateSetting,
+  IUpdateSettingParams,
+  IFetchSettingParams,
+} from "../src/types";
+
+describe("settings", () => {
+  let requestSpy: any;
+  const response = new Response("ok", { status: 200 });
+  const baseOpts: IDiscussionsRequestOptions = {
+    hubApiUrl: "https://hub.arcgis.com/api",
+    authentication: undefined,
+  };
+
+  beforeEach(() => {
+    requestSpy = spyOn(req, "request").and.returnValue(
+      Promise.resolve(response)
+    );
+  });
+
+  it("createSetting", async () => {
+    const body: ICreateSetting = {
+      id: "uuidv4",
+      type: SettingType.CONTENT,
+      settings: {
+        discussions: {
+          allowedChannelIds: ["aaa"],
+          allowedLocations: [
+            {
+              type: "Point",
+              coordinates: [-101.25, 37.996162679728116],
+            },
+          ],
+        },
+      },
+    };
+    const options: ICreateSettingParams = { ...baseOpts, data: body };
+
+    await createSetting(options);
+
+    expect(requestSpy.calls.count()).toEqual(1);
+    const [url, opts] = requestSpy.calls.argsFor(0);
+    expect(url).toEqual(`/settings`);
+    expect(opts).toEqual({ ...options, httpMethod: "POST" });
+  });
+
+  it("fetchSetting", async () => {
+    const id = "uuidv4";
+    const options: IFetchSettingParams = { ...baseOpts, id };
+
+    await fetchSetting(options);
+
+    expect(requestSpy.calls.count()).toEqual(1);
+    const [url, opts] = requestSpy.calls.argsFor(0);
+    expect(url).toEqual(`/settings/${id}`);
+    expect(opts).toEqual({ ...options, httpMethod: "GET" });
+  });
+
+  it("updateSetting", async () => {
+    const id = "uuidv4";
+    const body: IUpdateSetting = {
+      settings: {
+        discussions: {
+          allowedChannelIds: ["aaa"],
+          allowedLocations: [
+            {
+              type: "Point",
+              coordinates: [-101.25, 37.996162679728116],
+            },
+          ],
+        },
+      },
+    };
+    const options: IUpdateSettingParams = {
+      ...baseOpts,
+      id,
+      data: body,
+    };
+
+    await updateSetting(options);
+
+    expect(requestSpy.calls.count()).toEqual(1);
+    const [url, opts] = requestSpy.calls.argsFor(0);
+    expect(url).toEqual(`/settings/${id}`);
+    expect(opts).toEqual({ ...options, httpMethod: "PATCH" });
+  });
+
+  it("removeSetting", async () => {
+    const id = "uuidv4";
+    const options: IRemoveSettingParams = { ...baseOpts, id };
+
+    await removeSetting(options);
+
+    expect(requestSpy.calls.count()).toEqual(1);
+    const [url, opts] = requestSpy.calls.argsFor(0);
+    expect(url).toEqual(`/settings/${id}`);
+    expect(opts).toEqual({ ...options, httpMethod: "DELETE" });
+  });
+});
