2be07abf3429edb7b88d889c8cc04c1d43e190db
sansth1010 <sansth22@gmail.com>
2023-10-05 20:08:45 -0500

    feat(hub-common): add function to get feed configuration

---
+++ packages/common/src/sites/get-feed-configuration.ts
@@ -0,0 +1,65 @@
+import { IModel } from "../types";
+
+type FeedFormat = "dcat-us" | "dcat-ap" | "rss";
+
+/**
+ * Returns feed configuration from a site model
+ *
+ * @param {IModel} site - site model
+ * @param {FeedFormat} format - feed format
+ * @param {string} version  - semantic version
+ */
+export function getFeedConfiguration(
+  site: IModel,
+  format: FeedFormat,
+  version: string
+) {
+  if (format === "dcat-us") {
+    return getDcatUsConfig(site, version);
+  }
+
+  if (format === "dcat-ap") {
+    return getDcatApConfig(site, version);
+  }
+
+  if (format === "rss") {
+    return getRssConfig(site, version);
+  }
+}
+
+function getDcatApConfig(site: IModel, version: string) {
+  if (getMajorVersion(version) === "2") {
+    return {
+      config: site.data.feeds.dcatAP2XX || site.data.feeds.dcatAP201,
+      configPath: "data.feeds.dcatAP2XX",
+    };
+  }
+
+  throw new Error("Unsupported DCAT AP Version");
+}
+
+function getDcatUsConfig(site: IModel, version: string) {
+  if (getMajorVersion(version) === "1") {
+    return {
+      config: site.data.feeds.dcatUS1X || site.data.feeds.dcatUS11,
+      configPath: "data.feeds.dcatUS1X",
+    };
+  }
+
+  throw new Error("Unsupported DCAT US Version");
+}
+
+function getRssConfig(site: IModel, version: string) {
+  if (getMajorVersion(version) === "2") {
+    return {
+      config: site.data.feeds.rss2,
+      configPath: "data.feeds.rss2",
+    };
+  }
+
+  throw new Error("Unsupported RSS Version");
+}
+
+function getMajorVersion(version: string) {
+  return version.split(".")[0];
+}
--- packages/common/src/sites/index.ts
+++ packages/common/src/sites/index.ts
@@ -10,6 +10,7 @@ export * from "./HubSites";
 export * from "./site-schema-version";
 export * from "./themes";
 export * from "./upgrade-site-schema";
+export * from "./get-feed-configuration";
 // No longer exported b/c site app registration is now handled
 // by the domain service due to requirement to send signed HMAC request
 // export * from "./registerSiteAsApplication";
