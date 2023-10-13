cdabf338c2b884111002189f4b05bcebb9ca56a4
Aaron Putterman <60486980+abp6318@users.noreply.github.com>
2023-10-13 09:12:58 -0400

    feat(hub-common): add section heading for template ui schemas (#1274)
    * feat: more polishing of details pane
    
    * fix: pull missed element
    
    * feat: updated tests

--- packages/common/test/initiative-templates/_internal/InitiativeTemplateUiSchemaEdit.test.ts
+++ packages/common/test/initiative-templates/_internal/InitiativeTemplateUiSchemaEdit.test.ts
@@ -21,106 +21,112 @@ describe("buildUiSchema: initiative template edit", () => {
       type: "Layout",
       elements: [
         {
-          type: "Control",
-          scope: "/properties/name",
-          labelKey: `some.scope.fields.name.label`,
-          options: {
-            messages: [
-              {
-                type: UiSchemaMessageTypes.error,
-                keyword: "required",
-                icon: true,
-                labelKey: `some.scope.fields.name.requiredError`,
+          type: "Section",
+          labelKey: `some.scope.sections.basicInfo.label`,
+          elements: [
+            {
+              type: "Control",
+              scope: "/properties/name",
+              labelKey: `some.scope.fields.name.label`,
+              options: {
+                messages: [
+                  {
+                    type: UiSchemaMessageTypes.error,
+                    keyword: "required",
+                    icon: true,
+                    labelKey: `some.scope.fields.name.requiredError`,
+                  },
+                ],
               },
-            ],
-          },
-        },
-        {
-          type: "Control",
-          scope: "/properties/previewUrl",
-          labelKey: `some.scope.fields.previewUrl.label`,
-          options: {
-            helperText: {
-              labelKey: "some.scope.fields.previewUrl.helperText",
             },
-            messages: [
-              {
-                type: "ERROR",
-                keyword: "format",
-                icon: true,
-                labelKey: "some.scope.fields.previewUrl.formatError",
+            {
+              type: "Control",
+              scope: "/properties/previewUrl",
+              labelKey: `some.scope.fields.previewUrl.label`,
+              options: {
+                helperText: {
+                  labelKey: "some.scope.fields.previewUrl.helperText",
+                },
+                messages: [
+                  {
+                    type: "ERROR",
+                    keyword: "format",
+                    icon: true,
+                    labelKey: "some.scope.fields.previewUrl.formatError",
+                  },
+                  {
+                    type: "ERROR",
+                    keyword: "if",
+                    hidden: true,
+                  },
+                ],
               },
-              {
-                type: "ERROR",
-                keyword: "if",
-                hidden: true,
+            },
+            {
+              type: "Control",
+              scope: "/properties/summary",
+              labelKey: `some.scope.fields.summary.label`,
+              options: {
+                control: "hub-field-input-input",
+                type: "textarea",
+                helperText: {
+                  labelKey: "some.scope.fields.summary.helperText",
+                },
               },
-            ],
-          },
-        },
-        {
-          type: "Control",
-          scope: "/properties/summary",
-          labelKey: `some.scope.fields.summary.label`,
-          options: {
-            control: "hub-field-input-input",
-            type: "textarea",
-            helperText: {
-              labelKey: "some.scope.fields.summary.helperText",
             },
-          },
-        },
-        {
-          type: "Control",
-          scope: "/properties/description",
-          labelKey: `some.scope.fields.description.label`,
-          options: {
-            control: "hub-field-input-input",
-            type: "textarea",
-            helperText: {
-              labelKey: "some.scope.fields.description.helperText",
-            },
-          },
-        },
-        {
-          type: "Control",
-          scope: "/properties/_thumbnail",
-          labelKey: `shared.fields._thumbnail.label`,
-          options: {
-            control: "hub-field-input-image-picker",
-            imgSrc: "https://some-thumbnail-url.com",
-            maxWidth: 727,
-            maxHeight: 484,
-            aspectRatio: 1.5,
-            helperText: {
-              labelKey: "some.scope.fields._thumbnail.helperText",
-            },
-            sizeDescription: {
-              labelKey: "shared.fields._thumbnail.sizeDescription",
-            },
-            messages: [],
-          },
-        },
-        {
-          type: "Control",
-          scope: "/properties/recommendedTemplates",
-          labelKey: `some.scope.fields.recommendedTemplates.label`,
-          options: {
-            control: "hub-field-input-gallery-picker",
-            targetEntity: "item",
-            catalogs: [],
-            facets: [
-              {
-                label: `{{some.scope.fields.recommendedTemplates.facets.sharing:translate}}`,
-                key: "access",
-                field: "access",
-                display: "multi-select",
-                operation: "OR",
+            {
+              type: "Control",
+              scope: "/properties/description",
+              labelKey: `some.scope.fields.description.label`,
+              options: {
+                control: "hub-field-input-input",
+                type: "textarea",
+                helperText: {
+                  labelKey: "some.scope.fields.description.helperText",
+                },
               },
-            ],
-            canReorder: false,
-            linkTarget: "workspaceRelative",
-          },
+            },
+            {
+              type: "Control",
+              scope: "/properties/_thumbnail",
+              labelKey: `shared.fields._thumbnail.label`,
+              options: {
+                control: "hub-field-input-image-picker",
+                imgSrc: "https://some-thumbnail-url.com",
+                maxWidth: 727,
+                maxHeight: 484,
+                aspectRatio: 1.5,
+                helperText: {
+                  labelKey: "some.scope.fields._thumbnail.helperText",
+                },
+                sizeDescription: {
+                  labelKey: "shared.fields._thumbnail.sizeDescription",
+                },
+                messages: [],
+              },
+            },
+            {
+              type: "Control",
+              scope: "/properties/recommendedTemplates",
+              labelKey: `some.scope.fields.recommendedTemplates.label`,
+              options: {
+                control: "hub-field-input-gallery-picker",
+                targetEntity: "item",
+                catalogs: [],
+                facets: [
+                  {
+                    label: `{{some.scope.fields.recommendedTemplates.facets.sharing:translate}}`,
+                    key: "access",
+                    field: "access",
+                    display: "multi-select",
+                    operation: "OR",
+                  },
+                ],
+                canReorder: false,
+                linkTarget: "workspaceRelative",
+              },
+            },
+          ],
         },
       ],
     });
