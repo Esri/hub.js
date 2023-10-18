e95767a96400dd7e77be4ca2a9e9ad9e46acb5d6
Jordan Sanz <jsanz@esri.com>
2023-10-18 11:33:51 -0400

    docs(): change config

--- packages/common/src/core/schemas/getEditorConfig.ts
+++ packages/common/src/core/schemas/getEditorConfig.ts
@@ -2,27 +2,21 @@ import { IEditorConfig } from ".";
 import { IArcGISContext } from "../../ArcGISContext";
 import { EditorOptions } from "./internal/EditorOptions";
 import { getEditorSchemas } from "./internal/getEditorSchemas";
-import {
-  CardEditorType,
-  EditorType,
-  EntityEditorType,
-  validCardEditorTypes,
-} from "./types";
-import { getCardEditorSchemas } from "./internal/getCardEditorSchemas";
+import { EditorType } from "./types";
 
 /**
  * Construct the Editor Configuration (schema + uiSchema)
  * for a given entity editor type
  * @param i18nScope
  * @param type
- * @param options
+ * @param options - options to integrate into the schema + uiSchema
  * @param context
  * @returns
  */
 export async function getEditorConfig(
   i18nScope: string,
-  type: EditorType, // make this into an intersection type vs. either or
-  options: EditorOptions, //
+  type: EditorType,
+  options: EditorOptions,
   context: IArcGISContext
 ): Promise<IEditorConfig> {
   return getEditorSchemas(i18nScope, type, options, context);
