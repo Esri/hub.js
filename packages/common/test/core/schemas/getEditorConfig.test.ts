43027ac40d36ab0b71a71a0d6e118fab1bb4bbf6
Jordan Sanz <jsanz@esri.com>
2023-10-20 13:45:34 -0400

    test(): ensure tests use correct typing and not any

--- packages/common/test/core/schemas/getEditorConfig.test.ts
+++ packages/common/test/core/schemas/getEditorConfig.test.ts
@@ -1,8 +1,10 @@
-import { getEditorConfig } from "../../../src";
+import { getEditorConfig } from "../../../src/core/schemas";
+import { IArcGISContext } from "../../../src/ArcGISContext";
+import { EntityEditorOptions } from "../../../src/core/schemas/internal/EditorOptions";
 import * as GetEntitySchemasModule from "../../../src/core/schemas/internal/getEditorSchemas";
 
 describe("getEditorConfig:", () => {
-  it("delegates to getEditorSchemas", async () => {
+  it("delegates to getEditorSchemas with entity", async () => {
     const getEditorSchemas = spyOn(
       GetEntitySchemasModule,
       "getEditorSchemas"
@@ -13,8 +15,35 @@ describe("getEditorConfig:", () => {
     const chk = await getEditorConfig(
       "someScope",
       "hub:discussion:edit",
-      {} as any,
+      { type: "" },
       {} as any
+    );
+
+    expect(getEditorSchemas).toHaveBeenCalled();
+    // verify that the options are passed to the schemas
+    expect(getEditorSchemas).toHaveBeenCalledWith(
+      "someScope",
+      "hub:discussion:edit",
+      {},
+      {}
+    );
+    expect(chk).toBeDefined();
+    expect(chk.schema).toBeDefined();
+  });
+
+  it("delegates to getEditorSchemas with card", async () => {
+    const getEditorSchemas = spyOn(
+      GetEntitySchemasModule,
+      "getEditorSchemas"
+    ).and.callFake(() => {
+      return Promise.resolve({ schema: {} } as any);
+    });
+
+    const chk = await getEditorConfig(
+      "someScope",
+      "hub:card:stat",
+      { themeColors: [] },
+      {} as IArcGISContext
     );
 
     expect(getEditorSchemas).toHaveBeenCalled();
