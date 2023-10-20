4b0c6a823bd3a517f19203b50d7adf7fca1d676d
bri12416 <bri12416@esri.com>
2023-10-19 23:44:53 -0500

    feat(hub-discussions): removed legacy permission checks for modify channel

--- packages/discussions/src/utils/channels/can-modify-channel.ts
+++ packages/discussions/src/utils/channels/can-modify-channel.ts
@@ -1,7 +1,6 @@
-import { IGroup, IUser } from "@esri/arcgis-rest-types";
+import { IUser } from "@esri/arcgis-rest-types";
 import { IChannel, IDiscussionsUser, SharingAccess } from "../../types";
 import { ChannelPermission } from "../channel-permission";
-import { isOrgAdmin } from "../platform";
 
 const ADMIN_GROUP_ROLES = Object.freeze(["owner", "admin"]);
 
@@ -15,69 +14,8 @@ export function canModifyChannel(
   channel: IChannel,
   user: IUser | IDiscussionsUser = {}
 ): boolean {
-  const { channelAcl, creator } = channel;
+  const { channelAcl } = channel;
 
-  if (channelAcl) {
-    const channelPermission = new ChannelPermission(channelAcl, creator);
-    return channelPermission.canModerateChannel(user as IDiscussionsUser);
-  }
-
-  return isAuthorizedToModifyChannelByLegacyPermissions(user, channel);
-}
-
-function isAuthorizedToModifyChannelByLegacyPermissions(
-  user: IUser,
-  channel: IChannel
-): boolean {
-  const { username, groups: userGroups = [] } = user;
-  const {
-    access,
-    groups: channelGroups = [],
-    orgs: channelOrgs = [],
-    creator: channelCreator,
-  } = channel;
-
-  // ensure authenticated
-  if (!username) {
-    return false;
-  }
-
-  if (access === SharingAccess.PRIVATE) {
-    return isAuthorizedToModifyChannelByLegacyGroup(channelGroups, userGroups);
-  }
-
-  // public or org access
-  return (
-    isAuthorizedToModifyChannelByLegacyGroup(channelGroups, userGroups) ||
-    isChannelOrgAdmin(channelOrgs, user)
-  );
-}
-
-/**
- * Ensure the user is an owner/admin of one of the channel groups
- */
-function isAuthorizedToModifyChannelByLegacyGroup(
-  channelGroups: string[],
-  userGroups: IGroup[]
-) {
-  return channelGroups.some((channelGroupId: string) => {
-    return userGroups.some((group: IGroup) => {
-      const {
-        id: userGroupId,
-        userMembership: { memberType: userMemberType },
-      } = group;
-
-      return (
-        channelGroupId === userGroupId &&
-        ADMIN_GROUP_ROLES.includes(userMemberType)
-      );
-    });
-  });
-}
-
-function isChannelOrgAdmin(
-  channelOrgs: string[],
-  user: IUser | IDiscussionsUser
-): boolean {
-  return isOrgAdmin(user) && channelOrgs.includes(user.orgId);
+  const channelPermission = new ChannelPermission(channelAcl);
+  return channelPermission.canModerateChannel(user as IDiscussionsUser);
 }
