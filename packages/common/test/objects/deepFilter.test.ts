d4be40df087d45a023693305a574bf3021e66991
juliannemarik <julianne.marik@gmail.com>
2023-10-26 16:48:26 -0500

    fix: update tests

--- packages/common/test/objects/deepFilter.test.ts
+++ packages/common/test/objects/deepFilter.test.ts
@@ -19,15 +19,15 @@ describe("deepFilter:", () => {
       expect(chk).toEqual([{ notid: "a" }, { id: "c" }]);
     });
     it("filters simple objects", () => {
-      const test = { id: "b" };
-      const predicate = (obj: any) => obj?.id !== "b";
+      const test = { id: "a", someProp: { id: "b" } };
+      const predicate = (obj: any) => obj.id !== "b";
       const chk = deepFilter(test, predicate);
 
-      expect(chk).toEqual({ id: "b" });
+      expect(chk).toEqual({ id: "a" });
     });
     it("filters deeply nested entries in an array", () => {
       const predicate = (link: any) => link.key !== "00a";
-      const object = [
+      const test = [
         {
           key: "001",
           label: "Stop the Spotted Lanternfly",
@@ -48,7 +48,7 @@ describe("deepFilter:", () => {
         },
       ];
 
-      const chk = deepFilter(object, predicate);
+      const chk = deepFilter(test, predicate);
 
       expect(chk.length).toBe(2);
       expect(chk[0].key).toBe("001");
@@ -56,23 +56,23 @@ describe("deepFilter:", () => {
       expect(chk[1].children.length).toBe(1);
       expect(chk[1].children[0].key).toBe("00b");
     });
-    it("filters deeply nested entries in an object", () => {
-      const predicate = (link: any) => link.status !== "not started";
-      const object = {
+    it("filters deeply nested arrays in an object", () => {
+      const predicate = (link: any) => link.status !== "not-started";
+      const test = {
         key: "001",
-        status: "in progress",
+        status: "in-progress",
         children: [
           {
             key: "00a",
-            status: "not started",
+            status: "not-started",
           },
           {
             key: "00b",
-            status: "in progress",
+            status: "in-progress",
             children: [
               {
                 key: "00c",
-                status: "not started",
+                status: "not-started",
               },
               {
                 key: "00d",
@@ -83,11 +83,35 @@ describe("deepFilter:", () => {
         ],
       };
 
-      const chk = deepFilter(object, predicate);
+      const chk = deepFilter(test, predicate);
 
-      expect(chk.status).toBe("in progress");
+      expect(chk.status).toBe("in-progress");
       expect(chk.children.length).toBe(1);
       expect(chk.children[0].children.length).toBe(1);
+    });
+    it("filters deeply nested objects in an object", () => {
+      const predicate = (link: any) => link.status !== "complete";
+      const test = {
+        status: "in-progress",
+        level1: {
+          status: "in-progress",
+          level2: {
+            status: "in-progress",
+            level3a: {
+              status: "complete",
+            },
+            level3b: {
+              status: "not-started",
+            },
+          },
+        },
+      };
+
+      const chk = deepFilter(test, predicate);
+
+      expect(chk.status).toBe("in-progress");
+      expect(chk.level1.level2.level3a).toBeUndefined();
+      expect(chk.level1.level2.level3b.status).toBe("not-started");
     });
     it("skips dates", () => {
       const test = new Date();
