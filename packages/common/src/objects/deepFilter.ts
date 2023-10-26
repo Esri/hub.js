83bc124e8d51ac7f19b0825e4f39894e2ac646f0
juliannemarik <julianne.marik@gmail.com>
2023-10-26 16:48:12 -0500

    chore: add example to util comment

--- packages/common/src/objects/deepFilter.ts
+++ packages/common/src/objects/deepFilter.ts
@@ -4,6 +4,47 @@ import { setProp } from "./set-prop";
 /**
  * Traverse a graph filtering out entries that do
  * not pass the predicate
+ *
+ * example:
+ * const predicate = (link: any) => link.key !== "00a"
+ * const obj = [
+ *   {
+ *     key: "001",
+ *     label: "Stop the Spotted Lanternfly",
+ *   },
+ *   {
+ *     key: "002",
+ *     label: "Create a Map",
+ *     children: [
+ *       {
+ *         key: "00a",
+ *         label: "ArcGIS Map Viewer",
+ *       },
+ *       {
+ *         key: "00b",
+ *         label: "ArcGIS Map Viewer Classic",
+ *       },
+ *     ],
+ *   },
+ * ];
+ * const res = deepFilter(obj, predicate)
+ * res = [
+ *   {
+ *     key: "001",
+ *     label: "Stop the Spotted Lanternfly",
+ *   },
+ *   {
+ *     key: "002",
+ *     label: "Create a Map",
+ *     children: [
+ *       {
+ *         key: "00b",
+ *         label: "ArcGIS Map Viewer Classic",
+ *       },
+ *     ],
+ *   },
+ * ];
+ *
  * @param object
  * @param predicate
  */
@@ -25,11 +66,13 @@ export function deepFilter(
     }, []);
   } else if (isFindable(object)) {
     return Object.keys(object).reduce((acc, entry) => {
-      if (isFindable(object[entry])) {
-        const filteredEntry = deepFilter(object[entry], predicate);
-        setProp(entry, filteredEntry, acc);
-      } else {
-        setProp(entry, object[entry], acc);
+      if (predicate(object[entry])) {
+        if (isFindable(object[entry])) {
+          const filteredEntry = deepFilter(object[entry], predicate);
+          setProp(entry, filteredEntry, acc);
+        } else {
+          setProp(entry, object[entry], acc);
+        }
       }
       return acc;
     }, {});
