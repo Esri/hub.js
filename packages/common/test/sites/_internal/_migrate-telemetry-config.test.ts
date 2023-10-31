a00818b75338f3bacfde0da6a6181222d870367b
Mike Juniper <mjuniper@esri.com>
2023-10-31 13:17:21 -0600

    fix: add data.telemetry to site draft include list; redo telemetry config migration (#1306)
    affects: @esri/hub-common, @esri/hub-sites

--- packages/common/test/sites/_internal/_migrate-telemetry-config.test.ts
+++ packages/common/test/sites/_internal/_migrate-telemetry-config.test.ts
@@ -2,23 +2,23 @@ import { IModel } from "../../../src";
 import { _migrateTelemetryConfig } from "../../../src/sites/_internal/_migrate-telemetry-config";
 
 describe("_migrateTelemetryConfig", () => {
-  it("Bumps the item.properties.schemaVersion if schemaVersion is < 1.7", () => {
+  it("Bumps the item.properties.schemaVersion if schemaVersion is < 1.8", () => {
     const siteModel = {
-      item: { properties: { schemaVersion: 1.6 } },
+      item: { properties: { schemaVersion: 1.7 } },
       data: { values: {} },
     } as unknown as IModel;
     const result = _migrateTelemetryConfig(siteModel);
     expect(result.item.properties.schemaVersion).toEqual(
-      1.7,
-      "site.item.properties.schemaVersion should be 1.7"
+      1.8,
+      "site.item.properties.schemaVersion should be 1.8"
     );
   });
 
-  it("Does not run the migration if schemaVersion is >= 1.7", () => {
+  it("Does not run the migration if schemaVersion is >= 1.8", () => {
     const siteModel = {
       item: {
         properties: {
-          schemaVersion: 1.7,
+          schemaVersion: 1.8,
         },
       },
     } as unknown as IModel;
@@ -29,7 +29,7 @@ describe("_migrateTelemetryConfig", () => {
 
   it("should apply migration if schemaVersion < newSchemaVersion", () => {
     const model = {
-      item: { properties: { schemaVersion: 1.6 } },
+      item: { properties: { schemaVersion: 1.7 } },
       data: {
         values: {
           capabilities: ["consentNotice"],
@@ -38,7 +38,7 @@ describe("_migrateTelemetryConfig", () => {
       },
     };
     const result = _migrateTelemetryConfig(model);
-    expect(result.item.properties.schemaVersion).toEqual(1.7);
+    expect(result.item.properties.schemaVersion).toEqual(1.8);
     expect(
       (result.data as any).telemetry.consentNotice.allowPrivacyConfig
     ).toEqual(true);
