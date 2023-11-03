2a0a9f8fbab2f7be0ef5a4c3b0d2ae7b3a0accff
juliannemarik <julianne.marik@gmail.com>
2023-11-02 22:28:31 -0500

    chore: add type comments

--- packages/common/src/associations/types.ts
+++ packages/common/src/associations/types.ts
@@ -1,13 +1,29 @@
 import { HubEntityType } from "../core";
 import { IQuery } from "../search";
 
-export interface IHubAssociationRules {
-  /** schema version for migration purposes */
-  schemaVersion: number;
-  query: IQuery;
-}
-
+/**
+ * associations are hierarchical in nature e.g.
+ * there is always a parent and a child in the
+ * relationship. This interface allows us to
+ * define these hierarchies on an entity-by-entity
+ * basis
+ */
 export interface IHubAssociationHierarchy {
   children: HubEntityType[];
   parents: HubEntityType[];
+}
+
+/**
+ * rules stored on the parent entity in an
+ * association that define what is "included"
+ *
+ * For now, the query defines an association
+ * group and "included" simply means it has
+ * been shared with the association group
+ */
+export interface IHubAssociationRules {
+  /** schema version for migration purposes */
+  schemaVersion: number;
+  /** */
+  query: IQuery;
 }
