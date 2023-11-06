a7a47701254dfe68b38a80d087977b9b2d6de6ce
bri12416 <bri12416@esri.com>
2023-11-06 15:30:43 -0600

    feat(hub-discussions): updated canModerateChannel for v2

--- packages/discussions/src/utils/channels/can-modify-channel.ts
+++ packages/discussions/src/utils/channels/can-modify-channel.ts
@@ -1,6 +1,7 @@
-import { IUser } from "@esri/arcgis-rest-types";
+import { IGroup, IUser } from "@esri/arcgis-rest-types";
 import { IChannel, IDiscussionsUser, SharingAccess } from "../../types";
 import { ChannelPermission } from "../channel-permission";
+import { isOrgAdmin } from "../platform";
 
 const ADMIN_GROUP_ROLES = Object.freeze(["owner", "admin"]);
 
@@ -14,8 +15,73 @@ export function canModifyChannel(
   channel: IChannel,
   user: IUser | IDiscussionsUser = {}
 ): boolean {
-  const { channelAcl } = channel;
+  const { channelAcl, creator } = channel;
 
-  const channelPermission = new ChannelPermission(channelAcl);
-  return channelPermission.canModerateChannel(user as IDiscussionsUser);
+  if (channelAcl) {
+    const channelPermission = new ChannelPermission(channelAcl);
+    return channelPermission.canModerateChannel(user as IDiscussionsUser);
+  }
+
+  return isAuthorizedToModifyChannelByLegacyPermissions(user, channel);
+}
+
+function isAuthorizedToModifyChannelByLegacyPermissions(
+  user: IUser,
+  channel: IChannel
+): boolean {
+  const { username, groups: userGroups = [] } = user;
+  const {
+    access,
+    groups: channelGroups = [],
+    orgs: channelOrgs = [],
+    creator: channelCreator,
+  } = channel;
+
+  // ensure authenticated
+  if (!username) {
+    return false;
+  }
+
+  if (username === channelCreator) {
+    return true;
+  }
+
+  if (access === SharingAccess.PRIVATE) {
+    return isAuthorizedToModifyChannelByLegacyGroup(channelGroups, userGroups);
+  }
+
+  // public or org access
+  return (
+    isAuthorizedToModifyChannelByLegacyGroup(channelGroups, userGroups) ||
+    isChannelOrgAdmin(channelOrgs, user)
+  );
+}
+
+/**
+ * Ensure the user is an owner/admin of one of the channel groups
+ */
+function isAuthorizedToModifyChannelByLegacyGroup(
+  channelGroups: string[],
+  userGroups: IGroup[]
+) {
+  return channelGroups.some((channelGroupId: string) => {
+    return userGroups.some((group: IGroup) => {
+      const {
+        id: userGroupId,
+        userMembership: { memberType: userMemberType },
+      } = group;
+
+      return (
+        channelGroupId === userGroupId &&
+        ADMIN_GROUP_ROLES.includes(userMemberType)
+      );
+    });
+  });
+}
+
+function isChannelOrgAdmin(
+  channelOrgs: string[],
+  user: IUser | IDiscussionsUser
+): boolean {
+  return isOrgAdmin(user) && channelOrgs.includes(user.orgId);
 }
