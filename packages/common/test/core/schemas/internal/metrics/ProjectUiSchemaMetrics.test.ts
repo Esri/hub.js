16335c808690e5c3e48f7ed8df51d58523c7ea89
Jordan Sanz <jsanz@esri.com>
2023-11-09 09:36:39 -0500

    test(): update uiSchema metric test

--- packages/common/test/core/schemas/internal/metrics/ProjectUiSchemaMetrics.test.ts
+++ packages/common/test/core/schemas/internal/metrics/ProjectUiSchemaMetrics.test.ts
@@ -11,14 +11,47 @@ describe("buildUiSchema: metric", () => {
       elements: [
         {
           type: "Section",
-          labelKey: `some.scope.details.sectionTitle`,
+          labelKey: `some.scope.uiSchema.flowTitle.sectionTitle`,
           options: {
             section: "block",
+            helperText: `some.scope.uiSchema.flowTitle.description`,
           },
           elements: [
             {
-              labelKey: `some.scope.details.title`,
-              scope: "/properties/cardTitle",
+              labelKey: `some.scope.uiSchema.cardTitle.title`,
+              scope: "/properties/_metrics/cardTitle",
+              type: "Control",
+            },
+            {
+              labelKey: `some.scope.uiSchema.value.title`,
+              scope: "/properties/_metrics/value",
+              type: "Control",
+            },
+            {
+              labelKey: `some.scope.uiSchema.units.title`,
+              scope: "/properties/_metrics/units",
+              type: "Control",
+              options: {
+                helperText: {
+                  labelKey: `some.scope.uiSchema.units.helperText`,
+                  placement: "bottom",
+                },
+              },
+            },
+            {
+              labelKey: `some.scope.uiSchema.unitPosition.title`,
+              scope: "/properties/_metrics/unitPosition",
+              type: "Control",
+              options: {
+                control: "hub-field-input-select",
+                enum: {
+                  i18nScope: `some.scope.uiSchema.unitPosition.enum`,
+                },
+              },
+            },
+            {
+              labelKey: `some.scope.uiSchema.trailingText.title`,
+              scope: "/properties/_metrics/trailingText",
               type: "Control",
             },
           ],
