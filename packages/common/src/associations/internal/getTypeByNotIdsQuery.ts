5ecd5d093aefdee0679f4a9c96a7068ce9ab1a67
juliannemarik <julianne.marik@gmail.com>
2023-12-19 14:27:45 -0600

    feat: add new getTypeByNotIdsQuery util

---
+++ packages/common/src/associations/internal/getTypeByNotIdsQuery.ts
@@ -0,0 +1,38 @@
+import { getEntityTypeFromType } from "../../search/_internal/getEntityTypeFromType";
+import { IQuery } from "../../search/types/IHubCatalog";
+
+/**
+ * @private
+ * Construct an IQuery to fetch the inverse of a specified set
+ * of item id(s) by type(s). Note: if an array of types is provided,
+ * they must be the same underlying target entity type.
+ *
+ * @param itemType - a single item type or an array of item types
+ * @param ids - an array of ids
+ * @returns {IQuery}
+ */
+export function getTypeByNotIdsQuery(
+  itemType: string | string[],
+  ids: string[]
+): IQuery {
+  const targetEntity =
+    typeof itemType === "string"
+      ? getEntityTypeFromType(itemType)
+      : getEntityTypeFromType(itemType[0]);
+
+  const qry: IQuery = {
+    targetEntity,
+    filters: [
+      {
+        operation: "AND",
+        predicates: [
+          {
+            type: itemType,
+            id: { not: ids },
+          },
+        ],
+      },
+    ],
+  };
+  return qry;
+}
