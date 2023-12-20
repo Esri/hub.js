5821e7ee9e0c06c2f1ad1f51d717a78637c56859
juliannemarik <julianne.marik@gmail.com>
2023-12-20 15:29:02 -0600

    feat: add internal setAssociationKeyword util

---
+++ packages/common/src/associations/internal/setAssociationKeyword.ts
@@ -0,0 +1,24 @@
+import { HubEntityType } from "../../core/types";
+
+/**
+ * when a child sends an "outgoing" request or accepts an
+ * "incoming" request for association, the child "references"
+ * the parent via a typeKeyword (ref|<type>|<id>)
+ *
+ * @param typeKeywords - the child entity's typeKeywords
+ * @param type - the parent entity's type
+ * @param id - the parent entity's id
+ * @returns {string[]}
+ */
+export function setAssociationKeyword(
+  typeKeywords: string[] = [],
+  type: HubEntityType,
+  id: string
+): string[] {
+  const keyword = `ref|${type}|${id}`;
+  if (!typeKeywords.includes(keyword)) {
+    typeKeywords = [...typeKeywords, keyword];
+  }
+
+  return typeKeywords;
+}
