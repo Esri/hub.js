b4c2273d15db6d8914d45b38cc01c52d359d0ced
juliannemarik <julianne.marik@gmail.com>
2023-12-20 12:54:15 -0600

    chore: improve IWithAssociations comment

--- packages/common/src/core/traits/IWithAssociations.ts
+++ packages/common/src/core/traits/IWithAssociations.ts
@@ -3,7 +3,12 @@ import { IHubAssociationRules } from "../../associations";
 /** properties for entities with associations */
 export interface IWithAssociations {
   associations?: {
-    /** association group id */
+    /**
+     * association group id - this is exposed as
+     * its own property for convenience. It is
+     * also included in the association rules
+     * query as a group predicate.
+     */
     groupId: string;
     /** association rules */
     rules: IHubAssociationRules;
