2fce66edce6f09fe120c037858d61024476a8a17
Dave Bouwman <dave@davebouwman.com>
2024-01-10 13:12:55 -0700

    fix: migrate malformed basemap hash resulting from hub.py bug

--- packages/common/src/sites/upgrade-site-schema.ts
+++ packages/common/src/sites/upgrade-site-schema.ts
@@ -10,15 +10,14 @@ import { _migrateFeedConfig } from "./_internal/_migrate-feed-config";
 import { _migrateEventListCardConfigs } from "./_internal/_migrate-event-list-card-configs";
 import { migrateLegacyCapabilitiesToFeatures } from "./_internal/capabilities/migrateLegacyCapabilitiesToFeatures";
 import { _migrateTelemetryConfig } from "./_internal/_migrate-telemetry-config";
+import { migrateBadBasemap } from "./_internal/migrateBadBasemap";
 
 /**
  * Upgrades the schema upgrades
  * @param model IModel
  */
 export function upgradeSiteSchema(model: IModel) {
-  if (getProp(model, "item.properties.schemaVersion") === SITE_SCHEMA_VERSION) {
-    return model;
-  } else {
+  if (getProp(model, "item.properties.schemaVersion") !== SITE_SCHEMA_VERSION) {
     // apply upgrade functions in order...
     model = _applySiteSchema(model);
     model = _enforceLowercaseDomains(model);
@@ -29,10 +28,33 @@ export function upgradeSiteSchema(model: IModel) {
     model = _migrateEventListCardConfigs(model);
     model = migrateLegacyCapabilitiesToFeatures(model);
     model = _migrateTelemetryConfig(model);
-
-    // WARNING - If you are writing a site schema migration,
-    // you probably need to apply it to site drafts as well!
-    // See https://github.com/Esri/hub.js/issues/498 for more details.
-    return model;
   }
+
+  // apply versionless migrations
+  model = migrateBadBasemap(model);
+  return model;
+
+  // if (getProp(model, "item.properties.schemaVersion") === SITE_SCHEMA_VERSION) {
+  //   // still apply versionless migrations
+  //   model = migrateBadBasemap(model);
+  //   return model;
+  // } else {
+  //   // apply upgrade functions in order...
+  //   model = _applySiteSchema(model);
+  //   model = _enforceLowercaseDomains(model);
+  //   model = _ensureCatalog(model);
+  //   model = _purgeNonGuidsFromCatalog(model);
+  //   model = _ensureTelemetry<IModel>(model);
+  //   model = _migrateFeedConfig(model);
+  //   model = _migrateEventListCardConfigs(model);
+  //   model = migrateLegacyCapabilitiesToFeatures(model);
+  //   model = _migrateTelemetryConfig(model);
+  //   // versionless migrations
+  //   model = migrateBadBasemap(model);
+
+  //   // WARNING - If you are writing a site schema migration,
+  //   // you probably need to apply it to site drafts as well!
+  //   // See https://github.com/Esri/hub.js/issues/498 for more details.
+  //   return model;
+  // }
 }
