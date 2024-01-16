5e264f333d4c89e7924ab9f5d20c760476c12817
Jordan Sanz <jsanz@esri.com>
2024-01-16 18:32:14 -0500

    fix(): update subsetSchema

--- packages/common/src/core/schemas/internal/subsetSchema.ts
+++ packages/common/src/core/schemas/internal/subsetSchema.ts
@@ -14,15 +14,40 @@ export function subsetSchema(
   const subset: IConfigurationSchema = cloneObject(schema);
 
   // 1. remove un-specified properties from the "required" array
-  subset.required = [...subset.required].filter((required) =>
-    props.includes(required)
-  );
+  if (subset.required) {
+    subset.required = [...subset.required].filter((required) =>
+      props.includes(required)
+    );
+  }
 
   // 2. filter the rest of the schema down to the specified properties
-  Object.keys(subset.properties).forEach((key) => {
-    if (props.indexOf(key) === -1) {
-      delete subset.properties[key];
-    }
-  });
+  if (subset.properties) {
+    Object.keys(subset.properties).forEach((key) => {
+      if (props.indexOf(key) === -1) {
+        delete subset.properties[key];
+      }
+    });
+  }
+
+  // 3. filter schema compositions (anyOf, allOf, oneOf) down
+  // to specified properties
+
+  if (subset.allOf) {
+    subset.allOf = subset.allOf.map((s) =>
+      subsetSchema(s as IConfigurationSchema, props)
+    );
+  }
+
+  if (subset.if) {
+    subset.if = subsetSchema(subset.if as IConfigurationSchema, props);
+  }
+
+  if (subset.then) {
+    subset.then = subsetSchema(subset.then as IConfigurationSchema, props);
+  }
+
+  if (subset.else) {
+    subset.else = subsetSchema(subset.else as IConfigurationSchema, props);
+  }
   return subset;
 }
--- packages/common/test/core/schemas/internal/filterSchemaToUiSchema.test.ts
+++ packages/common/test/core/schemas/internal/filterSchemaToUiSchema.test.ts
@@ -6,57 +6,151 @@ import {
 
 describe("filterSchemaToUiSchema util:", () => {
   it("returns schema limited to props in UiSchema", () => {
-    const chk = filterSchemaToUiSchema(schema, SteppedUiSchema);
-    expect(chk.properties?.description).not.toBeDefined();
-  });
-});
-
-const schema: IConfigurationSchema = {
-  required: ["name"],
-  type: "object",
-  properties: {
-    name: {
-      type: "string",
-      minLength: 1,
-      maxLength: 250,
-    },
-    summary: {
-      type: "string",
-    },
-    description: {
-      type: "string",
-    },
-  },
-};
-
-const SteppedUiSchema: IUiSchema = {
-  type: "Layout",
-  elements: [
-    {
-      type: "Section",
-      labelKey: "section1.label.key",
-      options: {
-        section: "stepper",
-        open: true,
+    const schema: IConfigurationSchema = {
+      required: ["name"],
+      type: "object",
+      properties: {
+        name: {
+          type: "string",
+          minLength: 1,
+          maxLength: 250,
+        },
+        summary: {
+          type: "string",
+        },
+        description: {
+          type: "string",
+        },
       },
+    };
+
+    const SteppedUiSchema: IUiSchema = {
+      type: "Layout",
       elements: [
         {
-          type: "Step",
-          labelKey: "step1.label.key",
+          type: "Section",
+          labelKey: "section1.label.key",
+          options: {
+            section: "stepper",
+            open: true,
+          },
           elements: [
             {
-              labelKey: "property1.label.key",
-              scope: "/properties/name",
-              type: "Control",
-            },
-            {
-              labelKey: "property2.label.key",
-              scope: "/properties/summary",
-              type: "Control",
+              type: "Step",
+              labelKey: "step1.label.key",
+              elements: [
+                {
+                  labelKey: "property1.label.key",
+                  scope: "/properties/name",
+                  type: "Control",
+                },
+                {
+                  labelKey: "property2.label.key",
+                  scope: "/properties/summary",
+                  type: "Control",
+                },
+              ],
             },
           ],
         },
       ],
-    },
-  ],
-};
+    };
+    const chk = filterSchemaToUiSchema(schema, SteppedUiSchema);
+    expect(chk.properties?.description).not.toBeDefined();
+  });
+
+  it("returns schema limited to props in UiSchema when there is a conditional that does not apply", () => {
+    const schema: IConfigurationSchema = {
+      required: ["title"],
+      properties: {
+        status: {
+          type: "string",
+        },
+        groups: {
+          type: "array",
+        },
+        title: {
+          type: "string",
+        },
+        _metric: {
+          type: "object",
+          properties: {
+            type: {
+              type: "string",
+              enum: ["static", "dynamic"],
+            },
+            value: {
+              type: "string",
+            },
+          },
+        },
+      },
+      allOf: [
+        {
+          if: {
+            properties: {
+              _metric: {
+                properties: {
+                  type: {
+                    const: "static",
+                  },
+                },
+              },
+            },
+          },
+          then: {
+            properties: {
+              _metric: {
+                required: ["value"],
+              },
+            },
+          },
+        },
+      ],
+    };
+
+    const uiSchema: IUiSchema = {
+      type: "Layout",
+      elements: [
+        {
+          type: "Control",
+          scope: "/properties/title",
+          labelKey: "title",
+        },
+        {
+          type: "Control",
+          scope: "/properties/status",
+          labelKey: "status",
+        },
+        {
+          type: "Control",
+          scope: "/properties/groups",
+          labelKey: "groups",
+        },
+      ],
+    };
+
+    const chk = filterSchemaToUiSchema(schema, uiSchema);
+    expect(chk).toEqual({
+      required: ["title"],
+      properties: {
+        status: {
+          type: "string",
+        },
+        groups: {
+          type: "array",
+        },
+        title: {
+          type: "string",
+        },
+      },
+      allOf: [
+        {
+          if: { properties: {} },
+          then: { properties: {} },
+        },
+      ],
+    });
+    expect(chk.properties?._metric).not.toBeDefined();
+  });
+});
