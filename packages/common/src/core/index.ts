d8855a9c58fbd5a79588ff3f80053a83f652c8b0
Jordan Sanz <jsanz@esri.com>
2024-01-31 16:35:44 -0500

    fix(): put getEntityDefaultWorkspacePane back

--- packages/common/src/core/index.ts
+++ packages/common/src/core/index.ts
@@ -3,6 +3,7 @@ export * from "./types";
 export * from "./behaviors";
 export * from "./schemas";
 export * from "./fetchHubEntity";
+export * from "./getEntityDefaultWorkspacePane";
 export * from "./getTypeFromEntity";
 export * from "./getTypesFromEntityType";
 export * from "./getRelativeWorkspaceUrl";
