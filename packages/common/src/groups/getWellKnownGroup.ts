0f24d606e2ac764d3223fd23205f41ce70cd28a7
Ben Stoltz <bstoltz@esri.com>
2024-02-16 10:07:27 -0500

    feat: change up getWellKnownGroup a bit

--- packages/common/src/groups/getWellKnownGroup.ts
+++ packages/common/src/groups/getWellKnownGroup.ts
@@ -1,7 +1,8 @@
-import { WellKnownGroups } from "./_internal/WellKnownGroups";
-import { cloneObject } from "../util";
 import { IHubGroup } from "../core/types/IHubGroup";
+import { checkPermission } from "../permissions";
+import { IArcGISContext } from "../ArcGISContext";
 
+type WellKnownGroup = "hubViewGroup" | "hubEditGroup" | "hubFollowersGroup";
 /**
  * Fetches a well known group template based on a name
  *
@@ -9,8 +10,46 @@ import { IHubGroup } from "../core/types/IHubGroup";
  * @returns Group template
  */
 export function getWellKnownGroup(
-  groupType: keyof typeof WellKnownGroups
+  groupType: WellKnownGroup,
+  context: IArcGISContext
 ): Partial<IHubGroup> {
-  // return the group by name
-  return cloneObject(WellKnownGroups[groupType]);
+  const configs: Record<WellKnownGroup, Partial<IHubGroup>> = {
+    hubViewGroup: {
+      access: "org",
+      autoJoin: false,
+      isInvitationOnly: false,
+      hiddenMembers: false,
+      isViewOnly: true,
+      tags: ["Hub Group"],
+      membershipAccess: checkPermission(
+        "platform:portal:user:addExternalMembersToGroup",
+        context
+      ).access
+        ? "anyone"
+        : "organization",
+    },
+    hubEditGroup: {
+      access: "org",
+      autoJoin: false,
+      isSharedUpdate: true,
+      isInvitationOnly: false,
+      hiddenMembers: false,
+      isViewOnly: true,
+      tags: ["Hub Group"],
+      membershipAccess: checkPermission(
+        "platform:portal:user:addExternalMembersToGroup",
+        context
+      ).access
+        ? "collaborators"
+        : "organization",
+    },
+    hubFollowersGroup: {
+      access: "public",
+      autoJoin: true,
+      isInvitationOnly: false,
+      isViewOnly: true,
+    },
+  };
+
+  return configs[groupType];
 }
--- packages/common/test/groups/getWellKnownGroup.test.ts
+++ packages/common/test/groups/getWellKnownGroup.test.ts
@@ -1,13 +1,101 @@
 import { getWellKnownGroup } from "../../src/groups/getWellKnownGroup";
-import { WellKnownGroups } from "../../src/groups/_internal/WellKnownGroups";
+import { MOCK_CONTEXT, MOCK_AUTH } from "../mocks/mock-auth";
+import { ArcGISContext, IArcGISContext } from "../../src/ArcGISContext";
 
 describe("getWellKnownGroup: ", () => {
-  it("returns a well known group template based on a name", () => {
-    Object.keys(WellKnownGroups).forEach((key) => {
-      const result = getWellKnownGroup(key as keyof typeof WellKnownGroups);
-      expect(result).toEqual(
-        WellKnownGroups[key as keyof typeof WellKnownGroups]
-      );
+  const MOCK_CONTEXT_WITH_PRIVILEGES = new ArcGISContext({
+    id: 123,
+    currentUser: {
+      username: "mock_user",
+      favGroupId: "456abc",
+      orgId: "789def",
+      privileges: ["portal:user:addExternalMembersToGroup"],
+    },
+    portalUrl: "https://qaext.arcgis.com",
+    hubUrl: "https://hubqa.arcgis.com",
+    authentication: MOCK_AUTH,
+    portalSelf: {
+      id: "123",
+      name: "My org",
+      isPortal: false,
+      urlKey: "www",
+    },
+    serviceStatus: {
+      portal: "online",
+      discussions: "online",
+      events: "online",
+      metrics: "online",
+      notifications: "online",
+      "hub-search": "online",
+      domains: "online",
+    },
+    userHubSettings: {
+      schemaVersion: 1,
+    },
+  }) as IArcGISContext;
+  it("returns a followers group", () => {
+    const resp = getWellKnownGroup("hubFollowersGroup", MOCK_CONTEXT);
+    expect(resp).toEqual({
+      access: "public",
+      autoJoin: true,
+      isInvitationOnly: false,
+      isViewOnly: true,
+    });
+  });
+  it("returns a view group", () => {
+    const resp = getWellKnownGroup("hubViewGroup", MOCK_CONTEXT);
+    expect(resp).toEqual({
+      access: "org",
+      autoJoin: false,
+      isInvitationOnly: false,
+      hiddenMembers: false,
+      isViewOnly: true,
+      tags: ["Hub Group"],
+      membershipAccess: "organization",
+    });
+  });
+  it("returns a view group with membershipAccess set to anyone", () => {
+    const resp = getWellKnownGroup(
+      "hubViewGroup",
+      MOCK_CONTEXT_WITH_PRIVILEGES
+    );
+    expect(resp).toEqual({
+      access: "org",
+      autoJoin: false,
+      isInvitationOnly: false,
+      hiddenMembers: false,
+      isViewOnly: true,
+      tags: ["Hub Group"],
+      membershipAccess: "anyone",
+    });
+  });
+  it("returns an edit group", () => {
+    const resp = getWellKnownGroup("hubEditGroup", MOCK_CONTEXT);
+    expect(resp).toEqual({
+      access: "org",
+      autoJoin: false,
+      isSharedUpdate: true,
+      isInvitationOnly: false,
+      hiddenMembers: false,
+      isViewOnly: true,
+      tags: ["Hub Group"],
+      membershipAccess: "organization",
+    });
+  });
+  it("returns an edit group with membershipAccess set to collaborators", () => {
+    const resp = getWellKnownGroup(
+      "hubEditGroup",
+      MOCK_CONTEXT_WITH_PRIVILEGES
+    );
+    expect(resp).toEqual({
+      access: "org",
+      autoJoin: false,
+      isSharedUpdate: true,
+      isInvitationOnly: false,
+      hiddenMembers: false,
+      isViewOnly: true,
+      tags: ["Hub Group"],
+      membershipAccess: "collaborators",
     });
   });
 });
