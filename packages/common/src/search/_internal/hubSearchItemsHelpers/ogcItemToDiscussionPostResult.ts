9923f7e3e11d3a175fa4e03ebc281c5755147cb2
drspacemanphd <markabrown87@gmail.com>
2024-03-06 13:00:55 -0500

    feat(hub-common): added comments
    affects: @esri/hub-common

--- packages/common/src/search/_internal/hubSearchItemsHelpers/ogcItemToDiscussionPostResult.ts
+++ packages/common/src/search/_internal/hubSearchItemsHelpers/ogcItemToDiscussionPostResult.ts
@@ -2,6 +2,14 @@ import { IOgcItem } from "./interfaces";
 import { IPost } from "../../../discussions";
 import { IHubSearchResult } from "../../types";
 
+/**
+ * This method is responsible for converting an OGC item whose properties
+ * represent an IPost into an IHubSearchResult. Although some fields do not
+ * apply, this is being done such that result of a discussion post search
+ * can automatically be used in a gallery.
+ * @param ogcItem
+ * @returns IHubSearchResult
+ */
 export async function ogcItemToDiscussionPostResult(
   ogcItem: IOgcItem
 ): Promise<IHubSearchResult> {
