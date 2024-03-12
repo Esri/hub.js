5157a01ce529234cb0b05e2140e36363238d8595
Ethan Spiro <espiro@esri.com>
2024-03-12 11:51:18 -0700

    feat(hub-common): fix awaited type
    affects: @esri/hub-common

--- packages/common/src/newsletters/api/orval/awaited-type.ts
+++ packages/common/src/newsletters/api/orval/awaited-type.ts
@@ -1,14 +1,5 @@
 /**
  * Orval generates return types for functions using utility type Awaited
  * This was introduced in Typescript 4.5, but hub.js is using Typescript 3
- *
- * Recursively unwraps the "awaited type" of a type. Non-promise "thenables" should resolve to `never`. This emulates the behavior of `await`.
  */
-export type Awaited<T> = T extends null | undefined
-  ? T // special case for `null | undefined` when not in `--strictNullChecks` mode
-  : // eslint-disable-next-line @typescript-eslint/ban-types
-  T extends object & { then(onfulfilled: infer F, ...args: infer _): any } // `await` only unwraps object types with a callable `then`. Non-object types are not unwrapped
-  ? F extends (value: infer V, ...args: infer _) => any // if the argument to `then` is callable, extracts the first argument
-    ? Awaited<V> // recursively unwrap the value
-    : never // the argument to `then` was not callable
-  : T; // non-object or non-thenable
+export type Awaited<T> = T extends PromiseLike<infer U> ? U : T;
