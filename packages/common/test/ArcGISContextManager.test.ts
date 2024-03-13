53001b7a4d412777224fd4b75bcf97be5f2057de
Julianna Apicella <42875581+juliannaeapicella@users.noreply.github.com>
2024-03-13 11:07:51 -0400

    feat(hub-common): pr feedback

--- packages/common/test/ArcGISContextManager.test.ts
+++ packages/common/test/ArcGISContextManager.test.ts
@@ -1129,6 +1129,7 @@ describe("ArcGISContext:", () => {
       expect(mgr.context.discussionsServiceUrl).toBeUndefined();
       expect(mgr.context.hubSearchServiceUrl).toBeUndefined();
       expect(mgr.context.domainServiceUrl).toBeUndefined();
+      expect(mgr.context.survey123Url).toEqual("https://survey123.arcgis.com");
       expect(mgr.context.hubLicense).toBe("enterprise-sites");
     });
     it("verify props when passed session", async () => {
@@ -1153,6 +1154,7 @@ describe("ArcGISContext:", () => {
         MOCK_ENTERPRISE_AUTH.portal.replace(`/sharing/rest`, "")
       );
       expect(mgr.context.sharingApiUrl).toBe(MOCK_ENTERPRISE_AUTH.portal);
+      expect(mgr.context.survey123Url).toEqual("https://survey123.arcgis.com");
       // Partnered orgs
       expect(mgr.context.trustedOrgIds).toEqual([]);
       expect(mgr.context.trustedOrgs).toEqual([]);
@@ -1182,10 +1184,11 @@ describe("ArcGISContext:", () => {
       expect(mgr.context.portalUrl).toBe(
         MOCK_ENTERPRISE_AUTH.portal.replace(`/sharing/rest`, "")
       );
+      expect(mgr.context.survey123Url).toEqual("https://survey123.arcgis.com");
     });
   });
   describe("extra coverage:", () => {
-    it("handles qaext hubUrl", async () => {
+    it("handles devext urls", async () => {
       const mgr = await ArcGISContextManager.create({
         portalUrl: "https://qaext.arcgis.com",
       });
@@ -1194,7 +1197,7 @@ describe("ArcGISContext:", () => {
         "https://survey123qa.arcgis.com"
       );
     });
-    it("handles devext hubUrl", async () => {
+    it("handles devext urls", async () => {
       const mgr = await ArcGISContextManager.create({
         portalUrl: "https://devext.arcgis.com",
       });
@@ -1211,6 +1214,9 @@ describe("ArcGISContext:", () => {
       mgr.clearAuthentication();
       expect(mgr.context.hubUrl).toBe("https://hubqa.arcgis.com");
       expect(mgr.context.portalUrl).toBe("https://qaext.arcgis.com");
+      expect(mgr.context.survey123Url).toEqual(
+        "https://survey123qa.arcgis.com"
+      );
     });
     it("sign out on dev, resets portalUrl correctly", async () => {
       const mgr = await ArcGISContextManager.create({
@@ -1220,6 +1226,9 @@ describe("ArcGISContext:", () => {
       mgr.clearAuthentication();
       expect(mgr.context.hubUrl).toBe("https://hubdev.arcgis.com");
       expect(mgr.context.portalUrl).toBe("https://devext.arcgis.com");
+      expect(mgr.context.survey123Url).toEqual(
+        "https://survey123dev.arcgis.com"
+      );
     });
     it("handles ArcGISContext properties undefined", () => {
       const ctx = new ArcGISContext({
