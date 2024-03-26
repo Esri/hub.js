9dce7d013eab32511cca225a38ffc21b9ecca5b2
Ben Stoltz <bstoltz@esri.com>
2024-03-26 07:32:51 -0400

    fix: initiatives can be edited by premium and basic users (#1443)

--- packages/common/src/initiatives/_internal/InitiativeBusinessRules.ts
+++ packages/common/src/initiatives/_internal/InitiativeBusinessRules.ts
@@ -64,9 +64,10 @@ export const InitiativePermissionPolicies: IPermissionPolicy[] = [
   },
   {
     permission: "hub:initiative:edit",
-    dependencies: ["hub:initiative"],
+    services: ["portal"],
     authenticated: true,
     entityEdit: true,
+    licenses: ["hub-premium", "hub-basic"],
   },
   {
     permission: "hub:initiative:delete",
