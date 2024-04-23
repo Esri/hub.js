d17291030e4cd4683c8636652cc8884762fe532f
Ben Stoltz <bstoltz@esri.com>
2024-04-23 07:46:17 -0400

    feat: initiative workspace gated to alpha, but open to prod

--- packages/common/src/initiatives/_internal/InitiativeBusinessRules.ts
+++ packages/common/src/initiatives/_internal/InitiativeBusinessRules.ts
@@ -121,10 +121,10 @@ export const InitiativePermissionPolicies: IPermissionPolicy[] = [
   {
     permission: "hub:initiative:workspace",
     dependencies: ["hub:feature:workspace"],
-    environments: ["devext", "qaext"],
   },
   {
     permission: "hub:initiative:workspace:overview",
+    availability: ["alpha"],
     dependencies: ["hub:initiative:workspace", "hub:initiative:view"],
   },
   {
