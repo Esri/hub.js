d9ea92aa7891f812f867769f47682ddd374e65f1
Mike Juniper <mjuniper@esri.com>
2024-04-25 09:45:54 -0600

    fix: isDiscussable handles undefined subject (#1486)

--- packages/common/src/sites/HubSite.ts
+++ packages/common/src/sites/HubSite.ts
@@ -428,11 +428,7 @@ export class HubSite
     );
 
     const followersGroup = await this.getFollowersGroup();
-    setProp(
-      "_followers.isDiscussable",
-      isDiscussable(followersGroup || {}),
-      editor
-    );
+    setProp("_followers.isDiscussable", isDiscussable(followersGroup), editor);
 
     editor._discussions = this.entity.features["hub:site:feature:discussions"];
 
