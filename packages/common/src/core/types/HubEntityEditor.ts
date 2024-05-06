61a48f4b6082e93862d1e751e482190dfa577044
Jordan Sanz <jsanz@esri.com>
2024-05-06 08:46:07 -0400

    feat(): remove default groups (#1483)

--- packages/common/src/core/types/HubEntityEditor.ts
+++ packages/common/src/core/types/HubEntityEditor.ts
@@ -7,11 +7,6 @@ export type HubEntityEditor = Record<string, any>; // IHubProjectEditor; // | IH
  * Additional context that can be passed to `toEditor()` functions
  */
 export interface IEntityEditorContext {
-  // the concept of core/content groups might be going away in
-  // the future, but we'll include for now
-  collaborationGroupId?: string;
-  contentGroupId?: string;
-
   // represents the current metric id being edited in the editor experience.
   metricId?: string;
 }
