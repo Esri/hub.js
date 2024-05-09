ac3ac160d4fcb3999832a80270dd3e6264d63f64
Caleb Pomar <cpsharksfan@gmail.com>
2024-05-09 16:18:13 -0600

    feat(hub-common): show disabled scheduler setting with advisory notice for private items
    affects: @esri/hub-common

--- packages/common/src/content/_internal/ContentUiSchemaSettings.ts
+++ packages/common/src/content/_internal/ContentUiSchemaSettings.ts
@@ -1,7 +1,12 @@
 import { checkPermission } from "../..";
 import { IArcGISContext } from "../../ArcGISContext";
 import { EntityEditorOptions } from "../../core/schemas/internal/EditorOptions";
-import { IUiSchema, UiSchemaRuleEffects } from "../../core/schemas/types";
+import {
+  IUiSchema,
+  IUiSchemaRule,
+  UiSchemaMessageTypes,
+  UiSchemaRuleEffects,
+} from "../../core/schemas/types";
 import { IHubEditableContent } from "../../core/types/IHubEditableContent";
 import { isHostedFeatureServiceEntity } from "../hostedServiceUtils";
 
@@ -22,10 +27,36 @@ export const buildUiSchema = async (
   };
 
   if (
-    checkPermission("hub:content:workspace:settings:schedule", _context)
-      .access &&
-    options.access === "public"
+    checkPermission("hub:content:workspace:settings:schedule", _context).access
   ) {
+    // NOTE: unfortunately we have to add a hidden "access" field so
+    // we can use the value of "access" in other UI Schema rules.
+    const alwaysHiddenRule: IUiSchemaRule = {
+      effect: UiSchemaRuleEffects.SHOW,
+      condition: {
+        scope: "/properties/access",
+        schema: {
+          const: "non-existent-access-type",
+        },
+      },
+    };
+    uiSchema.elements.push({
+      type: "Control",
+      scope: "/properties/access",
+      rule: alwaysHiddenRule,
+    });
+
+    const isNotPublicCondition: IUiSchemaRule["condition"] = {
+      scope: "/properties/access",
+      schema: {
+        enum: ["private", "org", "shared"],
+      },
+    };
+    const disableScheduleRule: IUiSchemaRule = {
+      effect: UiSchemaRuleEffects.DISABLE,
+      condition: isNotPublicCondition,
+    };
+
     uiSchema.elements.push({
       type: "Section",
       labelKey: `${i18nScope}.sections.schedule.label`,
@@ -34,6 +65,7 @@ export const buildUiSchema = async (
           type: "Control",
           scope: "/properties/schedule",
           labelKey: `${i18nScope}.sections.schedule.helperText`,
+          rule: disableScheduleRule,
           options: {
             type: "Control",
             control: "hub-field-input-scheduler",
@@ -57,6 +89,7 @@ export const buildUiSchema = async (
         {
           type: "Control",
           scope: "/properties/_forceUpdate",
+          rule: disableScheduleRule,
           options: {
             control: "hub-field-input-tile-select",
             type: "checkbox",
@@ -65,6 +98,18 @@ export const buildUiSchema = async (
             ],
             descriptions: [
               `{{${i18nScope}.fields.schedule.forceUpdateButton.description:translate}}`,
+            ],
+            messages: [
+              {
+                type: UiSchemaMessageTypes.custom,
+                display: "notice",
+                kind: "warning",
+                icon: "exclamation-mark-triangle",
+                titleKey: `${i18nScope}.fields.schedule.unavailableNotice.title`,
+                labelKey: `${i18nScope}.fields.schedule.unavailableNotice.body`,
+                allowShowBeforeInteract: true,
+                condition: isNotPublicCondition,
+              },
             ],
           },
         },
