37233b8bde7ea93078321948f6dbe78f66930343
Caleb Pomar <cpsharksfan@gmail.com>
2024-05-13 12:44:34 -0600

    fix(hub-common): add workaround to properly disable the schedule control when the underlying entity
    affects: @esri/hub-common

--- packages/common/src/content/_internal/ContentUiSchemaSettings.ts
+++ packages/common/src/content/_internal/ContentUiSchemaSettings.ts
@@ -3,6 +3,8 @@ import { IArcGISContext } from "../../ArcGISContext";
 import { EntityEditorOptions } from "../../core/schemas/internal/EditorOptions";
 import {
   IUiSchema,
+  IUiSchemaElement,
+  IUiSchemaMessage,
   IUiSchemaRule,
   UiSchemaMessageTypes,
   UiSchemaRuleEffects,
@@ -25,95 +27,125 @@ export const buildUiSchema = async (
     type: "Layout",
     elements: [],
   };
-
   if (
     checkPermission("hub:content:workspace:settings:schedule", _context).access
   ) {
-    // NOTE: unfortunately we have to add a hidden "access" field so
-    // we can use the value of "access" in other UI Schema rules.
-    const alwaysHiddenRule: IUiSchemaRule = {
-      effect: UiSchemaRuleEffects.SHOW,
-      condition: {
-        scope: "/properties/access",
-        schema: {
-          const: "non-existent-access-type",
-        },
-      },
-    };
-    uiSchema.elements.push({
+    const scheduleControlElement: IUiSchemaElement = {
       type: "Control",
-      scope: "/properties/access",
-      rule: alwaysHiddenRule,
-    });
-
-    const isNotPublicCondition: IUiSchemaRule["condition"] = {
-      scope: "/properties/access",
-      schema: {
-        enum: ["private", "org", "shared"],
+      scope: "/properties/schedule",
+      labelKey: `${i18nScope}.sections.schedule.helperText`,
+      options: {
+        type: "Control",
+        control: "hub-field-input-scheduler",
+        labelKey: "fieldHeader",
+        format: "radio",
+        inputs: [
+          { type: "automatic" },
+          { type: "daily" },
+          { type: "weekly" },
+          { type: "monthly" },
+          { type: "yearly" },
+          {
+            type: "manual",
+            helperActionIcon: "information-f",
+            helperActionText: `{{${i18nScope}.fields.schedule.manual.helperActionText:translate}}`,
+          },
+        ],
       },
     };
-    const disableScheduleRule: IUiSchemaRule = {
-      effect: UiSchemaRuleEffects.DISABLE,
-      condition: isNotPublicCondition,
+    // force update checkbox -- TODO: replace with button once available
+    const forceUpdateControlElement: IUiSchemaElement = {
+      type: "Control",
+      scope: "/properties/_forceUpdate",
+      options: {
+        control: "hub-field-input-tile-select",
+        type: "checkbox",
+        labels: [
+          `{{${i18nScope}.fields.schedule.forceUpdateButton.label:translate}}`,
+        ],
+        descriptions: [
+          `{{${i18nScope}.fields.schedule.forceUpdateButton.description:translate}}`,
+        ],
+      },
     };
+
+    /**
+     * NOTE: Some crazy gymnastics up ahead if the content is not public.
+     *
+     * The entity editor component will re-calculate the _UI Schema_ passed into the configuration editor
+     * if the underlying entity has been changed from the outside (e.g., via the sharing level controls).
+     * However, it will not change _the values_ passed into the configuration editor.
+     *
+     * This means that `options.access` is guaranteed to be up-to-date, but the underlying
+     * configuration editor's `values.access` _will not be updated_.
+     *
+     * As such we cannot rely on an IUiSchemaRule to update the disabled state of the
+     * schedule and force update controls, because the rule will be evaluated against
+     * _the old value_ of `values.access`.
+     *
+     * Also, the configuration editor also does not provide a way to statically set the disabled
+     * state of a control. We have to rely on an IUiSchemaRule to disable a field.
+     *
+     * So, here's what we do as a workaround:
+     *
+     * - Create a "disabled" rule with a condition that always evaluates to true (i.e., this.entity.type === this.entity.type)
+     * - Create a "hide" rule with a condition that always evaluates to true (i.e., this.entity.type === this.entity.type)
+     * - Add a hidden "type" field to the UI Schema (needed so we can use the value of entity.type in a the "disabled" and "hide" rules)
+     * - Add the "disabled" rule to the schedule and force update controls
+     * - Add a special notice to the bottom of the force update control which informs the user that scheduling is unavailable
+     */
+    if (options.access !== "public") {
+      // checks if this.values.type === options.type, which is always true
+      const alwaysTrueCondition: IUiSchemaRule["condition"] = {
+        scope: "/properties/type",
+        schema: {
+          const: options.type,
+        },
+      };
+
+      // This rule will cause a control to always be hidden
+      const alwaysHiddenRule: IUiSchemaRule = {
+        effect: UiSchemaRuleEffects.HIDE,
+        condition: alwaysTrueCondition,
+      };
+
+      // This rule will cause a control to always be disabled
+      const alwaysDisabledRule: IUiSchemaRule = {
+        effect: UiSchemaRuleEffects.DISABLE,
+        condition: alwaysTrueCondition,
+      };
+
+      // NOTE: unfortunately we have to add a hidden "type" field to the ui schema. If we don't, we won't
+      // have access to `this.values.type` when evaluating `alwaysDisabledRule` or `alwaysHiddenRule`
+      uiSchema.elements.push({
+        type: "Control",
+        scope: "/properties/type",
+        rule: alwaysHiddenRule,
+      });
+
+      // Disable the schedule and force update controls
+      scheduleControlElement.rule = alwaysDisabledRule;
+      forceUpdateControlElement.rule = alwaysDisabledRule;
+
+      // Add a notice to the bottom of the force update control
+      forceUpdateControlElement.options.messages = [
+        {
+          type: UiSchemaMessageTypes.custom,
+          display: "notice",
+          kind: "warning",
+          icon: "exclamation-mark-triangle",
+          titleKey: `${i18nScope}.fields.schedule.unavailableNotice.title`,
+          labelKey: `${i18nScope}.fields.schedule.unavailableNotice.body`,
+          allowShowBeforeInteract: true,
+          alwaysShow: true,
+        },
+      ] as IUiSchemaMessage[];
+    }
 
     uiSchema.elements.push({
       type: "Section",
       labelKey: `${i18nScope}.sections.schedule.label`,
-      elements: [
-        {
-          type: "Control",
-          scope: "/properties/schedule",
-          labelKey: `${i18nScope}.sections.schedule.helperText`,
-          rule: disableScheduleRule,
-          options: {
-            type: "Control",
-            control: "hub-field-input-scheduler",
-            labelKey: "fieldHeader",
-            format: "radio",
-            inputs: [
-              { type: "automatic" },
-              { type: "daily" },
-              { type: "weekly" },
-              { type: "monthly" },
-              { type: "yearly" },
-              {
-                type: "manual",
-                helperActionIcon: "information-f",
-                helperActionText: `{{${i18nScope}.fields.schedule.manual.helperActionText:translate}}`,
-              },
-            ],
-          },
-        },
-        // force update checkbox -- TODO: replace with button once available
-        {
-          type: "Control",
-          scope: "/properties/_forceUpdate",
-          rule: disableScheduleRule,
-          options: {
-            control: "hub-field-input-tile-select",
-            type: "checkbox",
-            labels: [
-              `{{${i18nScope}.fields.schedule.forceUpdateButton.label:translate}}`,
-            ],
-            descriptions: [
-              `{{${i18nScope}.fields.schedule.forceUpdateButton.description:translate}}`,
-            ],
-            messages: [
-              {
-                type: UiSchemaMessageTypes.custom,
-                display: "notice",
-                kind: "warning",
-                icon: "exclamation-mark-triangle",
-                titleKey: `${i18nScope}.fields.schedule.unavailableNotice.title`,
-                labelKey: `${i18nScope}.fields.schedule.unavailableNotice.body`,
-                allowShowBeforeInteract: true,
-                condition: isNotPublicCondition,
-              },
-            ],
-          },
-        },
-      ],
+      elements: [scheduleControlElement, forceUpdateControlElement],
     });
   }
 
