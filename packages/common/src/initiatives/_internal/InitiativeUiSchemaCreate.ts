4cf48bab949b5c2bcd5096b0d2e4096febd4eb16
Vivian Zhang <vivian.mzh@gmail.com>
2024-06-21 13:30:51 -0700

    fix: inline errors in Initiative creation form validation now blocks next steps
    affects: @esri/hub-common
    
    '

--- packages/common/src/initiatives/_internal/InitiativeUiSchemaCreate.ts
+++ packages/common/src/initiatives/_internal/InitiativeUiSchemaCreate.ts
@@ -109,13 +109,33 @@ export const buildUiSchema = async (
             options: {
               section: "step",
             },
-            rule: {
-              effect: UiSchemaRuleEffects.DISABLE,
-              condition: {
-                scope: "/properties/name",
-                schema: { const: "" },
+            rules: [
+              {
+                effect: UiSchemaRuleEffects.DISABLE,
+                conditions: [
+                  {
+                    scope: "/properties/name",
+                    schema: { const: "" },
+                  },
+                ],
               },
-            },
+              {
+                effect: UiSchemaRuleEffects.DISABLE,
+                conditions: [
+                  {
+                    scope: "/properties/name",
+                    schema: { minLength: 250 },
+                  },
+                ],
+              },
+              {
+                effect: UiSchemaRuleEffects.DISABLE,
+                condition: {
+                  scope: "/properties/summary",
+                  schema: { minLength: 2048 },
+                },
+              },
+            ],
             elements: [
               {
                 type: "Section",
@@ -206,13 +226,33 @@ export const buildUiSchema = async (
             options: {
               section: "step",
             },
-            rule: {
-              effect: UiSchemaRuleEffects.DISABLE,
-              condition: {
-                scope: "/properties/name",
-                schema: { const: "" },
+            rules: [
+              {
+                effect: UiSchemaRuleEffects.DISABLE,
+                conditions: [
+                  {
+                    scope: "/properties/name",
+                    schema: { const: "" },
+                  },
+                ],
               },
-            },
+              {
+                effect: UiSchemaRuleEffects.DISABLE,
+                conditions: [
+                  {
+                    scope: "/properties/name",
+                    schema: { minLength: 250 },
+                  },
+                ],
+              },
+              {
+                effect: UiSchemaRuleEffects.DISABLE,
+                condition: {
+                  scope: "/properties/summary",
+                  schema: { minLength: 2048 },
+                },
+              },
+            ],
             elements: [
               {
                 scope: "/properties/access",
--- packages/common/test/initiatives/_internal/InitiativeUiSchemaCreate.test.ts
+++ packages/common/test/initiatives/_internal/InitiativeUiSchemaCreate.test.ts
@@ -127,13 +127,33 @@ describe("buildUiSchema: initiative create", () => {
                 {
                   type: "Section",
                   labelKey: "some.scope.sections.location.label",
-                  rule: {
-                    effect: UiSchemaRuleEffects.HIDE,
-                    condition: {
-                      scope: "/properties/view/properties/hero",
-                      schema: { const: "image" },
+                  rules: [
+                    {
+                      effect: UiSchemaRuleEffects.DISABLE,
+                      conditions: [
+                        {
+                          scope: "/properties/name",
+                          schema: { const: "" },
+                        },
+                      ],
                     },
-                  },
+                    {
+                      effect: UiSchemaRuleEffects.DISABLE,
+                      conditions: [
+                        {
+                          scope: "/properties/name",
+                          schema: { minLength: 250 },
+                        },
+                      ],
+                    },
+                    {
+                      effect: UiSchemaRuleEffects.DISABLE,
+                      condition: {
+                        scope: "/properties/summary",
+                        schema: { minLength: 2048 },
+                      },
+                    },
+                  ],
                   options: {
                     helperText: {
                       labelKey: "some.scope.sections.location.helperText",
@@ -442,13 +462,33 @@ describe("buildUiSchema: initiative create", () => {
               options: {
                 section: "step",
               },
-              rule: {
-                effect: UiSchemaRuleEffects.DISABLE,
-                condition: {
-                  scope: "/properties/name",
-                  schema: { const: "" },
+              rules: [
+                {
+                  effect: UiSchemaRuleEffects.DISABLE,
+                  conditions: [
+                    {
+                      scope: "/properties/name",
+                      schema: { const: "" },
+                    },
+                  ],
                 },
-              },
+                {
+                  effect: UiSchemaRuleEffects.DISABLE,
+                  conditions: [
+                    {
+                      scope: "/properties/name",
+                      schema: { minLength: 250 },
+                    },
+                  ],
+                },
+                {
+                  effect: UiSchemaRuleEffects.DISABLE,
+                  condition: {
+                    scope: "/properties/summary",
+                    schema: { minLength: 2048 },
+                  },
+                },
+              ],
               elements: [
                 {
                   scope: "/properties/access",
