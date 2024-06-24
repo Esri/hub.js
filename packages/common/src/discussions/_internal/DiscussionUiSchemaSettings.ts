e081f1452625636f53788005e176eef971734d6e
Joshua Tanner <jtanner@esri.com>
2024-06-24 11:09:56 -0700

    feat(hub-common): adds map settings to discussion board settings schema
    affects: @esri/hub-common
    
    ISSUES CLOSED: #10461

--- packages/common/src/discussions/_internal/DiscussionUiSchemaSettings.ts
+++ packages/common/src/discussions/_internal/DiscussionUiSchemaSettings.ts
@@ -39,6 +39,25 @@ export const buildUiSchema = async (
           },
         ],
       },
+      {
+        type: "Section",
+        labelKey: `${i18nScope}.sections.mapSettings.label`,
+        elements: [
+          {
+            type: "Control",
+            scope: "/properties/view/properties/mapSettings",
+            labelKey: `${i18nScope}.fields.mapSettings.label`,
+            options: {
+              type: "Control",
+              control: "hub-composite-input-map-settings",
+              // the settings that are visible for configuring the map
+              visibleSettings: ["gallery"],
+              // if the map preview is displayed
+              showPreview: true,
+            },
+          },
+        ],
+      },
     ],
   };
 };
