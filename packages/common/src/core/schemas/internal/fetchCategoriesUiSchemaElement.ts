848d47d88c05537651863168754ffe149dd6d179
Caleb Pomar <cpsharksfan@gmail.com>
2024-06-24 12:11:12 -0600

    feat(hub-common): remove redundant declaration of icon (external links should always use 'launch')
    affects: @esri/hub-common

--- packages/common/src/core/schemas/internal/fetchCategoriesUiSchemaElement.ts
+++ packages/common/src/core/schemas/internal/fetchCategoriesUiSchemaElement.ts
@@ -55,7 +55,6 @@ export async function fetchCategoriesUiSchemaElement(
             "{{shared.fields.categories.noCategoriesNotice.link:translate}}",
           href: "https://doc.arcgis.com/en/arcgis-online/reference/content-categories.htm",
           target: "_blank",
-          icon: "launch",
         },
         allowShowBeforeInteract: true,
         alwaysShow: true,
