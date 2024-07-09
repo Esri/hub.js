9f379ba19aedd1f15617b36ecdf6b36928d15e1d
juliannemarik <julianne.marik@gmail.com>
2024-07-09 08:44:25 -0500

    feat: add embed interfaces

---
+++ packages/common/src/core/types/Embeds.ts
@@ -0,0 +1,53 @@
+/**
+ * Type to represent a Hub embed. This is a discrminated
+ * union between app, map, survey, external, etc. embeds.
+ */
+export type HubEmbed =
+  | IHubEmbedApp
+  | IHubEmbedMap
+  | IHubEmbedSurvey
+  | IHubEmbedExternal;
+
+/** enum to discriminate between embed union members */
+enum EmbedKind {
+  app = "app",
+  map = "map",
+  feedback = "feedback",
+  external = "external",
+}
+
+/** base embed */
+interface IHubEmbedBase {
+  /** embed height */
+  height?: number;
+  /** indicates whether the embed should be scrollable */
+  isScrollable?: boolean;
+}
+
+/** app-specific embed */
+interface IHubEmbedApp extends IHubEmbedBase {
+  kind: EmbedKind.app;
+  /** application id */
+  id: string;
+}
+
+/** map-specific embed */
+interface IHubEmbedMap extends IHubEmbedBase {
+  kind: EmbedKind.map;
+  /** web map/scene id */
+  id: string;
+}
+
+/** external embed */
+export interface IHubEmbedExternal extends IHubEmbedBase {
+  kind: EmbedKind.external;
+  /** embed url */
+  url: string;
+}
+
+/** survey-specific embed */
+export interface IHubEmbedSurvey extends IHubEmbedBase {
+  kind: EmbedKind.feedback;
+  /** survey123 id */
+  id: string;
+}
