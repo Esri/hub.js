4c242149adb74bb155e81961af6b31cb9c829c82
juliannemarik <julianne.marik@gmail.com>
2024-07-12 12:18:35 -0500

    chore: add comment for clarity

--- packages/common/src/core/traits/IWithViewSettings.ts
+++ packages/common/src/core/traits/IWithViewSettings.ts
@@ -60,6 +60,15 @@ export interface IWithViewSettings {
    */
   heroActions?: HubActionLink[];
 
-  /** array of embedded content */
+  /**
+   * array of embedded content
+   *
+   * Note: for now, we are only allowing a single
+   * embed to be configured, but we've made this
+   * an array for future extensibility. If/when
+   * we do support configuring multiple embeds,
+   * we should revisit if/how this will map to a
+   * future layout system
+   */
   embeds?: HubEmbed[];
 }
