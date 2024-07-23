d3e5bfb4c4d909ba0e5ba627b53f7c39a6301408
velveetachef <jeremysabat@gmail.com>
2024-07-23 10:41:17 -0700

    fix(hub-discussions): revert breaking change
    affects: @esri/hub-discussions

--- packages/discussions/test/utils/reactions/can-create-reaction.test.ts
+++ packages/discussions/test/utils/reactions/can-create-reaction.test.ts
@@ -6,52 +6,55 @@ import * as canReadChannelModule from "../../../src/utils/channels/can-read-chan
 describe("Util: canCreateReaction", () => {
   const user: IUser = { username: "bob" };
 
-  it("returns true if channel allows all reactions and canReadChannel returns true", () => {
-    const canReadChannelSpy = spyOn(
-      canReadChannelModule,
-      "canReadChannel"
-    ).and.returnValue(true);
-    const channel = {
-      allowReaction: true,
-      allowedReactions: null,
-    } as unknown as IChannel;
+  // it("returns true if channel allows all reactions and canReadChannel returns true", () => {
+  //   const canReadChannelSpy = spyOn(
+  //     canReadChannelModule,
+  //     "canReadChannel"
+  //   ).and.returnValue(true);
+  //   const channel = {
+  //     allowReaction: true,
+  //     allowedReactions: null,
+  //   } as unknown as IChannel;
 
-    expect(canCreateReaction(channel, PostReaction.THUMBS_UP, user)).toBe(true);
-    expect(canReadChannelSpy).toHaveBeenCalledTimes(1);
-    expect(canReadChannelSpy).toHaveBeenCalledWith(channel, user);
-  });
+  //   expect(canCreateReaction(channel, PostReaction.THUMBS_UP)).toBe(true);
+  //   // expect(canCreateReaction(channel, PostReaction.THUMBS_UP, user)).toBe(true);
+  //   // expect(canReadChannelSpy).toHaveBeenCalledTimes(1);
+  //   // expect(canReadChannelSpy).toHaveBeenCalledWith(channel, user);
+  // });
 
-  it("returns true if user is undefined, channel allows all reactions, and canReadChannel returns true", () => {
-    const canReadChannelSpy = spyOn(
-      canReadChannelModule,
-      "canReadChannel"
-    ).and.returnValue(true);
-    const channel = {
-      allowReaction: true,
-      allowedReactions: null,
-    } as unknown as IChannel;
+  // it("returns true if user is undefined, channel allows all reactions, and canReadChannel returns true", () => {
+  //   const canReadChannelSpy = spyOn(
+  //     canReadChannelModule,
+  //     "canReadChannel"
+  //   ).and.returnValue(true);
+  //   const channel = {
+  //     allowReaction: true,
+  //     allowedReactions: null,
+  //   } as unknown as IChannel;
 
-    expect(canCreateReaction(channel, PostReaction.THUMBS_UP)).toBe(true);
-    expect(canReadChannelSpy).toHaveBeenCalledTimes(1);
-    expect(canReadChannelSpy).toHaveBeenCalledWith(channel, {});
-  });
+  //   expect(canCreateReaction(channel, PostReaction.THUMBS_UP)).toBe(true);
+  //   // expect(canCreateReaction(channel, PostReaction.THUMBS_UP)).toBe(true);
+  //   // expect(canReadChannelSpy).toHaveBeenCalledTimes(1);
+  //   // expect(canReadChannelSpy).toHaveBeenCalledWith(channel, {});
+  // });
 
-  it("returns false if channel allows all reactions and canReadChannel returns false", () => {
-    const canReadChannelSpy = spyOn(
-      canReadChannelModule,
-      "canReadChannel"
-    ).and.returnValue(false);
-    const channel = {
-      allowReaction: true,
-      allowedReactions: null,
-    } as unknown as IChannel;
+  // it("returns false if channel allows all reactions and canReadChannel returns false", () => {
+  //   const canReadChannelSpy = spyOn(
+  //     canReadChannelModule,
+  //     "canReadChannel"
+  //   ).and.returnValue(false);
+  //   const channel = {
+  //     allowReaction: true,
+  //     allowedReactions: null,
+  //   } as unknown as IChannel;
 
-    expect(canCreateReaction(channel, PostReaction.THUMBS_UP, user)).toBe(
-      false
-    );
-    expect(canReadChannelSpy).toHaveBeenCalledTimes(1);
-    expect(canReadChannelSpy).toHaveBeenCalledWith(channel, user);
-  });
+  //   expect(canCreateReaction(channel, PostReaction.THUMBS_UP)).toBe(
+  //   // expect(canCreateReaction(channel, PostReaction.THUMBS_UP, user)).toBe(
+  //     false
+  //   );
+  //   // expect(canReadChannelSpy).toHaveBeenCalledTimes(1);
+  //   // expect(canReadChannelSpy).toHaveBeenCalledWith(channel, user);
+  // });
 
   it("returns true if channel allows reaction, value included in allowed reactions, and canReadChannel returns true", () => {
     const canReadChannelSpy = spyOn(
@@ -63,9 +66,10 @@ describe("Util: canCreateReaction", () => {
       allowedReactions: ["thumbs_up"],
     } as IChannel;
 
-    expect(canCreateReaction(channel, PostReaction.THUMBS_UP, user)).toBe(true);
-    expect(canReadChannelSpy).toHaveBeenCalledTimes(1);
-    expect(canReadChannelSpy).toHaveBeenCalledWith(channel, user);
+    expect(canCreateReaction(channel, PostReaction.THUMBS_UP)).toBe(true);
+    // expect(canCreateReaction(channel, PostReaction.THUMBS_UP, user)).toBe(true);
+    // expect(canReadChannelSpy).toHaveBeenCalledTimes(1);
+    // expect(canReadChannelSpy).toHaveBeenCalledWith(channel, user);
   });
 
   it("returns false if channel allows reaction and value not included in allowed reactions", () => {
@@ -78,10 +82,11 @@ describe("Util: canCreateReaction", () => {
       allowedReactions: ["thumbs_up"],
     } as IChannel;
 
-    expect(canCreateReaction(channel, PostReaction.THUMBS_DOWN, user)).toBe(
+    expect(canCreateReaction(channel, PostReaction.THUMBS_DOWN)).toBe(
+      // expect(canCreateReaction(channel, PostReaction.THUMBS_DOWN, user)).toBe(
       false
     );
-    expect(canReadChannelSpy).not.toHaveBeenCalled();
+    // expect(canReadChannelSpy).not.toHaveBeenCalled();
   });
 
   it("returns false if channel does not allow reaction", () => {
@@ -91,9 +96,10 @@ describe("Util: canCreateReaction", () => {
     ).and.returnValue(true);
     const channel = { allowReaction: false } as IChannel;
 
-    expect(canCreateReaction(channel, PostReaction.THUMBS_DOWN, user)).toBe(
+    expect(canCreateReaction(channel, PostReaction.THUMBS_DOWN)).toBe(
+      // expect(canCreateReaction(channel, PostReaction.THUMBS_DOWN, user)).toBe(
       false
     );
-    expect(canReadChannelSpy).not.toHaveBeenCalled();
+    // expect(canReadChannelSpy).not.toHaveBeenCalled();
   });
 });
