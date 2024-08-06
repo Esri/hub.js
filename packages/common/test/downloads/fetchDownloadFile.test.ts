c62a9cebc72020df9f404a334e6db5ef5ea4002d
Caleb Pomar <cpsharksfan@gmail.com>
2024-08-06 15:18:53 -0600

    fix(hub-common): remove unimplemented flow for downloading files via export item (#1613)

--- packages/common/test/downloads/fetchDownloadFile.test.ts
+++ packages/common/test/downloads/fetchDownloadFile.test.ts
@@ -1,7 +1,5 @@
 import * as canUseHubDownloadApiModule from "../../src/downloads/canUseHubDownloadApi";
 import * as fetchHubApiDownloadFileModule from "../../src/downloads/_internal/file-url-fetchers/fetchHubApiDownloadFile";
-import * as canUseExportItemFlowModule from "../../src/downloads/_internal/canUseExportItemFlow";
-import * as fetchExportItemDownloadFileModule from "../../src/downloads/_internal/file-url-fetchers/fetchExportItemDownloadFile";
 import * as canUseExportImageFlowModule from "../../src/downloads/_internal/canUseExportImageFlow";
 import * as fetchExportImageDownloadFileModule from "../../src/downloads/_internal/file-url-fetchers/fetchExportImageDownloadFile";
 import { IHubEditableContent } from "../../src/core/types/IHubEditableContent";
@@ -11,8 +9,6 @@ import { fetchDownloadFile } from "../../src/downloads/fetchDownloadFile";
 describe("fetchDownloadFile", () => {
   let canUseHubDownloadApiSpy: jasmine.Spy;
   let fetchHubApiDownloadFileSpy: jasmine.Spy;
-  let canUseExportItemFlowSpy: jasmine.Spy;
-  let fetchExportItemDownloadFileSpy: jasmine.Spy;
   let canUseExportImageFlowSpy: jasmine.Spy;
   let fetchExportImageDownloadFileSpy: jasmine.Spy;
 
@@ -24,14 +20,6 @@ describe("fetchDownloadFile", () => {
     fetchHubApiDownloadFileSpy = spyOn(
       fetchHubApiDownloadFileModule,
       "fetchHubApiDownloadFile"
-    );
-    canUseExportItemFlowSpy = spyOn(
-      canUseExportItemFlowModule,
-      "canUseExportItemFlow"
-    );
-    fetchExportItemDownloadFileSpy = spyOn(
-      fetchExportItemDownloadFileModule,
-      "fetchExportItemDownloadFile"
     );
     canUseExportImageFlowSpy = spyOn(
       canUseExportImageFlowModule,
@@ -45,7 +33,6 @@ describe("fetchDownloadFile", () => {
 
   it("should throw an error if no download flow can be used", async () => {
     canUseHubDownloadApiSpy.and.returnValue(false);
-    canUseExportItemFlowSpy.and.returnValue(false);
     canUseExportImageFlowSpy.and.returnValue(false);
     try {
       await fetchDownloadFile({
@@ -62,8 +49,6 @@ describe("fetchDownloadFile", () => {
     }
     expect(canUseHubDownloadApiSpy).toHaveBeenCalledTimes(1);
     expect(fetchHubApiDownloadFileSpy).not.toHaveBeenCalled();
-    expect(canUseExportItemFlowSpy).toHaveBeenCalledTimes(1);
-    expect(fetchExportItemDownloadFileSpy).not.toHaveBeenCalled();
     expect(canUseExportImageFlowSpy).toHaveBeenCalledTimes(1);
     expect(fetchExportImageDownloadFileSpy).not.toHaveBeenCalled();
   });
@@ -71,7 +56,6 @@ describe("fetchDownloadFile", () => {
   it("should delegate to fetchHubApiDownloadFile when the Hub Download API can be used", async () => {
     canUseHubDownloadApiSpy.and.returnValue(true);
     canUseExportImageFlowSpy.and.returnValue(false);
-    canUseExportItemFlowSpy.and.returnValue(false);
 
     fetchHubApiDownloadFileSpy.and.returnValue(
       Promise.resolve({ type: "url", href: "hub-api-download-url" })
@@ -99,52 +83,12 @@ describe("fetchDownloadFile", () => {
       layers: [0],
       pollInterval: 1000,
     });
-    expect(canUseExportItemFlowSpy).not.toHaveBeenCalled();
-    expect(fetchExportItemDownloadFileSpy).not.toHaveBeenCalled();
-    expect(canUseExportImageFlowSpy).not.toHaveBeenCalled();
-    expect(fetchExportImageDownloadFileSpy).not.toHaveBeenCalled();
-  });
-
-  it("should delegate to fetchExportItemDownloadFile when the Hub Download API cannot be used but export item flow can be", async () => {
-    canUseHubDownloadApiSpy.and.returnValue(false);
-    canUseExportItemFlowSpy.and.returnValue(true);
-    canUseExportImageFlowSpy.and.returnValue(false);
-
-    fetchExportItemDownloadFileSpy.and.returnValue(
-      Promise.resolve({ type: "url", href: "export-item-download-url" })
-    );
-
-    const entity = {
-      id: "123",
-      type: "Feature Service",
-    } as unknown as IHubEditableContent;
-    const context = {} as unknown as IArcGISContext;
-    const result = await fetchDownloadFile({
-      entity,
-      context,
-      format: ServiceDownloadFormat.CSV,
-      layers: [0],
-      pollInterval: 1000,
-    });
-    expect(result).toEqual({ type: "url", href: "export-item-download-url" });
-    expect(canUseHubDownloadApiSpy).toHaveBeenCalledTimes(1);
-    expect(fetchHubApiDownloadFileSpy).not.toHaveBeenCalled();
-    expect(canUseExportItemFlowSpy).toHaveBeenCalledTimes(1);
-    expect(fetchExportItemDownloadFileSpy).toHaveBeenCalledTimes(1);
-    expect(fetchExportItemDownloadFileSpy).toHaveBeenCalledWith({
-      entity,
-      context,
-      format: ServiceDownloadFormat.CSV,
-      layers: [0],
-      pollInterval: 1000,
-    });
     expect(canUseExportImageFlowSpy).not.toHaveBeenCalled();
     expect(fetchExportImageDownloadFileSpy).not.toHaveBeenCalled();
   });
 
   it("should delegate to fetchExportImageDownloadFile when the Export Image flow can be used", async () => {
     canUseHubDownloadApiSpy.and.returnValue(false);
-    canUseExportItemFlowSpy.and.returnValue(false);
     canUseExportImageFlowSpy.and.returnValue(true);
 
     fetchExportImageDownloadFileSpy.and.returnValue(
@@ -169,8 +113,6 @@ describe("fetchDownloadFile", () => {
     });
     expect(canUseHubDownloadApiSpy).toHaveBeenCalledTimes(1);
     expect(fetchHubApiDownloadFileSpy).not.toHaveBeenCalled();
-    expect(canUseExportItemFlowSpy).toHaveBeenCalledTimes(1);
-    expect(fetchExportItemDownloadFileSpy).not.toHaveBeenCalled();
     expect(canUseExportImageFlowSpy).toHaveBeenCalledTimes(1);
     expect(fetchExportImageDownloadFileSpy).toHaveBeenCalledTimes(1);
     expect(fetchExportImageDownloadFileSpy).toHaveBeenCalledWith({
