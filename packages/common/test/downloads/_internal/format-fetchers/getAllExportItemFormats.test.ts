45902d65f8831a67bb62c171eab051df737b4a71
Caleb Pomar <cpsharksfan@gmail.com>
2024-08-06 14:22:45 -0600

    fix(hub-common): remove unimplemented flow for downloading files via export item
    affects: @esri/hub-common

--- packages/common/test/downloads/_internal/format-fetchers/getAllExportItemFormats.test.ts
+++
@@ -1,8 +0,0 @@
-import { getAllExportItemFormats } from "../../../../src/downloads/_internal/format-fetchers/getAllExportItemFormats";
-import { EXPORT_ITEM_FORMATS } from "../../../../src/downloads/_internal/_types";
-describe("getAllExportItemFormats", () => {
-  it("should return all export item formats", () => {
-    const result = getAllExportItemFormats();
-    expect(result.map((r) => r.format)).toEqual(EXPORT_ITEM_FORMATS);
-  });
-});
