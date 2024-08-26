edc3599ee99363028bfec734b538f282fa74bc0b
Joshua Tanner <jtanner@esri.com>
2024-08-26 10:36:26 -0700

    refactor(hub-common): moves getDownloadFormatsByFlow to own util
    affects: @esri/hub-common

---
+++ packages/common/src/downloads/_internal/getDownloadFormatsByFlow.ts
@@ -0,0 +1,35 @@
+import {
+  DownloadFlowType,
+  IHubEditableContent,
+} from "../../core/types/IHubEditableContent";
+import { IDynamicDownloadFormat } from "../types";
+import { getCreateReplicaFormats } from "./format-fetchers/getCreateReplicaFormats";
+import { getExportImageFormats } from "./format-fetchers/getExportImageFormats";
+import { getPagingJobFormats } from "./format-fetchers/getPagingJobFormats";
+
+/**
+ * Get the download formats for a given download flow and entity.
+ *
+ * @param downloadFlow DownloadFlowType
+ * @param entity IHubEditableContent
+ * @returns
+ */
+export function getDownloadFormatsByFlow(
+  downloadFlow: DownloadFlowType,
+  entity: IHubEditableContent
+): IDynamicDownloadFormat[] {
+  let downloadFormats: IDynamicDownloadFormat[] = [];
+  const actionsByFlow: Record<DownloadFlowType, () => void> = {
+    createReplica: () => {
+      downloadFormats = getCreateReplicaFormats(entity);
+    },
+    paging: () => {
+      downloadFormats = getPagingJobFormats();
+    },
+    exportImage: () => {
+      downloadFormats = getExportImageFormats(entity);
+    },
+  };
+  actionsByFlow[downloadFlow] && actionsByFlow[downloadFlow]();
+  return downloadFormats;
+}
--- packages/common/src/downloads/getDownloadConfiguration.ts
+++ packages/common/src/downloads/getDownloadConfiguration.ts
@@ -1,16 +1,12 @@
 import { IHubAdditionalResource } from "../core/types/IHubAdditionalResource";
 import {
-  DownloadFlowType,
   IDownloadFormatConfiguration,
   IEntityDownloadConfiguration,
   IHubEditableContent,
 } from "../core/types/IHubEditableContent";
 import { getProp } from "../objects/get-prop";
-import { IDynamicDownloadFormat } from "./types";
-import { getCreateReplicaFormats } from "./_internal/format-fetchers/getCreateReplicaFormats";
-import { getExportImageFormats } from "./_internal/format-fetchers/getExportImageFormats";
-import { getPagingJobFormats } from "./_internal/format-fetchers/getPagingJobFormats";
 import { getDownloadFlow } from "./_internal/getDownloadFlow";
+import { getDownloadFormatsByFlow } from "./_internal/getDownloadFormatsByFlow";
 
 /**
  * Returns the download configuration for an entity at this moment in time.
@@ -26,37 +22,13 @@ export function getDownloadConfiguration(
 ): IEntityDownloadConfiguration {
   // TODO: account for enterprise environments
   const downloadFlow = getDownloadFlow(entity);
-  let serverFormats: IDynamicDownloadFormat[] = [];
+  const serverFormats = getDownloadFormatsByFlow(downloadFlow, entity);
   const additionalResources: IHubAdditionalResource[] =
     getProp(entity, "extendedProps.additionalResources") || [];
   const existingConfiguration: IEntityDownloadConfiguration = getProp(
     entity,
     "extendedProps.downloads"
   );
-
-  // TODO: Could we make a formal helper function instead?
-  // Maybe something like this:
-  //
-  // type LogicByFlowMap = Record<DownloadFlowType, () => void>;
-  // function executeLogicByFlow(
-  //  flow: DownloadFlowType,
-  //  logicByFlow: LogicByFlowMap
-  // )
-  // OR a switch statement: https://medium.com/technogise/type-safe-and-exhaustive-switch-statements-aka-pattern-matching-in-typescript-e3febd433a7a
-
-  const actionsByFlow: Record<DownloadFlowType, () => void> = {
-    createReplica: () => {
-      serverFormats = getCreateReplicaFormats(entity);
-    },
-    paging: () => {
-      serverFormats = getPagingJobFormats();
-    },
-    exportImage: () => {
-      serverFormats = getExportImageFormats(entity);
-    },
-  };
-
-  actionsByFlow[downloadFlow] && actionsByFlow[downloadFlow]();
 
   // Base combined default formats
   const combinedDefaultFormats: IDownloadFormatConfiguration[] =
