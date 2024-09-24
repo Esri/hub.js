baf5764b5eec61d5444c522c8bf2e12025e1b4cc
Randy Weber <52967037+rweber-esri@users.noreply.github.com>
2024-09-24 09:49:32 -0400

    fix(hub-common): hide showAdditionalInfo field in event gallery card … (#1659)

--- packages/common/test/core/schemas/internal/events/EventGalleryCardUiSchema.test.ts
+++ packages/common/test/core/schemas/internal/events/EventGalleryCardUiSchema.test.ts
@@ -379,16 +379,17 @@ describe("EventGalleryCardUiSchema", () => {
                       enum: { i18nScope: "some.scope.appearance.shadow" },
                     },
                   },
-                  {
-                    label:
-                      "{{some.scope.appearance.showAdditionalInfo.label:translate}}",
-                    scope: "/properties/showAdditionalInfo",
-                    type: "Control",
-                    options: {
-                      control: "hub-field-input-switch",
-                      layout: "inline-space-between",
-                    },
-                  },
+                  // TODO: Re-add this once https://github.com/Esri/calcite-design-system/issues/10152 & https://github.com/Esri/calcite-design-system/issues/6059 are resolved
+                  // {
+                  //   label:
+                  //     "{{some.scope.appearance.showAdditionalInfo.label:translate}}",
+                  //   scope: "/properties/showAdditionalInfo",
+                  //   type: "Control",
+                  //   options: {
+                  //     control: "hub-field-input-switch",
+                  //     layout: "inline-space-between",
+                  //   },
+                  // },
                 ],
               },
               {
