8d5597c42861d562d85d42bce31a68c885b19aa2
Dave Bouwman <dave@davebouwman.com>
2024-10-22 08:10:58 -0600

    fix: small fixes for breadcrumbs and deep containment checks (#1692)

--- packages/common/test/core/_internal/deepContains.test.ts
+++ packages/common/test/core/_internal/deepContains.test.ts
@@ -1,6 +1,5 @@
 import {
   deepContains,
-  parsePath,
   pathToCatalogInfo,
 } from "../../../src/core/_internal/deepContains";
 import { ArcGISContextManager } from "../../../src/ArcGISContextManager";
@@ -163,45 +162,6 @@ describe("deepContains:", () => {
     expect(response.reason).toContain("not contained in catalog");
     expect(fetchCatalogSpy).toHaveBeenCalledTimes(0);
     expect(hubSearchSpy).toHaveBeenCalledTimes(1);
-  });
-});
-
-describe("parsePath:", () => {
-  // add tests for the parsePath function
-
-  it("path needs even number of parts", () => {
-    const path = "/sites/00b/initiatives";
-
-    const result = parsePath(path);
-    expect(result.valid).toBeFalsy();
-    expect(result.reason).toBe(
-      "Path does not contain an even number of parts."
-    );
-  });
-
-  it("path needs less than 10 parts", () => {
-    const path =
-      "sites/00a/initiatives/00b/projects/00c/content/00d/content/00c/content/00e";
-
-    const result = parsePath(path);
-    expect(result.valid).toBeFalsy();
-    expect(result.reason).toBe("Path is > 5 entities deep.");
-  });
-
-  it("path needs valid paths", () => {
-    const path = "sites/00a/initiatives/00b/projects/00c/wallaby/00d";
-
-    const result = parsePath(path);
-    expect(result.valid).toBeFalsy();
-    expect(result.reason).toBe("Path contains invalid segment: wallaby.");
-  });
-
-  it("good path parses clean", () => {
-    const path = "sites/00a/initiatives/00b/projects/00c";
-    const result = parsePath(path);
-    expect(result.valid).toBeTruthy();
-    expect(result.reason).toBe("");
-    expect(result.parts).toEqual(path.split("/"));
   });
 });
 
