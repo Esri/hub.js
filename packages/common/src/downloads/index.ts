10524853f350ac18134cc3dfe447b42a4cffd78b
Tom Wayson <tom@tomwayson.com>
2024-10-31 12:33:56 -0700

    refactor(hub-common): remove unused fetchDownloadFormats
    BREAKING CHANGE: remove fetchDownloadFormats function

--- packages/common/src/downloads/index.ts
+++ packages/common/src/downloads/index.ts
@@ -1,7 +1,6 @@
 export * from "./build-existing-exports-portal-query";
 export * from "./types";
 export * from "./fetchDownloadFile";
-export * from "./fetchDownloadFormats";
 export * from "./canUseCreateReplica";
 export * from "./canUseHubDownloadSystem";
 export * from "./canUseHubDownloadApi";
