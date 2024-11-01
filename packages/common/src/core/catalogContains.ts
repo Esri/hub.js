889e8663dc02d6329dea2a5267b05ee78b7ff3eb
Vivian Zhang <vivian.mzh@gmail.com>
2024-11-01 08:25:44 -0700

    refactor: break the logic in catalogContains into small functions for better readability
    affects: @esri/hub-common

--- packages/common/src/core/catalogContains.ts
+++ packages/common/src/core/catalogContains.ts
@@ -2,6 +2,7 @@ import {
   EntityType,
   IHubCatalog,
   IPredicate,
+  IQuery,
 } from "../search/types/IHubCatalog";
 import { IContainsOptions, IContainsResponse } from "../search/types/types";
 import { isCuid } from "../utils/is-cuid";
@@ -9,6 +10,8 @@ import { isGuid } from "../utils/is-guid";
 import { IArcGISContext } from "../ArcGISContext";
 import { hubSearch } from "../search/hubSearch";
 import { getWithDefault } from "../objects/get-with-default";
+import { IHubSearchResponse } from "../search/types/IHubSearchResponse";
+import { IHubSearchResult } from "../search/types/IHubSearchResult";
 
 /**
  * Given an identifier and a catalog, check if the catalog contains the identifier.
@@ -32,62 +35,107 @@ export async function catalogContains(
   };
 
   // construct the predicate
-  const pred: IPredicate = {};
+  // if the identifier is a guid or cuid, use it as the id,
+  // otherwise use it as a slug
+  const pred: IPredicate =
+    isGuid(identifier) || isCuid(identifier)
+      ? { id: identifier }
+      : { typekeywords: `slug|${identifier}` };
 
-  if (isGuid(identifier) || isCuid(identifier)) {
-    pred.id = identifier;
-  } else {
-    // treat as slug
-    pred.typekeywords = `slug|${identifier}`;
+  // build the queries based on the entityType and the predicate
+  const queries = buildQueries(catalog, options.entityType, pred);
+
+  // if no collections or scope for this entity type,
+  // then it must not be in the catalog
+  if (queries.length === 0) {
+    return Promise.resolve(response);
   }
-  // construct the queries
+
+  const results = await executeQueries(queries, catalog, context);
+
+  // if any of the queries returned a result,
+  // then the entity is contained
+  response.isContained = checkResults(results, pred);
+
+  return response;
+}
+
+/**
+ * Build queries, if entityType is provided, build queries for
+ * that entityType, otherwise build queries for all scopes
+ * @param catalog
+ * @param entityType
+ * @param pred
+ * @returns
+ */
+function buildQueries(
+  catalog: IHubCatalog,
+  entityType: EntityType,
+  pred: IPredicate
+): IQuery[] {
+  return entityType
+    ? buildQueriesForEntityType(catalog, entityType, pred)
+    : buildQueriesForAllScopes(catalog, pred);
+}
+
+/**
+ * Build the queries based on the scope for the entity type,
+ * then check any collections that match the entity type,
+ * if there are no scopes or collections with the entity type,
+ * then the entity is not contained in the catalog
+ * @param catalog
+ * @param entityType
+ * @param pred
+ * @returns
+ */
+function buildQueriesForEntityType(
+  catalog: IHubCatalog,
+  entityType: EntityType,
+  pred: IPredicate
+): IQuery[] {
   const queries = [];
-  if (options.entityType) {
-    // --------------------------------------------------------
-    // Check the scope for the entity type
-    // then check any collections that match the entity type
-    // if there are no scopes or collections with the entity type
-    // then the entity is not contained in the catalog
-    // --------------------------------------------------------
-    if (catalog.scopes && catalog.scopes[options.entityType]) {
-      // Scope is a "bounding query" for all collections involving the entity type
-      // so we can check if it's in the catalog by checking if it's in the scope
-      queries.push({
-        targetEntity: options.entityType,
-        filters: [{ predicates: [pred] }],
-      });
-    } else {
-      // If there is no scope for the entity type, we check the collections
-      const collections = catalog.collections || [];
-      const typeCollections = collections.filter(
-        (c) => c.targetEntity === options.entityType
-      );
-      if (typeCollections.length) {
-        // If there are collections for the entity type, we check them
-        typeCollections.forEach((collection) => {
-          queries.push({
-            targetEntity: collection.targetEntity,
-            filters: [{ predicates: [pred] }, ...collection.scope.filters],
-          });
-        });
-      } else {
-        // no collections or scope for this entity type
-        // thus it cannot be in the catalog
-        return Promise.resolve(response);
-      }
-    }
-  } else {
-    // Construct a query for each scope
-    Object.keys(catalog.scopes).forEach((type) => {
-      queries.push({
-        targetEntity: type as EntityType,
-        filters: [{ predicates: [pred] }],
-      });
+  if (catalog.scopes?.[entityType]) {
+    // Scope is a "bounding query" for all collections involving the entity type
+    // so we can check if it's in the catalog by checking if it's in the scope
+    queries.push({
+      targetEntity: entityType,
+      filters: [{ predicates: [pred] }],
     });
+  } else {
+    // If there is no scope for the entity type, we check the collections
+    const collections = catalog.collections || [];
+    const typeCollections = collections.filter(
+      (c) => c.targetEntity === entityType
+    );
+    if (typeCollections.length) {
+      // If there are collections for the entity type, we check them
+      typeCollections.forEach((collection) => {
+        queries.push({
+          targetEntity: collection.targetEntity,
+          filters: [{ predicates: [pred] }, ...collection.scope.filters],
+        });
+      });
+    }
   }
+  return queries;
+}
 
-  // execute the queries
-  const results = await Promise.all(
+function buildQueriesForAllScopes(
+  catalog: IHubCatalog,
+  pred: IPredicate
+): IQuery[] {
+  return Object.keys(catalog.scopes).map((type) => ({
+    targetEntity: type as EntityType,
+    filters: [{ predicates: [pred] }],
+  }));
+}
+
+async function executeQueries(
+  queries: IQuery[],
+  catalog: IHubCatalog,
+  context: IArcGISContext
+): Promise<Array<IHubSearchResponse<IHubSearchResult>>> {
+  return Promise.all(
     queries.map((query) => {
       // get the scope if it exists and merge filters
       if (catalog.scopes[query.targetEntity]) {
@@ -96,7 +144,8 @@ export async function catalogContains(
           ...catalog.scopes[query.targetEntity].filters,
         ];
       }
-      // We set num to be 10 to account for api not doing exact matching on slugs
+      // We set num to be 10 to account for api not doing
+      // exact matching on slugs
       return hubSearch(query, {
         targetEntity: query.targetEntity,
         num: 10,
@@ -104,37 +153,41 @@ export async function catalogContains(
       });
     })
   );
+}
 
-  // if any of the queries returned a result, then the entity is contained
-  response.isContained = results.reduce((isContained, queryResponse) => {
+/**
+ * Check if the results contain the entity based on the predicate
+ * @param results
+ * @param pred
+ * @returns
+ */
+function checkResults(
+  results: Array<IHubSearchResponse<IHubSearchResult>>,
+  pred: IPredicate
+): boolean {
+  return results.reduce((isContained, queryResponse) => {
     if (queryResponse.results.length) {
       if (pred.id) {
-        isContained = true;
+        return true;
       } else {
         // slug based search, which is not exact,
         // so we manually verify the exact slug matches
-        isContained = queryResponse.results.reduce(
-          (slugKeywordPresent, entry) => {
-            // try .keywords, then rawResult.typeKeywords, else empty array
-            const kwds = getWithDefault(
-              entry,
-              "typeKeywords",
-              getWithDefault(entry, "rawResult.typeKeywords", [])
-            );
-            // if (kwds.includes(pred.typekeywords)) {
-            //   slugKeywordPresent = true;
-            // }
-            if (entry.typeKeywords.includes(pred.typekeywords)) {
-              slugKeywordPresent = true;
-            }
-            return slugKeywordPresent;
-          },
-          false as boolean
-        );
+        return queryResponse.results.reduce((slugKeywordPresent, entry) => {
+          // try .keywords, then rawResult.typeKeywords, else empty array
+          const kwds = getWithDefault(
+            entry,
+            "typeKeywords",
+            getWithDefault(entry, "rawResult.typeKeywords", [])
+          );
+          // if (kwds.includes(pred.typekeywords)) {
+          //   slugKeywordPresent = true;
+          // }
+          if (entry.typeKeywords.includes(pred.typekeywords)) {
+            slugKeywordPresent = true;
+          }
+          return slugKeywordPresent;
+        }, false as boolean);
       }
     }
-    return isContained;
   }, false as boolean);
-
-  return response;
 }
