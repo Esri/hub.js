81a291769051e5c4ccdaa6fe4cca42e2c93bbcfe
sansth1010 <sansth22@gmail.com>
2024-11-14 15:37:10 -0600

    feat(hub-common): added dcat us template

--- packages/common/test/sites/feeds/setFeedTemplate.test.ts
+++ packages/common/test/sites/feeds/setFeedTemplate.test.ts
@@ -1,4 +1,3 @@
-import * as legacyFeedsModule from "../../../src/sites/feed-configuration";
 import { setFeedTemplate } from "../../../src/sites/feeds/setFeedTemplate";
 import {
   IFeedsConfiguration,
@@ -6,25 +5,223 @@ import {
 } from "../../../src/sites/feeds/types";
 
 describe("setFeedTemplate", () => {
-  let setFeedConfigurationSpy: jasmine.Spy;
-  beforeEach(() => {
-    setFeedConfigurationSpy = spyOn(
-      legacyFeedsModule,
-      "setFeedConfiguration"
-    ).and.callThrough();
-  });
-  it("delegates to setFeedConfiguration", () => {
-    const feedsConfig: IFeedsConfiguration = {};
+  it("sets DCAT US V1 configuration", async () => {
+    const updatedTemplate = {
+      title: "{{new_title}}",
+    };
+    const feedsConfig: IFeedsConfiguration = {
+      dcatUS1X: {
+        title: "{{old_title}}",
+      },
+    };
     const format: FeedFormat = "dcat-us";
     const version = "1.1";
-    const updatedTemplate = { foo: "bar" };
     const result = setFeedTemplate({
       feedsConfig,
       format,
       version,
       updatedTemplate,
     });
-    expect(result).toEqual({ dcatUS1X: { foo: "bar" } });
-    expect(setFeedConfigurationSpy).toHaveBeenCalledTimes(1);
+    expect(result.dcatUS1X).toBeDefined();
+    expect(result.dcatUS1X).toEqual(updatedTemplate);
+  });
+  it("sets DCAT US V3 configuration", async () => {
+    const updatedTemplate = {
+      title: "{{new_title}}",
+    };
+    const feedsConfig: IFeedsConfiguration = {
+      dcatUS1X: {
+        title: "{{old_title}}",
+      },
+    };
+    const format: FeedFormat = "dcat-us";
+    const version = "3.0";
+    const result = setFeedTemplate({
+      feedsConfig,
+      format,
+      version,
+      updatedTemplate,
+    });
+    expect(result.dcatUS3X).toBeDefined();
+    expect(result.dcatUS3X).toEqual(updatedTemplate);
+  });
+  it("sets DCAT US config from old dcatUS11 key", async () => {
+    const updatedTemplate = {
+      title: "{{new_title}}",
+    };
+    const feedsConfig: IFeedsConfiguration = {
+      dcatUS11: {
+        title: "{{old_title}}",
+      },
+    };
+    const format: FeedFormat = "dcat-us";
+    const version = "1.1";
+    const result = setFeedTemplate({
+      feedsConfig,
+      format,
+      version,
+      updatedTemplate,
+    });
+    expect(result.dcatUS1X).toBeDefined();
+    expect(result.dcatUS1X).toEqual(updatedTemplate);
+  });
+  it("throws error if DCAT US version is not supported", async () => {
+    const updatedTemplate = {
+      title: "{{new_title}}",
+    };
+    const feedsConfig: IFeedsConfiguration = {
+      dcatUS11: {
+        title: "{{old_title}}",
+      },
+    };
+    const format: FeedFormat = "dcat-us";
+    const version = "2.0";
+    try {
+      setFeedTemplate({
+        feedsConfig,
+        format,
+        version,
+        updatedTemplate,
+      });
+      expect(true).toBe(false);
+    } catch (err) {
+      expect(err).toBeDefined();
+    }
+  });
+  it("sets DCAT AP configuration", async () => {
+    const updatedTemplate = {
+      title: "{{new_title}}",
+    };
+    const feedsConfig: IFeedsConfiguration = {
+      dcatAP2XX: {
+        title: "{{old_title}}",
+      },
+    };
+    const format: FeedFormat = "dcat-ap";
+    const version = "2.1.1";
+    const result = setFeedTemplate({
+      feedsConfig,
+      format,
+      version,
+      updatedTemplate,
+    });
+    expect(result.dcatAP2XX).toBeDefined();
+    expect(result.dcatAP2XX).toEqual(updatedTemplate);
+  });
+  it("sets DCAT AP config from old dcatAP201 key", async () => {
+    const updatedTemplate = {
+      title: "{{new_title}}",
+    };
+    const feedsConfig: IFeedsConfiguration = {
+      dcatAP201: {
+        title: "{{old_title}}",
+      },
+    };
+    const format: FeedFormat = "dcat-ap";
+    const version = "2.0.1";
+    const result = setFeedTemplate({
+      feedsConfig,
+      format,
+      version,
+      updatedTemplate,
+    });
+    expect(result.dcatAP2XX).toBeDefined();
+    expect(result.dcatAP2XX).toEqual(updatedTemplate);
+  });
+  it("throws error if DCAT AP version is not supported", async () => {
+    const updatedTemplate = {
+      title: "{{new_title}}",
+    };
+    const feedsConfig: IFeedsConfiguration = {
+      dcatAP201: {
+        title: "{{old_title}}",
+      },
+    };
+    const format: FeedFormat = "dcat-ap";
+    const version = "3.0";
+    try {
+      setFeedTemplate({
+        feedsConfig,
+        format,
+        version,
+        updatedTemplate,
+      });
+      expect(true).toBe(false);
+    } catch (err) {
+      expect(err).toBeDefined();
+    }
+  });
+  it("sets RSS configuration", async () => {
+    const updatedTemplate = {
+      title: "{{new_title}}",
+    };
+    const feedsConfig: IFeedsConfiguration = {
+      rss2: {
+        title: "{{old_title}}",
+      },
+    };
+    const format: FeedFormat = "rss";
+    const version = "2.0";
+    const result = setFeedTemplate({
+      feedsConfig,
+      format,
+      version,
+      updatedTemplate,
+    });
+
+    setFeedTemplate({
+      feedsConfig,
+      format,
+      version,
+      updatedTemplate,
+    });
+    expect(result.rss2).toBeDefined();
+    expect(result.rss2).toEqual(updatedTemplate);
+  });
+  it("throws error if RSS version is not supported", async () => {
+    const updatedTemplate = {
+      title: "{{new_title}}",
+    };
+    const feedsConfig: IFeedsConfiguration = {
+      rss2: {
+        title: "{{old_title}}",
+      },
+    };
+    const format: FeedFormat = "rss";
+    const version = "3.0";
+    try {
+      setFeedTemplate({
+        feedsConfig,
+        format,
+        version,
+        updatedTemplate,
+      });
+      expect(true).toBe(false);
+    } catch (err) {
+      expect(err).toBeDefined();
+    }
+  });
+  it("throws error if feed is not supported", async () => {
+    const updatedTemplate = {
+      title: "{{new_title}}",
+    };
+    const feedsConfig: IFeedsConfiguration = {
+      rss2: {
+        title: "{{old_title}}",
+      },
+    };
+    const format = "unknown-feed" as any;
+    const version = "3.0";
+    try {
+      setFeedTemplate({
+        feedsConfig,
+        format,
+        version,
+        updatedTemplate,
+      });
+      expect(true).toBe(false);
+    } catch (err) {
+      expect(err).toBeDefined();
+    }
   });
 });
