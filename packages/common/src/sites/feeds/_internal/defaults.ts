87523fb0b1af58d8a393262a36e9866887732135
sansth1010 <sansth22@gmail.com>
2024-11-22 14:00:32 -0600

    fix(hub-common): fix default dcat-us 3 default template

--- packages/common/src/sites/feeds/_internal/defaults.ts
+++ packages/common/src/sites/feeds/_internal/defaults.ts
@@ -39,7 +39,7 @@ const DCAT_US_3X_DEFAULT = {
   "dct:description": "{{ description}}",
   "dct:issued": {
     "@value":
-      "{{ metadata.metadata.dataIdInfo.idCitation.date.pubDate || metadata.metadata.dataIdInfo.idCitation.date.createDate || created }}",
+      "{{ metadata.metadata.dataIdInfo.idCitation.date.pubDate:toISO || metadata.metadata.dataIdInfo.idCitation.date.createDate:toISO || created:toISO }}",
     "@type": "xsd:dateTime",
   },
 };
