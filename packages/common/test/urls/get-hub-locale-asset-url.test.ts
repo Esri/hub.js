560f570e8d0edbb621d40d485c6cb4b973a8f455
Michael Juniper <mjuniper@esri.com>
2025-01-16 09:09:08 -0700

    fix: fix hub_cdn_urls
    affects: @esri/hub-common

--- packages/common/test/urls/get-hub-locale-asset-url.test.ts
+++ packages/common/test/urls/get-hub-locale-asset-url.test.ts
@@ -2,35 +2,31 @@ import { IPortal } from "@esri/arcgis-rest-portal";
 import { getHubLocaleAssetUrl } from "../../src";
 import { cloneObject } from "../../src";
 
-describe("getHubLocaleAssetUrl", function() {
+describe("getHubLocaleAssetUrl", function () {
   const portal: IPortal = {
     id: "portal-id",
     name: "Some Portal",
     isPortal: true,
-    portalHostname: "devext.someportal.com"
+    portalHostname: "devext.someportal.com",
   };
 
-  it("isPortal = true", function() {
+  it("isPortal = true", function () {
     portal.isPortal = true;
     const url = getHubLocaleAssetUrl(portal);
     expect(url).toEqual("https://devext.someportal.com/apps/sites");
   });
 
-  it("isPortal = false", function() {
+  it("isPortal = false", function () {
     portal.isPortal = false;
     const url = getHubLocaleAssetUrl(portal);
-    expect(url).toEqual(
-      "https://d1011m9qpnz5mw.cloudfront.net/opendata-ui/assets"
-    );
+    expect(url).toEqual("https://hubdevcdn.arcgis.com/opendata-ui/assets");
   });
 
-  it("defaults to prod", function() {
+  it("defaults to prod", function () {
     const localPortal = cloneObject(portal);
     localPortal.isPortal = false;
     localPortal.portalHostname = "notexist.portal.com";
     const url = getHubLocaleAssetUrl(localPortal);
-    expect(url).toEqual(
-      "https://d1iq7pbacwn5rb.cloudfront.net/opendata-ui/assets"
-    );
+    expect(url).toEqual("https://hubcdn.arcgis.com/opendata-ui/assets");
   });
 });
