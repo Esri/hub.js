c9b1a7f157180a6be9d0c841d41359ad1dc62214
Mike Juniper <mjuniper@esri.com>
2025-01-16 10:51:32 -0700

    fix: fix hub_cdn_urls (#1774)

--- packages/common/src/urls/hub-cdn-urlmap.ts
+++ packages/common/src/urls/hub-cdn-urlmap.ts
@@ -2,7 +2,7 @@
  * Mapping between the AGO Env's and the Hub's asset CDNs
  */
 export const HUB_CDN_URLMAP = {
-  devext: "https://d1011m9qpnz5mw.cloudfront.net",
-  qaext: "https://dyq9ux9ryu4qj.cloudfront.net",
-  www: "https://d1iq7pbacwn5rb.cloudfront.net"
+  devext: "https://hubdevcdn.arcgis.com",
+  qaext: "https://hubqacdn.arcgis.com",
+  www: "https://hubcdn.arcgis.com",
 };
