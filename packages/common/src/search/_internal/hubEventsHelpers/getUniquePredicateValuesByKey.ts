6409d3cac9d59899a504a3de4cd0b40a115a09c5
velveetachef <jeremysabat@gmail.com>
2025-01-30 07:54:22 -0800

    refactor(hub-common): re-use getPredicateValuesByKey
    affects: @esri/hub-common

--- packages/common/src/search/_internal/hubEventsHelpers/getUniquePredicateValuesByKey.ts
+++ packages/common/src/search/_internal/hubEventsHelpers/getUniquePredicateValuesByKey.ts
@@ -1,21 +1,10 @@
 import { unique } from "../../../util";
 import { IFilter } from "../../types/IHubCatalog";
+import { getPredicateValuesByKey } from "./getPredicateValuesByKey";
 
 export const getUniquePredicateValuesByKey = <T>(
   filters: IFilter[],
   predicateKey: string
 ): T[] => {
-  const toPredicateValuesByKey = (a1: T[], filter: IFilter): T[] =>
-    filter.predicates.reduce<T[]>(
-      (a2, predicate) =>
-        Object.entries(predicate).reduce(
-          (a3, [key, val]) =>
-            key === predicateKey
-              ? [...a3, ...(Array.isArray(val) ? val : [val])]
-              : a3,
-          a2
-        ),
-      a1
-    );
-  return filters.reduce<T[]>(toPredicateValuesByKey, []).filter(unique);
+  return getPredicateValuesByKey<T>(filters, predicateKey).filter(unique);
 };
