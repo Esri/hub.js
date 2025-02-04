efdf12bd32bc1a727effd9d601b52bdc731312a9
Julianna Apicella <42875581+juliannaeapicella@users.noreply.github.com>
2025-02-04 13:36:50 -0500

    feat(hub-common): add content home url util (#1791)

---
+++ packages/common/src/urls/getContentHomeUrl.ts
@@ -0,0 +1,16 @@
+import { IPortal } from "@esri/arcgis-rest-portal/dist/esm/util/get-portal";
+import { IHubRequestOptions } from "../types";
+import { IRequestOptions } from "@esri/arcgis-rest-request/dist/esm/utils/IRequestOptions";
+import { getPortalUrl } from "./get-portal-url";
+
+/**
+ * Computes a URL to the "Content" tab in the AGO Home app for a given portal URL string, IPortal, IHubRequestOptions or IRequestOptions object
+ * @param portalUrlOrObject a portal URL string, IPortal, IHubRequestOptions or IRequestOptions object
+ * @returns a URL string for the AGO Home for the given portalUrlOrObject parameter
+ */
+export function getContentHomeUrl(
+  portalUrlOrObject?: string | IPortal | IHubRequestOptions | IRequestOptions
+): string {
+  const portalUrl = getPortalUrl(portalUrlOrObject);
+  return `${portalUrl}/home/content.html`;
+}
