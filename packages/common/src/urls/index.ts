ddbae1dd522e905196aee2e9c8c50aebf16bbd1b
Julianna Apicella <42875581+juliannaeapicella@users.noreply.github.com>
2025-02-04 13:15:11 -0500

    feat(hub-common): pr feedback

--- packages/common/src/urls/index.ts
+++ packages/common/src/urls/index.ts
@@ -16,6 +16,7 @@ export * from "./get-item-data-url";
 export * from "./convert-urls-to-anchor-tags";
 export * from "./getHubApiFromPortalUrl";
 export * from "./getPortalBaseFromOrgUrl";
+export * from "./getContentHomeUrl";
 export * from "./getGroupHomeUrl";
 export * from "./getUserHomeUrl";
 export * from "./get-campaign-url";
