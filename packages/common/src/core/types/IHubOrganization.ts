eaf0e9ed6e1723bc7dd5ed9e2eed5e8e55414817
Dave Bouwman <dave@davebouwman.com>
2025-02-19 17:03:50 -0700

    docs: add prop comments

--- packages/common/src/core/types/IHubOrganization.ts
+++ packages/common/src/core/types/IHubOrganization.ts
@@ -11,17 +11,53 @@ import { IHubLocation } from "./IHubLocation";
  * are useful for display in the arcgu-hub-entity-card
  */
 export interface IHubOrganization extends IHubEntityBase {
+  /**
+   * Access level of the org - only public or private
+   */
   access: AccessLevel;
+  /**
+   * Access to the whole Portal object
+   */
   portal: IPortal;
+  /**
+   * URL to the thumbnail image
+   */
   thumbnailUrl?: string;
+  /**
+   * URL to the org
+   */
   url: string;
+  /**
+   * ID of the org, same as the id property
+   */
   orgId?: string;
+  /**
+   * thumbnail image w/o URL
+   */
   thumbnail?: string;
+  /**
+   * description of the org
+   */
   description?: string;
   // PROPS THAT ARE NEEDED TO BE A HUBENTITY BUT DON'T APPLY TO A PORTAL
+  /**
+   * owner. Always null for an org
+   */
   owner?: string;
+  /**
+   * typeKeywords. Always empty for an org
+   */
   typeKeywords: string[];
+  /**
+   * tags. Always empty for an org
+   */
   tags: string[];
+  /**
+   * view settings - always null for an org
+   */
   view?: IWithViewSettings;
+  /**
+   * location - always null for an org
+   */
   location?: IHubLocation;
 }
