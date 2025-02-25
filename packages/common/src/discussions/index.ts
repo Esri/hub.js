9d79963c8534c2635794390b35044341c2e2b5d9
Randy Weber <rweber@esri.com>
2025-02-25 13:32:30 -0500

    feat(hub-common): re-add export for MENTION_ATTRIBUTE const, minor test updates
    affects: @esri/hub-common
    
    ISSUES CLOSED: 12495

--- packages/common/src/discussions/index.ts
+++ packages/common/src/discussions/index.ts
@@ -1,6 +1,6 @@
 export * from "./HubDiscussion";
 export * from "./edit";
 export * from "./fetch";
-export { CANNOT_DISCUSS } from "./constants";
+export * from "./constants";
 export * from "./utils";
 export * from "./api";
