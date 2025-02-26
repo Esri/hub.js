fb1166ca6748570e20a1b3d3fabdd6ddcafb71e5
Randy Weber <rweber@esri.com>
2025-02-26 16:21:44 -0500

    test(hub-common): unskip index.test.ts to see if it passes after clyclic deps removed
    affects: @esri/hub-common
    
    ISSUES CLOSED: 12495

--- packages/common/test/index.test.ts
+++ packages/common/test/index.test.ts
@@ -37,7 +37,7 @@ import * as hubCommon from "../src";
  * and again if/when we change our export strategy/order...
  */
 
-xdescribe("index", () => {
+describe("index", () => {
   const expectedMembers = [
     "OperationStack",
     "OperationError",
