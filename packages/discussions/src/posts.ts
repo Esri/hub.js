58c1b1d5403e322739dbb53667758cbaf5ac7cf6
Randy Weber <52967037+rweber-esri@users.noreply.github.com>
2025-02-27 10:04:47 -0500

    feat(hub-discussions): remove all fns, types & tests from hub-discuss… (#1811)

---
+++ packages/discussions/src/posts.ts
@@ -0,0 +1,18 @@
+export {
+  searchPosts,
+  exportPosts,
+  createPost,
+  createReply,
+  fetchPost,
+  removePost,
+  updatePost,
+  updatePostStatus,
+  searchPostsV2,
+  exportPostsV2,
+  createPostV2,
+  createReplyV2,
+  fetchPostV2,
+  removePostV2,
+  updatePostV2,
+  updatePostStatusV2,
+} from "@esri/hub-common";
---
+++ packages/discussions/src/utils/channels.ts
@@ -0,0 +1,15 @@
+export {
+  // V1
+  canCreateChannel,
+  canDeleteChannel,
+  canEditChannel,
+  canModifyChannel,
+  canPostToChannel,
+  canReadChannel,
+  canReadFromChannel,
+  // V2
+  canCreateChannelV2,
+  canDeleteChannelV2,
+  canEditChannelV2,
+  canReadChannelV2,
+} from "@esri/hub-common";
