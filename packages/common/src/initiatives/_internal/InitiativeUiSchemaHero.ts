69c8f39477f1b7a3d7cb887de480f51595c31dfd
juliannemarik <julianne.marik@gmail.com>
2025-03-07 13:35:29 -0600

    fix: add image editing and alt text to uiSchema

--- packages/common/src/initiatives/_internal/InitiativeUiSchemaHero.ts
+++ packages/common/src/initiatives/_internal/InitiativeUiSchemaHero.ts
@@ -1,6 +1,7 @@
-import { IUiSchema } from "../../core/schemas/types";
+import { IUiSchema, UiSchemaRuleEffects } from "../../core/schemas/types";
 import type { IArcGISContext } from "../../types/IArcGISContext";
 import { IHubInitiative } from "../../core";
+import { getAuthedImageUrl } from "../../core/_internal/getAuthedImageUrl";
 
 /**
  * @private
@@ -33,6 +34,51 @@ export const buildUiSchema = async (
             `{{${i18nScope}.fields.hero.image.description:translate}}`,
           ],
           icons: ["map-pin", "image"],
+        },
+      },
+      {
+        labelKey: `${i18nScope}.fields.featuredImage.label`,
+        scope: "/properties/view/properties/featuredImage",
+        type: "Control",
+        rule: {
+          effect: UiSchemaRuleEffects.HIDE,
+          condition: {
+            scope: "/properties/view/properties/hero",
+            schema: { const: "map" },
+          },
+        },
+        options: {
+          control: "hub-field-input-image-picker",
+          imgSrc: getAuthedImageUrl(
+            options.view?.featuredImageUrl,
+            context.requestOptions
+          ),
+          maxWidth: 727,
+          maxHeight: 484,
+          aspectRatio: 1.5,
+          sizeDescription: {
+            labelKey: `${i18nScope}.fields.featuredImage.sizeDescription`,
+          },
+          helperText: {
+            labelKey: `${i18nScope}.fields.featuredImage.helperText`,
+          },
+        },
+      },
+      {
+        labelKey: `${i18nScope}.fields.featuredImage.altText.label`,
+        scope: "/properties/view/properties/featuredImageAltText",
+        type: "Control",
+        rule: {
+          effect: UiSchemaRuleEffects.HIDE,
+          condition: {
+            scope: "/properties/view/properties/hero",
+            schema: { const: "map" },
+          },
+        },
+        options: {
+          helperText: {
+            labelKey: `${i18nScope}.fields.featuredImage.altText.helperText`,
+          },
         },
       },
     ],
