fef1c2c3591d4e7ae4955509b44a1f0b4a118846
Tom Wayson <tom@tomwayson.com>
2025-03-28 13:32:59 -0700

    feat(hub-downloads): upgrade to ArcGIS REST JS 4
    affects: @esri/hub-downloads
    
    BREAKING CHANGE:
    UserSession -> ArcGISIdentityManager

--- packages/downloads/src/poll-download-metadata.ts
+++ packages/downloads/src/poll-download-metadata.ts
@@ -1,5 +1,5 @@
 import * as EventEmitter from "eventemitter3";
-import type { UserSession } from "@esri/arcgis-rest-auth";
+import type { ArcGISIdentityManager } from "@esri/arcgis-rest-request";
 import { hubPollDownloadMetadata } from "./hub/hub-poll-download-metadata";
 import { portalPollExportJobStatus } from "./portal/portal-poll-export-job-status";
 import { DownloadFormat } from "./download-format";
@@ -26,7 +26,7 @@ export interface IPollDownloadMetadataRequestParams {
   /* A SQL-style WHERE filter for attribute values.  Applicable to Hub API downloads only. */
   where?: string;
   /* Required for Portal downloads only. */
-  authentication?: UserSession;
+  authentication?: ArcGISIdentityManager;
   /* Identifier for the export job. Required for Portal downloads only. */
   jobId?: string;
   /* Time-stamp for export start. Required for Portal downloads only. */
