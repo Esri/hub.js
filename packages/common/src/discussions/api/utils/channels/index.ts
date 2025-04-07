86a3669d0abe22567d2d6f11baf4fa178ed658b8
Randy Weber <rweber@esri.com>
2025-04-07 10:16:28 -0400

    refactor(hub-common): temp remove discussion api v1 fns
    affects: @esri/hub-common
    
    ISSUES CLOSED: 12668

--- packages/common/src/discussions/api/utils/channels/index.ts
+++ packages/common/src/discussions/api/utils/channels/index.ts
@@ -1,11 +1,3 @@
-// V1
-export { canCreateChannel } from "./can-create-channel";
-export { canDeleteChannel } from "./can-delete-channel";
-export { canEditChannel } from "./can-edit-channel";
-export { canModifyChannel } from "./can-modify-channel";
-export { canPostToChannel } from "./can-post-to-channel";
-export { canReadChannel, canReadFromChannel } from "./can-read-channel";
-
 // V2
 export { canCreateChannelV2 } from "./can-create-channel-v2";
 export { canDeleteChannelV2 } from "./can-delete-channel-v2";
--- packages/common/src/discussions/api/utils/posts/index.ts
+++ packages/common/src/discussions/api/utils/posts/index.ts
@@ -1,10 +1,3 @@
-// V1
-export { canCreatePost } from "./can-create-post";
-export { canCreateReply } from "./can-create-reply";
-export { canDeletePost } from "./can-delete-post";
-export { canModifyPostStatus, canEditPostStatus } from "./can-edit-post-status";
-export { canModifyPost, canEditPost } from "./can-edit-post";
-
 // V2
 export { parseDiscussionURI } from "./parse-discussion-uri";
 export { parseMentionedUsers } from "./parse-mentioned-users";
