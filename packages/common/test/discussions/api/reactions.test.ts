cf860d3fb6b4b0c7e9fd2ac127c9b267d94f63da
Randy Weber <rweber@esri.com>
2025-04-07 10:16:28 -0400

    refactor(hub-common): temp remove discussion api v1 fns
    affects: @esri/hub-common
    
    ISSUES CLOSED: 12668

--- packages/common/test/discussions/api/reactions.test.ts
+++ packages/common/test/discussions/api/reactions.test.ts
@@ -1,7 +1,5 @@
 import {
-  createReaction,
   createReactionV2,
-  removeReaction,
   removeReactionV2,
 } from "../../../src/discussions/api/reactions";
 import * as discussionsApiRequestModule from "../../../src/discussions/api/discussions-api-request";
@@ -9,55 +7,6 @@ import {
   IDiscussionsRequestOptions,
   PostReaction,
 } from "../../../src/discussions/api/types";
-
-describe("reactions", () => {
-  let requestSpy: any;
-  const response = new Response("ok", { status: 200 });
-  const baseOpts: IDiscussionsRequestOptions = {
-    hubApiUrl: "https://hub.arcgis.com/api",
-    authentication: undefined,
-  };
-
-  beforeEach(() => {
-    requestSpy = spyOn(
-      discussionsApiRequestModule,
-      "discussionsApiRequest"
-    ).and.returnValue(Promise.resolve(response));
-  });
-
-  it("creates a reaction to a post", (done) => {
-    const postId = "postId";
-    const body = { postId, value: PostReaction.THUMBS_UP };
-
-    const options = { ...baseOpts, data: body };
-
-    createReaction(options)
-      .then(() => {
-        expect(requestSpy.calls.count()).toEqual(1);
-        const [url, opts] = requestSpy.calls.argsFor(0);
-        expect(url).toEqual(`/reactions`);
-        expect(opts).toEqual({ ...options, httpMethod: "POST" });
-        done();
-      })
-      .catch(() => fail());
-  });
-
-  it("deletes a reaction to a post", (done) => {
-    const reactionId = "reactionId";
-
-    const options = { ...baseOpts, reactionId };
-
-    removeReaction(options)
-      .then(() => {
-        expect(requestSpy.calls.count()).toEqual(1);
-        const [url, opts] = requestSpy.calls.argsFor(0);
-        expect(url).toEqual(`/reactions/${reactionId}`);
-        expect(opts).toEqual({ ...options, httpMethod: "DELETE" });
-        done();
-      })
-      .catch(() => fail());
-  });
-});
 
 describe("reactions V2", () => {
   let requestSpyV2: any;
