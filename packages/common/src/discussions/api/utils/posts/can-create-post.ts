2e9328e9b2708c31ef3d37db8761b588cf3cbbf0
Randy Weber <rweber@esri.com>
2025-04-07 16:16:36 -0400

    Revert "refactor(hub-common): temp remove discussion api v1 fns"
    This reverts commit dd7ccfb48687d7e3c287cbde5cd3222e30447cdc.

---
+++ packages/common/src/discussions/api/utils/posts/can-create-post.ts
@@ -0,0 +1,81 @@
+import { IGroup, IUser } from "@esri/arcgis-rest-portal";
+import { IChannel, IDiscussionsUser, SharingAccess } from "../../types";
+import { hasOrgAdminUpdateRights } from "../portal-privilege";
+import { CANNOT_DISCUSS } from "../../../constants";
+
+const ALLOWED_GROUP_ROLES = Object.freeze(["owner", "admin", "member"]);
+
+type ILegacyChannelPermissions = Pick<IChannel, "groups" | "orgs" | "access">;
+
+/**
+ * Utility to determine if User has privileges to create a post in a channel
+ *
+ * @deprecated replace with canCreatePostV2 for v2 discussions
+ * @export
+ * @param channel
+ * @param user
+ * @returns {boolean}
+ */
+export function canCreatePost(
+  channel: IChannel,
+  user: IUser | IDiscussionsUser = {}
+): boolean {
+  const { access, groups, orgs } = channel;
+
+  if (hasOrgAdminUpdateRights(user, channel.orgId)) {
+    return true;
+  }
+
+  return isAuthorizedToPostByLegacyPermissions(user, {
+    access,
+    groups,
+    orgs,
+  });
+}
+
+function isAuthorizedToPostByLegacyPermissions(
+  user: IUser | IDiscussionsUser,
+  channelParams: ILegacyChannelPermissions
+): boolean {
+  const { username, groups: userGroups, orgId: userOrgId } = user;
+  const { access, groups, orgs } = channelParams;
+
+  if (!username) {
+    return false;
+  }
+
+  if (access === SharingAccess.PUBLIC) {
+    return true;
+  }
+
+  if (access === SharingAccess.PRIVATE) {
+    return isAuthorizedToPostByLegacyGroup(groups, userGroups);
+  }
+
+  if (access === SharingAccess.ORG) {
+    return orgs.includes(userOrgId);
+  }
+
+  return false;
+}
+
+function isAuthorizedToPostByLegacyGroup(
+  channelGroups: string[] = [],
+  userGroups: IGroup[] = []
+) {
+  return channelGroups.some((channelGroupId: string) => {
+    return userGroups.some((group: IGroup) => {
+      const {
+        id: userGroupId,
+        userMembership: { memberType: userMemberType },
+        typeKeywords,
+      } = group;
+
+      return (
+        channelGroupId === userGroupId &&
+        ALLOWED_GROUP_ROLES.includes(userMemberType) &&
+        !typeKeywords.includes(CANNOT_DISCUSS)
+      );
+    });
+  });
+}
