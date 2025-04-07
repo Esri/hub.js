0623a4afa043a41effadbd2290238c66f281107c
Randy Weber <rweber@esri.com>
2025-04-07 16:16:36 -0400

    Revert "refactor(hub-common): temp remove discussion api v1 fns"
    This reverts commit dd7ccfb48687d7e3c287cbde5cd3222e30447cdc.

--- packages/common/src/discussions/api/utils/reactions/index.ts
+++ packages/common/src/discussions/api/utils/reactions/index.ts
@@ -1,1 +1,2 @@
+export { canCreateReaction } from "./can-create-reaction";
 export { canCreateReactionV2 } from "./can-create-reaction-v2";
--- packages/common/test/discussions/api/channels.test.ts
+++ packages/common/test/discussions/api/channels.test.ts
@@ -1,4 +1,13 @@
 import {
+  createChannel,
+  searchChannels,
+  fetchChannel,
+  updateChannel,
+  removeChannel,
+  fetchChannelNotifcationOptOut,
+  createChannelNotificationOptOut,
+  removeChannelNotificationOptOut,
+  removeChannelActivity,
   createChannelV2,
   searchChannelsV2,
   fetchChannelV2,
@@ -15,6 +24,8 @@ import {
   AclSubCategory,
   IDiscussionsRequestOptions,
   IFetchChannelParams,
+  ICreateChannel,
+  IUpdateChannel,
   ICreateChannelV2,
   IUpdateChannelV2,
   PostReaction,
@@ -22,6 +33,180 @@ import {
   Role,
   SharingAccess,
 } from "../../../src/discussions/api/types";
+
+describe("channels V1", () => {
+  let requestSpy: any;
+  const response = new Response("ok", { status: 200 });
+  const baseOpts: IDiscussionsRequestOptions = {
+    hubApiUrl: "https://hub.arcgis.com/api",
+    authentication: null,
+  } as unknown as IDiscussionsRequestOptions;
+
+  beforeEach(() => {
+    requestSpy = spyOn(
+      discussionsApiRequestModule,
+      "discussionsApiRequest"
+    ).and.returnValue(Promise.resolve(response));
+  });
+
+  it("searchChannels", (done) => {
+    const query = {
+      access: [SharingAccess.PUBLIC],
+      groups: ["foo"],
+    };
+
+    const options = { ...baseOpts, data: query };
+    searchChannels(options)
+      .then(() => {
+        expect(requestSpy.calls.count()).toEqual(1);
+        const [url, opts] = requestSpy.calls.argsFor(0);
+        expect(url).toEqual(`/channels`);
+        expect(opts).toEqual({ ...options, httpMethod: "GET" });
+        done();
+      })
+      .catch(() => fail());
+  });
+
+  it("creates channel", (done) => {
+    const body: ICreateChannel = {
+      access: SharingAccess.PRIVATE,
+      groups: ["groupId"],
+      allowReply: true,
+      allowAnonymous: true,
+      softDelete: true,
+      defaultPostStatus: PostStatus.APPROVED,
+      allowReaction: true,
+      allowedReactions: [PostReaction.HEART],
+    };
+
+    const options = { ...baseOpts, data: body };
+
+    createChannel(options)
+      .then(() => {
+        expect(requestSpy.calls.count()).toEqual(1);
+        const [url, opts] = requestSpy.calls.argsFor(0);
+        expect(url).toEqual(`/channels`);
+        expect(opts).toEqual({ ...options, httpMethod: "POST" });
+        done();
+      })
+      .catch(() => fail());
+  });
+
+  it("gets channel", (done) => {
+    const channelId = "channelId";
+
+    const options = { ...baseOpts, channelId };
+
+    fetchChannel(options as IFetchChannelParams)
+      .then(() => {
+        expect(requestSpy.calls.count()).toEqual(1);
+        const [url, opts] = requestSpy.calls.argsFor(0);
+        expect(url).toEqual(`/channels/${channelId}`);
+        expect(opts).toEqual({ ...options, httpMethod: "GET" });
+        done();
+      })
+      .catch(() => fail());
+  });
+
+  it("updates channel", (done) => {
+    const channelId = "channelId";
+    const body: IUpdateChannel = {
+      allowReaction: false,
+    };
+
+    const options = { ...baseOpts, channelId, data: body };
+
+    updateChannel(options)
+      .then(() => {
+        expect(requestSpy.calls.count()).toEqual(1);
+        const [url, opts] = requestSpy.calls.argsFor(0);
+        expect(url).toEqual(`/channels/${channelId}`);
+        expect(opts).toEqual({ ...options, httpMethod: "PATCH" });
+        done();
+      })
+      .catch(() => fail());
+  });
+
+  it("deletes channel", (done) => {
+    const channelId = "channelId";
+
+    const options = { ...baseOpts, channelId };
+
+    removeChannel(options)
+      .then(() => {
+        expect(requestSpy.calls.count()).toEqual(1);
+        const [url, opts] = requestSpy.calls.argsFor(0);
+        expect(url).toEqual(`/channels/${channelId}`);
+        expect(opts).toEqual({ ...options, httpMethod: "DELETE" });
+        done();
+      })
+      .catch(() => fail());
+  });
+
+  it("gets channel opt out status", (done) => {
+    const channelId = "channelId";
+
+    const options = { ...baseOpts, channelId };
+
+    fetchChannelNotifcationOptOut(options)
+      .then(() => {
+        expect(requestSpy.calls.count()).toEqual(1);
+        const [url, opts] = requestSpy.calls.argsFor(0);
+        expect(url).toEqual(`/channels/${channelId}/notifications/opt-out`);
+        expect(opts).toEqual({ ...options, httpMethod: "GET" });
+        done();
+      })
+      .catch(() => fail());
+  });
+
+  it("creates channel opt out", (done) => {
+    const channelId = "channelId";
+
+    const options = { ...baseOpts, channelId };
+
+    createChannelNotificationOptOut(options)
+      .then(() => {
+        expect(requestSpy.calls.count()).toEqual(1);
+        const [url, opts] = requestSpy.calls.argsFor(0);
+        expect(url).toEqual(`/channels/${channelId}/notifications/opt-out`);
+        expect(opts).toEqual({ ...options, httpMethod: "POST" });
+        done();
+      })
+      .catch(() => fail());
+  });
+
+  it("deletes channel opt out", (done) => {
+    const channelId = "channelId";
+
+    const options = { ...baseOpts, channelId };
+
+    removeChannelNotificationOptOut(options)
+      .then(() => {
+        expect(requestSpy.calls.count()).toEqual(1);
+        const [url, opts] = requestSpy.calls.argsFor(0);
+        expect(url).toEqual(`/channels/${channelId}/notifications/opt-out`);
+        expect(opts).toEqual({ ...options, httpMethod: "DELETE" });
+        done();
+      })
+      .catch(() => fail());
+  });
+
+  it("deletes channel activity", (done) => {
+    const channelId = "channelId";
+
+    const options = { ...baseOpts, channelId };
+
+    removeChannelActivity(options)
+      .then(() => {
+        expect(requestSpy.calls.count()).toEqual(1);
+        const [url, opts] = requestSpy.calls.argsFor(0);
+        expect(url).toEqual(`/channels/${channelId}/activity`);
+        expect(opts).toEqual({ ...options, httpMethod: "DELETE" });
+        done();
+      })
+      .catch(() => fail());
+  });
+});
 
 describe("channels V2", () => {
   let requestSpyV2: any;
