e3fb7350a7aef5f8ac2c1458bf680f12c8b492e2
Randy Weber <52967037+rweber-esri@users.noreply.github.com>
2025-04-17 11:25:06 -0400

    feat(hub-common): updates the create and edit channel UISchemas to co… (#1868)

--- packages/common/src/discussions/api/utils/channels/index.ts
+++ packages/common/src/discussions/api/utils/channels/index.ts
@@ -11,3 +11,4 @@ export { canCreateChannelV2 } from "./can-create-channel-v2";
 export { canDeleteChannelV2 } from "./can-delete-channel-v2";
 export { canEditChannelV2 } from "./can-edit-channel-v2";
 export { canReadChannelV2 } from "./can-read-channel-v2";
+export { deriveUserRoleV2 } from "./derive-user-role-v2";
