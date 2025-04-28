6f3dc0644b912c766bf7c5c0753f44ba4b07ba02
Caleb Pomar <cpomar@esri.com>
2025-04-28 16:00:31 -0600

    fix(hub-common): actually ungate the user workspace for prod when opted into workspaces
    affects: @esri/hub-common

--- packages/common/src/users/_internal/UserBusinessRules.ts
+++ packages/common/src/users/_internal/UserBusinessRules.ts
@@ -48,7 +48,7 @@ export const UserPermissionPolicies: IPermissionPolicy[] = [
   },
   {
     permission: "hub:user:workspace",
-    environments: ["qaext", "devext"],
+    environments: ["production", "qaext", "devext"],
     // seems like this should also depend on hub:user,
     // but other entities don't do that
     dependencies: ["hub:feature:workspace"],
