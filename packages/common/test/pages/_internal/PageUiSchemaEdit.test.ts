9930bcb56e31ad13ceb280f9b7dd21b96ced8559
Aaron Putterman <60486980+abp6318@users.noreply.github.com>
2025-04-30 12:03:04 -0400

    feat: moving notice into image picker (#1884)

--- packages/common/test/pages/_internal/PageUiSchemaEdit.test.ts
+++ packages/common/test/pages/_internal/PageUiSchemaEdit.test.ts
@@ -128,31 +128,8 @@ describe("buildUiSchema: page edit", () => {
                 sizeDescription: {
                   labelKey: "shared.fields._thumbnail.sizeDescription",
                 },
+                notice: null,
               },
-            },
-            {
-              type: "Notice",
-              options: {
-                notice: {
-                  configuration: {
-                    id: "no-thumbnail-or-png-notice",
-                    noticeType: "notice",
-                    closable: false,
-                    icon: "lightbulb",
-                    kind: "info",
-                    scale: "m",
-                  },
-                  message:
-                    "{{shared.fields._thumbnail.defaultThumbnailNotice:translate}}",
-                  autoShow: true,
-                },
-              },
-              rules: [
-                {
-                  effect: UiSchemaRuleEffects.SHOW,
-                  conditions: [false],
-                },
-              ],
             },
             mockSlugElement,
             {
--- packages/common/test/sites/_internal/SiteUiSchemaEdit.test.ts
+++ packages/common/test/sites/_internal/SiteUiSchemaEdit.test.ts
@@ -128,31 +128,8 @@ describe("buildUiSchema: site edit", () => {
                 sizeDescription: {
                   labelKey: "shared.fields._thumbnail.sizeDescription",
                 },
+                notice: null,
               },
-            },
-            {
-              type: "Notice",
-              options: {
-                notice: {
-                  configuration: {
-                    id: "no-thumbnail-or-png-notice",
-                    noticeType: "notice",
-                    closable: false,
-                    icon: "lightbulb",
-                    kind: "info",
-                    scale: "m",
-                  },
-                  message:
-                    "{{shared.fields._thumbnail.defaultThumbnailNotice:translate}}",
-                  autoShow: true,
-                },
-              },
-              rules: [
-                {
-                  effect: UiSchemaRuleEffects.SHOW,
-                  conditions: [false],
-                },
-              ],
             },
             {
               labelKey: "some.scope.fields.tags.label",
@@ -346,31 +323,8 @@ describe("buildUiSchema: site edit", () => {
                 sizeDescription: {
                   labelKey: "shared.fields._thumbnail.sizeDescription",
                 },
+                notice: null,
               },
-            },
-            {
-              type: "Notice",
-              options: {
-                notice: {
-                  configuration: {
-                    id: "no-thumbnail-or-png-notice",
-                    noticeType: "notice",
-                    closable: false,
-                    icon: "lightbulb",
-                    kind: "info",
-                    scale: "m",
-                  },
-                  message:
-                    "{{shared.fields._thumbnail.defaultThumbnailNotice:translate}}",
-                  autoShow: true,
-                },
-              },
-              rules: [
-                {
-                  effect: UiSchemaRuleEffects.SHOW,
-                  conditions: [false],
-                },
-              ],
             },
             mockSlugElement,
             {
