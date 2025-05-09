b6cbcfe2b6d259f7491ac58f2cfa52302924044a
Michael Juniper <mjuniper@esri.com>
2025-05-09 14:22:39 -0600

    fix: properly prefix slug with orgUrlKey; fallback to looking for items with slug typeKeyword lackin
    affects: @esri/hub-common

--- packages/common/src/items/fetch.ts
+++ packages/common/src/items/fetch.ts
@@ -24,19 +24,33 @@ export function fetchItem(
   requestOptions: IFetchItemOptions
 ): Promise<IItem> {
   const { id, slug, orgKey } = parseIdentifier(identifier);
+
   if (id) {
     // use the id to fetch the item
     return getItem(id, requestOptions);
   }
+
   // we'll have to look up by slug
   // first, ensure the slug has the org key
   const fullyQualifiedSlug = addContextToSlug(
     slug,
     orgKey || requestOptions.siteOrgKey
   );
+
   const slugKeyword = uriSlugToKeywordSlug(fullyQualifiedSlug);
-  return findItemsBySlug({ slug: slugKeyword }, requestOptions).then(
-    (results) => {
+
+  return findItemsBySlug({ slug: slugKeyword }, requestOptions)
+    .then((results) => {
+      if (results.length) {
+        return results;
+      } else {
+        // we had a bug where page slugs could have been stored without the org url key prefix
+        // we decided to fall back to looking for them without the prefix
+        const slugWithoutOrgKey = slugKeyword.split("|").slice(1).join("|");
+        return findItemsBySlug({ slug: slugWithoutOrgKey }, requestOptions);
+      }
+    })
+    .then((results) => {
       if (results.length) {
         // search results only include a subset of properties of the item, so
         // issue a subsequent call to getItem to get the full item details
@@ -44,6 +58,5 @@ export function fetchItem(
       } else {
         return null;
       }
-    }
-  );
+    });
 }
