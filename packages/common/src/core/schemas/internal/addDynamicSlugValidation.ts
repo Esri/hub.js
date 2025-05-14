165138de91ed1ba34d1280c8a13b42725b23b719
Vivian Zhang <vivian.mzh@gmail.com>
2025-05-14 15:26:09 -0700

    fix: configuration editor now allows an empty value for the slug field
    affects: @esri/hub-common
    
    ISSUES CLOSED: 13184

--- packages/common/src/core/schemas/internal/addDynamicSlugValidation.ts
+++ packages/common/src/core/schemas/internal/addDynamicSlugValidation.ts
@@ -26,13 +26,29 @@ export const addDynamicSlugValidation = (
   _slug.maxLength = getSlugMaxLength(orgUrlKey);
   // add conditional validation to ensure slug is unique
   const allOf = cloneObject(schema.allOf) || [];
+
   const { pattern } = _slug;
+  // remove pattern from base schema so the form doesn't always apply the pattern
+  // validation. We will add it back to the allOf conditionally
+  delete _slug.pattern;
+
   const { id } = options as HubEntity;
-  allOf.push({
-    // only do async isUniqueSlug check if the slug is valid
-    if: { properties: { _slug: { pattern } } },
-    then: { properties: { _slug: { isUniqueSlug: { id, orgUrlKey } } as any } },
-  });
+
+  allOf.push(
+    {
+      // only enforce the pattern if slug has been entered
+      if: { properties: { _slug: { minLength: 1 } } },
+      then: { properties: { _slug: { pattern } } },
+    },
+    {
+      // only do async isUniqueSlug check if the slug is valid
+      if: { properties: { _slug: { pattern } } },
+      then: {
+        properties: { _slug: { isUniqueSlug: { id, orgUrlKey } } as any },
+      },
+    }
+  );
+
   const clone = cloneObject(schema);
   clone.properties._slug = _slug;
   clone.allOf = allOf;
