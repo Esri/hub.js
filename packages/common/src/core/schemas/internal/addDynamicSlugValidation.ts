f91846dfcf51d3d516c96c0fd14ec78172e54cb5
Vivian Zhang <vivian.mzh@gmail.com>
2025-05-15 12:28:46 -0700

    Revert "chore: move allOf with empty check to subschema instead"
    This reverts commit 80adc2df104194e7d7a2b8424766f7f1a0926e69.

--- packages/common/src/core/schemas/internal/addDynamicSlugValidation.ts
+++ packages/common/src/core/schemas/internal/addDynamicSlugValidation.ts
@@ -28,16 +28,26 @@ export const addDynamicSlugValidation = (
   const allOf = cloneObject(schema.allOf) || [];
 
   const { pattern } = _slug;
+  // remove pattern from base schema so the form doesn't always apply the pattern
+  // validation. We will add it back to the allOf conditionally
+  delete _slug.pattern;
 
   const { id } = options as HubEntity;
 
-  allOf.push({
-    // only do async isUniqueSlug check if the slug is valid
-    if: { properties: { _slug: { pattern } } },
-    then: {
-      properties: { _slug: { isUniqueSlug: { id, orgUrlKey } } as any },
+  allOf.push(
+    {
+      // only enforce the pattern if slug has been entered
+      if: { properties: { _slug: { minLength: 1 } } },
+      then: { properties: { _slug: { pattern } } },
     },
-  });
+    {
+      // only do async isUniqueSlug check if the slug is valid
+      if: { properties: { _slug: { pattern } } },
+      then: {
+        properties: { _slug: { isUniqueSlug: { id, orgUrlKey } } as any },
+      },
+    }
+  );
 
   const clone = cloneObject(schema);
   clone.properties._slug = _slug;
