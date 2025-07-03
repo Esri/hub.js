edf8f232eb70a95de914664c3b54f6767e31c628
Caleb Pomar <cpsharksfan@gmail.com>
2025-07-03 12:06:13 -0600

    fix(hub-common): fix issue where org home sites didn't have default group and user catalog scopes (#1933)

--- packages/common/test/search/upgradeCatalogSchema.test.ts
+++ packages/common/test/search/upgradeCatalogSchema.test.ts
@@ -52,19 +52,47 @@ describe("upgradeCatalogSchema", () => {
   });
 
   it("handles org-level home site legacy catalogs", () => {
-    const chk = upgradeCatalogSchema({ orgId: "a3g" });
+    const chk = upgradeCatalogSchema({
+      orgId: "{{orgId}}", // org sites have this value interpolated in later on
+      groups: ["abc"], // groups should be ignored for org-level sites
+    });
     expect(chk.title).toBe("Default Catalog");
     expect(chk.scopes).toBeDefined();
-    expect(chk.scopes?.item?.filters.length).toBe(1);
-    expect(chk.scopes?.item?.filters[0].operation).toEqual("AND");
-    expect(chk.scopes?.item?.filters[0].predicates[0].orgid).toEqual(["a3g"]);
-    expect(chk.scopes?.item?.filters[0].predicates[1].type).toEqual({
+
+    const scopeKeys = Object.keys(chk.scopes).sort();
+    expect(scopeKeys).toEqual(["event", "group", "item", "user"]);
+
+    expect(chk.scopes.item).toBeDefined();
+    expect(chk.scopes.item.filters.length).toBe(1);
+    expect(chk.scopes.item.filters[0].operation).toEqual("AND");
+    expect(chk.scopes.item.filters[0].predicates[0].orgid).toEqual([
+      "{{orgId}}",
+    ]);
+    expect(chk.scopes.item.filters[0].predicates[1].type).toEqual({
       not: ["Code Attachment"],
     });
-    expect(chk.scopes?.event?.filters.length).toBe(1);
-    expect(chk.scopes?.event?.filters[0].operation).toEqual("AND");
-    expect(chk.scopes?.event?.filters[0].predicates[0].orgId).toEqual("a3g");
-    expect(chk.scopes?.event?.filters[0].predicates[1]).toBeUndefined();
+
+    expect(chk.scopes.event).toBeDefined();
+    expect(chk.scopes.event.filters.length).toBe(1);
+    expect(chk.scopes.event.filters[0].operation).toEqual("AND");
+    expect(chk.scopes.event.filters[0].predicates[0].orgId).toEqual(
+      "{{orgId}}"
+    );
+    expect(chk.scopes.event.filters[0].predicates[1]).toBeUndefined();
+
+    expect(chk.scopes.group).toBeDefined();
+    expect(chk.scopes.group.filters.length).toBe(1);
+    expect(chk.scopes.group.filters[0].operation).toEqual("AND");
+    expect(chk.scopes.group.filters[0].predicates[0].orgid).toEqual(
+      "{{orgId}}"
+    );
+    expect(chk.scopes.group.filters[0].predicates[1]).toBeUndefined();
+
+    expect(chk.scopes.user).toBeDefined();
+    expect(chk.scopes.user.filters.length).toBe(1);
+    expect(chk.scopes.user.filters[0].operation).toEqual("AND");
+    expect(chk.scopes.user.filters[0].predicates[0].orgid).toEqual("{{orgId}}");
+    expect(chk.scopes.user.filters[0].predicates[1]).toBeUndefined();
   });
 
   it("skips upgrade if on the same version", () => {
