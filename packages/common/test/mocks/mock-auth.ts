97af3adf86dddb892987428a23b60dc33d28cee7
DavidSpriggs <dspriggs@esri.com>
2025-07-17 09:51:14 -0500

    fix(hub-common): fix unit tests for new service

--- packages/common/test/mocks/mock-auth.ts
+++ packages/common/test/mocks/mock-auth.ts
@@ -123,6 +123,7 @@ export function getMockContextWithPrivilenges(
       "hub-search": "online",
       domains: "online",
       "hub-downloads": "online",
+      "hub-ai-assistant": "online",
     },
     userHubSettings: {
       schemaVersion: 1,
@@ -158,6 +159,7 @@ export const MOCK_CONTEXT = new ArcGISContext({
     "hub-search": "online",
     domains: "online",
     "hub-downloads": "online",
+    "hub-ai-assistant": "online",
   },
   userHubSettings: {
     schemaVersion: 1,
@@ -219,6 +221,7 @@ export const MOCK_ANON_CONTEXT = new ArcGISContext({
     "hub-search": "online",
     domains: "online",
     "hub-downloads": "online",
+    "hub-ai-assistant": "online",
   },
   userHubSettings: {
     schemaVersion: 1,
@@ -251,6 +254,7 @@ export function createMockContext(): ArcGISContext {
       "hub-search": "online",
       domains: "online",
       "hub-downloads": "online",
+      "hub-ai-assistant": "online",
     },
     userHubSettings: {
       schemaVersion: 1,
@@ -280,6 +284,7 @@ export function createMockAnonContext(): ArcGISContext {
       "hub-search": "online",
       domains: "online",
       "hub-downloads": "online",
+      "hub-ai-assistant": "online",
     },
     userHubSettings: {
       schemaVersion: 1,
