3bd62103ab40f322666b907353ce6b15599d627d
juliannemarik <julianne.marik@gmail.com>
2025-07-29 14:55:41 -0500

    chore: update HubInitiative spec tests

--- packages/common/test/initiatives/HubInitiative.test.ts
+++ packages/common/test/initiatives/HubInitiative.test.ts
@@ -1,5 +1,5 @@
 import * as PortalModule from "@esri/arcgis-rest-portal";
-import { IHubInitiative, IMetric, IResolvedMetric, getProp } from "../../src";
+import { IHubInitiative, IMetric, IResolvedMetric } from "../../src";
 import { Catalog } from "../../src";
 import { ArcGISContextManager } from "../../src/ArcGISContextManager";
 import { HubInitiative } from "../../src/initiatives/HubInitiative";
@@ -9,9 +9,6 @@ import * as ResolveMetricModule from "../../src/metrics/resolveMetric";
 import * as viewModule from "../../src/initiatives/view";
 import * as EditConfigModule from "../../src/core/schemas/getEditorConfig";
 import * as EnrichEntityModule from "../../src/core/enrichEntity";
-import * as upsertResourceModule from "../../src/resources/upsertResource";
-import * as doesResourceExistModule from "../../src/resources/doesResourceExist";
-import * as removeResourceModule from "../../src/resources/removeResource";
 import * as metricToEditorModule from "../../src/metrics/metricToEditor";
 import * as restPortalModule from "@esri/arcgis-rest-portal";
 import { HubItemEntity } from "../../src/core/HubItemEntity";
@@ -19,9 +16,7 @@ import { IHubAssociationRules } from "../../src/associations/types";
 
 describe("HubInitiative Class:", () => {
   let authdCtxMgr: ArcGISContextManager;
-  let unauthdCtxMgr: ArcGISContextManager;
   beforeEach(async () => {
-    unauthdCtxMgr = await ArcGISContextManager.create();
     // When we pass in all this information, the context
     // manager will not try to fetch anything, so no need
     // to mock those calls
@@ -76,7 +71,7 @@ describe("HubInitiative Class:", () => {
       const fetchSpy = spyOn(
         HubInitiativesModule,
         "fetchInitiative"
-      ).and.callFake((id: string) => {
+      ).and.callFake(() => {
         const err = new Error(
           "CONT_0001: Item does not exist or is inaccessible."
         );
@@ -95,7 +90,7 @@ describe("HubInitiative Class:", () => {
       const fetchSpy = spyOn(
         HubInitiativesModule,
         "fetchInitiative"
-      ).and.callFake((id: string) => {
+      ).and.callFake(() => {
         const err = new Error("ZOMG!");
         return Promise.reject(err);
       });
@@ -447,24 +442,14 @@ describe("HubInitiative Class:", () => {
     });
 
     describe("fromEditor:", () => {
-      let updateSpy: jasmine.Spy;
-      let createSpy: jasmine.Spy;
+      let parentSpy: jasmine.Spy;
       beforeEach(() => {
-        updateSpy = spyOn(
-          HubInitiativesModule,
-          "updateInitiative"
-        ).and.callFake((p: IHubInitiative) => {
-          return Promise.resolve(p);
-        });
-        createSpy = spyOn(
-          HubInitiativesModule,
-          "createInitiative"
-        ).and.callFake((e: any) => {
-          e.id = "3ef";
-          return Promise.resolve(e);
-        });
+        parentSpy = spyOn(
+          HubItemEntity.prototype as any,
+          "_fromEditor"
+        ).and.callThrough();
       });
-      it("handles simple prop change", async () => {
+      it("delegates to the parent class to handle shared logic", async () => {
         const chk = HubInitiative.fromJson(
           {
             id: "bc3",
@@ -473,263 +458,10 @@ describe("HubInitiative Class:", () => {
           },
           authdCtxMgr.context
         );
-        // spy on the instance .save method and retrn void
-        const saveSpy = spyOn(chk, "save").and.returnValue(Promise.resolve());
-        // make changes to the editor
+        spyOn(chk, "save").and.returnValue(Promise.resolve());
         const editor = await chk.toEditor();
-        editor.name = "new name";
-        delete editor._groups;
-        // call fromEditor
-        const result = await chk.fromEditor(editor);
-        // expect the save method to have been called
-        expect(saveSpy).toHaveBeenCalledTimes(1);
-        // expect the name to have been updated
-        expect(result.name).toEqual("new name");
-      });
-      it("handles thumbnail change", async () => {
-        const chk = HubInitiative.fromJson(
-          {
-            id: "bc3",
-            name: "Test Entity",
-            thumbnailUrl: "https://myserver.com/thumbnail.png",
-          },
-          authdCtxMgr.context
-        );
-        // spy on the instance .save method and retrn void
-        const saveSpy = spyOn(chk, "save").and.returnValue(Promise.resolve());
-        // make changes to the editor
-        const editor = await chk.toEditor();
-        editor.name = "new name";
-        editor._thumbnail = {
-          blob: "fake blob",
-          filename: "thumbnail.png",
-        };
-        // call fromEditor
-        const result = await chk.fromEditor(editor);
-        // expect the save method to have been called
-        expect(saveSpy).toHaveBeenCalledTimes(1);
-        // since thumbnailCache is protected we can't really test that it's set
-        // other than via code-coverage
-        expect(getProp(result, "_thumbnail")).not.toBeDefined();
-      });
-
-      it("handles thumbnail clear", async () => {
-        const chk = HubInitiative.fromJson(
-          {
-            id: "bc3",
-            name: "Test Entity",
-            thumbnailUrl: "https://myserver.com/thumbnail.png",
-          },
-          authdCtxMgr.context
-        );
-        // spy on the instance .save method and retrn void
-        const saveSpy = spyOn(chk, "save").and.returnValue(Promise.resolve());
-        // make changes to the editor
-        const editor = await chk.toEditor();
-        editor.name = "new name";
-        editor._thumbnail = {};
-        // call fromEditor
-        const result = await chk.fromEditor(editor);
-        // expect the save method to have been called
-        expect(saveSpy).toHaveBeenCalledTimes(1);
-        // since thumbnailCache is protected we can't really test that it's set
-        // other than via code-coverage
-        expect(getProp(result, "_thumbnail")).not.toBeDefined();
-      });
-      it("handles setting featured image", async () => {
-        const chk = HubInitiative.fromJson(
-          {
-            id: "bc3",
-            name: "Test Entity",
-          },
-          authdCtxMgr.context
-        );
-        const editor = await chk.toEditor();
-        editor.view = {
-          featuredImage: {
-            blob: "some blob",
-            filename: "some-featuredImage.png",
-          },
-        };
-        const upsertResourceSpy = spyOn(
-          upsertResourceModule,
-          "upsertResource"
-        ).and.returnValue(
-          Promise.resolve("https://blah.com/some-featuredImage.png")
-        );
         await chk.fromEditor(editor);
-        expect(updateSpy).toHaveBeenCalledTimes(1);
-        expect(createSpy).not.toHaveBeenCalled();
-        expect(upsertResourceSpy).toHaveBeenCalledTimes(1);
-      });
-      it("handles setting featured image on creation", async () => {
-        const chk = HubInitiative.fromJson(
-          {
-            name: "Test Entity",
-          },
-          authdCtxMgr.context
-        );
-        const editor = await chk.toEditor();
-        editor.view = {
-          featuredImage: {
-            blob: "some blob",
-            filename: "some-featuredImage.png",
-          },
-        };
-        const upsertResourceSpy = spyOn(
-          upsertResourceModule,
-          "upsertResource"
-        ).and.returnValue(
-          Promise.resolve("https://blah.com/some-featuredImage.png")
-        );
-
-        editor.access = "org";
-
-        const accessSpy = spyOn(
-          HubItemEntity.prototype,
-          "setAccess"
-        ).and.returnValue(Promise.resolve());
-
-        await chk.fromEditor(editor);
-        expect(createSpy).toHaveBeenCalledTimes(1);
-        expect(updateSpy).toHaveBeenCalledTimes(1);
-        expect(upsertResourceSpy).toHaveBeenCalledTimes(1);
-        expect(accessSpy).toHaveBeenCalledTimes(1);
-        expect(accessSpy).toHaveBeenCalledWith("org");
-      });
-      it("handles setting featured image and clearing prior image", async () => {
-        const chk = HubInitiative.fromJson(
-          {
-            id: "bc3",
-            name: "Test Entity",
-            view: {
-              featuredImageUrl: "https://blah.com/some-featuredImage.png",
-            },
-          },
-          authdCtxMgr.context
-        );
-        const editor = await chk.toEditor();
-
-        editor.view = {
-          ...editor.view,
-          featuredImage: {
-            blob: "some blob",
-            filename: "some-featuredImage.png",
-          },
-        };
-        const upsertResourceSpy = spyOn(
-          upsertResourceModule,
-          "upsertResource"
-        ).and.returnValue(
-          Promise.resolve("https://blah.com/some-featuredImage.png")
-        );
-        await chk.fromEditor(editor);
-        expect(updateSpy).toHaveBeenCalledTimes(1);
-        expect(createSpy).not.toHaveBeenCalled();
-        expect(upsertResourceSpy).toHaveBeenCalledTimes(1);
-      });
-      it("handles clearing featured image", async () => {
-        const chk = HubInitiative.fromJson(
-          {
-            id: "bc3",
-            name: "Test Entity",
-          },
-          authdCtxMgr.context
-        );
-        const editor = await chk.toEditor();
-        editor.view = {
-          featuredImage: {},
-        };
-        const doesResourceExistSpy = spyOn(
-          doesResourceExistModule,
-          "doesResourceExist"
-        ).and.returnValue(Promise.resolve(true));
-        const removeResourceSpy = spyOn(
-          removeResourceModule,
-          "removeResource"
-        ).and.returnValue(Promise.resolve());
-
-        await chk.fromEditor(editor);
-        expect(updateSpy).toHaveBeenCalledTimes(1);
-        expect(createSpy).not.toHaveBeenCalled();
-        expect(doesResourceExistSpy).toHaveBeenCalledTimes(1);
-        expect(removeResourceSpy).toHaveBeenCalledTimes(1);
-      });
-      it("handles clearing featured image when resource doesnt exist", async () => {
-        const chk = HubInitiative.fromJson(
-          {
-            id: "bc3",
-            name: "Test Entity",
-          },
-          authdCtxMgr.context
-        );
-        const editor = await chk.toEditor();
-        editor.view = {
-          featuredImage: {},
-        };
-        const doesResourceExistSpy = spyOn(
-          doesResourceExistModule,
-          "doesResourceExist"
-        ).and.returnValue(Promise.resolve(false));
-        const removeResourceSpy = spyOn(
-          removeResourceModule,
-          "removeResource"
-        ).and.returnValue(Promise.resolve());
-
-        await chk.fromEditor(editor);
-        expect(updateSpy).toHaveBeenCalledTimes(1);
-        expect(createSpy).not.toHaveBeenCalled();
-        expect(doesResourceExistSpy).toHaveBeenCalledTimes(1);
-        expect(removeResourceSpy).not.toHaveBeenCalled();
-      });
-      it("sets access on create", async () => {
-        const chk = HubInitiative.fromJson(
-          {
-            name: "Test Entity",
-            thumbnailUrl: "https://myserver.com/thumbnail.png",
-          },
-          authdCtxMgr.context
-        );
-        const editor = await chk.toEditor();
-
-        editor.access = "org";
-
-        const accessSpy = spyOn(
-          HubItemEntity.prototype,
-          "setAccess"
-        ).and.returnValue(Promise.resolve());
-
-        await chk.fromEditor(editor);
-        expect(createSpy).toHaveBeenCalledTimes(1);
-        expect(accessSpy).toHaveBeenCalledTimes(1);
-        expect(accessSpy).toHaveBeenCalledWith("org");
-      });
-      it("handles sharing on create", async () => {
-        const chk = HubInitiative.fromJson(
-          {
-            name: "Test Entity",
-            thumbnailUrl: "https://myserver.com/thumbnail.png",
-          },
-          authdCtxMgr.context
-        );
-        const editor = await chk.toEditor();
-        editor._groups = ["3ef"];
-        editor.access = "org";
-        const accessSpy = spyOn(
-          HubItemEntity.prototype,
-          "setAccess"
-        ).and.returnValue(Promise.resolve());
-
-        const shareSpy = spyOn(
-          HubItemEntity.prototype,
-          "shareWithGroup"
-        ).and.returnValue(Promise.resolve());
-        await chk.fromEditor(editor);
-        expect(createSpy).toHaveBeenCalledTimes(1);
-        expect(accessSpy).toHaveBeenCalledTimes(1);
-        expect(accessSpy).toHaveBeenCalledWith("org");
-        expect(shareSpy).toHaveBeenCalledTimes(1);
-        expect(shareSpy).toHaveBeenCalledWith("3ef");
+        expect(parentSpy).toHaveBeenCalledTimes(1);
       });
     });
   });
