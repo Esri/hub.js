879a84e3916810a3eff1de77659d3200c6e51489
Aaron Putterman <60486980+abp6318@users.noreply.github.com>
2025-08-19 11:10:26 -0400

    fix: page slugify regex

--- packages/common/src/utils/slugify.ts
+++ packages/common/src/utils/slugify.ts
@@ -2,8 +2,9 @@
  * Perform the following operations on a string to make it slug-friendly:
  * 1. trim it
  * 2. convert to lowercase
- * 3. remove any character not a-z, 0-9, or _
+ * 3. remove any character not a-z or 0-9
  * 4. dasherize it
+ * 5. remove leading and trailing dashes
  * @param {String} value String to slugify
  */
 export function slugify<T>(value: T): T {
@@ -12,9 +13,9 @@ export function slugify<T>(value: T): T {
     return value
       .trim()
       .toLowerCase()
-      .replace(/ +/g, "-")
-      .replace(/[^\w-]/g, "")
-      .replace(/-+/g, "-");
+      .replace(/[^a-z0-9]+/g, "-") // replace runs of anything NOT a-z or 0-9 with "-"
+      .replace(/-+/g, "-") // collapse multiple "-" into one
+      .replace(/^-+|-+$/g, ""); // trim leading/trailing "-"
   } else {
     return value;
   }
