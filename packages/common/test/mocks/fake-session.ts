9a8b4213aca371da4f02bf14079ea7bce18272c9
Tom Wayson <tom@tomwayson.com>
2025-08-19 15:57:32 -0700

    test(hub-common): add missing fixture that was imported from initiatives
    affects: @esri/hub-common

---
+++ packages/common/test/mocks/fake-session.ts
@@ -0,0 +1,38 @@
+/* Copyright (c) 2018 Environmental Systems Research Institute, Inc.
+ * Apache-2.0 */
+
+import { IHubRequestOptions } from "../../src/hub-types";
+// Fake Session for use in tests...
+
+export const TOMORROW = (function () {
+  const now = new Date();
+  now.setDate(now.getDate() + 1);
+  return now;
+})();
+
+const token = "fake-token";
+const username = "vader";
+export const MOCK_USER_SESSION = {
+  getToken: () => Promise.resolve(token),
+  getUsername: () => Promise.resolve(username),
+  portal: "https://www.arcgis.com/sharing/rest",
+  username,
+  password: "123456",
+  token,
+  tokenExpires: TOMORROW,
+} as any;
+
+export const MOCK_REQUEST_OPTIONS = {
+  authentication: MOCK_USER_SESSION,
+};
+
+export const MOCK_HUB_REQOPTS = {
+  authentication: MOCK_USER_SESSION,
+  portalSelf: {
+    id: "orgIdFromPortalSelf",
+    name: "my spiffy org",
+    urlKey: "org",
+  },
+  isPortal: false,
+  hubApiUrl: "https://hubqa.arcgis.com",
+} as unknown as IHubRequestOptions;
