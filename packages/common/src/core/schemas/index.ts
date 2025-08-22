248fff2ce1dc0a35908ca929272e9db414913908
Caleb Pomar <cpomar@esri.com>
2025-08-22 12:17:19 -0600

    fix(hub-common): fix export issue
    affects: @esri/hub-common

--- packages/common/src/core/schemas/index.ts
+++ packages/common/src/core/schemas/index.ts
@@ -4,6 +4,9 @@ export * from "./shared";
 /**
  * we need to export these internal utils to use in
  * the catalog/query builder experience
+ *
+ * NOTE: If the internal utils use hubSearch(), you _must_ export the function
+ * at the root barrel file (packages/common/src/index.ts) to avoid circular
+ * dependency issues
  */
 export * from "./internal/getTagItems";
-export * from "./internal/fetchCategoriesUiSchemaElement";
