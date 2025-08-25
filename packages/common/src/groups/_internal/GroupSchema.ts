6a09063100669ed4191dda598274b09200ffa402
Julianna Apicella <42875581+juliannaeapicella@users.noreply.github.com>
2025-08-25 15:37:46 -0400

    feat(hub-common): add discussion settings schema

--- packages/common/src/groups/_internal/GroupSchema.ts
+++ packages/common/src/groups/_internal/GroupSchema.ts
@@ -1,5 +1,6 @@
 import { IConfigurationSchema } from "../../core";
 import {
+  DISCUSSION_SETTINGS_SCHEMA,
   ENTITY_IMAGE_SCHEMA,
   ENTITY_IS_DISCUSSABLE_SCHEMA,
   ENTITY_NAME_SCHEMA,
@@ -66,6 +67,7 @@ export const GroupSchema: IConfigurationSchema = {
       default: false,
     },
     isDiscussable: ENTITY_IS_DISCUSSABLE_SCHEMA,
+    discussionSettings: DISCUSSION_SETTINGS_SCHEMA,
     tags: ENTITY_TAGS_SCHEMA,
   },
 } as IConfigurationSchema;
