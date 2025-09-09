c70de591e89496f663cef3c1b2368f37532bd630
Randy Weber <rweber@esri.com>
2025-09-09 10:11:40 -0400

    refactor(hub-common): resolve TS errors in FollowCardUiSchema.test.ts
    affects: @esri/hub-common
    
    ISSUES CLOSED: 11444

--- packages/common/test/core/schemas/internal/follow/FollowCardUiSchema.test.ts
+++ packages/common/test/core/schemas/internal/follow/FollowCardUiSchema.test.ts
@@ -1,4 +1,3 @@
-import { CardEditorOptions } from "../../../../../src/core/schemas/internal/EditorOptions";
 import { buildUiSchema } from "../../../../../src/core/schemas/internal/follow/FollowCardUiSchema";
 import { MOCK_CONTEXT } from "../../../../mocks/mock-auth";
 
@@ -9,50 +8,50 @@ import { MOCK_CONTEXT } from "../../../../mocks/mock-auth";
 
 describe("buildUiSchema: follow", () => {
   it("returns the correct follow card uiSchema", () => {
-    const uiSchema = buildUiSchema("", {} as CardEditorOptions, MOCK_CONTEXT);
+    const uiSchema = buildUiSchema("", {}, MOCK_CONTEXT);
 
     expect(uiSchema.type).toBe("Layout");
-    expect(uiSchema.elements?.length).toBe(3);
-    expect(uiSchema.elements![0].scope).toBe("/properties/entityId");
-    expect(uiSchema.elements![0].options!.control).toBe(
+    expect(uiSchema.elements.length).toBe(3);
+    expect(uiSchema.elements[0].scope).toBe("/properties/entityId");
+    expect(uiSchema.elements[0].options.control).toBe(
       "hub-field-input-gallery-picker"
     );
 
-    expect(uiSchema.elements![1].elements?.length).toBe(2);
-    expect(uiSchema.elements![1].elements?.[0].scope).toBe(
+    expect(uiSchema.elements[1].elements.length).toBe(2);
+    expect(uiSchema.elements[1].elements[0].scope).toBe(
       "/properties/callToActionText"
     );
-    expect(uiSchema.elements![1].elements?.[0].options?.type).toBe("textarea");
-    expect(uiSchema.elements![1].elements?.[1].scope).toBe(
+    expect(uiSchema.elements[1].elements[0].options.type).toBe("textarea");
+    expect(uiSchema.elements[1].elements[1].scope).toBe(
       "/properties/callToActionAlign"
     );
-    expect(uiSchema.elements![1].elements?.[1].options?.control).toBe(
+    expect(uiSchema.elements[1].elements[1].options.control).toBe(
       "hub-field-input-alignment"
     );
 
-    expect(uiSchema.elements![2].elements?.length).toBe(4);
-    expect(uiSchema.elements![2].elements?.[0].scope).toBe(
+    expect(uiSchema.elements[2].elements.length).toBe(4);
+    expect(uiSchema.elements[2].elements[0].scope).toBe(
       "/properties/buttonText"
     );
-    expect(uiSchema.elements![2].elements?.[0].options?.control).toBe(
+    expect(uiSchema.elements[2].elements[0].options.control).toBe(
       "hub-field-input-input"
     );
-    expect(uiSchema.elements![2].elements?.[1].scope).toBe(
+    expect(uiSchema.elements[2].elements[1].scope).toBe(
       "/properties/unfollowButtonText"
     );
-    expect(uiSchema.elements![2].elements?.[1].options?.control).toBe(
+    expect(uiSchema.elements[2].elements[1].options.control).toBe(
       "hub-field-input-input"
     );
-    expect(uiSchema.elements![2].elements?.[2].scope).toBe(
+    expect(uiSchema.elements[2].elements[2].scope).toBe(
       "/properties/buttonAlign"
     );
-    expect(uiSchema.elements![2].elements?.[2].options?.control).toBe(
+    expect(uiSchema.elements[2].elements[2].options.control).toBe(
       "hub-field-input-alignment"
     );
-    expect(uiSchema.elements![2].elements?.[3].scope).toBe(
+    expect(uiSchema.elements[2].elements[3].scope).toBe(
       "/properties/buttonStyle"
     );
-    expect(uiSchema.elements![2].elements?.[3].options?.control).toBe(
+    expect(uiSchema.elements[2].elements[3].options.control).toBe(
       "hub-field-input-select"
     );
   });
