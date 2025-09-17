df0e013ebe149ba1451a5ac61715b0a92feb2c62
knbhagat <kbhagat@esri.com>
2025-09-17 16:53:08 -0400

    feat(hub-common): added q and bbox to IOgcAggregationQueryParams
    affects: @esri/hub-common
    
    ISSUES CLOSED: 12416

--- packages/common/src/search/_internal/hubSearchItemsHelpers/getOgcAggregationQueryParams.ts
+++ packages/common/src/search/_internal/hubSearchItemsHelpers/getOgcAggregationQueryParams.ts
@@ -2,10 +2,14 @@ import { getProp } from "../../../objects/get-prop";
 import { IQuery } from "../../types/IHubCatalog";
 import { IHubSearchOptions } from "../../types/IHubSearchOptions";
 import { getFilterQueryParam } from "./getFilterQueryParam";
+import { getQQueryParam } from "./getQQueryParam";
+import { getBboxQueryParam } from "./getBboxQueryParam";
 
 export interface IOgcAggregationQueryParams {
   aggregations: string;
   filter?: string;
+  bbox?: string;
+  q?: string;
   token?: string;
 }
 
@@ -16,6 +20,8 @@ export function getOgcAggregationQueryParams(
   // TODO: use options.aggLimit once the OGC API supports it
   const aggregations = `terms(fields=(${options.aggFields.join()}))`;
   const filter = getFilterQueryParam(query);
+  const bbox = getBboxQueryParam(query);
+  const q = getQQueryParam(query);
   const token = getProp(
     options,
     "requestOptions.authentication.token"
@@ -23,7 +29,9 @@ export function getOgcAggregationQueryParams(
 
   return {
     aggregations,
+    bbox,
     filter,
+    q,
     token,
   };
 }
