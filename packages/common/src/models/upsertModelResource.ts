73407f64eb5fbd0f61a91a32db8b4c5f7e1ff438
Randy Weber <rweber@esri.com>
2025-09-18 16:35:23 -0400

    refactor(hub-common): move source out of models/index.ts and urls/index.ts into separate modules
    affects: @esri/hub-common
    
    ISSUES CLOSED: 13985

---
+++ packages/common/src/models/upsertModelResource.ts
@@ -0,0 +1,57 @@
+import type { IUserRequestOptions } from "@esri/arcgis-rest-request";
+import { upsertResource } from "../resources";
+import { IModel } from "../hub-types";
+
+/**
+ * Takes an IModel and an array of resources and upserts them to the
+ * backing item. Then searches for the resources that were upserted
+ * and attaches them to the model, which is returned.
+ *
+ * @export
+ * @param {IModel} model
+ * @param {Array<{
+ *     resource: Record<string, any>;
+ *     filename: string;
+ *   }>} resources
+ * @param {IUserRequestOptions} requestOptions
+ * @return {*}  {Promise<IModel>}
+ */
+export async function upsertModelResources(
+  model: IModel,
+  resources: Array<{
+    resource: Record<string, any>;
+    filename: string;
+  }>,
+  requestOptions: IUserRequestOptions
+): Promise<IModel> {
+  // Set up promises array
+  const upsertPromises: Array<Promise<any>> = [];
+  const expectedResourceNames: string[] = [];
+  // loop through resources and create them
+  resources.forEach((value: any) => {
+    upsertPromises.push(
+      upsertResource(
+        model.item.id,
+        model.item.owner,
+        value.resource,
+        value.filename,
+        requestOptions
+      )
+    );
+    expectedResourceNames.push(value.filename);
+  });
+  // Promise.all to wait for all resources to be created
+  return Promise.all(upsertPromises).then(() => {
+    // Create a new object with the resources
+    const updatedResources = resources.reduce((acc: any, resource) => {
+      // Get the property name from the resource name
+      const prop = resource.filename.split(".").shift();
+      acc[prop] = resource.resource;
+      return acc;
+    }, {});
+    return {
+      resources: updatedResources,
+      ...model,
+    };
+  });
+}
