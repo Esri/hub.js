1953719190d04f099f679f7ea59240b2398b7a1a
Tommy Bramble <tbramble@esri.com>
2025-09-22 14:17:33 -0400

    feat(hub-common): add support for setting the layer style type on feature service content (#1997)

--- packages/common/src/core/types/IHubMapSettings.ts
+++ packages/common/src/core/types/IHubMapSettings.ts
@@ -1,8 +1,19 @@
 /**
+ * Enum for describing the feature layer styles.
+ */
+export enum FeatureLayerStyle {
+  Hub = "hub",
+  Item = "item",
+  Service = "service",
+}
+
+/**
  * Interface for Hub Map settings.  Used to configure the map for an entity.
  */
 export interface IHubMapSettings {
   // the item id for the map or scene
   // this is an array to support integration with the gallery picker
   baseViewItemId: string[];
+  // the feature layer style to use when rendering the data on a map
+  featureLayerStyle?: FeatureLayerStyle;
 }
