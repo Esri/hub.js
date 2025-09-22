0dbde6bc27f3ee70ccf3912d65605968a8750ba6
Vivian Zhang <vivian.mzh@gmail.com>
2025-09-22 14:13:59 -0700

    chore: update import paths to reduce cyclical dependencies
    affects: @esri/hub-common

--- packages/common/src/core/types/IHubInitiativeTemplate.ts
+++ packages/common/src/core/types/IHubInitiativeTemplate.ts
@@ -1,10 +1,8 @@
 import { IHubItemEntity } from "./IHubItemEntity";
-import {
-  IWithLayout,
-  IWithSlug,
-  IWithCatalog,
-  IWithViewSettings,
-} from "../traits";
+import { IWithLayout } from "../traits/IWithLayout";
+import { IWithSlug } from "../traits/IWithSlug";
+import { IWithCatalog } from "../traits/IWithCatalog";
+import { IWithViewSettings } from "../traits/IWithViewSettings";
 import { IHubItemEntityEditor } from "./IHubItemEntity";
 
 /** Defines the properties of a Hub Initiative Template object */
