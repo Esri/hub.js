78d0eb0d32b340720b9848aa7161170979dd56f4
Randy Weber <rweber@esri.com>
2025-10-08 12:30:21 -0400

    refactor(hub-common): add comment explaining why we're not translating post geometries to IHubLocati
    affects: @esri/hub-common
    
    ISSUES CLOSED: 14152

--- packages/common/src/search/_internal/hubDiscussionsHelpers/postToSearchResult.ts
+++ packages/common/src/search/_internal/hubDiscussionsHelpers/postToSearchResult.ts
@@ -6,7 +6,6 @@ import { IHubSearchResult } from "../../types/IHubSearchResult";
  * @param post An IPost record
  * @returns a IHubSearchResult for the given IPost record
  */
-// TODO: seem like geometry is deprecated in IHubSearchResult, should we add location here?
 export function postToSearchResult(post: IPost): IHubSearchResult {
   return {
     access: null,
@@ -14,7 +13,11 @@ export function postToSearchResult(post: IPost): IHubSearchResult {
     createdDateSource: "post.createdAt",
     family: "post",
     id: post.id,
-    location: null, // TODO
+    // Per Tom's advice, deferring supporting locations for posts for now. There are
+    // some known typing issues related to the `IHubLocation.geometries` property at
+    // this time. We should revisit this once `IHubLocation.geometries` type if fixed
+    // and make sure to support translating GeoJSON GeometryCollections appropriately
+    location: { type: "none" },
     name: post.title,
     owner: post.creator,
     rawResult: post,
