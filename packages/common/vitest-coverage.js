f411e88e0f61598d9e1851fd0d097ef066b7cf37
Tom Wayson <tom@tomwayson.com>
2025-10-14 17:30:45 -0700

    chore: sync Vitet and Karma coverage

---
+++ packages/common/vitest-coverage.js
@@ -0,0 +1,21 @@
+// NOTE: we are temporarily placing the vitest coverage config here
+// so that the karma config can exclude the files that are covered by vitest
+module.exports = {
+  enabled: true,
+  include: [
+    // ultimately we want all the src files to be covered
+    // "src/**/*.ts"
+    // but for now we are just getting started converting to vitest
+    "src/{api,util}.ts",
+    "src/associations/internal/{getIncludesAndReferencesQuery,getIncludesDoesNotReferenceQuery,getReferencesDoesNotIncludeQuery}.ts",
+    "src/associations/{requestAssociation,breakAssociation}.ts",
+    "src/core/_internal/sharedWith.ts",
+  ],
+  // we have so many pre-existing istanbul ignore comments
+  provider: "istanbul",
+  // reporter: ["json", "html", "cobertura"],
+  reportsDirectory: "./coverage",
+  thresholds: {
+    100: true
+  }
+};
--- packages/common/vitest.config.mjs
+++ packages/common/vitest.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import * as coverage from "./vitest-coverage.js";
 
 export default defineConfig({
   test: {
@@ -6,23 +7,6 @@ export default defineConfig({
       // we use .spec.ts for vitest and .test.ts for karma/jasmine
       "test/**/*.spec.ts"
     ],
-    coverage: {
-      enabled: true,
-      include: [
-        // ultimately we want all the src files to be covered
-        // "src/**/*.ts"
-        // but for now we are just getting started converting to vitest
-        "src/{api,util}.ts",
-        "src/associations/internal/getIncludesAndReferencesQuery.ts",
-        "src/core/_internal/sharedWith.ts",
-      ],
-      // we have so many pre-existing istanbul ignore comments
-      provider: "istanbul",
-      // reporter: ["json", "html", "cobertura"],
-      reportsDirectory: "./coverage",
-      thresholds: {
-        100: true
-      }
-    }
+    coverage
   }
 });
