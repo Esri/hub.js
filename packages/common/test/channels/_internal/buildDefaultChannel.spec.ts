a3ff5f7c9c8ed157afd373291e98203faf7c47da
Tom Wayson <tom@tomwayson.com>
2025-10-17 07:52:25 -0700

    C/vitest from jest (#2054)

---
+++ packages/common/test/channels/_internal/buildDefaultChannel.spec.ts
@@ -0,0 +1,46 @@
+import {
+  AclCategory,
+  AclSubCategory,
+  Role,
+} from "../../../src/discussions/api/types";
+import { buildDefaultChannel } from "../../../src/channels/_internal/buildDefaultChannel";
+import * as transformAclPermissionToEntityPermissionPolicyModule from "../../../src/channels/_internal/transformAclPermissionToEntityPermissionPolicy";
+import { IEntityPermissionPolicy } from "../../../src/permissions/types/IEntityPermissionPolicy";
+
+describe("buildDefaultChannel", () => {
+  it("should build a partial IHubChannel object for the given orgId", () => {
+    const entityPermissionPolicy: IEntityPermissionPolicy = {
+      permission: "hub:channel:manage",
+      collaborationType: "org-admin",
+      collaborationId: "orgId1",
+      id: "1aa",
+    };
+    const transformAclPermissionToEntityPermissionPolicySpy = vi
+      .spyOn(
+        transformAclPermissionToEntityPermissionPolicyModule,
+        "transformAclPermissionToEntityPermissionPolicy"
+      )
+      .mockReturnValue(entityPermissionPolicy);
+    const expected = {
+      name: "",
+      blockWords: [] as string[],
+      allowAsAnonymous: false,
+      permissions: [entityPermissionPolicy],
+      canEdit: true,
+      canDelete: true,
+    };
+    const result = buildDefaultChannel("orgId1");
+    expect(result).toEqual(expected);
+    expect(
+      transformAclPermissionToEntityPermissionPolicySpy
+    ).toHaveBeenCalledTimes(1);
+    expect(
+      transformAclPermissionToEntityPermissionPolicySpy
+    ).toHaveBeenCalledWith({
+      category: AclCategory.ORG,
+      subCategory: AclSubCategory.ADMIN,
+      role: Role.OWNER,
+      key: "orgId1",
+    });
+  });
+});
---
+++ packages/common/test/items/interpolate.spec.ts
@@ -0,0 +1,14 @@
+import * as adlibModule from "adlib";
+import { interpolate } from "../../src/items/interpolate";
+
+describe("interpolate", function () {
+  it("calls adlib", function () {
+    vi.spyOn(adlibModule, "adlib").mockImplementation(() => {});
+
+    const args = [{ id: "template" }, { id: "settings" }, { id: "transforms" }];
+
+    interpolate(args[0], args[1], args[2]);
+
+    expect(adlibModule.adlib).toHaveBeenCalledWith(...args);
+  });
+});
