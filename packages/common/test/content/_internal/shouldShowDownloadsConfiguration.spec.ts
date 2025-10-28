4fc06b24e7a38c91f171d30c92b8fd8695554547
Caleb Pomar <cpomar@esri.com>
2025-10-28 10:28:49 -0600

    test: convert other jasmine test file to vitest

--- packages/common/test/content/_internal/shouldShowDownloadsConfiguration.spec.ts
+++ packages/common/test/content/_internal/shouldShowDownloadsConfiguration.spec.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from "vitest";
 import { shouldShowDownloadsConfiguration } from "../../../src/content/_internal/shouldShowDownloadsConfiguration";
 
 describe("shouldShowDownloadsConfiguration", () => {
